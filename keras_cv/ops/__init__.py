839c2675e5ef987faea9a947a16f7c0a8a2cb4eb
Zhenyu Tan <tanzheny@google.com>
2020-10-09 13:21:39 -0700

    Internal change
    PiperOrigin-RevId: 336353171

--- keras_cv/ops/__init__.py
+++ keras_cv/ops/__init__.py
@@ -14,6 +14,6 @@
 # ==============================================================================
 """Keras-CV layers package definition."""
 from keras_cv.ops.anchor_generator import AnchorGenerator
-from keras_cv.ops.anchor_labeler import AnchorLabeler
 from keras_cv.ops.box_matcher import BoxMatcher
 from keras_cv.ops.iou_similarity import IouSimilarity
+from keras_cv.ops.target_gather import TargetGather
