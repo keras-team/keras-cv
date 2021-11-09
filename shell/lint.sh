404f686330a6d5c267565807956ac6116b44aa26
Luke Wood <lukewoodcs@gmail.com>
2021-11-09 12:11:25 -0800

    Create operational resources
    This commit begins implementing the operational pieces
    of the keras-cv repo.  This includes a setup.py package,
    a github action to lint, and scripts to complete linting.

---
+++ shell/lint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+isort --sl -c .
+if ! [ $? -eq 0 ]
+then
+  echo "Please run \"sh shell/format.sh\" to format the code."
+  exit 1
+fi
+flake8 .
+if ! [ $? -eq 0 ]
+then
+  echo "Please fix the code style issue."
+  exit 1
+fi
+black --check --line-length 85 .
+if ! [ $? -eq 0 ]
+then
+  echo "Please run \"sh shell/format.sh\" to format the code."
+    exit 1
+fi
+for i in $(find keras_cv tests -name '*.py') # or whatever other pattern...
+do
+  if ! grep -q Copyright $i
+  then
+    echo "Please run \"sh shell/format.sh\" to format the code."
+    exit 1
+  fi
+done
