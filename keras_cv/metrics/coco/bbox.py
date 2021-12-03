0eae5d0ce13f4756f161930305206be57844750d
Luke Wood <lukewoodcs@gmail.com>
2021-12-03 13:20:10 -0800

    Remove incorrect implementation.  Wrong testing structure, don't want to misguide contributors.

--- keras_cv/metrics/coco/bbox.py
+++
@@ -1,7 +0,0 @@
-"""Shared constants for use with bounding boxes."""
-# These are the dimensions used in Tensors to represent each corresponding side.
-LEFT, TOP, RIGHT, BOTTOM = 0, 1, 2, 3
-# Class is held in the 4th index
-CLASS = 4
-# Confidence exists only on y_pred, and is in the 5th index.
-CONFIDENCE = 5
