Figured out where numerical discrepency is coming from, need to mask otu categories.  Working on a fix.

keras_cv/metrics/coco/base.py
@@ -128,7 +128,7 @@ class COCOBase(keras.metrics.Metric):
                     min_area = area_range[0]
                     max_area = area_range[1]
                     area_filtered_y_true = util.filter_boxes_by_area_range(category_filtered_y_true, min_area, max_area)
-                    area_filtered_y_pred = util.filter_boxes_by_area_range(category_filtered_y_pred, min_area, max_area)
+                    area_filtered_y_pred =category_filtered_y_pred # area_filtered_y_pred = util.filter_boxes_by_area_range(category_filtered_y_pred, min_area, max_area)
                     ious = iou_lib.compute_ious_for_image(area_filtered_y_true, area_filtered_y_pred)
 
                     ground_truth_boxes_update = tf.tensor_scatter_nd_add(ground_truth_boxes_update, [[k_i, a_i]], [tf.cast(tf.shape(area_filtered_y_true)[0], tf.float32)])

keras_cv/metrics/coco/numerical_tests/recall_correctness_test.py
@@ -15,23 +15,24 @@ SINGLE_BOX_SAMPLE_FILE = (
 
 
 class RecallCorrectnesstTest(tf.test.TestCase):
-    def test_recall_correctness_maxdets_1(self):
-        """
-        cocoeval.py outputs:
+    """Unit tests that test Keras COCO metric results against the known good ones of cocoeval.py.
+    The bounding boxes in sample_boxes.npz were given to cocoeval.py which output the following values:
 
-         Average Precision  (AP) @[ IoU=0.50:0.95 | area=   all | maxDets=100 ] = 0.661
-         Average Precision  (AP) @[ IoU=0.50      | area=   all | maxDets=100 ] = 1.000
-         Average Precision  (AP) @[ IoU=0.75      | area=   all | maxDets=100 ] = 0.793
-         Average Precision  (AP) @[ IoU=0.50:0.95 | area= small | maxDets=100 ] = 0.651
-         Average Precision  (AP) @[ IoU=0.50:0.95 | area=medium | maxDets=100 ] = 0.676
-         Average Precision  (AP) @[ IoU=0.50:0.95 | area= large | maxDets=100 ] = 0.671
-         Average Recall     (AR) @[ IoU=0.50:0.95 | area=   all | maxDets=  1 ] = 0.504
-         Average Recall     (AR) @[ IoU=0.50:0.95 | area=   all | maxDets= 10 ] = 0.686
-         Average Recall     (AR) @[ IoU=0.50:0.95 | area=   all | maxDets=100 ] = 0.686
-         Average Recall     (AR) @[ IoU=0.50:0.95 | area= small | maxDets=100 ] = 0.674
-         Average Recall     (AR) @[ IoU=0.50:0.95 | area=medium | maxDets=100 ] = 0.681
-         Average Recall     (AR) @[ IoU=0.50:0.95 | area= large | maxDets=100 ] = 0.682
-        """
+        Average Precision  (AP) @[ IoU=0.50:0.95 | area=   all | maxDets=100 ] = 0.661
+        Average Precision  (AP) @[ IoU=0.50      | area=   all | maxDets=100 ] = 1.000
+        Average Precision  (AP) @[ IoU=0.75      | area=   all | maxDets=100 ] = 0.793
+        Average Precision  (AP) @[ IoU=0.50:0.95 | area= small | maxDets=100 ] = 0.651
+        Average Precision  (AP) @[ IoU=0.50:0.95 | area=medium | maxDets=100 ] = 0.676
+        Average Precision  (AP) @[ IoU=0.50:0.95 | area= large | maxDets=100 ] = 0.671
+        Average Recall     (AR) @[ IoU=0.50:0.95 | area=   all | maxDets=  1 ] = 0.504
+        Average Recall     (AR) @[ IoU=0.50:0.95 | area=   all | maxDets= 10 ] = 0.686
+        Average Recall     (AR) @[ IoU=0.50:0.95 | area=   all | maxDets=100 ] = 0.686
+        Average Recall     (AR) @[ IoU=0.50:0.95 | area= small | maxDets=100 ] = 0.674
+        Average Recall     (AR) @[ IoU=0.50:0.95 | area=medium | maxDets=100 ] = 0.681
+        Average Recall     (AR) @[ IoU=0.50:0.95 | area= large | maxDets=100 ] = 0.682
+    """
+    def test_recall_correctness_maxdets_1(self):
+
         y_true, y_pred, categories = load_samples(SAMPLE_FILE)
 
         # Area range all
@@ -43,8 +44,7 @@ class RecallCorrectnesstTest(tf.test.TestCase):
         recall = recall.result().numpy()
 
         # TODO(lukewood): re-enable
-        # self.assertAlmostEqual(recall, 0.504)
-        # recall_metric = #
+        self.assertAlmostEqual(recall, 0.504)
 
     def test_recall_max_dets_1_single_image(self):
         """
@@ -59,7 +59,7 @@ class RecallCorrectnesstTest(tf.test.TestCase):
         recall = COCORecall(
             category_ids=categories,
             max_detections=[1],
-            area_ranges=[(0, 1e5 ** 2)],
+            area_ranges=[(0, 1e9 ** 2)],
         )
 
         recall.update_state(y_true, y_pred)
@@ -74,7 +74,7 @@ class RecallCorrectnesstTest(tf.test.TestCase):
         recall = COCORecall(
             category_ids=categories,
             max_detections=[10],
-            area_ranges=[(0, 1e5 ** 2)],
+            area_ranges=[(0, 1e9 ** 2)],
         )
 
         recall.update_state(y_true, y_pred)
@@ -105,7 +105,6 @@ class RecallCorrectnesstTest(tf.test.TestCase):
             Average Recall     (AR) @[ IoU=0.50:0.95 | area= large | maxDets=100 ] = 0.500
         """
         y_true, y_pred, categories = load_samples(SINGLE_BOX_SAMPLE_FILE)
-
         # Area range all
         recall = COCORecall(
             category_ids=categories,
@@ -135,7 +134,7 @@ class RecallCorrectnesstTest(tf.test.TestCase):
 
     def test_recall_large_objects(self):
         y_true, y_pred, categories = load_samples(SINGLE_BOX_SAMPLE_FILE)
-
+    
         # Area range all
         recall = COCORecall(
             category_ids=categories,

keras_cv/metrics/coco/recall.py
@@ -23,9 +23,13 @@ class COCORecall(COCOBase):
     def _single_result(self, a_i, m_i):
         # TODO(lukewood): do I need to mask out -1s???
         # TODO(lukewood): do I need to mask out NaNs?
+        present_values = self.ground_truth_boxes != 0
+
         recalls = tf.math.divide_no_nan(
+            # t, k, a, m
             self.true_positives[:, :, a_i, m_i],
-            self.ground_truth_boxes[None, :, a_i, None],
+            # k, a
+            self.ground_truth_boxes[None, :, a_i],
         )
 
         return tf.math.reduce_mean(recalls)