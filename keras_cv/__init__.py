96621385861e951cf0142b67823e31479a62f3d6
Luke Wood <lukewood@google.com>
2022-01-20 16:37:07 -0800

    add init file declarations

--- keras_cv/__init__.py
+++ keras_cv/__init__.py
@@ -11,3 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from keras_cv import metrics
+from keras_cv import layers
+from keras_cv import util
--- keras_cv/layers/preprocessing/__init__.py
+++ keras_cv/layers/preprocessing/__init__.py
@@ -11,3 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from keras_cv.layers.preprocessing.cut_mix import CutMix
+from keras_cv.layers.preprocessing.mix_up import MixUp
