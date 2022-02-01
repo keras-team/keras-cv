7410c8bd41f5be0b7fa9ebe86a532fd6a8098f63
CRR <christofferhjort1@gmail.com>
2022-02-01 18:00:50 +0100

    refactor tests

--- keras_cv/utils/fill_utils_test.py
+++ keras_cv/utils/fill_utils_test.py
@@ -4,19 +4,16 @@ from keras_cv.utils import fill_utils
 
 
 class FillRectangleTest(tf.test.TestCase):
-    def test_rectangle_position(self):
+    def _run_test(self, img_w, img_h, cent_x, cent_y, rec_w, rec_h, expected):
         batch_size = 1
-        img_h, img_w = 8, 8
-        rec_h, rec_w = 3, 5
-        cent_x, cent_y = 2, 3
 
         batch_shape = (batch_size, img_h, img_w, 1)
-        images = tf.ones(batch_shape)
+        images = tf.ones(batch_shape, dtype=tf.int32)
 
         centers_x = tf.fill([batch_size], cent_x)
         centers_y = tf.fill([batch_size], cent_y)
-        height = tf.fill([batch_size], rec_h)
         width = tf.fill([batch_size], rec_w)
+        height = tf.fill([batch_size], rec_h)
         fill = tf.zeros_like(images)
 
         filled_images = fill_utils.fill_rectangle(
@@ -25,43 +22,32 @@ class FillRectangleTest(tf.test.TestCase):
         # remove batch dimension and channel dimension
         filled_images = filled_images[0, ..., 0]
 
+        tf.assert_equal(filled_images, expected)
+
+    def test_rectangle_position(self):
+        img_w, img_h = 8, 8
+        cent_x, cent_y = 4, 3
+        rec_w, rec_h = 5, 3
         expected = tf.constant(
             [
                 [1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 1, 1, 1, 1, 1, 1, 1],
-                [0, 0, 0, 0, 0, 1, 1, 1],
-                [0, 0, 0, 0, 0, 1, 1, 1],
-                [0, 0, 0, 0, 0, 1, 1, 1],
+                [1, 1, 0, 0, 0, 0, 0, 1],
+                [1, 1, 0, 0, 0, 0, 0, 1],
+                [1, 1, 0, 0, 0, 0, 0, 1],
                 [1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 1, 1, 1, 1, 1, 1, 1],
             ],
-            dtype=images.dtype,
+            dtype=tf.int32,
         )
-        tf.assert_equal(filled_images, expected)
+        self._run_test(img_w, img_h, cent_x, cent_y, rec_w, rec_h, expected)
 
     def test_width_out_of_lower_bound(self):
-        batch_size = 1
-        img_h, img_w = 8, 8
-        rec_h, rec_w = 3, 5
+        img_w, img_h = 8, 8
         cent_x, cent_y = 1, 3
-
-        batch_shape = (batch_size, img_h, img_w, 1)
-        images = tf.ones(batch_shape)
-
-        centers_x = tf.fill([batch_size], cent_x)
-        centers_y = tf.fill([batch_size], cent_y)
-        height = tf.fill([batch_size], rec_h)
-        width = tf.fill([batch_size], rec_w)
-        fill = tf.zeros_like(images)
-
-        filled_images = fill_utils.fill_rectangle(
-            images, centers_x, centers_y, width, height, fill
-        )
-        # remove batch dimension and channel dimension
-        filled_images = filled_images[0, ..., 0]
-
-        # assert width is truncated (from 5 to 4) when cent_x - rec_w < 0 (1 - 5 < 0)
+        rec_w, rec_h = 5, 3
+        # assert width is truncated when cent_x - rec_w < 0
         expected = tf.constant(
             [
                 [1, 1, 1, 1, 1, 1, 1, 1],
@@ -73,32 +59,15 @@ class FillRectangleTest(tf.test.TestCase):
                 [1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 1, 1, 1, 1, 1, 1, 1],
             ],
-            dtype=images.dtype,
+            dtype=tf.int32,
         )
-        tf.assert_equal(filled_images, expected)
+        self._run_test(img_w, img_h, cent_x, cent_y, rec_w, rec_h, expected)
 
     def test_width_out_of_upper_bound(self):
-        batch_size = 1
-        img_h, img_w = 8, 8
-        rec_h, rec_w = 3, 5
+        img_w, img_h = 8, 8
         cent_x, cent_y = 6, 3
-
-        batch_shape = (batch_size, img_h, img_w, 1)
-        images = tf.ones(batch_shape)
-
-        centers_x = tf.fill([batch_size], cent_x)
-        centers_y = tf.fill([batch_size], cent_y)
-        height = tf.fill([batch_size], rec_h)
-        width = tf.fill([batch_size], rec_w)
-        fill = tf.zeros_like(images)
-
-        filled_images = fill_utils.fill_rectangle(
-            images, centers_x, centers_y, width, height, fill
-        )
-        # remove batch dimension and channel dimension
-        filled_images = filled_images[0, ..., 0]
-
-        # assert width is truncated (from 5 to 4) when cent_x + rec_w > img_w (6 + 5 > 8)
+        rec_w, rec_h = 5, 3
+        # assert width is truncated when cent_x + rec_w > img_w
         expected = tf.constant(
             [
                 [1, 1, 1, 1, 1, 1, 1, 1],
@@ -110,6 +79,46 @@ class FillRectangleTest(tf.test.TestCase):
                 [1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 1, 1, 1, 1, 1, 1, 1],
             ],
-            dtype=images.dtype,
+            dtype=tf.int32,
         )
-        tf.assert_equal(filled_images, expected)
+        self._run_test(img_w, img_h, cent_x, cent_y, rec_w, rec_h, expected)
+
+    def test_height_out_of_lower_bound(self):
+        img_w, img_h = 8, 8
+        cent_x, cent_y = 4, 1
+        rec_w, rec_h = 3, 5
+        # assert height is truncated when cent_y - rec_h < 0
+        expected = tf.constant(
+            [
+                [1, 1, 1, 0, 0, 0, 1, 1],
+                [1, 1, 1, 0, 0, 0, 1, 1],
+                [1, 1, 1, 0, 0, 0, 1, 1],
+                [1, 1, 1, 0, 0, 0, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1],
+            ],
+            dtype=tf.int32,
+        )
+        self._run_test(img_w, img_h, cent_x, cent_y, rec_w, rec_h, expected)
+
+    def test_height_out_of_upper_bound(self):
+        img_w, img_h = 8, 8
+        cent_x, cent_y = 4, 6
+        rec_w, rec_h = 3, 5
+        # assert height is truncated when cent_y + rec_h > img_h
+        expected = tf.constant(
+            [
+                [1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 0, 0, 0, 1, 1],
+                [1, 1, 1, 0, 0, 0, 1, 1],
+                [1, 1, 1, 0, 0, 0, 1, 1],
+                [1, 1, 1, 0, 0, 0, 1, 1],
+            ],
+            dtype=tf.int32,
+        )
+        self._run_test(img_w, img_h, cent_x, cent_y, rec_w, rec_h, expected)
