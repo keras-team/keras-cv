a43dc1561ce9feed2f4457737344750e06122380
Luke Wood <lukewood@google.com>
2022-02-02 11:37:07 -0800

    Address Scott's comments

--- keras_cv/layers/preprocessing/equalize.py
+++ keras_cv/layers/preprocessing/equalize.py
@@ -17,16 +17,27 @@ import tensorflow as tf
 class Equalize(tf.keras.layers.Layer):
     """Equalize performs histogram equalization on a channel-wise basis.
 
+    Args:
+        bins: number of bins to use in histogram equalization.
+
+    Usage:
+        ```
+        equalize = Equalize()
+
+        (images, labels), _ = tf.keras.data.cifar10.load_data()
+        # Note that images are an int32 Tensor with values in the range [0, 255]
+        images = equalize(images)    
+        ```
 
     Call arguments:
         images: Tensor of type int, pixels in range [0, 255], in RGB format.
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, bins=256, **kwargs):
+        self.bins = bins
         super().__init__(**kwargs)
 
-    @staticmethod
-    def equalize_channel(image, channel_index):
+    def equalize_channel(self, image, channel_index):
         """equalize_channel performs histogram equalization on a single channel.
 
         Args:
@@ -34,9 +45,10 @@ class Equalize(tf.keras.layers.Layer):
                 with channels last
             channel_index: channel to equalize
         """
+        dtype = image.dtype
         image = tf.cast(image[..., channel_index], tf.int32)
         # Compute the histogram of the image channel.
-        histogram = tf.histogram_fixed_width(image, [0, 255], nbins=256)
+        histogram = tf.histogram_fixed_width(image, [0, 255], nbins=self.bins)
 
         # For the purposes of computing the step, filter out the nonzeros.
         nonzero = tf.where(tf.not_equal(histogram, 0))
@@ -61,7 +73,7 @@ class Equalize(tf.keras.layers.Layer):
             lambda: tf.gather(build_mapping(histogram, step), image),
         )
 
-        return tf.cast(result, tf.uint8)
+        return tf.cast(result, dtype)
 
     def call(self, images):
         if (
@@ -79,8 +91,10 @@ class Equalize(tf.keras.layers.Layer):
 
         # Assumes RGB for now.  Scales each channel independently
         # and then stacks the result.
-        r = tf.map_fn(lambda x: Equalize.equalize_channel(x, 0), images)
-        g = tf.map_fn(lambda x: Equalize.equalize_channel(x, 1), images)
-        b = tf.map_fn(lambda x: Equalize.equalize_channel(x, 2), images)
+        r = tf.map_fn(lambda x: self.equalize_channel(x, 0), images)
+        g = tf.map_fn(lambda x: self.equalize_channel(x, 1), images)
+        b = tf.map_fn(lambda x: self.equalize_channel(x, 2), images)
+        # TODO(lukewood): ideally this should be vectorized.
+        
         images = tf.stack([r, g, b], axis=-1)
         return images
