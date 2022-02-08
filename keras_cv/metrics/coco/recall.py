d836e25e06d01731630cc0983647f33352bd5270
Luke Wood <lukewood@google.com>
2022-02-07 21:16:06 -0800

    Begin addressing comments from Francois and Scott
    - updates docstring
    - changes some default parameters

--- keras_cv/metrics/coco/recall.py
+++ keras_cv/metrics/coco/recall.py
@@ -15,6 +15,10 @@ import tensorflow as tf
 import tensorflow.keras as keras
 import tensorflow.keras.initializers as initializers
 
+<<<<<<< HEAD
+=======
+from keras_cv.utils import iou as iou_lib
+>>>>>>> 1e14976 (Begin addressing comments from Francois and Scott)
 from keras_cv.metrics.coco import utils
 from keras_cv.utils import bbox
 from keras_cv.utils import iou as iou_lib
@@ -24,45 +28,56 @@ class COCORecall(keras.metrics.Metric):
     """COCORecall computes the COCO recall metric.
 
     Args:
-        iou_thresholds: defaults to [0.5:0.05:0.95].
-        category_ids: no default, users must provide.
-        area_range: area range to consider bounding boxes in. Defaults to all.
+        class_ids: The class IDs to evaluate the metric for.  To evaluate for
+            all classes in over a set of sequentially labelled classes, pass
+            `range(num_classes)`.
+        iou_thresholds: IoU thresholds over which to evaluate the recall.  Must
+            be a tuple of floats, defaults to [0.5:0.05:0.95].
+        area_range: area range to constrict the considered bounding boxes in
+            metric computation. Defaults to `None`, which makes the metric
+            count all bounding boxes.  Must be a tuple of floats.  The first
+            number in the tuple represents a lower bound for areas, while the
+            second value represents an upper bound.  For example, when
+            `(0, 32**2)` is passed to the metric, recall is only evaluated for
+            objects with areas less than `32*32`.  If `(32**2, 1000000**2)` is
+            passed the metric will only be evaluated for boxes with areas larger
+            than `32**2`, and smaller than `1000000**2`.
         max_detections: number of maximum detections a model is allowed to make.
-
+            Must be an integer, defaults to `100`.
     Usage:
-        COCORecall accepts two Tensors as input to it's `update_state` method.
-        These Tensors represent bounding boxes in `corners` format.  Utilities
-        to convert Tensors from `xywh` to `corners` format can be found in
-        `keras_cv.utils.bbox`.
 
-        Each image in a dataset may have a different number of bounding boxes,
-        both in the ground truth dataset and the prediction set.  In order to
-        account for this, you may either pass a `tf.RaggedTensor`, or pad Tensors
-        with `-1`s to indicate unused boxes.  A utility function to perform this
-        padding is available at `keras_cv_.utils.bbox.pad_bbox_batch_to_shape`.
+    COCORecall accepts two Tensors as input to it's `update_state` method.
+    These Tensors represent bounding boxes in `corners` format.  Utilities
+    to convert Tensors from `xywh` to `corners` format can be found in
+    `keras_cv.utils.bbox`.
 
-        ```
-        coco_recall = COCORecall(
-            max_detections=100,
-            category_ids=[1],
-            area_range=(32**2, 64**2),
-        )
+    Each image in a dataset may have a different number of bounding boxes,
+    both in the ground truth dataset and the prediction set.  In order to
+    account for this, you may either pass a `tf.RaggedTensor`, or pad Tensors
+    with `-1`s to indicate unused boxes.  A utility function to perform this
+    padding is available at `keras_cv_.utils.bbox.pad_bbox_batch_to_shape`.
 
-        y_true = np.array([[[0, 0, 10, 10, 1], [5, 5, 10, 10, 1]]]).astype(np.float32)
-        y_pred = np.array([[[0, 0, 10, 10, 1, 1.0], [5, 5, 10, 10, 1, 0.9]]]).astype(
-            np.float32
-        )
-        coco_recall.update_state(y_true, y_pred)
-        coco_recall.result()
-        # > 0.0
-        ```
+    ```python
+    coco_recall = keras_cv.metrics.coco.COCORecall(
+        max_detections=100,
+        class_ids=[1]
+    )
+
+    y_true = np.array([[[0, 0, 10, 10, 1], [20, 20, 10, 10, 1]]]).astype(np.float32)
+    y_pred = np.array([[[0, 0, 10, 10, 1, 1.0], [5, 5, 10, 10, 1, 0.9]]]).astype(
+        np.float32
+    )
+    coco_recall.update_state(y_true, y_pred)
+    coco_recall.result()
+    # 0.5
+    ```
     """
 
     def __init__(
         self,
-        category_ids,
+        class_ids,
         iou_thresholds=None,
-        area_range=(0, 1e9**2),
+        area_range=None,
         max_detections=100,
         **kwargs
     ):
@@ -71,14 +86,14 @@ class COCORecall(keras.metrics.Metric):
 
         iou_thresholds = iou_thresholds or [x / 100.0 for x in range(50, 100, 5)]
         self.iou_thresholds = iou_thresholds
-        self.category_ids = category_ids
+        self.class_ids = class_ids
 
         self.area_range = area_range
         self.max_detections = max_detections
 
         # Initialize result counters
         num_thresholds = len(iou_thresholds)
-        num_categories = len(category_ids)
+        num_categories = len(class_ids)
 
         self.true_positives = self.add_weight(
             name="true_positives",
@@ -110,6 +125,9 @@ class COCORecall(keras.metrics.Metric):
                 "sample_weight is not yet supported in keras_cv COCO metrics."
             )
 
+        # TODO(lukewood): Add first party RaggedTensor support.  Currently
+        # this could cause an OOM error if users are not expecting to convert
+        # these tensors to dense tensors.
         if isinstance(y_true, tf.RaggedTensor):
             y_true = y_true.to_tensor(default_value=-1)
         if isinstance(y_pred, tf.RaggedTensor):
@@ -118,32 +136,32 @@ class COCORecall(keras.metrics.Metric):
         num_images = tf.shape(y_true)[0]
 
         iou_thresholds = tf.constant(self.iou_thresholds, dtype=tf.float32)
-        category_ids = tf.constant(self.category_ids, dtype=tf.float32)
+        class_ids = tf.constant(self.class_ids, dtype=tf.float32)
 
         num_thresholds = tf.shape(iou_thresholds)[0]
-        num_categories = tf.shape(category_ids)[0]
+        num_categories = tf.shape(class_ids)[0]
 
         # Sort by bbox.CONFIDENCE to make maxDetections easy to compute.
         true_positives_update = tf.zeros_like(self.true_positives)
         ground_truth_boxes_update = tf.zeros_like(self.ground_truth_boxes)
 
         for img in tf.range(num_images):
-            sentinel_filtered_y_true = utils.filter_out_sentinels(y_true[img])
-            sentinel_filtered_y_pred = utils.filter_out_sentinels(y_pred[img])
+            y_true_for_image = utils.filter_out_sentinels(y_true[img])
+            y_pred_for_image = utils.filter_out_sentinels(y_pred[img])
 
-            area_filtered_y_true = utils.filter_boxes_by_area_range(
-                sentinel_filtered_y_true, self.area_range[0], self.area_range[1]
-            )
-            # TODO(lukewood): try filtering area after max dts.
-            area_filtered_y_pred = utils.filter_boxes_by_area_range(
-                sentinel_filtered_y_pred, self.area_range[0], self.area_range[1]
-            )
+            if self.area_range is not None:
+                y_true_for_image = utils.filter_boxes_by_area_range(
+                    y_true_for_image, self.area_range[0], self.area_range[1]
+                )
+                y_pred_for_image = utils.filter_boxes_by_area_range(
+                    y_pred_for_image, self.area_range[0], self.area_range[1]
+                )
 
             for k_i in tf.range(num_categories):
-                category = category_ids[k_i]
+                category = class_ids[k_i]
 
                 category_filtered_y_pred = utils.filter_boxes(
-                    area_filtered_y_pred, value=category, axis=bbox.CLASS
+                    y_pred_for_image, value=category, axis=bbox.CLASS
                 )
 
                 detections = category_filtered_y_pred
@@ -151,7 +169,7 @@ class COCORecall(keras.metrics.Metric):
                     detections = category_filtered_y_pred[: self.max_detections]
 
                 ground_truths = utils.filter_boxes(
-                    area_filtered_y_true, value=category, axis=bbox.CLASS
+                    y_true_for_image, value=category, axis=bbox.CLASS
                 )
 
                 ious = iou_lib.compute_ious_for_image(ground_truths, detections)
