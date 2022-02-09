25366c888f05289450506859ed37d5f9a1e4615f
Qianli Scott Zhu <scottzhu@google.com>
2022-02-09 15:14:37 -0800

    Add demo script for random brightness

---
+++ examples/layers/preprocessing/random_brightness_demo.py
@@ -0,0 +1,52 @@
+"""random_brightness_demo.py shows how to use the RandomBrightness preprocessing layer.
+
+Operates on the oxford_flowers102 dataset.  In this script the flowers
+are loaded, then are passed through the preprocessing layers.
+Finally, they are shown using matplotlib.
+"""
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+from keras_cv.layers import preprocessing
+
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 64
+
+
+def resize(image, label, num_classes=10):
+    image = tf.image.resize(image, IMG_SIZE)
+    label = tf.one_hot(label, num_classes)
+    return image, label
+
+
+def main():
+    data, ds_info = tfds.load("oxford_flowers102", with_info=True,
+                              as_supervised=True)
+    train_ds = data["train"]
+
+    num_classes = ds_info.features["label"].num_classes
+
+    train_ds = (
+        train_ds.map(lambda x, y: resize(x, y, num_classes=num_classes))
+        .batch(BATCH_SIZE)
+    )
+    random_brightness = preprocessing.RandomBrightness(
+        scale=(0.1, 0.3),   # Scale can be chosen between -1.0 and 1.0
+    )
+    train_ds = train_ds.map(
+        lambda x, y: (random_brightness(x, training=True), y),
+        num_parallel_calls=tf.data.AUTOTUNE
+    )
+
+    for images, labels in train_ds.take(1):
+        plt.figure(figsize=(8, 8))
+        for i in range(9):
+            plt.subplot(3, 3, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            plt.axis("off")
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
