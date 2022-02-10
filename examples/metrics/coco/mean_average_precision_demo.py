Update MaP, working copy, but it gets the wrong results for 2/6 test cases

examples/metrics/coco/mean_average_precision_demo.py
@@ -3,20 +3,27 @@ import tensorflow as tf
 
 from keras_cv.metrics import COCOMeanAveragePrecision
 
-map = COCOMeanAveragePrecision(category_ids=[1])
+map = COCOMeanAveragePrecision(category_ids=[1, 2])
 # These would match if they were in the area range
 y_true = np.array(
-    [[[0, 0, 10, 10, 1], [0, 0, 10, 10, 2]], [[0, 0, 10, 10, 1], [0, 0, 10, 10, 2]]]
+    [
+        [
+            [0, 0, 10, 10, 1], [0, 0, 10, 10, 2]
+        ], 
+        [[
+            0, 0, 10, 10, 1], [0, 0, 10, 10, 2]
+        ]
+    ]
 ).astype(np.float32)
 y_pred = np.array(
     [
         [
             [-100, 0, 10, 10, 1, 1.0],
-            [0, 0, 10, 10, 1, 0.7],
+            [0, 0, 10, 10, 2, 0.7],
         ],
         [
             [0, 0, 10, 10, 1, 0.5],
-            [0, 0, 10, 10, 1, 0.5],
+            [0, 0, 10, 10, 2, 0.5],
         ],
     ]
 ).astype(np.float32)

keras_cv/metrics/coco/mean_average_precision.py
@@ -37,7 +37,7 @@ class COCOMeanAveragePrecision(tf.keras.metrics.Metric):
         category_ids,
         recall_thresholds=None,
         iou_thresholds=None,
-        area_range=(0, 1e9**2),
+        area_range=None,
         max_detections=100,
         num_buckets=10000,
         **kwargs
@@ -85,12 +85,14 @@ class COCOMeanAveragePrecision(tf.keras.metrics.Metric):
         for img in tf.range(num_images):
             ground_truths = utils.filter_out_sentinels(y_true[img])
             detections = utils.filter_out_sentinels(y_pred[img])
-            ground_truths = utils.filter_boxes_by_area_range(
-                ground_truths, self.area_range[0], self.area_range[1]
-            )
-            detections = utils.filter_boxes_by_area_range(
-                detections, self.area_range[0], self.area_range[1]
-            )
+
+            if self.area_range is not None:
+                ground_truths = utils.filter_boxes_by_area_range(
+                    ground_truths, self.area_range[0], self.area_range[1]
+                )
+                detections = utils.filter_boxes_by_area_range(
+                    detections, self.area_range[0], self.area_range[1]
+                )
 
             true_positives_update = tf.TensorArray(tf.int32, size=self.num_category_ids * self.num_iou_thresholds)
             false_positives_update = tf.TensorArray(tf.int32, size=self.num_category_ids * self.num_iou_thresholds)
@@ -134,8 +136,8 @@ class COCOMeanAveragePrecision(tf.keras.metrics.Metric):
                     fp_counts_per_bucket = tf.math.bincount(
                         fps_by_bucket, minlength=self.num_buckets, maxlength=self.num_buckets
                     )
-                    true_positives_update = true_positives_update.write((iou_i * c_i) + iou_i, tp_counts_per_bucket)
-                    false_positives_update = false_positives_update.write((iou_i * c_i) + iou_i, fp_counts_per_bucket)
+                    true_positives_update = true_positives_update.write((self.num_iou_thresholds * c_i) + iou_i, tp_counts_per_bucket)
+                    false_positives_update = false_positives_update.write((self.num_iou_thresholds * c_i) + iou_i, fp_counts_per_bucket)
 
             true_positives_update = tf.reshape(true_positives_update.stack(), (self.num_category_ids, self.num_iou_thresholds, self.num_buckets))
             false_positives_update = tf.reshape(false_positives_update.stack(), (self.num_category_ids, self.num_iou_thresholds, self.num_buckets))
@@ -144,17 +146,6 @@ class COCOMeanAveragePrecision(tf.keras.metrics.Metric):
             false_positive_buckets_update = false_positive_buckets_update + false_positives_update
             ground_truth_boxes_update = ground_truth_boxes_update + ground_truths_update.stack()
 
-                #     indices = [[c_i, iou_i, i] for i in range(self.num_buckets)]
-                #     true_positive_buckets_update = tf.tensor_scatter_nd_add(
-                #         true_positive_buckets_update,
-                #         [indices],
-                #         tp_counts_per_bucket
-                #     )
-                #     false_positive_buckets_update = tf.tensor_scatter_nd_add(
-                #         false_positive_buckets_update,
-                #         [indices],
-                #         fp_counts_per_bucket
-                #     )
         self.ground_truths.assign_add(ground_truth_boxes_update)
         self.true_positive_buckets.assign_add(true_positive_buckets_update)
         self.false_positive_buckets.assign_add(false_positive_buckets_update)
@@ -192,7 +183,7 @@ class COCOMeanAveragePrecision(tf.keras.metrics.Metric):
 
                 pr_per_recall_threshold = precision_result.stack()
                 result_ij = tf.math.reduce_mean(pr_per_recall_threshold, axis=-1)
-                result = result.write(j + i*j, result_ij)
+                result = result.write(j + i*self.num_iou_thresholds, result_ij)
 
         result = tf.reshape(result.stack(), (self.num_category_ids, self.num_iou_thresholds))
         result = tf.math.reduce_mean(result, axis=-1)

keras_cv/metrics/coco/numerical_tests/mean_average_precision_test.py
@@ -28,79 +28,78 @@ class MeanAveragePrecisionTest(tf.test.TestCase):
         mean_average_precision = COCOMeanAveragePrecision(
             category_ids=categories + [1000],
             max_detections=100,
-            area_range=(0, 1e9 ** 2),
         )
 
         mean_average_precision.update_state(y_true, y_pred)
         result = mean_average_precision.result().numpy()
         self.assertAlmostEqual(result, 0.643, delta=0.05)
 
-    # def test_mean_average_precision_correctness_large(self):
-    #     y_true, y_pred, categories = load_samples(SAMPLE_FILE)
+    def test_mean_average_precision_correctness_large(self):
+        y_true, y_pred, categories = load_samples(SAMPLE_FILE)
 
-    #     mean_average_precision = COCOMeanAveragePrecision(
-    #         category_ids=categories + [1000],
-    #         max_detections=100,
-    #         area_range=(0, 32 ** 2),
-    #     )
+        mean_average_precision = COCOMeanAveragePrecision(
+            category_ids=categories + [1000],
+            max_detections=100,
+            area_range=(0, 32 ** 2),
+        )
 
-    #     mean_average_precision.update_state(y_true, y_pred)
-    #     result = mean_average_precision.result().numpy()
-    #     self.assertAlmostEqual(result, 0.689, delta=0.05)
+        mean_average_precision.update_state(y_true, y_pred)
+        result = mean_average_precision.result().numpy()
+        self.assertAlmostEqual(result, 0.689, delta=0.05)
 
-    # def test_mean_average_precision_correctness_medium(self):
-    #     y_true, y_pred, categories = load_samples(SAMPLE_FILE)
+    def test_mean_average_precision_correctness_medium(self):
+        y_true, y_pred, categories = load_samples(SAMPLE_FILE)
 
-    #     mean_average_precision = COCOMeanAveragePrecision(
-    #         category_ids=categories + [1000],
-    #         max_detections=100,
-    #         area_range=(0, 32 ** 2),
-    #     )
+        mean_average_precision = COCOMeanAveragePrecision(
+            category_ids=categories + [1000],
+            max_detections=100,
+            area_range=(0, 32 ** 2),
+        )
 
-    #     mean_average_precision.update_state(y_true, y_pred)
-    #     result = mean_average_precision.result().numpy()
-    #     self.assertAlmostEqual(result, 0.633, delta=0.05)
+        mean_average_precision.update_state(y_true, y_pred)
+        result = mean_average_precision.result().numpy()
+        self.assertAlmostEqual(result, 0.633, delta=0.05)
 
-    # def test_mean_average_precision_correctness_small(self):
-    #     y_true, y_pred, categories = load_samples(SAMPLE_FILE)
+    def test_mean_average_precision_correctness_small(self):
+        y_true, y_pred, categories = load_samples(SAMPLE_FILE)
 
-    #     mean_average_precision = COCOMeanAveragePrecision(
-    #         category_ids=categories + [1000],
-    #         max_detections=100,
-    #         area_range=(0, 32 ** 2),
-    #     )
+        mean_average_precision = COCOMeanAveragePrecision(
+            category_ids=categories + [1000],
+            max_detections=100,
+            area_range=(0, 32 ** 2),
+        )
 
-    #     mean_average_precision.update_state(y_true, y_pred)
-    #     result = mean_average_precision.result().numpy()
-    #     self.assertAlmostEqual(result, 0.644, delta=0.05)
+        mean_average_precision.update_state(y_true, y_pred)
+        result = mean_average_precision.result().numpy()
+        self.assertAlmostEqual(result, 0.644, delta=0.05)
 
-    # def test_mean_average_precision_correctness_iou_05(self):
-    #     y_true, y_pred, categories = load_samples(SAMPLE_FILE)
+    def test_mean_average_precision_correctness_iou_05(self):
+        y_true, y_pred, categories = load_samples(SAMPLE_FILE)
 
-    #     mean_average_precision = COCOMeanAveragePrecision(
-    #         category_ids=categories + [1000],
-    #         iou_thresholds=[0.5],
-    #         max_detections=100,
-    #         area_range=(0, 1e5 ** 2),
-    #     )
+        mean_average_precision = COCOMeanAveragePrecision(
+            category_ids=categories + [1000],
+            iou_thresholds=[0.5],
+            max_detections=100,
+            area_range=(0, 1e5 ** 2),
+        )
 
-    #     mean_average_precision.update_state(y_true, y_pred)
-    #     result = mean_average_precision.result().numpy()
-    #     self.assertAlmostEqual(result, 1.0, delta=0.05)
+        mean_average_precision.update_state(y_true, y_pred)
+        result = mean_average_precision.result().numpy()
+        self.assertAlmostEqual(result, 1.0, delta=0.05)
 
-    # def test_mean_average_precision_correctness_iou_75(self):
-    #     y_true, y_pred, categories = load_samples(SAMPLE_FILE)
+    def test_mean_average_precision_correctness_iou_75(self):
+        y_true, y_pred, categories = load_samples(SAMPLE_FILE)
 
-    #     mean_average_precision = COCOMeanAveragePrecision(
-    #         category_ids=categories + [1000],
-    #         iou_thresholds=[0.75],
-    #         max_detections=100,
-    #         area_range=(0, 1e5 ** 2),
-    #     )
+        mean_average_precision = COCOMeanAveragePrecision(
+            category_ids=categories + [1000],
+            iou_thresholds=[0.75],
+            max_detections=100,
+            area_range=(0, 1e5 ** 2),
+        )
 
-    #     mean_average_precision.update_state(y_true, y_pred)
-    #     result = mean_average_precision.result().numpy()
-    #     self.assertAlmostEqual(result, 0.729, delta=0.05)
+        mean_average_precision.update_state(y_true, y_pred)
+        result = mean_average_precision.result().numpy()
+        self.assertAlmostEqual(result, 0.729, delta=0.05)
 
 
 def load_samples(fname):