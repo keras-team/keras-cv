38085a3aff50cc5cdc6c05e4b3b18b38a4b1b231
Sebastian <mocart15@gmail.com>
2022-02-10 21:24:39 +0100

    Added Solarize preprocessing layer. (#106)
    * Added Invert preprocessing layer.
    
    * Added Invert layer constructor params. Refactored Invert layer tests.
    
    * Added get_config method to Invert layer.
    
    * Modified Invert layer docstring.
    
    * Renamed Invert layer to Solarization.
    
    * Added optional threshold parameter to Solarization layer.
    
    * Added input values clipping in Solarization layer.
    
    * Added reference in the docstring for Solarization layer.
    
    * Update Solarization per API design decision
    
    * Update get_config
    
    * Run black
    
    Co-authored-by: Luke Wood <lukewood@google.com>

--- keras_cv/layers/preprocessing/__init__.py
+++ keras_cv/layers/preprocessing/__init__.py
@@ -15,3 +15,4 @@
 from keras_cv.layers.preprocessing.cut_mix import CutMix
 from keras_cv.layers.preprocessing.mix_up import MixUp
 from keras_cv.layers.preprocessing.random_cutout import RandomCutout
+from keras_cv.layers.preprocessing.solarization import Solarization
