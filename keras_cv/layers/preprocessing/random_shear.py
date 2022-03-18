b7c65b2767431272e7dc86b76c28f9e09f060211
Luke Wood <lukewoodcs@gmail.com>
2022-03-18 10:51:55 -0700

    Begin random shear

--- keras_cv/layers/preprocessing/random_shear.py
+++ keras_cv/layers/preprocessing/random_shear.py
@@ -3,5 +3,13 @@ from tensorflow import keras
 
 
 class RandomShear(tf.keras.__internal__.layers.BaseImageAugmentationLayer):
-    def __init__(self):
+
+    def __init__(self, x_range=None, y_range=None, **kwargs):
+        super(**kwargs)
         pass
+
+    def get_random_transformation(self):
+        
+
+    def augment_image(self, image, transformation=None):
+        return image
