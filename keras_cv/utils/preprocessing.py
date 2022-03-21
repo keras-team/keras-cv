9c890f81debc3217065da04dc5be282ec432ba4d
Luke Wood <lukewoodcs@gmail.com>
2022-03-21 12:50:10 -0700

    Rebase preprocessing

--- keras_cv/utils/preprocessing.py
+++ keras_cv/utils/preprocessing.py
@@ -15,23 +15,18 @@ import tensorflow as tf
 from tensorflow.keras import backend
 
 
-def transform_value_range(images, original_range, target_range, dtype=None):
+def transform_value_range(images, original_range, target_range, dtype=tf.float32):
     """transforms values in input tensor from original_range to target_range.
-
     This function is intended to be used in preprocessing layers that
     rely upon color values.  This allows us to assume internally that
     the input tensor is always in the range [0, 255].
-
-    Note that
-
     Args:
-        images: the set of images to transform to the [0, 255] range.
+        images: the set of images to transform to the target range range.
         original_range: the value range to transform from.
         target_range: the value range to transform to.
         dtype: the dtype to compute the conversion with.  Defaults to tf.float32.
     Returns:
         a new Tensor with values in the target range.
-
     Usage:
     ```python
     original_range = [0, 1]
@@ -42,14 +37,13 @@ def transform_value_range(images, original_range, target_range, dtype=None):
         target_range
     )
     images = tf.math.minimum(images + 10, 255)
-    images = keras_cv.utils.transform_value_range(
+    images = keras_cv.utils.preprocessing.transform_value_range(
         images,
         target_range,
         original_range
     )
     ```
     """
-    dtype = dtype or tf.float32
     images = tf.cast(images, dtype=dtype)
     original_min_value, original_max_value = _unwrap_value_range(
         original_range, dtype=dtype
@@ -63,8 +57,7 @@ def transform_value_range(images, original_range, target_range, dtype=None):
     return (images * scale_factor) + target_min_value
 
 
-def _unwrap_value_range(value_range, dtype=None):
-    dtype = dtype or tf.float32
+def _unwrap_value_range(value_range, dtype=tf.float32):
     min_value, max_value = value_range
     min_value = tf.cast(min_value, dtype=dtype)
     max_value = tf.cast(max_value, dtype=dtype)
