2ac965a2b616ea4105af4e03e1ac9070d2aed975
Luke Wood <lukewoodcs@gmail.com>
2022-03-21 14:20:24 -0700

    Update docstring

--- keras_cv/layers/preprocessing/random_shear.py
+++ keras_cv/layers/preprocessing/random_shear.py
@@ -24,12 +24,12 @@ class RandomShear(tf.keras.__internal__.layers.BaseImageAugmentationLayer):
     Args:
         x: float, 2 element tuple, or `None`.  For each augmented image a value is
             sampled from the provided range.  If a float is passed, the range is
-            interpreted as `(0, x)`.  Provided values should be positive.  If `None` is
-            passed, no shear occurs on the X axis.  Defaults to `None`.
+            interpreted as `(0, x)`.  All provided values should be positive.  If 
+            `None` is passed, no shear occurs on the X axis.  Defaults to `None`.
         y: float, 2 element tuple, or `None`.  For each augmented image a value is
             sampled from the provided range.  If a float is passed, the range is
-            interpreted as `(0, y)`.  Provided values should be positive.  If `None` is
-            passed, no shear occurs on the Y axis.  Defaults to `None`.
+            interpreted as `(0, y)`.  All provided values should be positive.  If 
+            `None` is passed, no shear occurs on the Y axis.  Defaults to `None`.
         interpolation: interpolation method used in the `ImageProjectiveTransformV3` op.
              Supported values are `"nearest"` and `"bilinear"`.
              Defaults to `"bilinear"`.
