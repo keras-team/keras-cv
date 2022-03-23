671c876dcb39ea2e1d79ff96501be84d0d749e58
Luke Wood <lukewoodcs@gmail.com>
2022-03-23 15:37:39 -0700

    reformat

--- keras_cv/layers/preprocessing/random_sharpness_test.py
+++ keras_cv/layers/preprocessing/random_sharpness_test.py
@@ -32,15 +32,17 @@ class RandomSharpnessTest(tf.test.TestCase):
 
     def test_random_sharpness_blur_effect_single_channel(self):
         xs = tf.expand_dims(
-            tf.constant([
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 1, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-            ]),
+            tf.constant(
+                [
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                ]
+            ),
             axis=-1,
         )
         xs = tf.expand_dims(xs, axis=0)
@@ -51,18 +53,19 @@ class RandomSharpnessTest(tf.test.TestCase):
         self.assertEqual(xs.shape, ys.shape)
 
         result = tf.expand_dims(
-            tf.constant([
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 1/13, 1/13, 1/13, 0, 0],
-                [0, 0, 1/13, 5/13, 1/13, 0, 0],
-                [0, 0, 1/13, 1/13, 1/13, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0],
-            ]),
+            tf.constant(
+                [
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 1 / 13, 1 / 13, 1 / 13, 0, 0],
+                    [0, 0, 1 / 13, 5 / 13, 1 / 13, 0, 0],
+                    [0, 0, 1 / 13, 1 / 13, 1 / 13, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                ]
+            ),
             axis=-1,
         )
         result = tf.expand_dims(result, axis=0)
-
 
         self.assertAllClose(ys, result)
