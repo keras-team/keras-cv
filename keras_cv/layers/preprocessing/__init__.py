54fcf3bffe905e46253ca454b7bea67c970c0342
Luke Wood <LukeWood@users.noreply.github.com>
2022-03-23 11:29:40 -0700

    Creates an autocontrast KPL (#194)
    * begin autocontrast
    
    * begin value range tests
    
    * Implement autocontrast layer
    
    * Reformat
    
    * Fix lint issues
    
    * Revert spurious changes
    
    * Vectorize autocontrast
    
    * Re-vectorize autocontrast
    
    * Update docstring
    
    * add new complex test case
    
    * Reformat
    
    * remove unneeded comments
    
    * Update autocontrast test

--- keras_cv/layers/preprocessing/__init__.py
+++ keras_cv/layers/preprocessing/__init__.py
@@ -27,6 +27,7 @@ from tensorflow.keras.layers import RandomZoom
 from tensorflow.keras.layers import Rescaling
 from tensorflow.keras.layers import Resizing
 
+from keras_cv.layers.preprocessing.auto_contrast import AutoContrast
 from keras_cv.layers.preprocessing.channel_shuffle import ChannelShuffle
 from keras_cv.layers.preprocessing.cut_mix import CutMix
 from keras_cv.layers.preprocessing.equalization import Equalization
