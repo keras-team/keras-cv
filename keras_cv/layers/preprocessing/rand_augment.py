f43079a6132cbbe01e1a445c1075c740a0469d4b
Luke Wood <lukewoodcs@gmail.com>
2022-03-24 12:02:45 -0700

    begin randaugment

---
+++ keras_cv/layers/preprocessing/rand_augment.py
@@ -0,0 +1,10 @@
+from tensorflow import keras
+
+
+class RandAugment(keras.layers.Layer):
+    def __init__(
+        self,
+        num_layers,
+        magnitude,
+        prob_to_apply):
+        pass
---
+++ keras_cv/layers/preprocessing/rand_augment_test.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+
+class RandAugmentTest(tf.test.TestCase):
+    pass
