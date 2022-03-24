7cd234c8ad70f704d92ddd55ff0d5347e7614725
Luke Wood <lukewoodcs@gmail.com>
2022-03-24 11:36:56 -0700

    implement random color degeneration layer

--- keras_cv/layers/preprocessing/__init__.py
+++ keras_cv/layers/preprocessing/__init__.py
@@ -39,3 +39,4 @@ from keras_cv.layers.preprocessing.random_cutout import RandomCutout
 from keras_cv.layers.preprocessing.random_sharpness import RandomSharpness
 from keras_cv.layers.preprocessing.random_shear import RandomShear
 from keras_cv.layers.preprocessing.solarization import Solarization
+from keras_cv.layers.preprocessing.random_color_degeneration import RandomColorDegeneration
