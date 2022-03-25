0c2e6c4a6e067f01aba6b3cb076e48aedffd989c
Luke Wood <LukeWood@users.noreply.github.com>
2022-03-25 16:14:18 -0700

    Update solarization in preparation for use in RandAugment (#213)
    * Update solarization in preparation for use in RandAugment
    
    * Update threshold default
    
    * Update equalize_test to use parameterized
    
    * Remove helper function
    
    * Rename solizare method
    
    * update solarization dummy input name

--- keras_cv/layers/preprocessing/solarization.py
+++ keras_cv/layers/preprocessing/solarization.py
@@ -13,8 +13,10 @@
 # limitations under the License.
 import tensorflow as tf
 
+from keras_cv.utils import preprocessing
 
-class Solarization(tf.keras.layers.Layer):
+
+class Solarization(tf.keras.__internal__.layers.BaseImageAugmentationLayer):
     """Applies (max_value - pixel + min_value) for each pixel in the image.
 
     When created without `threshold` parameter, the layer performs solarization to
@@ -25,22 +27,29 @@ class Solarization(tf.keras.layers.Layer):
     - [AutoAugment: Learning Augmentation Policies from Data](
         https://arxiv.org/abs/1805.09501
     )
+    - [RandAugment](https://arxiv.org/pdf/1909.13719.pdf)
 
     Args:
-        threshold: (Optionally) int or float. If specified, only pixel values above this
+        addition: (Optional) int or float.  If specified, this value is added to each
+            pixel before solarization and thresholding.  The addition value should be
+            scaled accoring to the value range (0, 255).  Defaults to 0.0.
+        threshold: (Optional) int or float. If specified, only pixel values above this
             threshold will be solarized.
+        value_range: a tuple or a list of two elements. The first value represents
+            the lower bound for values in passed images, the second represents the
+            upper bound. Images passed to the layer should have values within
+            `value_range`. Defaults to `(0, 255)`.
 
     Usage:
-
     ```python
-        (images, labels), _ = tf.keras.datasets.cifar10.load_data()
-        print(images[0, 0, 0])
-        # [59 62 63]
-        # Note that images are Tensor with values in the range [0, 255]
-        solarization = Solarization()
-        images = solarization(images)
-        print(images[0, 0, 0])
-        # [196, 193, 192]
+    (images, labels), _ = tf.keras.datasets.cifar10.load_data()
+    print(images[0, 0, 0])
+    # [59 62 63]
+    # Note that images are Tensor with values in the range [0, 255]
+    solarization = Solarization()
+    images = solarization(images)
+    print(images[0, 0, 0])
+    # [196, 193, 192]
     ```
 
     Call arguments:
@@ -49,24 +58,37 @@ class Solarization(tf.keras.layers.Layer):
             or [height, width, channels].
     """
 
-    def __init__(self, threshold=None):
+    def __init__(
+        self,
+        addition=0.0,
+        threshold=0.0,
+        value_range=(0, 255),
+    ):
         super().__init__()
+        self.addition = addition
         self.threshold = threshold
+        self.value_range = value_range
 
-    def call(self, images):
-        images = tf.clip_by_value(images, clip_value_min=0, clip_value_max=255)
-        if self.threshold is None:
-            return self._solarize(images)
-        else:
-            return self._solarize_above_threshold(images)
+    def augment_image(self, image, transformation=None):
+        image = preprocessing.transform_value_range(
+            image, original_range=self.value_range, target_range=(0, 255)
+        )
+        result = image + self.addition
+        result = tf.clip_by_value(result, 0, 255)
+        result = self._solarize(result)
+        result = preprocessing.transform_value_range(
+            result, original_range=(0, 255), target_range=self.value_range
+        )
+        return result
 
     def _solarize(self, images):
-        return 255 - images
-
-    def _solarize_above_threshold(self, images):
-        return tf.where(images < self.threshold, images, self._solarize(images))
+        return tf.where(images < self.threshold, images, 255 - images)
 
     def get_config(self):
-        config = {"threshold": self.threshold}
+        config = {
+            "threshold": self.threshold,
+            "addition": self.addition,
+            "value_range": self.value_range,
+        }
         base_config = super().get_config()
         return dict(list(base_config.items()) + list(config.items()))
