189f5dc2da65e934452614c257aaad139d690daf
Luke Wood <lukewoodcs@gmail.com>
2022-03-29 14:32:48 -0700

    Udpate example

--- examples/layers/preprocessing/rand_augment_demo.py
+++ examples/layers/preprocessing/rand_augment_demo.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""mix_up_demo.py shows how to use the MixUp preprocessing layer.
+"""rand_augment_demo.py shows how to use the RandAugment preprocessing layer.
 
 Uses the oxford_flowers102 dataset.  In this script the flowers
 are loaded, then are passed through the preprocessing layers.
