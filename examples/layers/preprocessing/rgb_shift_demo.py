ad858220d4df48eb0fafe3e4773f3d64f5ea71a2
M.Innat <innat1994@gmail.com>
2022-03-31 01:36:05 +0600

    fix format

--- examples/layers/preprocessing/rgb_shift_demo.py
+++ examples/layers/preprocessing/rgb_shift_demo.py
@@ -7,6 +7,7 @@ Finally, they are shown using matplotlib.
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow_datasets as tfds
+
 from keras_cv.layers import preprocessing
 
 IMG_SIZE = (224, 224)
@@ -31,13 +32,11 @@ def main():
         .batch(BATCH_SIZE)
     )
 
-    rgbshift = preprocessing.RGBShift(
-        factor=0.4
-        )
-        
+    rgbshift = preprocessing.RGBShift(factor=0.4)
+
     train_ds = train_ds.map(
         lambda x, y: (rgbshift(x, training=True), y),
-        num_parallel_calls=tf.data.AUTOTUNE
+        num_parallel_calls=tf.data.AUTOTUNE,
     )
 
     for images, labels in train_ds.take(1):
@@ -45,4 +44,3 @@ def main():
         for i in range(9):
             plt.subplot(3, 3, i + 1)
             plt.imshow(images[i].numpy().astype("uint8"))
-            
