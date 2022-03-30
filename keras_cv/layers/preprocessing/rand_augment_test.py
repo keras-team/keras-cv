7375f50d832f3ce0a079f2e651b8ff36182a383b
Luke Wood <lukewoodcs@gmail.com>
2022-03-29 23:25:39 -0700

    Run format

--- keras_cv/layers/preprocessing/rand_augment_test.py
+++ keras_cv/layers/preprocessing/rand_augment_test.py
@@ -17,11 +17,3 @@ class RandAugmentTest(tf.test.TestCase, parameterized.TestCase):
         xs = tf.ones((2, 512, 512, 3))
         ys = rand_augment(xs)
         self.assertEqual(ys.shape, (2, 512, 512, 3))
-
-    def test_runs_with_dict_input(self):
-        rand_augment = layers.RandAugment(magnitude=1.0)
-        xs = tf.ones((2, 512, 512, 3))
-        labels = tf.ones((2,), dtype=tf.float32)
-        inputs = {"images": xs, "labels": labels}
-        result = rand_augment(inputs)
-        self.assertEqual(result["images"].shape, xs.shape)
