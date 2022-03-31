77289ebff57004bc3ca6024b2afa13a1025ddcc0
lucasdavid <lucasolivdavid@gmail.com>
2022-03-31 10:06:50 -0300

    Improve docs in Grad-CAM demo

--- examples/visualization/gradcam_demo.py
+++ examples/visualization/gradcam_demo.py
@@ -3,42 +3,42 @@ import numpy as np
 import tensorflow as tf
 
 from keras_cv import visualization
-from keras_cv.visualization.utils import normalize
+from keras_cv.visualization.utils import normalize, collect_endpoints
 
 SIZES = (299, 299)
 LABELS = 5
 
 # Load images from the web
+def load_image(path):
+    image = tf.keras.preprocessing.image.load_img(path, target_size=SIZES)
+    return tf.keras.preprocessing.image.img_to_array(image)
+
 paths = [
     tf.keras.utils.get_file("african_elephant.jpg", "https://i.imgur.com/Bvro0YD.png"),
     tf.keras.utils.get_file("wolf.jpg", "https://i.imgur.com/rRcsL2D.jpeg"),
     tf.keras.utils.get_file("cat_and_dog.jpg", "https://i.imgur.com/6wxtMEp.jpeg"),
 ]
 
-
-def load_and_prepare(paths):
-    return np.stack(
-        [
-            tf.keras.preprocessing.image.img_to_array(
-                tf.keras.preprocessing.image.load_img(path, target_size=SIZES)
-            )
-            for path in paths
-        ]
-    ).astype("uint8")
-
-
-images = load_and_prepare(paths)
+images = np.stack([load_image(path) for path in paths]).astype("uint8")
 x = tf.convert_to_tensor(images.astype("float32") / 127.5 - 1)
 
-# Load pre-trained model over ImageNet
+# Load a model with pre-trained weights from ImageNet
 input_tensor = tf.keras.Input([None, None, 3], name="images")
 rn50 = tf.keras.applications.ResNet50V2(
     input_tensor=input_tensor, weights="imagenet", classifier_activation=None
 )
 
-# Collect the input tensor of the average pooling layer,
-# the most common positional signal used when computing Grad-CAM:
-endpoints = visualization.utils.collect_endpoints(
+# Grad-CAM works by computing the gradient of an score (logit) unit with
+# respect to an intermediate activation positional signal A^k_{ij} in the
+# network.
+#
+# We define a new model that outputs:
+#
+#   (a) The last positional signal of the network (the input tensor of the
+#       Global Average Pooling layer);
+#   (b) The output scores (logits) of the network.
+#
+endpoints = collect_endpoints(
     rn50,
     endpoints=[
         {"name": "avg_pool", "link": "input"},
@@ -60,12 +60,14 @@ decoded = tf.keras.applications.imagenet_utils.decode_predictions(probs.numpy())
 
 logits, maps = visualization.gradcam(rn50_w_acts, x, labels)
 
-# # We are not concerned with pixels that negatively contribute
-# # to its classification, only pixels that belong to that class.
+# As we are only interested in pixels that positively contribute to the
+# classification of a label, we crunch pixels that negatively contribute to
+# its classification and unrelated pixels (zero contribution) together:
 maps = tf.nn.relu(maps)
 maps = normalize(maps)
 maps = tf.image.resize(maps, SIZES).numpy()
 
+# Visualize maps
 rows = len(images)
 cols = LABELS
 
