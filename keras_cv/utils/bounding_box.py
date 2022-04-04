8f0222828dd08f011a320d8ebc4d94db6bca7a99
Luke Wood <lukewoodcs@gmail.com>
2022-04-04 13:12:10 -0700

    Address scott comment

--- keras_cv/utils/bounding_box.py
+++ keras_cv/utils/bounding_box.py
@@ -15,26 +15,11 @@
 """Shared utility functions for working with bounding boxes.
 
 Usually bounding boxes is a 2D Tensor with shape [batch, 4]. The second dimension
-will contain 4 numbers based on 2 different formats:
+will contain 4 numbers based on 2 different formats.  In KerasCV, we will use the
+`corners` format, which is [LEFT, TOP, RIGHT, BOTTOM].
 
-1. LEFT, TOP, RIGHT, BOTTOM, where LEFT, TOP represent the top-left corner
-   coordinates, and RIGHT, BOTTOM represent the bottom-right corner coordinates.
-2. X, Y, WIDTH, HEIGHT, where X and Y are the coordinates for the center of the box.
-
-Math wise:
-LEFT = X - WIDTH / 2
-TOP = Y - HEIGHT / 2
-RIGHT = X + WIDTH / 2
-BOTTOM = Y + HEIGHT / 2
-
-X = (LEFT + RIGHT) / 2
-Y = (TOP + BOTTOM) / 2
-WIDTH = RIGHT - LEFT
-HEIGHT = BOTTOM - TOP
-
-Note that these two formats are both commonly used. Corners format are mostly used
-for IOU computation, whereas XYWH are easy for bounding box generation with different
-center and width/height ratio.
+In this file, provide utility functions for manipulating bounding boxes and converting
+their formats.
 """
 
 import tensorflow as tf
@@ -49,12 +34,14 @@ CLASS = 4
 CONFIDENCE = 5
 
 
-def convert_to_corners(bounding_boxes, format="coco"):
+def convert_to_corners(bounding_boxes, format):
     """Converts bounding_boxes to corners format.
+
+    Converts bounding boxes from the provided format to corners format, which is:
+    `[left, top, right, bottom]`.
 
     args:
         format:  one of "coco" or "yolo".  The formats are as follows-
-            pascal_voc=[x_min, y_min, x_max, y_max]
             coco=[x_min, y_min, width, height]
             yolo=[x_center, y_center, width, height]
     """
