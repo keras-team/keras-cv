769eaef5400734bc0c7998ad00126b08ad7f6cda
Luke Wood <lukewoodcs@gmail.com>
2022-04-04 12:55:40 -0700

    Factor sampler rename

--- keras_cv/core/factor_sampler/__init__.py
+++ keras_cv/core/factor_sampler/__init__.py
@@ -11,13 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-<<<<<<< HEAD:keras_cv/core/factor_sampler/__init__.py
 from keras_cv.core.factor_sampler.constant_factor_sampler import ConstantFactorSampler
 from keras_cv.core.factor_sampler.factor_sampler import FactorSampler
 from keras_cv.core.factor_sampler.uniform_factor_sampler import UniformFactorSampler
-=======
-from keras_cv.core.factor.constant_factor import ConstantFactor
-from keras_cv.core.factor.factor import Factor
-from keras_cv.core.factor.normal_factor import NormalFactor
-from keras_cv.core.factor.uniform_factor import UniformFactor
->>>>>>> afa90ec (Add some unit tests for RandAugment):keras_cv/core/factor/__init__.py
+from keras_cv.core.factorr_sampler.normal_factorr_sampler import NormalFactorSampler
