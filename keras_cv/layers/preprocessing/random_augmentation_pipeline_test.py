1ee69922df43b932dc70f7a79abc52e796f4d21e
Luke Wood <lukewoodcs@gmail.com>
2022-04-07 19:35:47 -0700

    Add custom layer to count invocations

--- keras_cv/layers/preprocessing/random_augmentation_pipeline_test.py
+++ keras_cv/layers/preprocessing/random_augmentation_pipeline_test.py
@@ -23,9 +23,8 @@ class CountInvocations(tf.keras.layers.Layer):
         self.calls = tf.Variable(0, trainable=False)
 
     def call(self, inputs):
-        self.calls.assign_add(1)
+        calls = self.calls.assign_add(1)
         return inputs
-
 
 class RandomAugmentationPipelineTest(tf.test.TestCase, parameterized.TestCase):
     @parameterized.named_parameters(("1", 1), ("3", 3), ("5", 5))
@@ -37,7 +36,7 @@ class RandomAugmentationPipelineTest(tf.test.TestCase, parameterized.TestCase):
         xs = tf.random.uniform((2, 512, 512, 3), 0, 255, dtype=tf.float32)
         os = pipeline(xs)
         self.assertAllClose(xs, os)
-        self.assertEqual(layer.calls, 2 * augmentations_per_image)
+        self.assertEqual(layer.calls, 2*(augmentations_per_image-1))
 
     @parameterized.named_parameters(("1", 1), ("3", 3), ("5", 5))
     def test_calls_layers_augmentations_per_image_times_single_image(
@@ -49,5 +48,6 @@ class RandomAugmentationPipelineTest(tf.test.TestCase, parameterized.TestCase):
         )
         xs = tf.random.uniform((512, 512, 3), 0, 255, dtype=tf.float32)
         os = pipeline(xs)
+        
         self.assertAllClose(xs, os)
-        self.assertEqual(layer.calls, augmentations_per_image)
+        self.assertEqual(layer.calls, augmentations_per_image-1)
