b9a3303d526600f407539a416122266592bda52f
Kartik Sharma <kartik4949@gmail.com>
2022-04-12 01:28:31 +0530

    better test cases

--- keras_cv/visualization/test_segmentation_mask.py
+++ keras_cv/visualization/test_segmentation_mask.py
@@ -52,13 +52,16 @@ class DrawSegmentationTest(tf.test.TestCase):
 
     def test_draw_segmentation_full_factor(self):
         images = draw_segmentation(self.images, self.masks, color=self.color, alpha=1.0)
-        image_mask_section = images[
+        mask_section = images[
             :, self.MASK_SIZE : self.mask_y, self.MASK_SIZE : self.mask_y
         ]
-        self.assertAllEqual(
-            image_mask_section,
-            tf.ones_like(image_mask_section) * colors.get(self.color),
-        )
+
+        actual_images = tf.Variable(tf.identity(self.images))
+        actual_images[
+            :, self.MASK_SIZE : self.mask_y, self.MASK_SIZE : self.mask_y
+        ].assign(mask_section)
+
+        self.assertAllEqual(images, actual_images)
 
     def test_draw_segmentation_no_factor(self):
         images = draw_segmentation(self.images, self.masks, color=self.color, alpha=0.0)
@@ -71,9 +74,7 @@ class DrawSegmentationTest(tf.test.TestCase):
         )
         color_rgb = colors.get(self.color)
         alpha_tf = tf.constant(alpha)
-        mask_section = images[
-            :, self.MASK_SIZE : self.mask_y, self.MASK_SIZE : self.mask_y
-        ]
+
         image_section = tf.cast(
             self.images[:, self.MASK_SIZE : self.mask_y, self.MASK_SIZE : self.mask_y],
             tf.float32,
@@ -81,7 +82,11 @@ class DrawSegmentationTest(tf.test.TestCase):
         actual_mask_section = tf.round(
             image_section * (1 - alpha_tf) + alpha_tf * color_rgb
         )
-        self.assertAllEqual(mask_section, actual_mask_section)
+        actual_images = tf.Variable(tf.identity(self.images))
+        actual_images[
+            :, self.MASK_SIZE : self.mask_y, self.MASK_SIZE : self.mask_y
+        ].assign(tf.cast(actual_mask_section, actual_images.dtype))
+        self.assertAllEqual(images, actual_images)
 
     def test_draw_segmentation_color_map_base_case(self):
         images = draw_segmentation(
@@ -90,13 +95,17 @@ class DrawSegmentationTest(tf.test.TestCase):
         mask_section = images[
             :, self.MASK_SIZE : self.mask_y, self.MASK_SIZE : self.mask_y
         ]
+        actual_images = tf.Variable(tf.identity(self.images))
+        actual_images[
+            :, self.MASK_SIZE : self.mask_y, self.MASK_SIZE : self.mask_y
+        ].assign(mask_section)
 
         _color_masks = []
         for c in self.color_map.values():
             color_rgb = colors.get(c)
             _color_masks.append(tf.ones_like(mask_section[0]) * color_rgb)
         actual_mask_section = tf.stack(_color_masks, axis=0)
-        self.assertAllEqual(mask_section, actual_mask_section)
+        self.assertAllEqual(images, actual_images)
 
     def test_draw_segmentation_exception_handling(self):
 
