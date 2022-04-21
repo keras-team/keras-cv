b7e1606e67ef07b864fd10be8cf92be020a8aca9
artu1999 <artus5153@gmail.com>
2022-04-21 13:02:20 +0100

    final gaussian blur changes for CLA

--- keras_cv/layers/preprocessing/gaussian_blur_test.py
+++ keras_cv/layers/preprocessing/gaussian_blur_test.py
@@ -11,60 +11,96 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import tensorflow as tf
 
-from keras_cv.layers.preprocessing.gaussian_blur import GaussianBlur
+from keras_cv.layers import preprocessing
 
 
 class GaussianBlurTest(tf.test.TestCase):
     def test_return_shapes(self):
-        # RGB images
-        xs_rgb = tf.ones((2, 512, 512, 3))
-        # grayscale images
-        xs_g = tf.ones((2, 512, 512, 1))
+        layer = preprocessing.GaussianBlur(kernel_size=(3, 7), sigma=(0, 2))
 
-        layer = GaussianBlur(kernel_size=(7, 3), sigma=1)
-        xs1 = layer(xs_rgb, training=True)
-        xs2 = layer(xs_g, training=True)
+        # RGB
+        xs = tf.ones((2, 512, 512, 3))
+        xs = layer(xs)
+        self.assertEqual(xs.shape, [2, 512, 512, 3])
 
-        self.assertEqual(xs1.shape, [2, 512, 512, 3])
-        self.assertEqual(xs2.shape, [2, 512, 512, 1])
-
-    def test_in_tf_function(self):
-        xs = tf.cast(
-            tf.stack([2 * tf.ones((100, 100, 3)), tf.ones((100, 100, 3))], axis=0),
-            tf.float32,
-        )
-
-        # test 1
-        layer = GaussianBlur(kernel_size=(7, 3), sigma=1)
-
-        @tf.function
-        def augment(x):
-            return layer(x, training=True)
-
-        xs1 = augment(xs)
-
-        self.assertEqual(xs1.shape, [2, 100, 100, 3])
-
-    def test_non_square_image(self):
-        xs = tf.cast(
-            tf.stack([2 * tf.ones((512, 1024, 3)), tf.ones((512, 1024, 3))], axis=0),
-            tf.float32,
-        )
-
-        layer = GaussianBlur(kernel_size=(7, 3), sigma=1)
-        xs1 = layer(xs, training=True)
-
-        self.assertEqual(xs1.shape, [2, 512, 1024, 3])
+        # greyscale
+        xs = tf.ones((2, 512, 512, 1))
+        xs = layer(xs)
+        self.assertEqual(xs.shape, [2, 512, 512, 1])
 
     def test_in_single_image(self):
+        layer = preprocessing.GaussianBlur(kernel_size=(3, 7), sigma=(0, 2))
+
+        # RGB
         xs = tf.cast(
             tf.ones((512, 512, 3)),
             dtype=tf.float32,
         )
 
-        layer = GaussianBlur(kernel_size=(7, 3), sigma=1)
-        xs1 = layer(xs, training=True)
+        xs = layer(xs)
+        self.assertEqual(xs.shape, [512, 512, 3])
 
-        self.assertEqual(xs1.shape, [512, 512, 3])
+        # greyscale
+        xs = tf.cast(
+            tf.ones((512, 512, 1)),
+            dtype=tf.float32,
+        )
+
+        xs = layer(xs)
+        self.assertEqual(xs.shape, [512, 512, 1])
+
+    def test_non_square_images(self):
+        layer = preprocessing.GaussianBlur(kernel_size=(3, 7), sigma=(0, 2))
+
+        # RGB
+        xs = tf.ones((2, 256, 512, 3))
+        xs = layer(xs)
+        self.assertEqual(xs.shape, [2, 256, 512, 3])
+
+        # greyscale
+        xs = tf.ones((2, 256, 512, 1))
+        xs = layer(xs)
+        self.assertEqual(xs.shape, [2, 256, 512, 1])
+
+    def test_single_input_args(self):
+        layer = preprocessing.GaussianBlur(kernel_size=7, sigma=2)
+
+        # RGB
+        xs = tf.ones((2, 512, 512, 3))
+        xs = layer(xs)
+        self.assertEqual(xs.shape, [2, 512, 512, 3])
+
+        # greyscale
+        xs = tf.ones((2, 512, 512, 1))
+        xs = layer(xs)
+        self.assertEqual(xs.shape, [2, 512, 512, 1])
+
+    def test_numerical(self):
+        layer = preprocessing.GaussianBlur(kernel_size=3, sigma=(1.0, 1.0))
+
+        xs = tf.expand_dims(
+            tf.constant([[0, 0, 0], [0, 1, 0], [0, 0, 0]]),
+            axis=-1,
+        )
+
+        xs = tf.expand_dims(xs, axis=0)
+
+        # Result expected to be identical to gaussian blur kernel of
+        # size 3x3 and sigma=1.0
+        result = tf.expand_dims(
+            tf.constant(
+                [
+                    [0.07511361, 0.1238414, 0.07511361],
+                    [0.1238414, 0.20417996, 0.1238414],
+                    [0.07511361, 0.1238414, 0.07511361],
+                ]
+            ),
+            axis=-1,
+        )
+        result = tf.expand_dims(result, axis=0)
+        xs = layer(xs)
+
+        self.assertAllClose(xs, result)
