4ba79e91fdf8ebd699381b37fb9da343fc21a7d9
artu1999 <artus5153@gmail.com>
2022-04-21 12:35:48 +0100

    final changes for gaussian blur

--- keras_cv/layers/__init__.py
+++ keras_cv/layers/__init__.py
@@ -25,9 +25,9 @@ from tensorflow.keras.layers import RandomZoom
 from tensorflow.keras.layers import Rescaling
 from tensorflow.keras.layers import Resizing
 
-
 from keras_cv.layers.preprocessing.equalization import Equalization
 from keras_cv.layers.preprocessing.fourier_mix import FourierMix
+from keras_cv.layers.preprocessing.gaussianblur import GaussianBlur
 from keras_cv.layers.preprocessing.grayscale import Grayscale
 from keras_cv.layers.preprocessing.grid_mask import GridMask
 from keras_cv.layers.preprocessing.mix_up import MixUp
