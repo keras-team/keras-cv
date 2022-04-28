2ffbcd5fd0e36ea5be8b442af5c50866dcf4cc2d
Anurag Dhadse <anudhadse66@gmail.com>
2022-04-28 21:10:03 +0530

    Whitelisted format and linting files and folders for bash scripts

--- shell/format.sh
+++ shell/format.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-isort --sl --profile=black .
-black .
+base_dir=$(dirname $(dirname $0))
+targets="${base_dir}/*.py ${base_dir}/benchmarks/
+${base_dir}/examples/
+${base_dir}/keras_cv/"
+
+isort --sp "${base_dir}/setup.cfg" --sl --profile=black ${targets}
+black ${targets}
--- shell/lint.sh
+++ shell/lint.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-isort --sl -c --profile=black .
+base_dir=$(dirname $(dirname $0))
+targets="${base_dir}/*.py ${base_dir}/examples/ ${base_dir}/keras_cv/"
+
+isort --sp "${base_dir}/setup.cfg" --sl -c ${targets}
 if ! [ $? -eq 0 ]
 then
   echo "Please run \"sh shell/format.sh\" to format the code."
   exit 1
 fi
 echo "no issues with isort"
-flake8 .
+flake8 --config "${base_dir}/setup.cfg" ${targets}
 if ! [ $? -eq 0 ]
 then
   echo "Please fix the code style issue."
   exit 1
 fi
 echo "no issues with flake8"
-black --check .
+black --check ${targets}
 if ! [ $? -eq 0 ]
 then
   echo "Please run \"sh shell/format.sh\" to format the code."
     exit 1
 fi
 echo "no issues with black"
-for i in $(find examples keras_cv -name '*.py') # or whatever other pattern...
+for i in $(find ${targets} -name '*.py') # or whatever other pattern...
 do
   if ! grep -q Copyright $i
   then
