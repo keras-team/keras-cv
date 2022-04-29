cbdf0459f9f2072c5958fc1e7488aaaf22a95088
Kartik Sharma <kartik4949@gmail.com>
2022-04-29 19:32:20 +0530

    update logic

--- keras_cv/layers/preprocessing/random_snow.py
+++ keras_cv/layers/preprocessing/random_snow.py
@@ -50,6 +50,7 @@ class RandomSnow(tf.keras.__internal__.layers.BaseImageAugmentationLayer):
         )
         self.value_range = value_range
         self.seed = seed
+        self.brightness_coefficient = tf.constant(2.5)
 
     def get_random_transformation(self, image=None, label=None, bounding_box=None):
         return self.factor()
@@ -64,22 +65,33 @@ class RandomSnow(tf.keras.__internal__.layers.BaseImageAugmentationLayer):
         transformation *= 255 / 2
         transformation += 255 / 3
 
-        brightness_coefficient = tf.constant(2.5, tf.float64)
-        transformed_image_V = tf.where(
-            image_HSV[:, :, 2] < transformation,
-            image_HSV[:, :, 2] * brightness_coefficient,
-            image_HSV[:, :, 2],
-        )
+        # Saturation or channel (S).
+        image_S = image_HSV[:, :, 1]
 
-        # channel (V) should have range [0-255].
-        transformed_image_V = tf.clip_by_value(
-            transformed_image_V, clip_value_min=0, clip_value_max=255
+        # Value or Brightness channel.
+        image_V = image_HSV[:, :, 2]
+
+        # Calculate lightness in HSL color space and scale.
+        image_L = (
+            tf.math.reduce_min(image, axis=-1) + tf.math.reduce_max(image, axis=-1)
+        ) / tf.constant(2, tf.float32)
+        image_L = tf.where(
+            image_L < transformation, image_L * self.brightness_coefficient, image_L
         )
-        image_HSV = tf.stack(
-            [image_HSV[:, :, 0], image_HSV[:, :, 1], transformed_image_V], axis=-1
+        image_L = tf.clip_by_value(image_L, clip_value_min=0, clip_value_max=255)
+
+        # Get corresponding channel (V) in HSV space.
+        image_V = image_L / (1 - (image_S / tf.constant(2, tf.float32)))
+        image_V = tf.clip_by_value(image_V, clip_value_min=0, clip_value_max=255)
+
+        # Get corresponding channel (S) in HSV space.
+        image_S = tf.where(
+            image_V == 0, 0.0, tf.constant(2.0) * (tf.constant(1.0) - image_L / image_V)
         )
+        image_S = tf.clip_by_value(image_S, clip_value_min=0, clip_value_max=1.0)
+
+        image_HSV = tf.stack([image_HSV[:, :, 0], image_S, image_V], axis=-1)
         image_RGB = tf.image.hsv_to_rgb(image_HSV)
-
         image = preprocessing.transform_value_range(image_RGB, (0, 1), self.value_range)
         return image
 
