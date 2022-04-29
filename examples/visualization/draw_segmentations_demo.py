fbcff7d6f8b9f1c7f2854f7b23b1ca7dc9f14e57
Kartik Sharma <kartik4949@gmail.com>
2022-04-30 00:46:27 +0530

    format(viz): added copyright in examples

--- examples/visualization/draw_segmentations_demo.py
+++ examples/visualization/draw_segmentations_demo.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
 """A demo for visualization of segmentation masks on flower
 dataset i.e `oxford_flowers102`.
 
