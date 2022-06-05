6c8140c39594ad8179c7730a9ef3920d8f2eddfb
bhack <bhack@users.noreply.github.com>
2022-06-05 20:07:38 +0000

    test

--- keras_cv/layers/preprocessing/auto_contrast.py
+++ keras_cv/layers/preprocessing/auto_contrast.py
@@ -9,7 +9,7 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions andffffffffffffffffffffffffffffffffffffffffffffffffffffff
+# See the License for the specific language governing permissions andfffffffffffffffffffffffffffffffffffffffffffffffffffff
 # limitations under the License.
 
 import tensorflow as tf
