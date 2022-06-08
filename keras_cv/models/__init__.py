4a91f610bea066a7523d5f3e2c797e650a994a55
Luke Wood <lukewoodcs@gmail.com>
2022-06-08 13:37:42 -0700

    Update __init__.py

--- keras_cv/models/__init__.py
+++ keras_cv/models/__init__.py
@@ -11,3 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from keras_cv.models.densenet import DenseNet
+from keras_cv.models.densenet import DenseNet121
+from keras_cv.models.densenet import DenseNet169
+from keras_cv.models.densenet import DenseNet201
