dd2edbec34ecfc95fc3f4cb7373a9c1108df5f60
Sarvagya Malaviya <45961148+quantumalaviya@users.noreply.github.com>
2022-06-20 04:00:42 +0530

    Update feature_pyramid.py

--- keras_cv/layers/fpn/feature_pyramid.py
+++ keras_cv/layers/fpn/feature_pyramid.py
@@ -14,6 +14,7 @@
 
 import tensorflow as tf
 
+
 @tf.keras.utils.register_keras_serializable(package="keras_cv")
 class FeaturePyramid(tf.keras.layers.Layer):
     """Implements a Feature Pyramid Network.
