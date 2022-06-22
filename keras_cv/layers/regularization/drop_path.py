ed8629bc7b4e052ed0433bdd9b5eed8b107b1ef6
Aditya Kane <adityakane1@gmail.com>
2022-06-22 11:42:39 +0530

    Made requested changes

--- keras_cv/layers/regularization/drop_path.py
+++ keras_cv/layers/regularization/drop_path.py
@@ -15,18 +15,21 @@ import tensorflow as tf
 
 
 @tf.keras.utils.register_keras_serializable(package="keras_cv")
-class DropPath(tf.keras.layers.Layer):
+class DropPath(tf.keras.__internal__.layers.BaseRandomLayer):
     """
-    Implements the DropPath layer. Some samples from the batch are randomly
-    dropped during training with dropping probability `rate`. Note that
-    this layer drop individual samples within a batch and not the entire batch.
+    Implements the DropPath layer. DropPath randomly drops samples during training
+     with a probability of `rate`. Note that this layer drops individual samples
+    within a batch and not the entire batch. DropPath randomly drops some of the
+    individual samples from a batch, whereas StachasticDepth randomly drops the
+    entire batch.
 
     References:
         - [FractalNet](https://arxiv.org/abs/1605.07648v4).
-        - [rwightman/pytorch-image-models](https://tinyurl.com/timm-droppath)
+        - [rwightman/pytorch-image-models](https://github.com/rwightman/pytorch-image-models/blob/7c67d6aca992f039eece0af5f7c29a43d48c00e4/timm/models/layers/drop.py#L135)
 
     Args:
         rate: float, the probability of the residual branch being dropped.
+        seed: (Optional) Integer. Used to create a random seed.
 
     Usage:
     `DropPath` can be used in any network as follows:
@@ -40,9 +43,10 @@ class DropPath(tf.keras.layers.Layer):
     ```
     """
 
-    def __init__(self, rate=0.5, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, rate=0.5, seed=None, **kwargs):
+        super().__init__(seed=seed, **kwargs)
         self.rate = rate
+        self.seed = seed
 
     def call(self, x, training=None):
         if self.rate == 0.0 or not training:
@@ -50,12 +54,12 @@ class DropPath(tf.keras.layers.Layer):
         else:
             keep_prob = 1 - self.rate
             drop_map_shape = (x.shape[0],) + (1,) * (len(x.shape) - 1)
-            drop_map = tf.keras.backend.random_bernoulli(drop_map_shape, p=keep_prob)
+            drop_map = tf.keras.backend.random_bernoulli(drop_map_shape, p=keep_prob, seed=self.seed)
             x = x / keep_prob
             x = x * drop_map
             return x
 
     def get_config(self):
-        config = {"rate": self.rate}
+        config = {"rate": self.rate, "seed": self.seed}
         base_config = super().get_config()
         return dict(list(base_config.items()) + list(config.items()))
