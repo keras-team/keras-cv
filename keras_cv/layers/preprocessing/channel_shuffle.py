8e5904acac85ea044a97cb1adf1362c94b6356cd
Aditya Kane <64411306+AdityaKane2001@users.noreply.github.com>
2022-06-23 23:19:31 +0530

    Remove "tf.keras.__internal__.layers" from all files (#516)
    * Remove "tf.keras.__internal__.layers" from all files
    
    * Formatted
    
    * Fix imports and formatting
    
    * Formatted
    
    * Correction

--- keras_cv/layers/preprocessing/channel_shuffle.py
+++ keras_cv/layers/preprocessing/channel_shuffle.py
@@ -14,9 +14,13 @@
 
 import tensorflow as tf
 
+from keras_cv.layers.preprocessing.base_image_augmentation_layer import (
+    BaseImageAugmentationLayer,
+)
+
 
 @tf.keras.utils.register_keras_serializable(package="keras_cv")
-class ChannelShuffle(tf.keras.__internal__.layers.BaseImageAugmentationLayer):
+class ChannelShuffle(BaseImageAugmentationLayer):
     """Shuffle channels of an input image.
 
     Input shape:
