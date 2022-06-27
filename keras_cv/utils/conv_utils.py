567ee0df0220cd98b9b4c4fa242940e3795b2d23
Ricardo Prins <ricardoprins@gmail.com>
2022-06-26 23:34:54 -0600

    Remove data_format kwarg

--- keras_cv/utils/conv_utils.py
+++ keras_cv/utils/conv_utils.py
@@ -14,18 +14,6 @@
 import tensorflow as tf
 
 
-def normalize_data_format(value):
-    if value is None:
-        value = tf.keras.backend.image_data_format()
-    data_format = value.lower()
-    if data_format not in {"channels_first", "channels_last"}:
-        raise ValueError(
-            "The `data_format` argument must be one of "
-            f'"channels_first", "channels_last". Received: {value}'
-        )
-    return data_format
-
-
 def normalize_tuple(value, n, name, allow_zero=False):
     """Transforms non-negative/positive integer/integers into an integer tuple.
     Args:
