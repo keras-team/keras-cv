45c81d0dbb0fed0f1e4695a526a664ebbee6ea68
Rishit Dagli <rishit.dagli@gmail.com>
2022-06-28 09:53:10 +0000

    Remove `nonlocal`

--- keras_cv/models/mobilenet_v3.py
+++ keras_cv/models/mobilenet_v3.py
@@ -29,11 +29,11 @@ channel_axis = -1
 def Depth(divisor=8, min_value=None, name=None):
     if name is None:
         name = f"depth_{backend.get_uid('depth')}"
+        
+    if min_value is None:
+        min_value = divisor
 
-    def apply(x):
-        nonlocal min_value
-        if min_value is None:
-            min_value = divisor
+    def apply(x):        
         new_x = max(min_value, int(x + divisor / 2) // divisor * divisor)
         # Make sure that round down does not go down by more than 10%.
         if new_x < 0.9 * x:
