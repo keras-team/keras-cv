b52321b7582085a531d284b98ee20fbc9dd5fbfa
Luke Wood <LukeWood@users.noreply.github.com>
2022-07-07 10:15:19 -0700

    Add support for unbatch bounding boxes to convert_format() (#556)
    * Support unbatched inputs to bounding_box.convert_format()
    
    * Format bounding box
    
    * Add unbatch bbox support
    
    * Fix broken type

--- keras_cv/bounding_box/converters_test.py
+++ keras_cv/bounding_box/converters_test.py
@@ -19,18 +19,18 @@ from absl.testing import parameterized
 
 from keras_cv import bounding_box
 
-xyxy_box = tf.constant([[10, 20, 110, 120], [20, 30, 120, 130]], dtype=tf.float32)
-yxyx_box = tf.constant([[20, 10, 120, 110], [30, 20, 130, 120]], dtype=tf.float32)
+xyxy_box = tf.constant([[[10, 20, 110, 120], [20, 30, 120, 130]]], dtype=tf.float32)
+yxyx_box = tf.constant([[[20, 10, 120, 110], [30, 20, 130, 120]]], dtype=tf.float32)
 rel_xyxy_box = tf.constant(
-    [[0.01, 0.02, 0.11, 0.12], [0.02, 0.03, 0.12, 0.13]], dtype=tf.float32
+    [[[0.01, 0.02, 0.11, 0.12], [0.02, 0.03, 0.12, 0.13]]], dtype=tf.float32
 )
 rel_yxyx_box = tf.constant(
-    [[0.02, 0.01, 0.12, 0.11], [0.03, 0.02, 0.13, 0.12]], dtype=tf.float32
+    [[[0.02, 0.01, 0.12, 0.11], [0.03, 0.02, 0.13, 0.12]]], dtype=tf.float32
 )
 center_xywh_box = tf.constant(
-    [[60, 70, 100, 100], [70, 80, 100, 100]], dtype=tf.float32
+    [[[60, 70, 100, 100], [70, 80, 100, 100]]], dtype=tf.float32
 )
-xywh_box = tf.constant([[10, 20, 100, 100], [20, 30, 100, 100]], dtype=tf.float32)
+xywh_box = tf.constant([[[10, 20, 100, 100], [20, 30, 100, 100]]], dtype=tf.float32)
 
 images = tf.ones([2, 1000, 1000, 3])
 
@@ -46,7 +46,7 @@ boxes = {
 test_cases = [
     (f"{source}_{target}", source, target)
     for (source, target) in itertools.permutations(boxes.keys(), 2)
-]
+] + [("xyxy_xyxy", "xyxy", "xyxy")]
 
 
 class ConvertersTestCase(tf.test.TestCase, parameterized.TestCase):
@@ -59,5 +59,32 @@ class ConvertersTestCase(tf.test.TestCase, parameterized.TestCase):
             bounding_box.convert_format(
                 source_box, source=source, target=target, images=images
             ),
+            target_box,
+        )
+
+    @parameterized.named_parameters(*test_cases)
+    def test_converters_unbatched(self, source, target):
+        source_box = boxes[source][0]
+        target_box = boxes[target][0]
+
+        self.assertAllClose(
+            bounding_box.convert_format(
+                source_box, source=source, target=target, images=images[0]
+            ),
+            target_box,
+        )
+
+    def test_raises_with_different_image_rank(self):
+        source_box = boxes["xyxy"][0]
+        with self.assertRaises(ValueError):
+            bounding_box.convert_format(
+                source_box, source="xyxy", target="xywh", images=images
+            )
+
+    def test_without_images(self):
+        source_box = boxes["xyxy"]
+        target_box = boxes["xywh"]
+        self.assertAllClose(
+            bounding_box.convert_format(source_box, source="xyxy", target="xywh"),
             target_box,
         )
