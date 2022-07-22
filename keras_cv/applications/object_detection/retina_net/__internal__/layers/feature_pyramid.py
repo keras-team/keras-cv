20460a1c06a8ea6264fc34acb0a62748c03284b7
Luke Wood <lukewoodcs@gmail.com>
2022-07-21 17:53:20 -0700

    Reformat code to pass lint checks

--- keras_cv/applications/object_detection/retina_net/__internal__/layers/feature_pyramid.py
+++ keras_cv/applications/object_detection/retina_net/__internal__/layers/feature_pyramid.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from tensorflow import keras
 import tensorflow as tf
+from tensorflow import keras
 
 
 class FeaturePyramid(keras.layers.Layer):
