daef7de7ec38aa766c7e0904e98ab09d89cb50ec
ianjjohnson <ianjjohnson@google.com>
2022-07-29 14:55:37 -0600

    Formatting

--- shell/weights/update_training_history.py
+++ shell/weights/update_training_history.py
@@ -1,13 +1,17 @@
+import json
+import os
 import struct
 import sys
-import os
-import json
 
-from tensorflow.python.summary.summary_iterator import summary_iterator
 from absl import flags
+from tensorflow.python.summary.summary_iterator import summary_iterator
 
 flags.DEFINE_string("logs_path", None, "Path of TensorBoard validation logs to load")
-flags.DEFINE_string("training_script_json", None, "Path to the JSON file for run history for the producing training script")
+flags.DEFINE_string(
+    "training_script_json",
+    None,
+    "Path to the JSON file for run history for the producing training script",
+)
 flags.DEFINE_string("weights_version", None, "The version of these weights (e.g. 'v0')")
 
 FLAGS = flags.FLAGS
@@ -17,7 +21,7 @@ FLAGS(sys.argv)
 files = [filename for _, _, filename in os.walk(FLAGS.logs_path)]
 assert len(files) == 1
 assert len(files[0]) == 1
-filename = FLAGS.logs_path+'/'+files[0][0]
+filename = FLAGS.logs_path + "/" + files[0][0]
 
 events = [e for e in summary_iterator(filename)]
 
@@ -26,7 +30,9 @@ for event in events[1:]:
     assert len(event.summary.value) == 1
     if not event.summary.value[0].tag == "evaluation_accuracy_vs_iterations":
         continue
-    validation_accuracy = struct.unpack('f',event.summary.value[0].tensor.tensor_content)[0]
+    validation_accuracy = struct.unpack(
+        "f", event.summary.value[0].tensor.tensor_content
+    )[0]
     max_validation_accuracy = max(max_validation_accuracy, validation_accuracy)
 
 max_validation_accuracy = f"{max_validation_accuracy:.4f}"
@@ -36,12 +42,12 @@ print(f"Max validation accuracy: {max_validation_accuracy}")
 new_results = {FLAGS.weights_version: {"validation_accuracy": max_validation_accuracy}}
 
 # Check if the file already exists
-results_file = open(FLAGS.training_script_json, 'r')
+results_file = open(FLAGS.training_script_json, "r")
 results_string = results_file.read()
 results = json.loads(results_string) if results_string != "" else {}
 results_file.close()
 
-results_file = open(FLAGS.training_script_json, 'w')
+results_file = open(FLAGS.training_script_json, "w")
 results[FLAGS.weights_version] = {"validation_accuracy": max_validation_accuracy}
 json.dump(results, results_file)
 results_file.close()
