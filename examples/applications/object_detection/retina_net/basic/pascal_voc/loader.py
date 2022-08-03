881e58480c9833122bb944732e219fc9ed039f4f
Luke Wood <lukewoodcs@gmail.com>
2022-08-02 21:39:58 -0700

    Reformat loader

--- examples/applications/object_detection/retina_net/basic/pascal_voc/loader.py
+++ examples/applications/object_detection/retina_net/basic/pascal_voc/loader.py
@@ -11,56 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import tensorflow_datasets as tfds
-from tensorflow import keras
-
-from keras_cv import bounding_box
-
-
-def unpackage_pascalvoc(bounding_box_format, img_size):
-    """mapping function to create batched image and bbox coordinates"""
-
-    resizing = keras.layers.Resizing(
-        height=img_size[0], width=img_size[1], crop_to_aspect_ratio=False
-    )
-
-    def apply(inputs):
-        inputs["image"] = resizing(inputs["image"])
-        inputs["objects"]["bbox"] = bounding_box.convert_format(
-            inputs["objects"]["bbox"],
-            images=inputs["image"],
-            source="rel_yxyx",
-            target=bounding_box_format,
-        )
-
-        bounding_boxes = inputs["objects"]["bbox"]
-        labels = tf.cast(inputs["objects"]["label"], tf.float32)
-        labels = tf.expand_dims(labels, axis=-1)
-        bounding_boxes = tf.concat([bounding_boxes, labels], axis=-1)
-        return {"images": inputs["image"], "bounding_boxes": bounding_boxes}
-
-    return apply
-
-
-def load_pascal_voc(
-    split, bounding_box_format, batch_size, shuffle=True, img_size=(512, 512)
-):
-    dataset, dataset_info = tfds.load(
-        "voc/2007", split=split, shuffle_files=shuffle, with_info=True
-    )
-    dataset = dataset.map(
-        unpackage_pascalvoc(bounding_box_format=bounding_box_format, img_size=img_size),
-        num_parallel_calls=tf.data.AUTOTUNE,
-    )
-    dataset = dataset.shuffle(100)
-    dataset = dataset.apply(
-        tf.data.experimental.dense_to_ragged_batch(batch_size=batch_size)
-    )
-    dataset = dataset.prefetch(tf.data.AUTOTUNE)
-    return dataset, dataset_info
-
 
 def main():
     batch_size = 9
