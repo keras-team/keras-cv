3ac6486cbdb5868b4bdbc837f93feffdbdf4abe2
Ian Johnson <ianjjohnson@users.noreply.github.com>
2022-08-03 12:42:02 -0600

    Switch to tensorboard.dev for training metadata handling (#648)
    * Start the tensorflow.dev version of logs parsing
    
    * Finish revisions to update_training_history
    
    * Formatting
    
    * Address review comments
    
    * Formatting

--- shell/weights/update_training_history.py
+++ shell/weights/update_training_history.py
@@ -1,54 +1,99 @@
 import json
 import os
-import struct
 import sys
 
+import tensorboard as tb
 from absl import flags
-from tensorflow.python.summary.summary_iterator import summary_iterator
 
-flags.DEFINE_string("logs_path", None, "Path of TensorBoard validation logs to load")
+flags.DEFINE_string("tensorboard_logs_path", None, "Path to tensorboard logs to load")
+flags.DEFINE_string("training_script_path", None, "Path to the training script")
 flags.DEFINE_string(
-    "training_script_json",
+    "weights_version",
     None,
-    "Path to the JSON file for run history for the producing training script",
+    "The version of the training script used to produce the latest weights. For example, v0",
 )
-flags.DEFINE_string("weights_version", None, "The version of these weights (e.g. 'v0')")
+flags.DEFINE_string(
+    "pr_number", None, "The PR number that merged the training script into KerasCV"
+)
+flags.DEFINE_string(
+    "pr_author", None, "The GitHub username of the author of the training script"
+)
 
 FLAGS = flags.FLAGS
 FLAGS(sys.argv)
 
-# Find the single validation logs in the supplied logs dir
-files = os.listdir(FLAGS.logs_path)
-# Tensorboard logs are named based on timestamp, so the largest name alphabetically is the most recent.
-most_recent_file = max(files)
+weights_version = FLAGS.weights_version or input(
+    "Input the weights version for your script\n"
+)
 
-filename = FLAGS.logs_path + "/" + most_recent_file
+training_script_path = FLAGS.training_script_path or input(
+    "Input the path to your training script\n"
+)
+training_script_json_path = training_script_path.replace(".py", ".json")
+full_training_script_path = os.path.abspath(training_script_path)
 
-events = [e for e in summary_iterator(filename)]
+# Build an experiment name structured as dataset/task-version (this experiment name will then match the name of the weights in GCS)
+training_script_rooted_at_training = full_training_script_path[
+    full_training_script_path.index("keras-cv/examples/training/") + 27 :
+]
+training_script_dirs = training_script_rooted_at_training.split("/")
+tensorboard_experiment_name = (
+    f"{training_script_dirs[2]}/{training_script_dirs[0]}-{weights_version}"
+)
 
-max_validation_accuracy = 0
-for event in events[1:]:
-    assert len(event.summary.value) == 1
-    if not event.summary.value[0].tag == "evaluation_accuracy_vs_iterations":
-        continue
-    validation_accuracy = struct.unpack(
-        "f", event.summary.value[0].tensor.tensor_content
-    )[0]
-    max_validation_accuracy = max(max_validation_accuracy, validation_accuracy)
+tensorboard_logs_path = FLAGS.tensorboard_logs_path or input(
+    "Input the path to the TensorBoard logs\n"
+)
+tensorboard_experiment_id = (
+    os.popen(
+        f"tensorboard dev upload --logdir {tensorboard_logs_path} --name {tensorboard_experiment_name} --one_shot --verbose 0"
+    )
+    .read()
+    .split("/")[-2]
+)
 
+tensorboard_experiment = tb.data.experimental.ExperimentFromDev(
+    tensorboard_experiment_id
+)
+
+tensorboard_results = tensorboard_experiment.get_scalars()
+
+training_epochs = max(tensorboard_results[tensorboard_results.run == "train"].step)
+max_validation_accuracy = max(
+    tensorboard_results[
+        (tensorboard_results.run == "validation")
+        & (tensorboard_results.tag == "epoch_accuracy")
+    ].value
+)
 max_validation_accuracy = f"{max_validation_accuracy:.4f}"
 
-print(f"Max validation accuracy: {max_validation_accuracy}")
+pr_link = (
+    f"https://github.com/keras-team/keras-cv/pull/{FLAGS.pr_number}"
+    if FLAGS.pr_number
+    else None
+)
+pr_link = pr_link or input(
+    "Input a link to the PR that merged your script into KerasCV\n"
+)
+pr_author = FLAGS.pr_author or input(
+    "Input your GitHub username (or the username of the PR author, if you're not the author)\n"
+)
 
-new_results = {FLAGS.weights_version: {"validation_accuracy": max_validation_accuracy}}
+new_results = {
+    "validation_accuracy": max_validation_accuracy,
+    "epochs_trained": training_epochs,
+    "tensorboard_logs": f"https://tensorboard.dev/experiment/{tensorboard_experiment_id}/",
+    "pr_author": pr_author,
+    "pr_link": pr_link,
+}
 
-# Check if the file already exists
-results_file = open(FLAGS.training_script_json, "r")
+# Check if the JSON file already exists
+results_file = open(training_script_json_path, "r")
 results_string = results_file.read()
 results = json.loads(results_string) if results_string != "" else {}
 results_file.close()
 
-results_file = open(FLAGS.training_script_json, "w")
-results[FLAGS.weights_version] = {"validation_accuracy": max_validation_accuracy}
+results_file = open(training_script_json_path, "w")
+results[weights_version] = new_results
 json.dump(results, results_file)
 results_file.close()
