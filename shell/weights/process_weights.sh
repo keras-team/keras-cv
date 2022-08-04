206ea10388ed7d615b5308f8461286a9e6b36697
ianjjohnson <ianjjohnson@google.com>
2022-08-04 09:46:47 -0600

    Add weights for DenseNet169

--- shell/weights/process_weights.sh
+++ shell/weights/process_weights.sh
@@ -10,13 +10,13 @@ GCS_PATH=$4
 
 python3 remove_top.py --weights_path=$WEIGHTS --output_weights_path=$OUTPUT_WEIGHTS --model_name=$MODEL
 
-echo With top: $GCS_PATH$WEIGHTS
+echo With top: $GCS_PATH/$WEIGHTS
 echo With top checksum: $(shasum -a 256 $WEIGHTS)
-echo Without top: $GCS_PATH$OUTPUT_WEIGHTS
+echo Without top: $GCS_PATH/$OUTPUT_WEIGHTS
 echo Without top checksum: $(shasum -a 256 $OUTPUT_WEIGHTS)
 
-gsutil cp $WEIGHTS $GCS_PATH
-gsutil cp $OUTPUT_WEIGHTS $GCS_PATH
+gsutil cp $WEIGHTS $GCS_PATH/
+gsutil cp $OUTPUT_WEIGHTS $GCS_PATH/
 
 gsutil acl ch -u AllUsers:R $GCS_PATH/$WEIGHTS
 gsutil acl ch -u AllUsers:R $GCS_PATH/$OUTPUT_WEIGHTS
