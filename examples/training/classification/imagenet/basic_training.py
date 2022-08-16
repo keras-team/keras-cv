keras-team/keras-cv

commit 3288c3ab31ce1c35fe7505e245fdfa9c593af78e

Make initial learning rate flag-controlled in imagenet basic classification training (#687)

examples/training/classification/imagenet/basic_training.py
@@ -64,6 +64,11 @@ flags.DEFINE_integer("batch_size", 256, "Batch size for training and evaluation.
 flags.DEFINE_boolean(
     "use_xla", True, "Whether or not to use XLA (jit_compile) for training."
 )
+flags.DEFINE_boolean(
+    "initial_learning_rate",
+    0.005,
+    "Initial learning rate which will reduce on plateau.",
+)
 
 
 FLAGS = flags.FLAGS
@@ -188,7 +193,7 @@ Note that learning rate will decrease over time due to the ReduceLROnPlateau cal
 """
 
 
-optimizer = optimizers.Adam(learning_rate=0.005)
+optimizer = optimizers.Adam(learning_rate=FLAGS.initial_learning_rate)
 
 
 """

commit 5256a528cae260845495300c3869dccfc238678e

Fix flag type for initial_learning_rate
The basic_training script was broken by #687, and this resolves it.

examples/training/classification/imagenet/basic_training.py
@@ -64,7 +64,7 @@ flags.DEFINE_integer("batch_size", 256, "Batch size for training and evaluation.
 flags.DEFINE_boolean(
     "use_xla", True, "Whether or not to use XLA (jit_compile) for training."
 )
-flags.DEFINE_boolean(
+flags.DEFINE_float(
     "initial_learning_rate",
     0.005,
     "Initial learning rate which will reduce on plateau.",