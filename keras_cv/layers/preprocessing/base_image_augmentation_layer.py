16d18d0a0fd6ec83106cb404dd6be30ecb3c3b09
divyashreepathihalli <divyasreepat@google.com>
2022-08-16 23:03:04 +0000

    nit edit

--- keras_cv/layers/preprocessing/base_image_augmentation_layer.py
+++ keras_cv/layers/preprocessing/base_image_augmentation_layer.py
@@ -230,12 +230,12 @@ class BaseImageAugmentationLayer(tf.keras.__internal__.layers.BaseRandomLayer):
     def call(self, inputs, training=True):
         inputs = self._ensure_inputs_are_compute_dtype(inputs)
         if training:
-            inputs, meta_data = self._format_inputs(inputs)
+            inputs, metadata = self._format_inputs(inputs)
             images = inputs[IMAGES]
             if images.shape.rank == 3:
-                return self._format_output(self._augment(inputs), meta_data)
+                return self._format_output(self._augment(inputs), metadata)
             elif images.shape.rank == 4:
-                return self._format_output(self._batch_augment(inputs), meta_data)
+                return self._format_output(self._batch_augment(inputs), metadata)
             else:
                 raise ValueError(
                     "Image augmentation layers are expecting inputs to be "
@@ -296,50 +296,52 @@ class BaseImageAugmentationLayer(tf.keras.__internal__.layers.BaseRandomLayer):
         return augmented_batch
 
     def _format_inputs(self, inputs):
+        if not isinstance(inputs, dict):
+            raise ValueError(
+                f"Expect the inputs to be image tensor or dict. Got {inputs}"
+            )
+        metadata = {"is_dict": True}
         if tf.is_tensor(inputs):
             # single image input tensor
-            meta_data = {"is_dict": False, "use_targets": False}
+            metadata = {"is_dict": False, "use_targets": False}
             inputs = {IMAGES: inputs}
         elif isinstance(inputs, dict) and TARGETS in inputs:
             # TODO(scottzhu): Check if it only contains the valid keys
             inputs[LABELS] = inputs[TARGETS]
             del inputs[TARGETS]
-            meta_data = {"is_dict": True, "use_targets": True}
+            metadata = {"is_dict": True, "use_targets": True}
         elif isinstance(inputs, dict):
-            meta_data = {"is_dict": True, "use_targets": False}
-        else:
-            raise ValueError(
-                f"Expect the inputs to be image tensor or dict. Got {inputs}"
-            )
+            metadata = {"is_dict": True, "use_targets": False}
+
         if BOUNDING_BOXES in inputs:
             if isinstance(inputs[BOUNDING_BOXES], tf.RaggedTensor):
-                meta_data["is_bounding_boxes_ragged"] = True
-                meta_data["ragged_row_lengths"] = inputs[
+                metadata["is_bounding_boxes_ragged"] = True
+                metadata["ragged_row_lengths"] = inputs[
                     BOUNDING_BOXES
                 ].nested_row_lengths()
                 inputs[BOUNDING_BOXES] = bounding_box.pad_with_sentinels(
                     inputs[BOUNDING_BOXES]
                 )
             else:
-                meta_data["is_bounding_boxes_ragged"] = False
+                metadata["is_bounding_boxes_ragged"] = False
             if tf.shape(inputs[BOUNDING_BOXES])[-1] != 5:
                 raise ValueError(
                     "Bounding boxes are missing class_id. If you would like to pad the "
                     "bounding boxes with '0' class_id, use `keras_cv.bounding_box.pad_with_class_id`"
                 )
-        return inputs, meta_data
+        return inputs, metadata
 
-    def _format_output(self, output, meta_data):
-        if not meta_data["is_dict"]:
+    def _format_output(self, output, metadata):
+        if not metadata["is_dict"]:
             return output[IMAGES]
-        elif meta_data["use_targets"]:
+        elif metadata["use_targets"]:
             output[TARGETS] = output[LABELS]
             del output[LABELS]
         if BOUNDING_BOXES in output:
-            if meta_data["is_bounding_boxes_ragged"]:
+            if metadata["is_bounding_boxes_ragged"]:
                 output[BOUNDING_BOXES] = tf.RaggedTensor.from_tensor(
                     output[BOUNDING_BOXES],
-                    lengths=meta_data["ragged_row_lengths"],
+                    lengths=metadata["ragged_row_lengths"],
                 )
                 output[BOUNDING_BOXES] = bounding_box.filter_sentinels(
                     output[BOUNDING_BOXES]
