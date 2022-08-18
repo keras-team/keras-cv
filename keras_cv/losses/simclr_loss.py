b4b388d5034c10e5d5b8090226f7855bd93f3139
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2022-08-18 09:26:03 -0600

    Add docs and tests for SimCLRLoss

--- keras_cv/losses/simclr_loss.py
+++ keras_cv/losses/simclr_loss.py
@@ -17,27 +17,34 @@ from tensorflow import keras
 
 
 class SimCLRLoss(tf.keras.losses.Loss):
-    """Implements SimCLR Cosine Similarity loss."""
+    """Implements SimCLR Cosine Similarity loss.
+
+    SimCLR loss is used for contrastive self-supervised learning.
+
+    Args:
+        temperature: a float value between 0 and 1, used as a scaling factor for cosine similarity.
+
+    References:
+        - [SimCLR paper](https://arxiv.org/pdf/2002.05709)
+    """
 
     def __init__(self, temperature, **kwargs):
         super().__init__(**kwargs)
         self.temperature = temperature
 
     def call(self, projections_1, projections_2):
-        # Cosine similarity: the dot product of the l2-normalized feature vectors
+        # Compute the dot product of the L2 norms of the projections
         projections_1 = tf.math.l2_normalize(projections_1, axis=1)
         projections_2 = tf.math.l2_normalize(projections_2, axis=1)
         similarities = (
             tf.matmul(projections_1, projections_2, transpose_b=True) / self.temperature
         )
 
-        # The similarity between the representations of two augmented views of the
-        # same image should be higher than their similarity with other views
+        # Produce artificial labels, 1 for each image in the batch.
         batch_size = tf.shape(projections_1)[0]
         contrastive_labels = tf.range(batch_size)
 
-        # The temperature-scaled similarities are used as logits for cross-entropy
-        # a symmetrized version of the loss is used here
+        # The similarities are used as logits for cross-entropy against the artificial labels.
         loss_1_2 = keras.losses.sparse_categorical_crossentropy(
             contrastive_labels, similarities, from_logits=True
         )
--- keras_cv/losses/simclr_loss_test.py
+++ keras_cv/losses/simclr_loss_test.py
@@ -14,8 +14,37 @@
 
 import tensorflow as tf
 
+from keras_cv.losses.simclr_loss import SimCLRLoss
+
 
 class SimCLRLossTest(tf.test.TestCase):
-    def test(self):
-        # TODO(ianstenbit) add tests
-        pass
+    def test_output_shape(self):
+        projections_1 = tf.random.uniform(
+            shape=(10, 128), minval=0, maxval=10, dtype=tf.float32
+        )
+        projections_2 = tf.random.uniform(
+            shape=(10, 128), minval=0, maxval=10, dtype=tf.float32
+        )
+
+        simclr_loss = SimCLRLoss(temperature=1)
+
+        self.assertAllEqual(simclr_loss(projections_1, projections_2).shape, ())
+
+    def test_output_value(self):
+        projections_1 = [
+            [1.0, 2.0, 3.0, 4.0],
+            [2.0, 3.0, 4.0, 5.0],
+            [3.0, 4.0, 5.0, 6.0],
+        ]
+
+        projections_2 = [
+            [6.0, 5.0, 4.0, 3.0],
+            [5.0, 4.0, 3.0, 2.0],
+            [4.0, 3.0, 2.0, 1.0],
+        ]
+
+        simclr_loss = SimCLRLoss(temperature=0.5)
+        self.assertAllClose(simclr_loss(projections_1, projections_2), 1.0945123)
+
+        simclr_loss = SimCLRLoss(temperature=0.1)
+        self.assertAllClose(simclr_loss(projections_1, projections_2), 1.1511706)
