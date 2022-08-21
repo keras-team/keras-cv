737010cd215f0bf763311607cd10fadc82f2c0bd
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2022-08-21 09:43:11 -0600

    update augmentation defaults for simclr

--- keras_cv/training/contrastive/simclr_trainer.py
+++ keras_cv/training/contrastive/simclr_trainer.py
@@ -29,28 +29,43 @@ class SimCLRTrainer(ContrastiveTrainer):
     Args:
         encoder: a `keras.Model` to be pre-trained. In most cases, this encoder
             should not include a top dense layer.
+        include_probe: Whether to include a single fully-connected layer during
+            training for probing classification accuracy using the learned encoding.
+            Note that this should be specified iff training with labeled images.
+            If provided, `classes` must be provided.
         value_range: the range of values the incoming images will have.
             Represented as a two number tuple written [low, high].
             This is typically either `[0, 1]` or `[0, 255]` depending
             on how your preprocessing pipeline is setup.
+        target_size: A tuple of two integers used as the target size to resize
+            images to in the augmentation setp.
     """
 
     def __init__(
-        self, encoder, include_probe, value_range, projection_width=128, **kwargs
+        self,
+        encoder,
+        include_probe,
+        value_range,
+        target_size=(128, 128),
+        projection_width=128,
+        **kwargs
     ):
         super().__init__(
             encoder=encoder,
             augmenter=preprocessing.Augmenter(
                 [
                     preprocessing.RandomFlip(),
-                    preprocessing.RandomTranslation(0.25, 0.25),
-                    preprocessing.RandomZoom((-0.5, 0.0), (-0.5, 0.0)),
+                    preprocessing.RandomResizedCrop(
+                        target_size,
+                        crop_area_factor=(0.25, 1),
+                        aspect_ratio_factor=(3 / 4, 4 / 3),
+                    ),
                     preprocessing.RandomColorJitter(
                         value_range=value_range,
-                        brightness_factor=0.5,
+                        brightness_factor=0.25,
                         contrast_factor=0.5,
                         saturation_factor=(0.3, 0.7),
-                        hue_factor=0.5,
+                        hue_factor=0.2,
                     ),
                 ]
             ),
