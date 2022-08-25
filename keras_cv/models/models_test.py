6760c392b3ff26537ab8ed61980d02812561c0dc
Luke Wood <lukewoodcs@gmail.com>
2022-08-25 09:48:59 -0700

    Remove accidental paste

--- keras_cv/models/models_test.py
+++ keras_cv/models/models_test.py
@@ -1,4 +1,4 @@
-/Users/lukewood/workspace/keras-cv/keras_cv/models/models_test.py# Copyright 2022 The KerasCV Authors
+# Copyright 2022 The KerasCV Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
