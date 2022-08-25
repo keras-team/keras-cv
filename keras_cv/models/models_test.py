2edbcad92f20d6deb9a776afb77ec9981a37ba0e
Sayak Paul <spsayakpaul@gmail.com>
2022-08-25 23:01:33 +0530

    Add ConvNeXt (#678)
    * initial model implementation.
    
    * add: test and fix minor things in model defn.
    
    * fix: formatting.
    
    * address pr comments.
    
    * fix: code formatting/
    
    * PR feedback II.
    
    * PR feedback III.
    
    * chore: add serialization decorator to LayerScale.
    
    * skip retinanet test
    
    * Skip tests
    
    * Add os import
    
    * Remove accidental paste
    
    Co-authored-by: Luke Wood <lukewoodcs@gmail.com>

--- keras_cv/models/models_test.py
+++ keras_cv/models/models_test.py
@@ -13,12 +13,15 @@
 # limitations under the License.
 """Integration tests for KerasCV models."""
 
+import os
+
 import pytest
 import tensorflow as tf
 from absl.testing import parameterized
 from tensorflow import keras
 from tensorflow.keras import backend
 
+from keras_cv.models import convnext
 from keras_cv.models import csp_darknet
 from keras_cv.models import darknet
 from keras_cv.models import densenet
@@ -29,6 +32,34 @@ from keras_cv.models import resnet_v2
 from keras_cv.models import vgg19
 
 MODEL_LIST = [
+    (
+        convnext.ConvNeXtTiny,
+        768,
+        {"drop_path_rate": 0.1, "layer_scale_init_value": 1e-6},
+    ),
+    (
+        convnext.ConvNeXtSmall,
+        768,
+        {
+            "drop_path_rate": 0.1,
+            "layer_scale_init_value": 1e-6,
+        },
+    ),
+    (
+        convnext.ConvNeXtBase,
+        1024,
+        {"drop_path_rate": 0.1, "layer_scale_init_value": 1e-6},
+    ),
+    (
+        convnext.ConvNeXtLarge,
+        1536,
+        {"drop_path_rate": 0.1, "layer_scale_init_value": 1e-6},
+    ),
+    (
+        convnext.ConvNeXtXLarge,
+        2048,
+        {"drop_path_rate": 0.1, "layer_scale_init_value": 1e-6},
+    ),
     (csp_darknet.CSPDarkNet, 1024, {}),
     (darknet.DarkNet21, 512, {}),
     (darknet.DarkNet53, 512, {}),
@@ -72,6 +103,12 @@ class ModelsTest(tf.test.TestCase, parameterized.TestCase):
         yield
         tf.keras.backend.clear_session()
 
+    @pytest.mark.skipif(
+        "INTEGRATION" not in os.environ or os.environ["INTEGRATION"] != "true",
+        reason="Takes a long time to run, only runs when INTEGRATION "
+        "environment variable is set.  To run the test please run: \n"
+        "`INTEGRATION=true pytest keras_cv/",
+    )
     @parameterized.parameters(*MODEL_LIST)
     def test_application_base(self, app, _, args):
         # Can be instantiated with default arguments
@@ -86,6 +123,12 @@ class ModelsTest(tf.test.TestCase, parameterized.TestCase):
         with self.assertRaises(ValueError):
             model.get_layer(name="rescaling")
 
+    @pytest.mark.skipif(
+        "INTEGRATION" not in os.environ or os.environ["INTEGRATION"] != "true",
+        reason="Takes a long time to run, only runs when INTEGRATION "
+        "environment variable is set.  To run the test please run: \n"
+        "`INTEGRATION=true pytest keras_cv/",
+    )
     @parameterized.parameters(*MODEL_LIST)
     def test_application_with_rescaling(self, app, last_dim, args):
         model = app(include_rescaling=True, include_top=False, **args)
