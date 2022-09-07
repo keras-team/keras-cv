2bf51cc75d40e579838755c35202fe894f0c2ca2
Luke Wood <lukewoodcs@gmail.com>
2022-09-07 12:00:31 -0700

    Update prediction head

--- keras_cv/models/object_detection/retina_net/__internal__/layers/prediction_head.py
+++ keras_cv/models/object_detection/retina_net/__internal__/layers/prediction_head.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 import tensorflow as tf
-from tensorflow.keras import initializers
 from tensorflow.keras import layers
 from tensorflow.keras import regularizers
 
