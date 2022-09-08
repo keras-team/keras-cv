573db76a2a9b7f24c984ba7c70761ad55f658f00
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2022-09-08 11:34:20 -0600

    Make shared _flip_image method

--- keras_cv/layers/preprocessing/random_flip.py
+++ keras_cv/layers/preprocessing/random_flip.py
@@ -85,18 +85,7 @@ class RandomFlip(BaseImageAugmentationLayer):
         return label
 
     def augment_image(self, image, transformation, **kwargs):
-        flipped_output = tf.cond(
-            transformation["flip_horizontal"],
-            lambda: tf.image.flip_left_right(image),
-            lambda: image,
-        )
-        flipped_output = tf.cond(
-            transformation["flip_vertical"],
-            lambda: tf.image.flip_up_down(flipped_output),
-            lambda: flipped_output,
-        )
-        flipped_output.set_shape(image.shape)
-        return flipped_output
+        return RandomFlip._flip_image(image, transformation)
 
     def get_random_transformation(self, **kwargs):
         flip_horizontal = False
@@ -109,6 +98,20 @@ class RandomFlip(BaseImageAugmentationLayer):
             "flip_horizontal": tf.cast(flip_horizontal, dtype=tf.bool),
             "flip_vertical": tf.cast(flip_vertical, dtype=tf.bool),
         }
+
+    def _flip_image(image, transformation):
+        flipped_output = tf.cond(
+            transformation["flip_horizontal"],
+            lambda: tf.image.flip_left_right(image),
+            lambda: image,
+        )
+        flipped_output = tf.cond(
+            transformation["flip_vertical"],
+            lambda: tf.image.flip_up_down(flipped_output),
+            lambda: flipped_output,
+        )
+        flipped_output.set_shape(image.shape)
+        return flipped_output
 
     def _flip_bounding_boxes_horizontal(bounding_boxes):
         x1, x2, x3, x4, rest = tf.split(
@@ -189,18 +192,7 @@ class RandomFlip(BaseImageAugmentationLayer):
     def augment_segmentation_mask(
         self, segmentation_mask, transformation=None, **kwargs
     ):
-        flipped_mask = tf.cond(
-            transformation["flip_horizontal"],
-            lambda: tf.image.flip_left_right(segmentation_mask),
-            lambda: segmentation_mask,
-        )
-        flipped_mask = tf.cond(
-            transformation["flip_vertical"],
-            lambda: tf.image.flip_up_down(flipped_mask),
-            lambda: flipped_mask,
-        )
-        flipped_mask.set_shape(segmentation_mask.shape)
-        return flipped_mask
+        return RandomFlip._flip_image(segmentation_mask, transformation)
 
     def compute_output_shape(self, input_shape):
         return input_shape
