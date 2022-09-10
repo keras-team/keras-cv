c667356cff1e8c215ecb752ae59f9a739e6c4de8
Luke Wood <lukewoodcs@gmail.com>
2022-09-09 23:08:55 -0700

    update rescaling for consistency

--- keras_cv/models/efficientnet_v2.py
+++ keras_cv/models/efficientnet_v2.py
@@ -893,8 +893,7 @@ def EfficientNetV2(
     x = img_input
 
     if include_rescaling:
-        # Use common rescaling strategy across keras_cv
-        x = layers.Rescaling(scale=1.0 / 128.0, offset=-1)(x)
+        x = layers.Rescaling(scale=1/255.0)(x)
 
     # Build stem
     stem_filters = round_filters(
