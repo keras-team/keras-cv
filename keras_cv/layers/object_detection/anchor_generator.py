34f467ea5ef7fb099bc5007bb4662a10cb12bc6f
Luke Wood <LukeWood@users.noreply.github.com>
2022-09-14 19:49:14 -0700

    Update anchor_generator.py

--- keras_cv/layers/object_detection/anchor_generator.py
+++ keras_cv/layers/object_detection/anchor_generator.py
@@ -70,7 +70,7 @@ class AnchorGenerator(keras.layers.Layer):
     Input shape: an image with shape `[H, W, C]`
     Output: a dictionary with integer keys corresponding to each level of the feature
         pyramid.  The size of the anchors at each level will be
-        `(H/strides * W/strides * K, 4)`.
+        `(H/strides * W/strides * anchors_per_location, 4)`.
     """
 
     def __init__(
