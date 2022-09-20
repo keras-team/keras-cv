e24f951ebc97319457311ba9d6f9159700bfa35f
chunxiangzheng <ranger083@gmail.com>
2022-09-20 05:49:31 +0000

    Add training script.

---
+++ examples/training/style_transfer/ada_in/training.py
@@ -0,0 +1,193 @@
+# Copyright 2022 The KerasCV Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Title: Training a AdaIn model for Style transfer
+"""
+
+import os
+import glob
+import imageio
+import keras_cv
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+from absl import flags
+from keras_cv.models import AdaIN, AdainTrainer
+from tensorflow import keras
+from tqdm import tqdm
+
+
+"""
+## Setup and flags
+"""
+
+flags.DEFINE_string(
+    "style_images", None, "The directory of style images."
+)
+flags.DEFINE_integer(
+    "epochs", 1, "The number of epoch for training.",
+)
+
+FLAGS = flags.FLAGS
+FLAGS(sys.argv)
+
+
+IMAGE_SIZE = (224, 224)
+BATCH_SIZE = 64
+AUTOTUNE = tf.data.AUTOTUNE
+
+
+"""
+## Data loading
+"""
+
+# Get the image file paths for the style images.
+style_images = os.listdir(flags.STYLE_IMAGES)
+style_images = [os.path.join(flags.STYLE_IMAGES, path) for path in style_images]
+
+# split the style images in train, val and test
+total_style_images = len(style_images)
+train_style = style_images[: int(0.8 * total_style_images)]
+val_style = style_images[int(0.8 * total_style_images) : int(0.9 * total_style_images)]
+test_style = style_images[int(0.9 * total_style_images) :]
+
+
+def decode_and_resize(image_path):
+    """Decodes and resizes an image from the image file path.
+
+    Args:
+        image_path: The image file path.
+        size: The size of the image to be resized to.
+
+    Returns:
+        A resized image.
+    """
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.convert_image_dtype(image, dtype="float32")
+    image = tf.image.resize(image, IMAGE_SIZE)
+    return image
+
+def extract_image_from_voc(element):
+    """Extracts image from the PascalVOC dataset.
+
+    Args:
+        element: A dictionary of data.
+        size: The size of the image to be resized to.
+
+    Returns:
+        A resized image.
+    """
+    image = element["image"]
+    image = tf.image.convert_image_dtype(image, dtype="float32")
+    image = tf.image.resize(image, IMAGE_SIZE)
+    return image
+
+#Build the style and content tf.data datasets.
+train_style_ds = (
+   tf.data.Dataset.from_tensor_slices(train_style)
+   .map(decode_and_resize, num_parallel_calls=AUTOTUNE)
+   .repeat()
+)
+train_content_ds = tfds.load("voc", split="train").map(extract_image_from_voc).repeat()
+
+val_style_ds = (
+    tf.data.Dataset.from_tensor_slices(val_style)
+    .map(decode_and_resize, num_parallel_calls=AUTOTUNE)
+    .repeat()
+)
+val_content_ds = (
+    tfds.load("voc", split="validation").map(extract_image_from_voc).repeat()
+)
+
+test_style_ds = (
+    tf.data.Dataset.from_tensor_slices(test_style)
+    .map(decode_and_resize, num_parallel_calls=AUTOTUNE)
+    .repeat()
+)
+test_content_ds = (
+    tfds.load("voc", split="test")
+    .map(extract_image_from_voc, num_parallel_calls=AUTOTUNE)
+    .repeat()
+)
+
+# Zipping the style and content datasets.
+train_ds = (
+    tf.data.Dataset.zip((train_style_ds, train_content_ds))
+    .shuffle(BATCH_SIZE * 2)
+    .apply(tf.data.experimental.dense_to_ragged_batch(batch_size=BATCH_SIZE))
+    .prefetch(AUTOTUNE)
+)
+
+val_ds = (
+    tf.data.Dataset.zip((val_style_ds, val_content_ds))
+    .shuffle(BATCH_SIZE * 2)
+    .apply(tf.data.experimental.dense_to_ragged_batch(batch_size=BATCH_SIZE))
+    .batch(BATCH_SIZE)
+    .prefetch(AUTOTUNE)
+)
+
+test_ds = (
+    tf.data.Dataset.zip((test_style_ds, test_content_ds))
+    .shuffle(BATCH_SIZE * 2)
+    .apply(tf.data.experimental.dense_to_ragged_batch(batch_size=BATCH_SIZE))
+    .batch(BATCH_SIZE)
+    .prefetch(AUTOTUNE)
+)
+
+trainer = AdaInTrainer(include_rescaling=True)
+optimizer = keras.optimizers.Adam(learning_rate=1e-5)
+loss_fn = keras.losses.MeanSquaredError()
+
+# Create a callback to monitor the training process
+test_style, test_content = next(iter(test_ds))
+
+class TrainMonitor(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        # Encode the style and content image.
+        test_style_encoded = self.model.encoder(test_style)
+        test_content_encoded = self.model.encoder(test_content)
+
+        # Compute the AdaIN features.
+        test_t = AdaIN()((test_style_encoded, test_content_encoded))
+        test_reconstructed_image = self.model.decoder(test_t)
+
+        # Plot the Style, Content and the NST image.
+        fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(20, 5))
+        ax[0].imshow(tf.keras.preprocessing.image.array_to_img(test_style[0]))
+        ax[0].set_title(f"Style: {epoch:03d}")
+
+        ax[1].imshow(tf.keras.preprocessing.image.array_to_img(test_content[0]))
+        ax[1].set_title(f"Content: {epoch:03d}")
+
+        ax[2].imshow(
+            tf.keras.preprocessing.image.array_to_img(test_reconstructed_image[0])
+        )
+        ax[2].set_title(f"NST: {epoch:03d}")
+
+        plt.show()
+        plt.close()
+
+trainer.compile(optimizer=optimizer, loss_fn=loss_fn)
+history = trainer.fit(
+    train_ds,
+    epochs=flags.EPOCHS,
+    steps_per_epoch=50,
+    validation_data=val_ds,
+    validation_steps=50,
+    callbacks=[TrainMonitor()],
+)
+
