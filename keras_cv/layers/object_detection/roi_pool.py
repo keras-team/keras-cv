1562c780b96dade3ab5d4e4513bb26da21363a9b
tanzhenyu <tanzheny@google.com>
2022-09-19 18:21:57 -0700

    quick fix.

--- keras_cv/layers/object_detection/roi_pool.py
+++ keras_cv/layers/object_detection/roi_pool.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from email.mime import image
-
 import tensorflow as tf
 
 from keras_cv import bounding_box
