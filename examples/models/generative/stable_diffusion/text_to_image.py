917346c72b2d572ef7a92f35e7669722c5c94dc7
Francois Chollet <francois.chollet@gmail.com>
2022-09-24 19:08:21 -0700

    Add example

---
+++ examples/models/generative/stable_diffusion/text_to_image.py
@@ -0,0 +1,17 @@
+"""
+Title: Generate an image from a text prompt using StableDiffusion
+Author: fchollet
+Date created: 2022/09/24
+Last modified: 2022/09/24
+Description: Use StableDiffusion to generate an image according to a short text description.
+"""
+
+from keras_cv.models import StableDiffusion
+from PIL import Image
+
+model = StableDiffusion(img_height=512, img_width=512, jit_compile=True)
+img = model.text_to_image(
+    "Photograph of a beautiful horse running through a field"
+)
+Image.fromarray(img[0]).save("horse.png")
+print(f"Saved at horse.png")
