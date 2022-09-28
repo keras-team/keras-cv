c8064e64043a35360e42023be2f411c9f5520b29
Luke Wood <LukeWood@users.noreply.github.com>
2022-09-28 13:48:36 -0700

    Update __init__.py
    Release for SD warnings

--- keras_cv/__init__.py
+++ keras_cv/__init__.py
@@ -30,4 +30,4 @@ from keras_cv.core import FactorSampler
 from keras_cv.core import NormalFactorSampler
 from keras_cv.core import UniformFactorSampler
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
