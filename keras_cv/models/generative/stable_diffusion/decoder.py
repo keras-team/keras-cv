095070ff239e3de56fad46514a6e96f9a03ca4e1
Luke Wood <lukewoodcs@gmail.com>
2022-10-04 09:58:26 -0700

    for license statements & linting

--- keras_cv/models/generative/stable_diffusion/decoder.py
+++ keras_cv/models/generative/stable_diffusion/decoder.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import tensorflow as tf
 from tensorflow import keras
 
 from keras_cv.models.generative.stable_diffusion.__internal__.layers.attention_block import (
