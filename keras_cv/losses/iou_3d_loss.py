6bcac826eb4a48b9d422ce17e80e6c9e413fe485
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2022-10-06 17:47:27 -0600

    Initial custom ops testing

--- keras_cv/losses/iou_3d_loss.py
+++ keras_cv/losses/iou_3d_loss.py
@@ -1,20 +1,22 @@
-# Copyright 2022 The KerasCV Authors
+# Copyright 2018 The Sonnet Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# ============================================================================
+"""Use iou_3d ops in python."""
 
-from keras_cv.losses.focal import FocalLoss
-from keras_cv.losses.iou_3d_loss import iou_3d as IoU3DLoss
-from keras_cv.losses.iou_loss import IoULoss
-from keras_cv.losses.simclr_loss import SimCLRLoss
-from keras_cv.losses.smooth_l1 import SmoothL1Loss
-from keras_cv.losses.smooth_l1 import SmoothL1Loss
+from tensorflow.python.framework import load_library
+from tensorflow.python.platform import resource_loader
+
+iou_3d_ops = load_library.load_op_library(
+    resource_loader.get_path_to_datafile('../custom_ops/_zero_out_ops.so'))
+iou_3d = iou_3d_ops.zero_out
--- keras_cv/losses/iou_3d_loss_test.py
+++ keras_cv/losses/iou_3d_loss_test.py
@@ -1,20 +1,29 @@
-# Copyright 2022 The KerasCV Authors
+# Copyright 2018 The Sonnet Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# ============================================================================
+"""Tests for zero_out ops."""
+import tensorflow as tf
 
-from keras_cv.losses.focal import FocalLoss
-from keras_cv.losses.iou_3d_loss import iou_3d as IoU3DLoss
-from keras_cv.losses.iou_loss import IoULoss
-from keras_cv.losses.simclr_loss import SimCLRLoss
-from keras_cv.losses.smooth_l1 import SmoothL1Loss
-from keras_cv.losses.smooth_l1 import SmoothL1Loss
+from keras_cv.losses import IoU3DLoss
+
+
+class IoU3DTest(test.TestCase):
+
+  def testOpCall(self):
+      self.assertAllClose(
+          IoU3DLoss([[1, 2], [3, 4]]), np.array([[1, 0], [0, 0]]))
+
+
+if __name__ == '__main__':
+  test.main()
