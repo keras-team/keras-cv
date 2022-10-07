930e7741549f95f5cdf4e11cb7320c6db2829d3e
Luke Wood <lukewoodcs@gmail.com>
2022-10-07 10:10:15 -0700

    Reformat

--- keras_cv/layers/preprocessing/mosaic.py
+++ keras_cv/layers/preprocessing/mosaic.py
@@ -72,9 +72,7 @@ class Mosaic(BaseImageAugmentationLayer):
         self.offset = offset
         self.bounding_box_format = bounding_box_format
         self.center_sampler = preprocessing.parse_factor(
-            offset,
-            param_name='offset',
-            seed=seed
+            offset, param_name="offset", seed=seed
         )
         self.seed = seed
 
