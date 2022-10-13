18e87fed88c474134cf9f533d2671fc66a5dfd8d
David Landup <david.landup.0@gmail.com>
2022-10-13 03:12:09 +0200

    added swt to init file

--- keras_cv/models/__init__.py
+++ keras_cv/models/__init__.py
@@ -55,3 +55,4 @@ from keras_cv.models.resnet_v2 import ResNet50V2
 from keras_cv.models.resnet_v2 import ResNet101V2
 from keras_cv.models.resnet_v2 import ResNet152V2
 from keras_cv.models.vgg19 import VGG19
+from keras_cv.models.swin_transformer import SwinTransformer
