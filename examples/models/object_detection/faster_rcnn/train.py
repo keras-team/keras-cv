c5aa130ee62da3554cddbd7a4f6ed21c170555b9
tanzhenyu <tanzheny@google.com>
2022-10-13 12:44:46 -0700

    Add model fit for FasterRCNN.

--- examples/models/object_detection/faster_rcnn/train.py
+++ examples/models/object_detection/faster_rcnn/train.py
@@ -18,16 +18,36 @@ Date created: 2022/09/27
 Last modified: 2022/09/27
 Description: Use KerasCV to train a RetinaNet on Pascal VOC 2007.
 """
-from datetime import datetime
+import sys
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from absl import flags
 
 import keras_cv
 
+flags.DEFINE_string(
+    "weights_path", "~", "Directory which will be used to store weight checkpoints."
+)
+flags.DEFINE_string(
+    "tensorboard_path",
+    "~/logs",
+    "Directory which will be used to store tensorboard logs.",
+)
+FLAGS = flags.FLAGS
+FLAGS(sys.argv)
+
 # parameters from FasterRCNN [paper](https://arxiv.org/pdf/1506.01497.pdf)
 
-strategy = tf.distribute.MirroredStrategy()
+# Try to detect an available TPU. If none is present, default to MirroredStrategy
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver.connect()
+    strategy = tf.distribute.TPUStrategy(tpu)
+except ValueError:
+    # MirroredStrategy is best for a single machine with one or multiple GPUs
+    strategy = tf.distribute.MirroredStrategy()
+print("Number of accelerators: ", strategy.num_replicas_in_sync)
+
 local_batch = 4
 global_batch = local_batch * strategy.num_replicas_in_sync
 base_lr = 0.01 * global_batch / 16
@@ -241,13 +261,13 @@ eval_ds = eval_ds.prefetch(2)
 with strategy.scope():
     # The keras huber loss will sum all losses and divide by BS * N * 4, instead we want divide by BS
     # using NONE reduction as also summing creates issues
-    rpn_reg_loss_fn = tf.keras.losses.Huber(reduction=tf.keras.losses.Reduction.NONE)
+    rpn_reg_loss_fn = tf.keras.losses.Huber(reduction=tf.keras.losses.Reduction.SUM)
     rpn_cls_loss_fn = tf.keras.losses.BinaryCrossentropy(
-        from_logits=True, reduction=tf.keras.losses.Reduction.NONE
+        from_logits=True, reduction=tf.keras.losses.Reduction.SUM
     )
-    rcnn_reg_loss_fn = tf.keras.losses.Huber(reduction=tf.keras.losses.Reduction.NONE)
+    rcnn_reg_loss_fn = tf.keras.losses.Huber(reduction=tf.keras.losses.Reduction.SUM)
     rcnn_cls_loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(
-        reduction=tf.keras.losses.Reduction.NONE
+        reduction=tf.keras.losses.Reduction.SUM
     )
 
     rpn_reg_metric = tf.keras.metrics.Mean()
@@ -267,167 +287,11 @@ with strategy.scope():
 weight_decay = 0.0001
 step = 0
 
-
-def compute_loss(examples, training):
-    image = examples["images"]
-    gt_boxes = examples["gt_boxes"]
-    gt_classes = examples["gt_classes"]
-    anchors = model.anchor_generator(image_shape=image_size)
-    anchors = tf.concat(tf.nest.flatten(anchors), axis=0)
-    box_targets, box_weights, cls_targets, cls_weights = model.rpn_labeler(
-        anchors, gt_boxes, gt_classes
-    )
-    outputs = model(image, gt_boxes=gt_boxes, gt_classes=gt_classes, training=training)
-    rpn_box_pred, rpn_cls_pred = outputs["rpn_box_pred"], outputs["rpn_cls_pred"]
-    rpn_reg_loss = tf.reduce_sum(
-        rpn_reg_loss_fn(box_targets, rpn_box_pred, box_weights)
-    )
-    # avoid divide by zero
-    # positive_boxes = tf.reduce_sum(box_weights) + 0.01
-    # x4 given huber loss reduce_mean on the box dimension
-    # rpn_reg_loss /= positive_boxes * 0.25
-    rpn_reg_loss /= model.rpn_labeler.samples_per_image * global_batch * 0.25
-    rpn_cls_loss = tf.reduce_sum(
-        rpn_cls_loss_fn(cls_targets, rpn_cls_pred, cls_weights)
-    )
-    rpn_cls_loss /= model.rpn_labeler.samples_per_image * global_batch
-
-    rcnn_reg_loss = tf.reduce_sum(
-        rcnn_reg_loss_fn(
-            outputs["rcnn_box_targets"],
-            outputs["rcnn_box_pred"],
-            outputs["rcnn_box_weights"],
-        )
-    )
-    # avoid divide by zero
-    # positive_rois = tf.reduce_sum(outputs["rcnn_box_weights"]) + 0.01
-    # rcnn_reg_loss /= positive_rois * 0.25
-    rcnn_reg_loss /= model.roi_sampler.num_sampled_rois * global_batch * 0.25
-    rcnn_cls_loss = tf.reduce_sum(
-        rcnn_cls_loss_fn(
-            outputs["rcnn_cls_targets"],
-            outputs["rcnn_cls_pred"],
-            outputs["rcnn_cls_weights"],
-        )
-    )
-    # 512 is for num_sampled_rois
-    rcnn_cls_loss /= model.roi_sampler.num_sampled_rois * global_batch
-    l2_vars = []
-    for var in model.trainable_variables:
-        if "bn" not in var.name:
-            l2_vars.append(var)
-    l2_loss = weight_decay * tf.add_n([tf.nn.l2_loss(var) for var in l2_vars])
-    total_loss = rpn_reg_loss + rpn_cls_loss + rcnn_reg_loss + rcnn_cls_loss + l2_loss
-    return rpn_reg_loss, rpn_cls_loss, rcnn_reg_loss, rcnn_cls_loss, l2_loss, total_loss
-
-
-@tf.function
-def train_step(examples):
-    with tf.GradientTape() as tape:
-        (
-            rpn_reg_loss,
-            rpn_cls_loss,
-            rcnn_reg_loss,
-            rcnn_cls_loss,
-            l2_loss,
-            total_loss,
-        ) = compute_loss(examples, training=True)
-    gradients = tape.gradient(total_loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-    return rpn_reg_loss, rpn_cls_loss, rcnn_reg_loss, rcnn_cls_loss, l2_loss, total_loss
-
-
-@tf.function
-def eval_step(examples):
-    (
-        rpn_reg_loss,
-        rpn_cls_loss,
-        rcnn_reg_loss,
-        rcnn_cls_loss,
-        _,
-        total_loss,
-    ) = compute_loss(examples, training=True)
-    # the loss is already divided by global batch, so Mean metrics need to scale back
-    num_syncs = strategy.num_replicas_in_sync
-    rpn_reg_metric.update_state(rpn_reg_loss * num_syncs)
-    rpn_cls_metric.update_state(rpn_cls_loss * num_syncs)
-    rcnn_reg_metric.update_state(rcnn_reg_loss * num_syncs)
-    rcnn_cls_metric.update_state(rcnn_cls_loss * num_syncs)
-    return total_loss
-
-
-@tf.function
-def distributed_train_step(dataset_inputs):
-    per_replica_losses = strategy.run(train_step, args=(dataset_inputs,))
-    return strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses, axis=None)
-
-
-@tf.function
-def distributed_eval_step(dataset_inputs):
-    return strategy.run(eval_step, args=(dataset_inputs,))
-
-
-rpn_reg_loss = 0.0
-rpn_cls_loss = 0.0
-rcnn_reg_loss = 0.0
-rcnn_cls_loss = 0.0
-l2_loss = 0.0
-step_size = 500
-
-dist_train_ds = strategy.experimental_distribute_dataset(train_ds)
-dist_eval_ds = strategy.experimental_distribute_dataset(eval_ds)
-
-for epoch in range(1, 19):
-    for examples in dist_train_ds:
-        (
-            step_rpn_reg_loss,
-            step_rpn_cls_loss,
-            step_rcnn_reg_loss,
-            step_rcnn_cls_loss,
-            step_l2_loss,
-            total_loss,
-        ) = distributed_train_step(examples)
-        rpn_reg_loss += step_rpn_reg_loss
-        rpn_cls_loss += step_rpn_cls_loss
-        rcnn_reg_loss += step_rcnn_reg_loss
-        rcnn_cls_loss += step_rcnn_cls_loss
-        l2_loss += step_l2_loss
-        step += 1
-        if step % step_size == 0:
-            print(
-                "step {} rpn_reg {:.4}, rpn_cls {:.4}, rcnn_reg {:.4}, rcnn_cls {:.4}, l2 {:.4}, pos_rois {:.4}, neg_rois {:.4}".format(
-                    step,
-                    rpn_reg_loss / step_size,
-                    rpn_cls_loss / step_size,
-                    rcnn_reg_loss / step_size,
-                    rcnn_cls_loss / step_size,
-                    l2_loss / step_size,
-                    model.roi_sampler._positives.result(),
-                    model.roi_sampler._negatives.result(),
-                )
-            )
-            rpn_reg_loss = 0.0
-            rpn_cls_loss = 0.0
-            rcnn_reg_loss = 0.0
-            rcnn_cls_loss = 0.0
-            l2_loss = 0.0
-            model.roi_sampler._positives.reset_state()
-            model.roi_sampler._negatives.reset_state()
-    for examples in dist_eval_ds:
-        _ = distributed_eval_step(examples)
-    print(
-        "epoch {} rpn reg loss {:.4}, rpn cls loss {:.4}, rcnn reg loss {:.4}, rcnn cls loss {:.4}".format(
-            epoch,
-            rpn_reg_metric.result(),
-            rpn_cls_metric.result(),
-            rcnn_reg_metric.result(),
-            rcnn_cls_metric.result(),
-        )
-    )
-    rpn_reg_metric.reset_state()
-    rpn_cls_metric.reset_state()
-    rcnn_reg_metric.reset_state()
-    rcnn_cls_metric.reset_state()
-    print(f"{datetime.now()} finished epoch {epoch}", flush=True)
-
-    model.save_weights(f"./weights_{epoch}.h5")
+callbacks = [
+    tf.keras.callbacks.ModelCheckpoint(FLAGS.weights_path, save_weights_only=True),
+    tf.keras.callbacks.TensorBoard(
+        log_dir=FLAGS.tensorboard_path, write_steps_per_second=True
+    ),
+]
+model.compile(optimizer=optimizer)
+model.fit(train_ds, epochs=18, validation_data=eval_ds, callbacks=callbacks)
