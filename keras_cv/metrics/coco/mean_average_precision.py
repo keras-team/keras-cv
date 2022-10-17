fix MaP iou computation

keras_cv/metrics/coco/mean_average_precision.py
@@ -229,7 +229,9 @@ class COCOMeanAveragePrecision(tf.keras.metrics.Metric):
                 )
 
                 ious = iou_lib.compute_iou(
-                    ground_truths_by_category, detections_by_category, "xyxy"
+                    ground_truths_by_category,
+                    detections_by_category,
+                    bounding_box_format="xyxy",
                 )
 
                 for iou_i in tf.range(self.num_iou_thresholds):