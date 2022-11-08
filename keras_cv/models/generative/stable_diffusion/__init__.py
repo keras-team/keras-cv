57d2eddc89dc4de6862f7f6744c429efa1db6d83
Ian Stenbit <3072903+ianstenbit@users.noreply.github.com>
2022-11-07 17:21:14 -0700

    Lazy-load components of StableDiffusion and make them visible to users (#989)
    * Switch to lazy loading for all StableDiffusion components
    
    * Disable tests

--- keras_cv/models/generative/stable_diffusion/__init__.py
+++ keras_cv/models/generative/stable_diffusion/__init__.py
@@ -12,4 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from keras_cv.models.generative.stable_diffusion.stable_diffusion import Decoder
+from keras_cv.models.generative.stable_diffusion.stable_diffusion import DiffusionModel
+from keras_cv.models.generative.stable_diffusion.stable_diffusion import ImageEncoder
+from keras_cv.models.generative.stable_diffusion.stable_diffusion import SimpleTokenizer
 from keras_cv.models.generative.stable_diffusion.stable_diffusion import StableDiffusion
+from keras_cv.models.generative.stable_diffusion.stable_diffusion import TextEncoder
