9756c640d929668e514b3379265180ab677d892a
Luke Wood <lukewoodcs@gmail.com>
2022-11-14 15:32:12 -0800

    Update base image augmentation layaer to support ragged images

--- keras_cv/layers/preprocessing/base_image_augmentation_layer.py
+++ keras_cv/layers/preprocessing/base_image_augmentation_layer.py
@@ -141,18 +141,12 @@ class BaseImageAugmentationLayer(tf.keras.__internal__.layers.BaseRandomLayer):
         return tf.TensorSpec(images.shape[1:], self.compute_dtype)
 
     def _compute_bounding_box_signature(self, bounding_boxes):
-        if isinstance(bounding_boxes, tf.RaggedTensor):
-            ragged_spec = tf.RaggedTensorSpec(
-                shape=[None, bounding_boxes.shape[2]],
-                ragged_rank=1,
-                dtype=self.compute_dtype,
-            )
-            return ragged_spec
-
-        return tf.TensorSpec(
+        ragged_spec = tf.RaggedTensorSpec(
             shape=[None, bounding_boxes.shape[2]],
+            ragged_rank=1,
             dtype=self.compute_dtype,
         )
+        return ragged_spec
 
     def _compute_keypoints_signature(self, keypoints):
         if isinstance(keypoints, tf.RaggedTensor):
@@ -401,6 +395,8 @@ class BaseImageAugmentationLayer(tf.keras.__internal__.layers.BaseRandomLayer):
                 label=label,
                 image=image,
             )
+            if not isinstance(bounding_boxes, tf.RaggedTensor):
+                bounding_boxes = tf.RaggedTensor.from_tensor(bounding_boxes)
             result[BOUNDING_BOXES] = bounding_boxes
 
         if keypoints is not None:
