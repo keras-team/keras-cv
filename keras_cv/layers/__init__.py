aaf28249bca4f2473dda8f0aaddd1926c9a2b7ec
David Landup <60978046+DavidLandup0@users.noreply.github.com>
2022-11-14 20:15:34 +0100

    Adding ViT Components/Layers (#929)
    * add initial version of new ViT layers
    
    * super calls fixed, imported in init
    
    * support for kwargs
    
    * removed erroneous import
    
    * fix
    
    * Documentation and small fix
    
    * fix
    
    * fix
    
    * changes to projection dims
    
    * changes to projection dims
    
    * changes to projection dims
    
    * Added class tokenizing, enhanced documentation
    
    * Added class tokenization before positional embedding
    
    * Formatting with shell/format
    
    * Fixed import
    
    * fix for patch encoding
    
    * Documentation enhancement
    
    * adding name to the learnable token to avoid exception with graph ops
    
    * Added build method for class tokenizer
    
    * Ditched using tf.Variable
    
    * zeros_initializer -> random.normal
    
    * Adding mlp_units as layer attribute
    
    * moved layer instantiations from init to call for patchencoding
    
    * hopefully properly registering layers as attributes
    
    * fixed typo
    
    * fixed typo
    
    * formatting
    
    * refactored encoding into embedding
    
    * added interpolation option for patch embedding
    
    * fixed interpolation option
    
    * default intermediate size for transformer encoder change
    
    * fixed attention dropout, updated docs
    
    * patch -> output
    
    * separated layer norms
    
    * made class token learnable
    
    * formatting
    
    * updated input shape for class token
    
    * fixed error
    
    * separated MLP intermediate dims from encoder intermediate dims
    
    * turned f-string to string for linter
    
    * Collapsed ViT components into one file, simplified patch embedding layer
    
    * Fixed odd bug
    
    * added returns to docstrings in vit_layers
    
    * added returns to docstrings in vit_layers
    
    * Updated based on review
    
    * Docstring expansion, turning classes private
    
    * formatting fix
    
    * Formatting to google style, exposed padding flag
    
    * Fixed exposed padding
    
    * Separated one-line explanations for doc rendering
    
    * Formatting
    
    * removed defaults
    
    * Updated call() docstrings
    
    * serialization tests
    
    * serialization tests for transformer encoder
    
    * basic transformer tests
    
    * Interpolation fix, test cases
    
    * Padding validation in constructor
    
    * removed unnecessary export
    
    * Added numerical tests for interpolation and patching, changed interpolation logic to isolate interpolation effect
    
    * Removed imports

--- keras_cv/layers/__init__.py
+++ keras_cv/layers/__init__.py
@@ -73,3 +73,4 @@ from keras_cv.layers.regularization.dropblock_2d import DropBlock2D
 from keras_cv.layers.regularization.squeeze_excite import SqueezeAndExcite2D
 from keras_cv.layers.regularization.stochastic_depth import StochasticDepth
 from keras_cv.layers.spatial_pyramid import SpatialPyramidPooling
+from keras_cv.layers.transformer_encoder import TransformerEncoder
