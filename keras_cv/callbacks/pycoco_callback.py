b80a48b9acb4194ec7c0b1154a17001a651b5b07
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2022-11-16 23:21:19 -0700

    Speed up the PyCOCO callback by only calling model.predict once

--- keras_cv/callbacks/pycoco_callback.py
+++ keras_cv/callbacks/pycoco_callback.py
@@ -29,74 +29,61 @@ class PyCOCOCallback(Callback):
     def on_epoch_end(self, epoch, logs=None):
         logs = logs or {}
 
-        gt = {}
-        preds = {}
+        def images_only(images, boxes):
+            return images
 
-        for i, batch in enumerate(self.val_data):
-            gt_i, preds_i = self._eval_batch(batch, i)
+        def boxes_only(images, boxes):
+            return boxes
 
-            for k, v in six.iteritems(preds_i):
-                if k not in preds:
-                    preds[k] = [v]
-                else:
-                    preds[k].append(v)
+        images_only_ds = self.val_data.map(images_only)
+        y_pred = model.predict(images_only_ds)
 
-            for k, v in six.iteritems(gt_i):
-                if k not in gt:
-                    gt[k] = [v]
-                else:
-                    gt[k].append(v)
+        gt = [boxes for boxes in self.val_data.map(boxes_only)]
+        gt_boxes = tf.concat(
+            [tf.RaggedTensor.from_tensor(boxes["gt_boxes"]) for boxes in gt], axis=0
+        )
+        gt_classes = tf.concat(
+            [tf.RaggedTensor.from_tensor(boxes["gt_classes"]) for boxes in gt], axis=0
+        )
 
-        metrics = compute_pycoco_metrics(gt, preds)
-        # Mark these as validation metrics by prepending a val_ prefix
-        metrics = {"val_" + name: val for name, val in metrics.items()}
-
-        logs.update(metrics)
-
-    def _eval_batch(self, batch, index):
-        images, y = batch
-        gt_boxes = y["gt_boxes"]
-        gt_classes = y["gt_classes"]
-        batch_size = images.shape[0]
-        height = images.shape[1]
-        width = images.shape[2]
+        first_image_batch = next(iter(images_only_ds))
+        height = first_image_batch.shape[1]
+        width = first_image_batch.shape[2]
+        total_images = gt_boxes.shape[0]
 
         gt_boxes = bounding_box.convert_format(
             gt_boxes, source=self.bounding_box_format, target="yxyx"
         )
 
-        source_ids = tf.strings.join(
-            [
-                tf.strings.as_string(tf.tile(tf.constant([index]), [batch_size])),
-                tf.strings.as_string(
-                    tf.linspace(1, batch_size, batch_size), precision=0
-                ),
-            ],
-            separator="/",
+        source_ids = tf.strings.as_string(
+            tf.linspace(1, total_images, total_images), precision=0
         )
 
         ground_truth = {}
-        ground_truth["source_id"] = source_ids
-        ground_truth["height"] = tf.tile(tf.constant([height]), [batch_size])
-        ground_truth["width"] = tf.tile(tf.constant([width]), [batch_size])
+        ground_truth["source_id"] = [source_ids]
+        ground_truth["height"] = [tf.tile(tf.constant([height]), [total_images])]
+        ground_truth["width"] = [tf.tile(tf.constant([width]), [total_images])]
 
         num_dets = gt_classes.get_shape().as_list()[1]
-        ground_truth["num_detections"] = tf.tile(tf.constant([num_dets]), [batch_size])
-        ground_truth["boxes"] = gt_boxes
-        ground_truth["classes"] = gt_classes
+        ground_truth["num_detections"] = [gt_boxes.row_lengths(axis=1)]
+        ground_truth["boxes"] = [gt_boxes.to_tensor(-1)]
+        ground_truth["classes"] = [gt_classes.to_tensor(-1)]
 
-        y_pred = self.model.predict(images)
         y_pred = bounding_box.convert_format(
             y_pred, source=self.bounding_box_format, target="yxyx"
         )
 
         predictions = {}
-        predictions["num_detections"] = y_pred.row_lengths()
+        predictions["num_detections"] = [y_pred.row_lengths()]
         y_pred = y_pred.to_tensor(-1)
 
-        predictions["source_id"] = source_ids
-        predictions["detection_boxes"] = y_pred[:, :, :4]
-        predictions["detection_classes"] = y_pred[:, :, 4]
-        predictions["detection_scores"] = y_pred[:, :, 5]
+        predictions["source_id"] = [source_ids]
+        predictions["detection_boxes"] = [y_pred[:, :, :4]]
+        predictions["detection_classes"] = [y_pred[:, :, 4]]
+        predictions["detection_scores"] = [y_pred[:, :, 5]]
 
-        return ground_truth, predictions
+        metrics = compute_pycoco_metrics(ground_truth, predictions)
+        # Mark these as validation metrics by prepending a val_ prefix
+        metrics = {"val_" + name: val for name, val in metrics.items()}
+
+        logs.update(metrics)
