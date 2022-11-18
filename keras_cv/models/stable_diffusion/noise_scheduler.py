239bb585f5eaa87e33b9f373105afe4faca70c11
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2022-11-18 14:35:19 -0700

    tf, not np

--- keras_cv/models/stable_diffusion/noise_scheduler.py
+++ keras_cv/models/stable_diffusion/noise_scheduler.py
@@ -53,13 +53,7 @@ class NoiseScheduler:
         elif beta_schedule == "scaled_linear":
             # this schedule is very specific to the latent diffusion model.
             self.betas = (
-                np.linspace(
-                    beta_start**0.5,
-                    beta_end**0.5,
-                    num_train_timesteps,
-                    dtype=np.float32,
-                )
-                ** 2
+                tf.linspace(beta_start**0.5, beta_end**0.5, train_timesteps) ** 2
             )
         else:
             raise ValueError(f"Invalid beta schedule: {beta_schedule}.")
