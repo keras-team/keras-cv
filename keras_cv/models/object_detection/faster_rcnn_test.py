19f4a17eb6e6475758cdf512716d50a012b9e91a
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2022-11-21 16:19:07 -0700

    Remove mac os test fix

--- keras_cv/models/object_detection/faster_rcnn_test.py
+++ keras_cv/models/object_detection/faster_rcnn_test.py
@@ -19,7 +19,6 @@ from keras_cv.models.object_detection.faster_rcnn import FasterRCNN
 
 class FasterRCNNTest(tf.test.TestCase):
     def test_faster_rcnn_infer(self):
-        tf.config.set_visible_devices([], "GPU")
         model = FasterRCNN(classes=80, bounding_box_format="xyxy")
         images = tf.random.normal([2, 512, 512, 3])
         outputs = model(images, training=False)
@@ -28,7 +27,6 @@ class FasterRCNNTest(tf.test.TestCase):
         self.assertAllEqual([2, 1000, 4], outputs[0].shape)
 
     def test_faster_rcnn_predict(self):
-        tf.config.set_visible_devices([], "GPU")
         model = FasterRCNN(classes=80, bounding_box_format="xyxy")
         images = tf.random.normal([2, 512, 512, 3])
         outputs = model.predict(images, batch_size=2)
@@ -36,7 +34,6 @@ class FasterRCNNTest(tf.test.TestCase):
         self.assertAllEqual([2, None, 6], outputs.shape)
 
     def test_faster_rcnn_train(self):
-        tf.config.set_visible_devices([], "GPU")
         model = FasterRCNN(classes=80, bounding_box_format="xyxy")
         images = tf.random.normal([2, 512, 512, 3])
         outputs = model(images, training=True)
@@ -44,7 +41,6 @@ class FasterRCNNTest(tf.test.TestCase):
         self.assertAllEqual([2, 1000, 4], outputs[0].shape)
 
     def test_invalid_compile(self):
-        tf.config.set_visible_devices([], "GPU")
         model = FasterRCNN(classes=80, bounding_box_format="yxyx")
         with self.assertRaisesRegex(ValueError, "only accepts"):
             model.compile(rpn_box_loss="binary_crossentropy")
