038992f375c971a52636b1e00c0ef6eb2ef03ce0
Leng Zhaoqi <lengzhaoqi@google.com>
2022-11-21 22:05:11 -0800

    Crop object.

---
+++ keras_cv/layers/preprocessing3d/crop_objects.py
@@ -0,0 +1,196 @@
+# Copyright 2022 The KerasCV Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tensorflow as tf
+
+from keras_cv.layers.preprocessing3d import base_augmentation_layer_3d
+from keras_cv.ops import iou_3d
+from keras_cv.ops.point_cloud import is_within_box3d
+
+POINT_CLOUDS = base_augmentation_layer_3d.POINT_CLOUDS
+BOUNDING_BOXES = base_augmentation_layer_3d.BOUNDING_BOXES
+OBJECT_POINT_CLOUDS = base_augmentation_layer_3d.OBJECT_POINT_CLOUDS
+OBJECT_BOUNDING_BOXES = base_augmentation_layer_3d.OBJECT_BOUNDING_BOXES
+
+
+class CropObjects(base_augmentation_layer_3d.BaseAugmentationLayer3D):
+    """A preprocessing layer which crops objects from point clouds during training.
+
+    This layer will crops points based on object boxes and generates a OBJECT_POINT_CLOUDS tensor.
+    During inference time, the output will be identical to input. Call the layer with `training=True` to crop the input points.
+
+    Input shape:
+      point_clouds: 3D (multi frames) float32 Tensor with shape
+        [num of frames, num of points, num of point features].
+        The first 5 features are [x, y, z, class, range].
+      bounding_boxes: 3D (multi frames) float32 Tensor with shape
+        [num of frames, num of boxes, num of box features].
+        The first 8 features are [x, y, z, dx, dy, dz, phi, box class].
+
+    Output shape:
+      A dictionary of Tensors with the same shape as input Tensors and an additional item for object_point_clouds.
+
+    Arguments:
+      label_index: A int scalar or Tensor sets the target object index.
+    """
+
+    def __init__(
+        self,
+        label_index=0,
+        min_points_per_bounding_boxes=0,
+        max_points_per_bounding_boxes=2000,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        if label_index < 0:
+            raise ValueError("label_index must be >=0.")
+        if min_points_per_bounding_boxes < 0:
+            raise ValueError("min_points_per_bounding_boxes must be >=0.")
+        if max_points_per_bounding_boxes < 0:
+            raise ValueError("max_points_per_bounding_boxes must be >=0.")
+        if min_points_per_bounding_boxes > max_points_per_bounding_boxes:
+            raise ValueError(
+                "max_paste_bounding_boxes must be >= min_points_per_bounding_boxes."
+            )
+
+        self._label_index = label_index
+        self._min_points_per_bounding_boxes = min_points_per_bounding_boxes
+        self._max_points_per_bounding_boxes = max_points_per_bounding_boxes
+        self._iou_3d = iou_3d.IoU3D
+
+    def augment_point_clouds_bounding_boxes(
+        self, point_clouds, bounding_boxes, **kwargs
+    ):
+        if self._label_index > 0:
+            bounding_boxes_mask = tf.math.equal(
+                bounding_boxes[0, :, 7], self._label_index
+            )
+            object_bounding_boxes = tf.boolean_mask(
+                bounding_boxes, bounding_boxes_mask, axis=1
+            )
+        else:
+            bounding_boxes_mask = ~tf.math.equal(bounding_boxes[0, :, 7], 0.0)
+            object_bounding_boxes = tf.boolean_mask(
+                bounding_boxes, bounding_boxes_mask, axis=1
+            )
+
+        points_in_bounding_boxes = is_within_box3d(
+            point_clouds, object_bounding_boxes[:, :, :7]
+        )
+        # Filter bounding boxes using the current frame.
+        min_points_filter = (
+            tf.reduce_sum(tf.cast(points_in_bounding_boxes[0], dtype=tf.int32), axis=0)
+            >= self._min_points_per_bounding_boxes
+        )
+
+        object_bounding_boxes = tf.boolean_mask(
+            object_bounding_boxes, min_points_filter, axis=1
+        )
+
+        points_in_bounding_boxes = tf.boolean_mask(
+            points_in_bounding_boxes, min_points_filter, axis=2
+        )
+        # [num of frames, num of boxes, num of points].
+        points_in_bounding_boxes = tf.transpose(points_in_bounding_boxes, [0, 2, 1])
+        points_in_bounding_boxes = tf.cast(points_in_bounding_boxes, tf.int32)
+        sort_valid_index = tf.argsort(
+            points_in_bounding_boxes, axis=-1, direction="DESCENDING"
+        )
+        sort_valid_mask = tf.gather(
+            points_in_bounding_boxes, sort_valid_index, axis=2, batch_dims=2
+        )[:, :, : self._max_points_per_bounding_boxes]
+        # [num of frames, num of boxes, self._max_points_per_bounding_boxes, num of point features].
+        object_point_clouds = point_clouds[:, tf.newaxis, :, :]
+        num_valid_bounding_boxes = tf.shape(object_bounding_boxes)[1]
+        object_point_clouds = tf.tile(
+            object_point_clouds, [1, num_valid_bounding_boxes, 1, 1]
+        )
+        object_point_clouds = tf.gather(
+            object_point_clouds, sort_valid_index, axis=2, batch_dims=2
+        )[:, :, : self._max_points_per_bounding_boxes, :]
+
+        object_point_clouds = tf.where(
+            sort_valid_mask[:, :, :, tf.newaxis] > 0, object_point_clouds, 0.0
+        )
+
+        return (
+            object_point_clouds,
+            object_bounding_boxes,
+        )
+
+    def _augment(self, inputs):
+        point_clouds = inputs[POINT_CLOUDS]
+        bounding_boxes = inputs[BOUNDING_BOXES]
+
+        transformation = self.get_random_transformation(
+            point_clouds=point_clouds,
+            bounding_boxes=bounding_boxes,
+        )
+        (
+            object_point_clouds,
+            object_bounding_boxes,
+        ) = self.augment_point_clouds_bounding_boxes(
+            point_clouds,
+            bounding_boxes=bounding_boxes,
+            transformation=transformation,
+        )
+
+        result = {
+            OBJECT_POINT_CLOUDS: object_point_clouds,
+            OBJECT_BOUNDING_BOXES: object_bounding_boxes,
+        }
+
+        # preserve any additional inputs unmodified by this layer.
+        for key in inputs.keys() - result.keys():
+            result[key] = inputs[key]
+        return result
+
+    def call(self, inputs, training=True):
+        if training:
+            point_clouds = inputs[POINT_CLOUDS]
+            bounding_boxes = inputs[BOUNDING_BOXES]
+            if point_clouds.shape.rank == 3 and bounding_boxes.shape.rank == 3:
+                return self._augment(inputs)
+            elif point_clouds.shape.rank == 4 and bounding_boxes.shape.rank == 4:
+                batch = point_clouds.get_shape().as_list()[0]
+                object_point_clouds_list = []
+                object_bounding_boxes_list = []
+                for i in range(batch):
+                    (
+                        object_point_clouds,
+                        object_bounding_boxes,
+                    ) = self.augment_point_clouds_bounding_boxes(
+                        inputs[POINT_CLOUDS][i], inputs[BOUNDING_BOXES][i]
+                    )
+                    object_point_clouds_list += [object_point_clouds[tf.newaxis, ...]]
+                    object_bounding_boxes_list += [
+                        object_bounding_boxes[tf.newaxis, ...]
+                    ]
+
+                inputs[OBJECT_POINT_CLOUDS] = tf.concat(
+                    object_point_clouds_list, axis=0
+                )
+                inputs[OBJECT_BOUNDING_BOXES] = tf.concat(
+                    object_bounding_boxes_list, axis=0
+                )
+                return inputs
+            else:
+                raise ValueError(
+                    "Point clouds augmentation layers are expecting inputs point clouds and bounding boxes to "
+                    "be rank 3D (Frame, Point, Feature) or 4D (Batch, Frame, Point, Feature) tensors. Got shape: {} and {}".format(
+                        point_clouds.shape, bounding_boxes.shape
+                    )
+                )
+        else:
+            return inputs
---
+++ keras_cv/layers/preprocessing3d/crop_objects_test.py
@@ -0,0 +1,116 @@
+# Copyright 2022 The KerasCV Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import numpy as np
+import tensorflow as tf
+
+from keras_cv.layers.preprocessing3d import base_augmentation_layer_3d
+from keras_cv.layers.preprocessing3d.crop_objects import CropObjects
+
+POINT_CLOUDS = base_augmentation_layer_3d.POINT_CLOUDS
+BOUNDING_BOXES = base_augmentation_layer_3d.BOUNDING_BOXES
+OBJECT_POINT_CLOUDS = base_augmentation_layer_3d.OBJECT_POINT_CLOUDS
+OBJECT_BOUNDING_BOXES = base_augmentation_layer_3d.OBJECT_BOUNDING_BOXES
+
+
+class GlobalDropPointsnTest(tf.test.TestCase):
+    def test_augment_point_clouds_and_bounding_boxes(self):
+        add_layer = CropObjects(
+            label_index=1,
+            min_points_per_bounding_boxes=1,
+            max_points_per_bounding_boxes=2,
+        )
+        point_clouds = np.array(
+            [[[0, 1, 2, 3, 4], [10, 1, 2, 3, 4], [0, -1, 2, 3, 4], [100, 100, 2, 3, 4]]]
+            * 2
+        ).astype("float32")
+        bounding_boxes = np.array(
+            [
+                [
+                    [0, 0, 0, 4, 4, 4, 0, 1],
+                    [10, 1, 2, 2, 2, 2, 0, 1],
+                    [20, 20, 20, 1, 1, 1, 0, 1],
+                ]
+            ]
+            * 2
+        ).astype("float32")
+        inputs = {POINT_CLOUDS: point_clouds, BOUNDING_BOXES: bounding_boxes}
+        outputs = add_layer(inputs)
+        object_point_clouds = np.array(
+            [[[[0, 1, 2, 3, 4], [0, -1, 2, 3, 4]], [[10, 1, 2, 3, 4], [0, 0, 0, 0, 0]]]]
+            * 2
+        ).astype("float32")
+        object_bounding_boxes = np.array(
+            [[[0, 0, 0, 4, 4, 4, 0, 1], [10, 1, 2, 2, 2, 2, 0, 1]]] * 2
+        ).astype("float32")
+        self.assertAllClose(inputs[POINT_CLOUDS], outputs[POINT_CLOUDS])
+        self.assertAllClose(inputs[BOUNDING_BOXES], outputs[BOUNDING_BOXES])
+        self.assertAllClose(outputs[OBJECT_POINT_CLOUDS], object_point_clouds)
+        self.assertAllClose(outputs[OBJECT_BOUNDING_BOXES], object_bounding_boxes)
+
+    def test_augment_batch_point_clouds_and_bounding_boxes(self):
+        add_layer = CropObjects(
+            label_index=1,
+            min_points_per_bounding_boxes=1,
+            max_points_per_bounding_boxes=2,
+        )
+        point_clouds = np.array(
+            [
+                [
+                    [
+                        [0, 1, 2, 3, 4],
+                        [10, 1, 2, 3, 4],
+                        [0, -1, 2, 3, 4],
+                        [100, 100, 2, 3, 4],
+                    ]
+                ]
+                * 2
+            ]
+            * 3
+        ).astype("float32")
+        bounding_boxes = np.array(
+            [
+                [
+                    [
+                        [0, 0, 0, 4, 4, 4, 0, 1],
+                        [10, 1, 2, 2, 2, 2, 0, 1],
+                        [20, 20, 20, 1, 1, 1, 0, 1],
+                    ]
+                ]
+                * 2
+            ]
+            * 3
+        ).astype("float32")
+        tf.print(("asd", point_clouds.shape, bounding_boxes.shape))
+        inputs = {POINT_CLOUDS: point_clouds, BOUNDING_BOXES: bounding_boxes}
+        outputs = add_layer(inputs)
+        object_point_clouds = np.array(
+            [
+                [
+                    [
+                        [[0, 1, 2, 3, 4], [0, -1, 2, 3, 4]],
+                        [[10, 1, 2, 3, 4], [0, 0, 0, 0, 0]],
+                    ]
+                ]
+                * 2
+            ]
+            * 3
+        ).astype("float32")
+        object_bounding_boxes = np.array(
+            [[[[0, 0, 0, 4, 4, 4, 0, 1], [10, 1, 2, 2, 2, 2, 0, 1]]] * 2] * 3
+        ).astype("float32")
+        tf.print(("asd00", object_point_clouds.shape, object_bounding_boxes.shape))
+        self.assertAllClose(inputs[POINT_CLOUDS], outputs[POINT_CLOUDS])
+        self.assertAllClose(inputs[BOUNDING_BOXES], outputs[BOUNDING_BOXES])
+        self.assertAllClose(outputs[OBJECT_POINT_CLOUDS], object_point_clouds)
+        self.assertAllClose(outputs[OBJECT_BOUNDING_BOXES], object_bounding_boxes)
