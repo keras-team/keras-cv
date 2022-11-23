6a96eafb04ac54f2005cc87b48f4b91ce0be386b
David Landup <david.landup.0@gmail.com>
2022-11-24 00:44:57 +0100

    separated binary dice

--- keras_cv/losses/dice_test.py
+++ keras_cv/losses/dice_test.py
@@ -21,47 +21,6 @@ from keras_cv.losses.dice import SparseDice
 
 
 class DiceTest(tf.test.TestCase):
-    def test_categorical_output_shape(self):
-        """
-        Both y_true and y_pred are one-hot vectors
-        """
-
-        y_true = tf.random.uniform(
-            shape=(3, 10, 10, 5), minval=0, maxval=5, dtype=tf.int32
-        )
-
-        y_pred = tf.random.uniform(
-            shape=(3, 10, 10, 5), minval=0, maxval=5, dtype=tf.float32
-        )
-
-        categorical_dice_loss = CategoricalDice()
-        self.assertAllEqual(categorical_dice_loss(y_true, y_pred).shape, ())
-
-        sample_weight = [[0.5, 0.5, 0.2, 0.2, 0.5]]
-        self.assertAllEqual(
-            categorical_dice_loss(y_true, y_pred, sample_weight=sample_weight).shape, ()
-        )
-
-    def test_sparse_output_shape(self):
-        """
-        y_true is a sparse vector, y_pred is a one-hot vector
-        """
-        y_true = tf.random.uniform(
-            shape=(3, 10, 10, 1), minval=0, maxval=5, dtype=tf.int32
-        )
-
-        y_pred = tf.random.uniform(
-            shape=(3, 10, 10, 5), minval=0, maxval=5, dtype=tf.float32
-        )
-
-        sparse_dice_loss = SparseDice()
-        self.assertAllEqual(sparse_dice_loss(y_true, y_pred).shape, ())
-
-        sample_weight = [[0.5, 0.5, 0.2, 0.2, 0.5]]
-        self.assertAllEqual(
-            sparse_dice_loss(y_true, y_pred, sample_weight=sample_weight).shape, ()
-        )
-
     def test_binary_output_shape(self):
         y_true = tf.random.uniform(
             shape=(3, 5, 5, 1), minval=0, maxval=1, dtype=tf.int32
@@ -72,79 +31,6 @@ class DiceTest(tf.test.TestCase):
 
         binary_dice_loss = BinaryDice()
         self.assertAllEqual(binary_dice_loss(y_true, y_pred).shape, ())
-
-    def test_categorical_output_value(self):
-        y_true = np.array([[[0, 0, 0], [0, 0, 0], [0, 0, 0]]])
-
-        y_pred = np.array(
-            [
-                [
-                    [[0.0, 1.0, 0.0], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]],
-                    [[0.0, 1.0, 0.0], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]],
-                    [[0.0, 1.0, 0.0], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]],
-                ]
-            ]
-        )
-
-        y_true = tf.one_hot(y_true, depth=3)
-
-        categorical_dice_loss = CategoricalDice(from_logits=True)
-        self.assertAllClose(categorical_dice_loss(y_true, y_pred), 0.8834148)
-
-        categorical_dice_loss = CategoricalDice(from_logits=False)
-        self.assertAllClose(categorical_dice_loss(y_true, y_pred), 0.6666666)
-
-    def test_sparse_output_value(self):
-        y_true = np.array([[[[0], [0], [0]], [[0], [0], [0]], [[0], [0], [0]]]])
-
-        y_pred = np.array(
-            [
-                [
-                    [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]],
-                    [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]],
-                    [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]],
-                ]
-            ]
-        )
-
-        sparse_dice_loss = SparseDice(from_logits=False)
-        self.assertAllClose(sparse_dice_loss(y_true, y_pred), 0.3333333296296296)
-        sparse_dice_loss = SparseDice(from_logits=True)
-        self.assertAllClose(sparse_dice_loss(y_true, y_pred), 0.833333309722223)
-
-        y_true = np.array([[[0, 0, 0], [0, 0, 0], [0, 0, 0]]])
-
-        y_pred = np.array(
-            [
-                [
-                    [[0.0, 1.0, 0.0], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]],
-                    [[0.0, 1.0, 0.0], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]],
-                    [[0.0, 1.0, 0.0], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]],
-                ]
-            ]
-        )
-
-        sparse_dice_loss = SparseDice(from_logits=False)
-        self.assertAllClose(sparse_dice_loss(y_true, y_pred), 0.6666666592592594)
-        sparse_dice_loss = SparseDice(from_logits=True)
-        self.assertAllClose(sparse_dice_loss(y_true, y_pred), 0.8834148377575873)
-
-        y_true = np.array([[[1, 1, 1], [1, 1, 1], [1, 1, 1]]])
-
-        y_pred = np.array(
-            [
-                [
-                    [[0.0, 1.0, 0.0], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]],
-                    [[0.0, 1.0, 0.0], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]],
-                    [[0.0, 1.0, 0.0], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]],
-                ]
-            ]
-        )
-
-        sparse_dice_loss = SparseDice(from_logits=False)
-        self.assertAllClose(sparse_dice_loss(y_true, y_pred), 0.0)
-        sparse_dice_loss = SparseDice(from_logits=True)
-        self.assertAllClose(sparse_dice_loss(y_true, y_pred), 0.7563137715411146)
 
     def test_binary_output_value(self):
         # All wrong
