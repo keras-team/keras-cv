4c2ace6442669c2cfd2172ed5cc19c39457531dd
Ian Stenbit <3072903+ianstenbit@users.noreply.github.com>
2022-12-07 14:29:20 -0700

    Update random_copy_paste_test.py

--- keras_cv/layers/preprocessing3d/random_copy_paste_test.py
+++ keras_cv/layers/preprocessing3d/random_copy_paste_test.py
@@ -11,8 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import numpy as np
 import os
+
+import numpy as np
 import pytest
 import tensorflow as tf
 
