a08863e5d62e201fe668adfdb627c33778fe1a51
tanzhenyu <tanzheny@google.com>
2022-12-08 20:41:59 -0800

    Fix RetinaNet training on TPU. (#1113)
    * Fix RetinaNet training on TPU.
    
    * add author.
    
    * remove unused line.
    
    * address comment.

--- examples/training/object_detection/pascal_voc/retina_net.py
+++ examples/training/object_detection/pascal_voc/retina_net.py
@@ -1,15 +1,37 @@
+# Copyright 2022 The KerasCV Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Title: Train an Object Detection Model on Pascal VOC 2007 using KerasCV
+Author: [lukewood](https://github.com/LukeWood), [tanzhenyu](https://github.com/tanzhenyu)
+Date created: 2022/09/27
+Last modified: 2022/12/08
+Description: Use KerasCV to train a RetinaNet on Pascal VOC 2007.
+"""
 import resource
+import sys
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from absl import flags
 from tensorflow import keras
 
 import keras_cv
+from keras_cv.callbacks import PyCOCOCallback
 
 low, high = resource.getrlimit(resource.RLIMIT_NOFILE)
 resource.setrlimit(resource.RLIMIT_NOFILE, (high, high))
 
-BATCH_SIZE = 8
 EPOCHS = 100
 CHECKPOINT_PATH = "checkpoint/"
 
@@ -38,9 +60,39 @@ class_ids = [
 ]
 class_mapping = dict(zip(range(len(class_ids)), class_ids))
 
-image_size = [640, 640, 3]
+flags.DEFINE_string(
+    "weights_name",
+    "weights_{epoch:02d}.h5",
+    "Directory which will be used to store weight checkpoints.",
+)
+flags.DEFINE_string(
+    "tensorboard_path",
+    "logs",
+    "Directory which will be used to store tensorboard logs.",
+)
+FLAGS = flags.FLAGS
+FLAGS(sys.argv)
+
+# parameters from RetinaNet [paper](https://arxiv.org/abs/1708.02002)
+
+# Try to detect an available TPU. If none is present, default to MirroredStrategy
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver.connect()
+    strategy = tf.distribute.TPUStrategy(tpu)
+except ValueError:
+    # MirroredStrategy is best for a single machine with one or multiple GPUs
+    strategy = tf.distribute.MirroredStrategy()
+
+BATCH_SIZE = 4
+GLOBAL_BATCH_SIZE = BATCH_SIZE * strategy.num_replicas_in_sync
+BASE_LR = 0.01 * GLOBAL_BATCH_SIZE / 16
+print("Number of accelerators: ", strategy.num_replicas_in_sync)
+print("Global Batch Size: ", GLOBAL_BATCH_SIZE)
+
+IMG_SIZE = 640
+image_size = [IMG_SIZE, IMG_SIZE, 3]
 train_ds = tfds.load(
-    "voc/2007", split="train+test", with_info=False, shuffle_files=True
+    "voc/2007", split="train+validation", with_info=False, shuffle_files=True
 )
 train_ds = train_ds.concatenate(
     tfds.load("voc/2012", split="train+validation", with_info=False, shuffle_files=True)
@@ -202,40 +254,84 @@ def proc_train_fn(bounding_box_format, img_size):
         bounding_boxes = keras_cv.bounding_box.convert_format(
             bounding_boxes, images=image, source="yxyx", target=bounding_box_format
         )
-        return {"images": image, "bounding_boxes": bounding_boxes}
+        return image, bounding_boxes
 
     return apply
+
+
+def proc_eval_fn(bounding_box_format, target_size):
+    def apply(inputs):
+        raw_image = inputs["image"]
+        raw_image = tf.cast(raw_image, tf.float32)
+
+        img_size = tf.shape(raw_image)
+        height = img_size[0]
+        width = img_size[1]
+
+        target_height = tf.cond(
+            height > width,
+            lambda: float(IMG_SIZE),
+            lambda: tf.cast(height / width * IMG_SIZE, tf.float32),
+        )
+        target_width = tf.cond(
+            width > height,
+            lambda: float(IMG_SIZE),
+            lambda: tf.cast(width / height * IMG_SIZE, tf.float32),
+        )
+        image = tf.image.resize(
+            raw_image, (target_height, target_width), antialias=False
+        )
+
+        gt_boxes = keras_cv.bounding_box.convert_format(
+            inputs["objects"]["bbox"],
+            images=image,
+            source="rel_yxyx",
+            target="xyxy",
+        )
+        image = tf.image.pad_to_bounding_box(
+            image, 0, 0, target_size[0], target_size[1]
+        )
+        gt_boxes = keras_cv.bounding_box.convert_format(
+            gt_boxes,
+            images=image,
+            source="xyxy",
+            target=bounding_box_format,
+        )
+        gt_classes = tf.cast(inputs["objects"]["label"], tf.float32)
+        gt_classes = tf.expand_dims(gt_classes, axis=-1)
+
+        bounding_boxes = tf.concat([gt_boxes, gt_classes], axis=-1)
+
+        return image, bounding_boxes
+
+    return apply
+
+
+def pad_fn(image, boxes):
+    return image, boxes.to_tensor(default_value=-1.0)
 
 
 train_ds = train_ds.map(
     proc_train_fn("xywh", image_size), num_parallel_calls=tf.data.AUTOTUNE
 )
 train_ds = train_ds.apply(
-    tf.data.experimental.dense_to_ragged_batch(BATCH_SIZE, drop_remainder=True)
+    tf.data.experimental.dense_to_ragged_batch(GLOBAL_BATCH_SIZE, drop_remainder=True)
 )
+
+train_ds = train_ds.map(pad_fn, num_parallel_calls=tf.data.AUTOTUNE)
+train_ds = train_ds.shuffle(8 * strategy.num_replicas_in_sync)
+train_ds = train_ds.prefetch(2)
+
 eval_ds = eval_ds.map(
-    proc_train_fn(bounding_box_format="xywh", img_size=image_size),
+    proc_eval_fn(bounding_box_format="xywh", target_size=image_size),
     num_parallel_calls=tf.data.AUTOTUNE,
 )
 eval_ds = eval_ds.apply(
-    tf.data.experimental.dense_to_ragged_batch(BATCH_SIZE, drop_remainder=True)
+    tf.data.experimental.dense_to_ragged_batch(GLOBAL_BATCH_SIZE, drop_remainder=True)
 )
-
-"""
-Looks like everything is structured as expected.  Now we can move on to constructing our
-data augmentation pipeline.
-"""
-train_ds = train_ds.prefetch(2)
-train_ds = train_ds.shuffle(BATCH_SIZE)
+eval_ds = eval_ds.map(pad_fn, num_parallel_calls=tf.data.AUTOTUNE)
 eval_ds = eval_ds.prefetch(2)
 
-
-def unpackage_dict(inputs):
-    return inputs["images"], inputs["bounding_boxes"]
-
-
-train_ds = train_ds.map(unpackage_dict, num_parallel_calls=tf.data.AUTOTUNE)
-eval_ds = eval_ds.map(unpackage_dict, num_parallel_calls=tf.data.AUTOTUNE)
 
 """
 Our data pipeline is now complete.  We can now move on to model creation and training.
@@ -250,190 +346,48 @@ freeze the backbone before training.  When `include_rescaling=True` is set, inpu
 the model are expected to be in the range `[0, 255]`.
 """
 
-model = keras_cv.models.RetinaNet(
-    # number of classes to be used in box classification
-    classes=len(class_ids),
-    # For more info on supported bounding box formats, visit
-    # https://keras.io/api/keras_cv/bounding_box/
-    bounding_box_format="xywh",
-    # KerasCV offers a set of pre-configured backbones
-    backbone="resnet50",
-    # Each backbone comes with multiple pre-trained weights
-    # These weights match the weights available in the `keras_cv.model` class.
-    backbone_weights="imagenet",
-    # include_rescaling tells the model whether your input images are in the default
-    # pixel range (0, 255) or if you have already rescaled your inputs to the range
-    # (0, 1).  In our case, we feed our model images with inputs in the range (0, 255).
-    include_rescaling=True,
-)
-# Fine-tuning a RetinaNet is as simple as setting backbone.trainable = False
-model.backbone.trainable = False
+with strategy.scope():
+    model = keras_cv.models.RetinaNet(
+        # number of classes to be used in box classification
+        classes=len(class_ids),
+        # For more info on supported bounding box formats, visit
+        # https://keras.io/api/keras_cv/bounding_box/
+        bounding_box_format="xywh",
+        # KerasCV offers a set of pre-configured backbones
+        backbone="resnet50",
+        # Each backbone comes with multiple pre-trained weights
+        # These weights match the weights available in the `keras_cv.model` class.
+        backbone_weights="imagenet",
+        # include_rescaling tells the model whether your input images are in the default
+        # pixel range (0, 255) or if you have already rescaled your inputs to the range
+        # (0, 1).  In our case, we feed our model images with inputs in the range (0, 255).
+        include_rescaling=True,
+    )
+    # Fine-tuning a RetinaNet is as simple as setting backbone.trainable = False
+    model.backbone.trainable = False
 
-metrics = [
-    keras_cv.metrics.COCOMeanAveragePrecision(
-        class_ids=range(21),
-        bounding_box_format="xywh",
-        name="Mean Average Precision",
-    ),
-    keras_cv.metrics.COCORecall(
-        class_ids=range(21),
-        bounding_box_format="xywh",
-        max_detections=100,
-        name="Recall",
-    ),
-]
-optimizer = tf.optimizers.SGD(global_clipnorm=10.0)
+    optimizer = tf.optimizers.SGD(learning_rate=BASE_LR, global_clipnorm=10.0)
 model.compile(
     classification_loss="focal",
     box_loss="smoothl1",
     optimizer=optimizer,
-    metrics=metrics,
 )
+
+
+def convert_to_dict(images, boxes):
+    return images, {"gt_boxes": boxes[:, :, :4], "gt_classes": boxes[:, :, 4]}
+
 
 callbacks = [
     keras.callbacks.TensorBoard(log_dir="logs"),
     keras.callbacks.ReduceLROnPlateau(patience=5),
     keras.callbacks.EarlyStopping(patience=10),
     keras.callbacks.ModelCheckpoint(CHECKPOINT_PATH, save_weights_only=True),
+    PyCOCOCallback(eval_ds.map(convert_to_dict), "xywh"),
 ]
 
 history = model.fit(
     train_ds,
-    validation_data=eval_ds.take(10),
     epochs=100,
     callbacks=callbacks,
 )
-
-"""
-## Evaluation
-"""
-
-coco_suite = [
-    keras_cv.metrics.COCOMeanAveragePrecision(
-        bounding_box_format="xywh",
-        class_ids=range(len(class_ids)),
-        max_detections=100,
-        name="MaP Standard",
-    ),
-    keras_cv.metrics.COCOMeanAveragePrecision(
-        bounding_box_format="xywh",
-        class_ids=range(len(class_ids)),
-        iou_thresholds=[0.75],
-        max_detections=100,
-        name="MaP@IoU=0.75",
-    ),
-    keras_cv.metrics.COCOMeanAveragePrecision(
-        bounding_box_format="xywh",
-        class_ids=range(len(class_ids)),
-        iou_thresholds=[0.5],
-        max_detections=100,
-        name="MaP@IoU=0.5",
-    ),
-    keras_cv.metrics.COCOMeanAveragePrecision(
-        bounding_box_format="xywh",
-        class_ids=range(len(class_ids)),
-        area_range=(0, 32**2),
-        max_detections=100,
-        name="MaP Small",
-    ),
-    keras_cv.metrics.COCOMeanAveragePrecision(
-        bounding_box_format="xywh",
-        class_ids=range(len(class_ids)),
-        area_range=(32**2, 96**2),
-        max_detections=100,
-        name="MaP Medium",
-    ),
-    keras_cv.metrics.COCOMeanAveragePrecision(
-        bounding_box_format="xywh",
-        class_ids=range(len(class_ids)),
-        area_range=(96**2, 1e5**2),
-        max_detections=100,
-        name="MaP Large",
-    ),
-    keras_cv.metrics.COCORecall(
-        class_ids=range(len(class_ids)),
-        bounding_box_format="xywh",
-        max_detections=100,
-        name="Recall Standard",
-    ),
-    keras_cv.metrics.COCORecall(
-        class_ids=range(len(class_ids)),
-        bounding_box_format="xywh",
-        max_detections=1,
-        name="Recall MaxDets=1",
-    ),
-    keras_cv.metrics.COCORecall(
-        class_ids=range(len(class_ids)),
-        bounding_box_format="xywh",
-        max_detections=10,
-        name="Recall MaxDets=10",
-    ),
-    keras_cv.metrics.COCORecall(
-        class_ids=range(len(class_ids)),
-        bounding_box_format="xywh",
-        max_detections=100,
-        area_range=(0, 32**2),
-        name="Recall Small",
-    ),
-    keras_cv.metrics.COCORecall(
-        class_ids=range(len(class_ids)),
-        bounding_box_format="xywh",
-        max_detections=100,
-        area_range=(32**2, 96**2),
-        name="Recall Medium",
-    ),
-    keras_cv.metrics.COCORecall(
-        class_ids=range(len(class_ids)),
-        bounding_box_format="xywh",
-        max_detections=100,
-        area_range=(96**2, 1e5**2),
-        name="Recall Large",
-    ),
-]
-model.compile(
-    classification_loss=keras_cv.losses.FocalLoss(from_logits=True, reduction="none"),
-    box_loss=keras_cv.losses.SmoothL1Loss(l1_cutoff=1.0, reduction="none"),
-    optimizer=optimizer,
-    metrics=coco_suite,
-)
-
-model.load_weights(CHECKPOINT_PATH)
-
-
-def proc_eval_fn(bounding_box_format, target_size):
-    resizer = keras_cv.layers.Resizing(
-        target_size[0],
-        target_size[1],
-        pad_to_aspect_ratio=True,
-        bounding_box_format=bounding_box_format,
-    )
-
-    def apply(inputs):
-        image = tf.cast(inputs["image"], tf.float32)
-        gt_boxes = keras_cv.bounding_box.convert_format(
-            inputs["objects"]["bbox"],
-            images=image,
-            source="rel_yxyx",
-            target=bounding_box_format,
-        )
-        gt_classes = tf.cast(inputs["objects"]["label"], tf.float32)
-        gt_classes = tf.expand_dims(gt_classes, axis=-1)
-        bounding_boxes = tf.concat([gt_boxes, gt_classes], axis=-1)
-        inputs = {"images": image, "bounding_boxes": bounding_boxes}
-        o = resizer(inputs)
-        return o["images"], o["bounding_boxes"]
-
-    return apply
-
-
-eval_ds = tfds.load("voc/2007", split="test", with_info=False, shuffle_files=True)
-eval_ds = eval_ds.map(
-    proc_eval_fn("xywh", [640, 640, 3]), num_parallel_calls=tf.data.AUTOTUNE
-)
-eval_ds = eval_ds.apply(
-    tf.data.experimental.dense_to_ragged_batch(BATCH_SIZE, drop_remainder=True)
-)
-eval_ds = eval_ds.prefetch(tf.data.AUTOTUNE)
-
-keras_cv_metrics = model.evaluate(eval_ds, return_dict=True)
-print("Metrics:", keras_cv_metrics)
