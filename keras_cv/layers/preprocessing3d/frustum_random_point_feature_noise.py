3957a39f531186d3d71f6dd3c21922b82c9ab8e1
lengzhaoqi <lengzhaoqi@Gmail.com>
2022-12-09 16:05:22 -0800

    Introduce frustum-based point feature noise. (#1098)
    * Add base augmentation layer for 3D preception.
    
    * Fix format.
    
    * Add copyright.
    
    * Minor change.
    
    * revert the minor change in the test file.
    
    * 1. Add global_z_rotation data augmentation.
    2. Add wrap_angle_rad helper function.
    
    * Auto format.
    
    * 1. Standardize POINT_CLOUDS and BOUNDING_BOXES
    2. Change rads -> radians
    3. Good point. We could add X or Y rotation as future work. For this PR, let's check in Z rotation first.
    
    * Format.
    
    * Delete base_augmentation_layer_3d.py
    
    * Delete base_augmentation_layer_3d_test.py
    
    * Standardize POINT_CLOUDS and BOUNDING_BOXES names.
    
    * Change GlobalZRotation to GlobalRandomZRotation
    
    * Support rotation along X, Y and Z axes.
    
    * format.
    
    * Change file name from global_rotation to global_random_rotation.
    
    * Add four more global data augmentations for 3d.
    
    * format.
    
    * Remove unused import.
    
    * Fix a typo in GlobalRandomFlippingY.
    
    * Support scaling x, y, and z.
    
    * Format.
    
    * update random scaling.
    
    * Modified based on comments.
    
    * follow up.
    
    * Fix a typo in random_scaling_test.py
    
    * Update.
    
    * Fix two typos.
    
    * Crop object.
    
    * Remove tf.print in test.
    
    * Fix typos.
    
    * test failed when using is_within_box3d + map function
    
    * update.
    
    * Rename to group_points_by_bounding_boxes.
    
    * Update.
    
    * Update.
    
    * Format.
    
    * Format.
    
    * Fix a typo.
    
    * Update.
    
    * fix a typo.
    
    * Introducing randomly pasting object data augmentation.
    
    * Update.
    
    * Update.
    
    * update.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Swap background data augmentation.
    
    * Update.
    
    * Update.
    
    * Clean up.
    
    * Update.
    
    * Update
    
    * Random drop box augmentation.
    
    * Update.
    
    * Update.
    
    * Introduce Frustum drop points.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Update.
    
    * Fix typos and add augmentations to init.
    
    * Introduce Frustum point feature noise.
    
    * Update.
    
    * Update.
    
    * Update.
    
    Co-authored-by: Leng Zhaoqi <lengzhaoqi@google.com>

--- keras_cv/layers/preprocessing3d/frustum_random_point_feature_noise.py
+++ keras_cv/layers/preprocessing3d/frustum_random_point_feature_noise.py
@@ -20,24 +20,29 @@ from keras_cv.ops import point_cloud
 POINT_CLOUDS = base_augmentation_layer_3d.POINT_CLOUDS
 BOUNDING_BOXES = base_augmentation_layer_3d.BOUNDING_BOXES
 POINTCLOUD_LABEL_INDEX = base_augmentation_layer_3d.POINTCLOUD_LABEL_INDEX
+POINTCLOUD_FEATURE_INDEX = base_augmentation_layer_3d.POINTCLOUD_FEATURE_INDEX
 
 
-class FrustumRandomDroppingPoints(base_augmentation_layer_3d.BaseAugmentationLayer3D):
-    """A preprocessing layer which randomly drops point within a randomly generated frustum during training.
+class FrustumRandomPointFeatureNoise(
+    base_augmentation_layer_3d.BaseAugmentationLayer3D
+):
+    """A preprocessing layer which randomly add noise to point features within a randomly generated frustum during training.
 
     This layer will randomly select a point from the point cloud as the center of a frustum then generate a frustum based
-    on r_distance, theta_width, and phi_width. Points inside the selected frustum are randomly dropped (setting all features to zero)
-    based on drop_rate.
+    on r_distance, theta_width, and phi_width. Uniformly sampled features noise from [1-max_noise_level, 1+max_noise_level] will be multiplied
+    to points inside the selected frustum. Here, we perturbe point features other than (x, y, z, class).
     The point_clouds tensor shape must be specific and cannot be dynamic.
-    During inference time, the output will be identical to input. Call the layer with `training=True` to drop the input points.
+    During inference time, the output will be identical to input. Call the layer with `training=True` to add noise to the input points.
 
     Input shape:
       point_clouds: 3D (multi frames) float32 Tensor with shape
         [num of frames, num of points, num of point features].
-        The first 5 features are [x, y, z, class, range].
+        The first 4 features are [x, y, z, class, additional features].
       bounding_boxes: 3D (multi frames) float32 Tensor with shape
-        [num of frames, num of boxes, num of box features].
-        The first 7 features are [x, y, z, dx, dy, dz, phi].
+        [num of frames, num of boxes, num of box features]. Boxes are expected
+        to follow the CENTER_XYZ_DXDYDZ_PHI format. Refer to
+        https://github.com/keras-team/keras-cv/blob/master/keras_cv/bounding_box_3d/formats.py
+        for more details on supported bounding box formats.
 
     Output shape:
       A dictionary of Tensors with the same shape as input Tensors.
@@ -46,10 +51,13 @@ class FrustumRandomDroppingPoints(base_augmentation_layer_3d.BaseAugmentationLay
       r_distance: A float scalar sets the starting distance of a frustum.
       theta_width: A float scalar sets the theta width of a frustum.
       phi_width: A float scalar sets the phi width of a frustum.
-      drop_rate: A float scalar sets the probability threshold for dropping the points.
+      max_noise_level: A float scalar sets the sampled feature noise range [1-max_noise_level, 1+max_noise_level].
+
     """
 
-    def __init__(self, r_distance, theta_width, phi_width, drop_rate=None, **kwargs):
+    def __init__(
+        self, r_distance, theta_width, phi_width, max_noise_level=None, **kwargs
+    ):
         super().__init__(**kwargs)
 
         if r_distance < 0:
@@ -58,28 +66,28 @@ class FrustumRandomDroppingPoints(base_augmentation_layer_3d.BaseAugmentationLay
             raise ValueError("theta_width must be >=0.")
         if phi_width < 0:
             raise ValueError("phi_width must be >=0.")
-        drop_rate = drop_rate if drop_rate else 0.0
-        if drop_rate > 1:
-            raise ValueError("drop_rate must be <=1.")
+        max_noise_level = max_noise_level if max_noise_level else 0.0
+        if max_noise_level < 0 or max_noise_level > 1:
+            raise ValueError("max_noise_level must be >=0 and <=1.")
 
         self._r_distance = r_distance
         self._theta_width = theta_width
         self._phi_width = phi_width
-        keep_probability = 1 - drop_rate
-        self._keep_probability = keep_probability
+        self._max_noise_level = max_noise_level
 
     def get_random_transformation(self, point_clouds, **kwargs):
         # Randomly select a point from the first frame as the center of the frustum.
-        num_valid_points = tf.cast(
-            tf.math.reduce_sum(point_clouds[0, :, POINTCLOUD_LABEL_INDEX]), tf.int32
-        )
+        valid_points = point_clouds[0, :, POINTCLOUD_LABEL_INDEX] > 0
+        num_valid_points = tf.math.reduce_sum(tf.cast(valid_points, tf.int32))
         randomly_select_point_index = tf.random.uniform(
             (), minval=0, maxval=num_valid_points, dtype=tf.int32
         )
         randomly_select_frustum_center = point_clouds[
             0, randomly_select_point_index, :POINTCLOUD_LABEL_INDEX
         ]
-        num_frames, num_points, _ = point_clouds.get_shape().as_list()
+        tf.print(("khk", randomly_select_frustum_center))
+
+        num_frames, num_points, num_features = point_clouds.get_shape().as_list()
         frustum_mask = []
         for f in range(num_frames):
             frustum_mask.append(
@@ -92,19 +100,25 @@ class FrustumRandomDroppingPoints(base_augmentation_layer_3d.BaseAugmentationLay
                 )[tf.newaxis, :, tf.newaxis]
             )
         frustum_mask = tf.concat(frustum_mask, axis=0)
-        random_point_mask = (
-            self._random_generator.random_uniform(
-                [num_frames, num_points, 1], minval=0.0, maxval=1
-            )
-            < self._keep_probability
+        feature_noise = tf.random.uniform(
+            [num_frames, num_points, num_features - POINTCLOUD_FEATURE_INDEX],
+            minval=1 - self._max_noise_level,
+            maxval=1 + self._max_noise_level,
         )
-        # Do not drop points outside the frustum mask.
-        random_point_mask = tf.where(~frustum_mask, True, random_point_mask)
-        return {"point_mask": random_point_mask}
+        noise = tf.concat(
+            [
+                tf.ones([num_frames, num_points, POINTCLOUD_FEATURE_INDEX]),
+                feature_noise,
+            ],
+            axis=-1,
+        )
+        # Do add feature noise outside the frustum mask.
+        random_point_noise = tf.where(~frustum_mask, 1.0, noise)
+        return {"point_noise": random_point_noise}
 
     def augment_point_clouds_bounding_boxes(
         self, point_clouds, bounding_boxes, transformation, **kwargs
     ):
-        point_mask = transformation["point_mask"]
-        point_clouds = tf.where(point_mask, point_clouds, 0.0)
+        point_noise = transformation["point_noise"]
+        point_clouds *= point_noise
         return (point_clouds, bounding_boxes)
