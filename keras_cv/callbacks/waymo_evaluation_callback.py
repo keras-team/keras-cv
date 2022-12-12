9bee0428230fbdd73aa6dab9c3448a8e4aa92181
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2022-12-12 15:23:12 -0700

    Callback impl

--- keras_cv/callbacks/waymo_evaluation_callback.py
+++ keras_cv/callbacks/waymo_evaluation_callback.py
@@ -14,76 +14,66 @@
 import six
 import tensorflow as tf
 from keras.callbacks import Callback
+from waymo_open_dataset.metrics.python.wod_detection_evaluator import (
+    WODDetectionEvaluator,
+)
 
-from keras_cv import bounding_box
-from waymo_open_dataset.metrics.python.wod_detection_evaluator import WODDetectionEvaluator
+from keras_cv.bounding_box_3d import CENTER_XYZ_DXDYDZ_PHI
 
 
 class WaymoEvaluationCallback(Callback):
-    def __init__(self, validation_data, config, **kwargs):
+    def __init__(self, validation_data, config=None, **kwargs):
+        """Creates a callback to evaluate Waymo Open Dataset (WOD) metrics on a
+        validation dataset.
+
+        Args:
+            validation_data: a tf.data.Dataset containing validation data. Entries
+                should have the form ```{"point_clouds": point_clouds,
+                "bounding_boxes": bounding_boxes}```.
+            config: an optional `metrics_pb2.Config` object from WOD to specify
+                what metrics should be evaluated.
+        """
         self.model = None
         self.val_data = validation_data
-        self.config = config
+        self.evaluator = WODDetectionEvaluator(config=config)
         super().__init__(**kwargs)
 
     def on_epoch_end(self, epoch, logs=None):
         logs = logs or {}
 
-        for i, batch in enumerate(self.val_data):
-            gt_i, preds_i = self._eval_batch(batch, i)
+        frame_id = 0
+        for batch in self.val_data:
+            gt_i, preds_i, batch_size = self._eval_batch(batch, frame_id)
+            frame_id += batch_size
             evaluator.update_state(gt_i, preds_i)
 
         metrics = evaluator.evaluate()
 
         logs.update(metrics)
 
-    def _eval_batch(self, batch, index):
-        # images, y = batch
-        # gt_boxes = y["gt_boxes"]
-        # gt_classes = y["gt_classes"]
-        # batch_size = images.shape[0]
-        # height = images.shape[1]
-        # width = images.shape[2]
-        #
-        # gt_boxes = bounding_box.convert_format(
-        #     gt_boxes, source=self.bounding_box_format, target="yxyx"
-        # )
-        #
-        # source_ids = tf.strings.join(
-        #     [
-        #         tf.strings.as_string(tf.tile(tf.constant([index]), [batch_size])),
-        #         tf.strings.as_string(
-        #             tf.linspace(1, batch_size, batch_size), precision=0
-        #         ),
-        #     ],
-        #     separator="/",
-        # )
-        #
-        # ground_truth = {}
-        # ground_truth["source_id"] = source_ids
-        # ground_truth["height"] = tf.tile(tf.constant([height]), [batch_size])
-        # ground_truth["width"] = tf.tile(tf.constant([width]), [batch_size])
-        #
-        # num_dets = gt_classes.get_shape().as_list()[1]
-        # ground_truth["num_detections"] = tf.tile(tf.constant([num_dets]), [batch_size])
-        # ground_truth["boxes"] = gt_boxes
-        # ground_truth["classes"] = gt_classes
-        #
-        # y_pred = self.model.predict(images)
-        # y_pred = bounding_box.convert_format(
-        #     y_pred, source=self.bounding_box_format, target="yxyx"
-        # )
-        #
-        # predictions = {}
-        # predictions["num_detections"] = y_pred.row_lengths()
-        # y_pred = y_pred.to_tensor(-1)
-        #
-        # predictions["source_id"] = source_ids
-        # predictions["detection_boxes"] = y_pred[:, :, :4]
-        # predictions["detection_classes"] = y_pred[:, :, 4]
-        # predictions["detection_scores"] = y_pred[:, :, 5]
+    def _eval_batch(self, batch, frame_id):
+        point_clouds = batch["point_clouds"]
+        boxes = batch["bounding_boxes"]
+        batch_size = boxes.shape[0]
+
+        frame_ids = tf.linspace(frame_id, frame_id + batch_size - 1, batch_size)
 
         ground_truth = {}
+        ground_truth["ground_truth_frame_id"] = frame_ids
+        ground_truth["ground_truth_bbox"] = boxes[:, :: CENTER_XYZ_DXDYDZ_PHI.DZ + 1]
+        ground_truth["ground_truth_type"] = boxes[:, :, CENTER_XYZ_DXDYDZ_PHI.CLASS]
+        ground_truth["ground_truth_difficulty"] = boxes[
+            :, :, CENTER_XYZ_DXDYDZ_PHI.CLASS + 1
+        ]
+
+        y_pred = self.model.predict_on_batch(point_clouds)
+
         predictions = {}
+
+        predictions["prediction_frame_id"] = source_ids
+        predictions["prediction_bbox"] = y_pred[:, :, : CENTER_XYZ_DXDYDZ_PHI.DZ + 1]
+        predictions["prediction_type"] = y_pred[:, :, CENTER_XYZ_DXDYDZ_PHI.CLASS]
+        predictions["prediction_score"] = y_pred[:, :, CENTER_XYZ_DXDYDZ_PHI.CLASS + 1]
+        predictions["prediction_overlap_nlz"] = tf.zeros(y_pred.shape[:-1])
 
         return ground_truth, predictions
--- keras_cv/callbacks/waymo_evaluation_callback_test.py
+++ keras_cv/callbacks/waymo_evaluation_callback_test.py
@@ -11,3 +11,45 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import tensorflow as tf
+
+import keras_cv
+from keras_cv.callbacks import WaymoEvaluationCallback
+from keras_cv.metrics.coco.pycoco_wrapper import METRIC_NAMES
+from keras_cv.models.object_detection.__test_utils__ import _create_bounding_box_dataset
+
+NUM_RECORDS = 10
+POINT_FEATURES = 3
+NUM_BOXES = 2
+BOX_FEATURES = 8
+
+METRIC_KEYS = [
+    "average_precision",
+    "average_precision_ha_weighted",
+    "precision_recall",
+    "precision_recall_ha_weighted",
+    "breakdown",
+]
+
+
+class WaymoEvaluationCallbackTest(tf.test.TestCase):
+    def test_model_fit(self):
+        # Silly hypothetical model
+        model = keras.Sequential(layers=[layers.Dense(BOX_FEATURES)])
+        model.compile(optimizer="adam", loss="mse")
+
+        fake_dataset = tf.data.Dataset.from_tensor_slices(
+            (
+                {
+                    "point_clouds": tf.random.normal((NUM_RECORDS, POINT_FEATURES)),
+                    "bounding_boxes": tf.random.normal(
+                        NUM_RECORDS, NUM_BOXES, BOX_FEATURES
+                    ),
+                }
+            )
+        )
+
+        callback = WaymoEvaluationCallback(validation_data=fake_dataset)
+        history = model.fit(fake_dataset, callbacks=[callback])
+
+        self.assertAllInSet(METRIC_KEYS, history.history.keys())
