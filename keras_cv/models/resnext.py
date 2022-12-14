238b77dcb15decb64a23a941661c90bfc7e27b20
Vision <gitesh.ch.0912@gmail.com>
2022-12-14 18:38:20 +0530

    Update

--- keras_cv/models/resnext.py
+++ keras_cv/models/resnext.py
@@ -15,20 +15,42 @@
 import tensorflow as tf
 
 
-def GroupConv2D(inputs, filters, kernel_size, strides, cardinality):
-    groups = []
-    grp_ch = int(filters / cardinality)
-    for i in range(cardinality):
-        x = tf.keras.layers.Lambda(lambda z: z[:, :, :, i * grp_ch:(i + 1) * grp_ch])(inputs)
-        x = tf.keras.layers.Conv2D(filters, kernel_size, strides=strides, padding="same", kernel_initializer="he_normal")(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.Activation('relu')(x)
+class GroupConv2D(tf.keras.layers.Layer):
+    def __init__(self, input_channels, output_channels, groups, kernel_size, strides=(1, 1), padding='valid', **kwargs):
+        super(GroupConv2D, self).__init__()
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = padding
+        self.group_in_num = input_channels // groups
+        self.group_out_num = output_channels // groups
+        self.conv_list = []
+        for i in range(self.groups):
+            self.conv_list.append(tf.keras.layers.Conv2D(filters=self.group_out_num, kernel_size=kernel_size, strides=strides,
+                                                         padding=padding,
+                                                         **kwargs))
 
-        x = tf.keras.layers.Conv2D(grp_ch, kernel_size, strides=strides, padding="same", kernel_initializer="he_normal")(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.Activation('relu')(x)
-        groups.append(x)
+    def call(self, inputs, **kwargs):
+        feature_map_list = []
+        for i in range(self.groups):
+            x_i = self.conv_list[i](inputs[:, :, :, i*self.group_in_num: (i + 1) * self.group_in_num])
+            feature_map_list.append(x_i)
+        out = tf.concat(feature_map_list, axis=-1)
+        return out                                                     
 
-    concatenate = tf.keras.layers.concatenate(groups, axis=-1)
-    return concatenate
 
+def ConvBlock(inputs, filters, kernel_size, strides, padding):
+    x = tf.keras.layers.Conv2D(filters=filters,kernel_size=kernel_size,strides=strides,padding=padding)(inputs)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.nn.relu(x)
+    return x
+
+def ResNeXt_BottleNeck(inputs, filters, strides, groups):
+    x = ConvBlock(inputs, filters, (1,1), 1, "same")
+    x = GroupConv2D(input_channels=filters,output_channels=filters, kernel_size=(3, 3),
+                    strides=strides,padding="same",groups=groups)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.nn.relu(x)
+    x = ConvBlock(x, 2 * filters, (1,1), 1, "same")
+    shortcut = ConvBlock(inputs, filters = 2 * filters, kernel_size=(1,1), strides=strides, padding="same")
+    outputs = tf.keras.layers.add([x, shortcut])
+    return outputs
