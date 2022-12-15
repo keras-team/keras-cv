aad5cabd2842a64ebd8d3cf3fbafab9589baccf0
Luke Wood <lukewoodcs@gmail.com>
2022-12-15 19:05:18 +0000

    Update training script to work

--- examples/training/object_detection/pascal_voc/retina_net.py
+++ examples/training/object_detection/pascal_voc/retina_net.py
@@ -191,12 +191,10 @@ model.compile(
     box_loss="smoothl1",
     optimizer=optimizer,
     metrics=[
-        keras_cv.metrics.COCORecall(
-            bounding_box_format="xywh", class_ids=range(20)
-        ),
+        keras_cv.metrics.COCORecall(bounding_box_format="xywh", class_ids=range(20)),
         keras_cv.metrics.COCOMeanAveragePrecision(
             bounding_box_format="xywh", class_ids=range(20)
-        )
+        ),
     ],
 )
 
