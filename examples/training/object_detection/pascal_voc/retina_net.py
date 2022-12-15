b26e1dc643fcbd66b8947b261b773e6dc233bdf1
Luke Wood <lukewoodcs@gmail.com>
2022-12-15 18:59:08 +0000

    Return to COCO metrics

--- examples/training/object_detection/pascal_voc/retina_net.py
+++ examples/training/object_detection/pascal_voc/retina_net.py
@@ -49,20 +49,10 @@ flags.DEFINE_string(
 FLAGS = flags.FLAGS
 FLAGS(sys.argv)
 
-# parameters from RetinaNet [paper](https://arxiv.org/abs/1708.02002)
-
-# Try to detect an available TPU. If none is present, default to MirroredStrategy
-try:
-    tpu = tf.distribute.cluster_resolver.TPUClusterResolver.connect()
-    strategy = tf.distribute.TPUStrategy(tpu)
-except ValueError:
-    # MirroredStrategy is best for a single machine with one or multiple GPUs
-    strategy = tf.distribute.MirroredStrategy()
-
 BATCH_SIZE = 4
-GLOBAL_BATCH_SIZE = BATCH_SIZE * strategy.num_replicas_in_sync
+GLOBAL_BATCH_SIZE = BATCH_SIZE * 1
 BASE_LR = 0.01 * GLOBAL_BATCH_SIZE / 16
-print("Number of accelerators: ", strategy.num_replicas_in_sync)
+print("Number of accelerators: ", 1)
 print("Global Batch Size: ", GLOBAL_BATCH_SIZE)
 
 IMG_SIZE = 640
@@ -91,16 +81,16 @@ def unpackage_inputs(bounding_box_format):
         )
         bounding_boxes = tf.concat([gt_boxes, gt_classes], axis=-1)
         return {"images": image, "bounding_boxes": bounding_boxes}
+
     return apply
 
-train_ds = train_ds.map(
-    unpackage_inputs("xywh"), num_parallel_calls=tf.data.AUTOTUNE
-)
+
+train_ds = train_ds.map(unpackage_inputs("xywh"), num_parallel_calls=tf.data.AUTOTUNE)
 train_ds = train_ds.apply(
     tf.data.experimental.dense_to_ragged_batch(GLOBAL_BATCH_SIZE, drop_remainder=True)
 )
 
-train_ds = train_ds.shuffle(8 * strategy.num_replicas_in_sync)
+train_ds = train_ds.shuffle(8 * 1)
 train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
 
 eval_ds = eval_ds.map(
@@ -146,42 +136,47 @@ freeze the backbone before training.  When `include_rescaling=True` is set, inpu
 the model are expected to be in the range `[0, 255]`.
 """
 
+
 def unpackage_inputs(data):
-    return data['images'], data['bounding_boxes']
+    return data["images"], data["bounding_boxes"]
+
 
 train_ds = train_ds.map(unpackage_inputs, num_parallel_calls=tf.data.AUTOTUNE)
 eval_ds = eval_ds.map(unpackage_inputs, num_parallel_calls=tf.data.AUTOTUNE)
 
-with strategy.scope():
-    model = keras_cv.models.RetinaNet(
-        # number of classes to be used in box classification
-        classes=20,
-        # For more info on supported bounding box formats, visit
-        # https://keras.io/api/keras_cv/bounding_box/
-        bounding_box_format="xywh",
-        # KerasCV offers a set of pre-configured backbones
-        backbone="resnet50",
-        # Each backbone comes with multiple pre-trained weights
-        # These weights match the weights available in the `keras_cv.model` class.
-        backbone_weights="imagenet",
-        # include_rescaling tells the model whether your input images are in the default
-        # pixel range (0, 255) or if you have already rescaled your inputs to the range
-        # (0, 1).  In our case, we feed our model images with inputs in the range (0, 255).
-        include_rescaling=True,
-    )
-    # Fine-tuning a RetinaNet is as simple as setting backbone.trainable = False
-    model.backbone.trainable = False
-    optimizer = tf.optimizers.SGD(learning_rate=BASE_LR, global_clipnorm=10.0)
+model = keras_cv.models.RetinaNet(
+    # number of classes to be used in box classification
+    classes=20,
+    # For more info on supported bounding box formats, visit
+    # https://keras.io/api/keras_cv/bounding_box/
+    bounding_box_format="xywh",
+    # KerasCV offers a set of pre-configured backbones
+    backbone="resnet50",
+    # Each backbone comes with multiple pre-trained weights
+    # These weights match the weights available in the `keras_cv.model` class.
+    backbone_weights="imagenet",
+    # include_rescaling tells the model whether your input images are in the default
+    # pixel range (0, 255) or if you have already rescaled your inputs to the range
+    # (0, 1).  In our case, we feed our model images with inputs in the range (0, 255).
+    include_rescaling=True,
+)
+# Fine-tuning a RetinaNet is as simple as setting backbone.trainable = False
+model.backbone.trainable = False
+optimizer = tf.optimizers.SGD(learning_rate=BASE_LR, global_clipnorm=10.0)
 
 model.compile(
     classification_loss="focal",
     box_loss="smoothl1",
     optimizer=optimizer,
+    metrics=[
+        keras_cv.metrics.COCORecall(
+            bounding_box_format="xywh", class_ids=range(20)
+        ),
+        keras_cv.metrics.COCOMeanAveragePrecision(
+            bounding_box_format="xywh", class_ids=range(20)
+        )
+    ],
 )
-
-
-def convert_to_dict(images, boxes):
-    return images, {"gt_boxes": boxes[:, :, :4], "gt_classes": boxes[:, :, 4]}
 
 
 callbacks = [
@@ -189,11 +184,11 @@ callbacks = [
     keras.callbacks.ReduceLROnPlateau(patience=5),
     keras.callbacks.EarlyStopping(patience=10),
     keras.callbacks.ModelCheckpoint(CHECKPOINT_PATH, save_weights_only=True),
-    PyCOCOCallback(eval_ds.map(convert_to_dict), "xywh"),
 ]
 
 history = model.fit(
     train_ds,
+    validation_data=eval_ds.take(20),
     epochs=100,
     callbacks=callbacks,
 )
