5f512f1f82ec28a72c4dfb27b9ad4111a8d72a24
Vision <gitesh.ch.0912@gmail.com>
2022-12-20 00:09:28 +0530

    tests

--- keras_cv/models/vgg16_test.py
+++ keras_cv/models/vgg16_test.py
@@ -15,6 +15,36 @@
 import tensorflow as tf
 from absl.testing import parameterized
 
-from keras_cv.models import vgg19
+from keras_cv.models import vgg16
 
 from .models_test import ModelsTest
+
+MODEL_LIST = [
+    (vgg16.VGG16, 512, {}),
+]
+
+
+class VGG19Test(ModelsTest, tf.test.TestCase, parameterized.TestCase):
+    @parameterized.parameters(*MODEL_LIST)
+    def test_application_base(self, app, _, args):
+        super()._test_application_base(app, _, args)
+
+    @parameterized.parameters(*MODEL_LIST)
+    def test_application_with_rescaling(self, app, last_dim, args):
+        super()._test_application_with_rescaling(app, last_dim, args)
+
+    @parameterized.parameters(*MODEL_LIST)
+    def test_application_pooling(self, app, last_dim, args):
+        super()._test_application_pooling(app, last_dim, args)
+
+    @parameterized.parameters(*MODEL_LIST)
+    def test_application_variable_input_channels(self, app, last_dim, args):
+        super()._test_application_variable_input_channels(app, last_dim, args)
+
+    @parameterized.parameters(*MODEL_LIST)
+    def test_model_can_be_used_as_backbone(self, app, last_dim, args):
+        super()._test_model_can_be_used_as_backbone(app, last_dim, args)
+
+
+if __name__ == "__main__":
+    tf.test.main()
