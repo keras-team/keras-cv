55ab153b348db336a13ffe6cd3b64476b7638719
David Landup <david.landup.0@gmail.com>
2022-12-21 15:37:45 +0900

    fixtures

--- keras_cv/layers/fusedmbconv_test.py
+++ keras_cv/layers/fusedmbconv_test.py
@@ -11,17 +11,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import pytest
 import tensorflow as tf
 
 from keras_cv.layers.fusedmbconv import FusedMBConvBlock
 
 
 class FusedMBConvBlockTest(tf.test.TestCase):
+    @pytest.fixture(autouse=True)
+    def cleanup_global_session(self):
+        # Code before yield runs before the test
+        tf.config.set_soft_device_placement(False)
+        yield
+        # Reset soft device placement to not interfere with other unit test files
+        tf.config.set_soft_device_placement(True)
+        tf.keras.backend.clear_session()
+
     def test_same_input_output_shapes(self):
         inputs = tf.random.normal(shape=(1, 64, 64, 32), dtype=tf.float32)
         layer = FusedMBConvBlock(input_filters=32, output_filters=32)
 
-        output = layer(inputs, training=True)
+        output = layer(inputs)
         self.assertEquals(output.shape, [1, 64, 64, 32])
         self.assertLen(output, 1)
         self.assertTrue(isinstance(output, tf.Tensor))
@@ -30,7 +40,7 @@ class FusedMBConvBlockTest(tf.test.TestCase):
         inputs = tf.random.normal(shape=(1, 64, 64, 32), dtype=tf.float32)
         layer = FusedMBConvBlock(input_filters=32, output_filters=48)
 
-        output = layer(inputs, training=True)
+        output = layer(inputs)
         self.assertEquals(output.shape, [1, 64, 64, 48])
         self.assertLen(output, 1)
         self.assertTrue(isinstance(output, tf.Tensor))
@@ -39,7 +49,7 @@ class FusedMBConvBlockTest(tf.test.TestCase):
         inputs = tf.random.normal(shape=(1, 64, 64, 32), dtype=tf.float32)
         layer = FusedMBConvBlock(input_filters=32, output_filters=48, se_ratio=0.25)
 
-        output = layer(inputs, training=True)
+        output = layer(inputs)
         self.assertEquals(output.shape, [1, 64, 64, 48])
         self.assertLen(output, 1)
         self.assertTrue(isinstance(output, tf.Tensor))
