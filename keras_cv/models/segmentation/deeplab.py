5ebfd729fb0aed63d39245f8d2124e6e9b96878e
David Landup <david.landup.0@gmail.com>
2022-12-24 14:31:50 +0900

    backbone cleanup

--- keras_cv/models/segmentation/deeplab.py
+++ keras_cv/models/segmentation/deeplab.py
@@ -18,8 +18,6 @@ from tensorflow.keras import layers
 
 from keras_cv.layers.spatial_pyramid import SpatialPyramidPooling
 from keras_cv.models import utils
-from keras_cv.models.resnet_v2 import ResNetV2
-from keras_cv.models.weights import parse_weights
 
 BACKBONE_CONFIG = {
     "ResNet50V2": {
@@ -41,16 +39,7 @@ class DeepLabV3(keras.Model):
             the classes doesn't contain the background class, and the classes
             from the data should be represented by integers with range
             [0, classes).
-        include_rescaling: boolean, whether to Rescale the inputs. If set to True,
-            inputs will be passed through a `Rescaling(1/255.0)` layer.
-        backbone: an optional backbone network for the model. Can be a `tf.keras.layers.Layer`
-            instance. The supported pre-defined backbone models are:
-            1. "resnet50_v2", a ResNet50 V2 model
-            Default to 'resnet50_v2'.
-        backbone_weights: weights for the backbone model. one of `None` (random
-            initialization), a pretrained weight file path, or a reference to
-            pre-trained weights (e.g. 'imagenet/classification') (see available
-            pre-trained weights in weights.py)
+        backbone: an optional backbone network for the model. Should be a KerasCV model.
         weights: weights for the complete DeepLabV3 model. one of `None` (random
             initialization), a pretrained weight file path, or a reference to
             pre-trained weights (e.g. 'imagenet/classification') (see available
@@ -65,9 +54,7 @@ class DeepLabV3(keras.Model):
     def __init__(
         self,
         classes,
-        include_rescaling,
         backbone,
-        backbone_weights=None,
         spatial_pyramid_pooling=None,
         segmentation_head=None,
         segmentation_head_activation=None,
@@ -82,34 +69,10 @@ class DeepLabV3(keras.Model):
 
         x = inputs
 
-        if include_rescaling:
-            x = layers.Rescaling(1 / 255.0)(x)
-
-        if isinstance(backbone, str):
-            supported_premade_backbone = [
-                "resnet50_v2",
-            ]
-            if backbone not in supported_premade_backbone:
-                raise ValueError(
-                    "Supported premade backbones are: "
-                    f'{supported_premade_backbone}, received "{backbone}"'
-                )
-
-            if backbone == "resnet50_v2":
-                if backbone_weights and input_shape[-1] == 1:
-                    raise ValueError(
-                        "The input shape is set up for greyscale images with one channel, but backbone weights are trained on colored images and cannot be loaded."
-                    )
-                backbone = get_resnet_backbone(
-                    backbone_weights, include_rescaling, input_shape, **kwargs
-                )
-
-        else:
-            if not isinstance(backbone, tf.keras.layers.Layer):
-                raise ValueError(
-                    "Backbone need to be a `tf.keras.layers.Layer`, "
-                    f"received {backbone}"
-                )
+        if not isinstance(backbone, tf.keras.layers.Layer):
+            raise ValueError(
+                "Backbone need to be a `tf.keras.layers.Layer`, " f"received {backbone}"
+            )
 
         feature_map = backbone(x)
         if spatial_pyramid_pooling is None:
@@ -146,9 +109,7 @@ class DeepLabV3(keras.Model):
 
         # All references to `self` below this line
         self.classes = classes
-        self.include_rescaling = include_rescaling
         self.backbone = backbone
-        self.backbone_weights = backbone_weights
         self.spatial_pyramid_pooling = spatial_pyramid_pooling
         self.segmentation_head = segmentation_head
         self.segmentation_head_activation = segmentation_head_activation
@@ -156,29 +117,11 @@ class DeepLabV3(keras.Model):
     def get_config(self):
         return {
             "classes": self.classes,
-            "include_rescaling": self.include_rescaling,
             "backbone": self.backbone,
-            "backbone_weights": self.backbone_weights,
             "spatial_pyramid_pooling": self.spatial_pyramid_pooling,
             "segmentation_head": self.segmentation_head,
             "segmentation_head_activation": self.segmentation_head_activation,
         }
-
-
-def get_resnet_backbone(backbone_weights, include_rescaling, input_shape, **kwargs):
-    return ResNetV2(
-        stackwise_filters=BACKBONE_CONFIG["ResNet50V2"]["stackwise_filters"],
-        stackwise_blocks=BACKBONE_CONFIG["ResNet50V2"]["stackwise_blocks"],
-        stackwise_strides=BACKBONE_CONFIG["ResNet50V2"]["stackwise_strides"],
-        stackwise_dilations=BACKBONE_CONFIG["ResNet50V2"]["stackwise_dilations"],
-        include_rescaling=include_rescaling,
-        include_top=False,
-        name="resnet50v2",
-        input_shape=input_shape,
-        weights=parse_weights(backbone_weights, False, "resnet50v2"),
-        pooling=None,
-        **kwargs,
-    )
 
 
 @tf.keras.utils.register_keras_serializable(package="keras_cv")
