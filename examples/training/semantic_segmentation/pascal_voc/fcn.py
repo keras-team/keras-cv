550e9f96d0defc459697e21aa86e28ecfc880f9c
Vision <gitesh.ch.0912@gmail.com>
2022-12-27 02:05:48 +0530

    updated training script

--- examples/training/semantic_segmentation/pascal_voc/fcn.py
+++ examples/training/semantic_segmentation/pascal_voc/fcn.py
@@ -11,13 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-Title: Train an Semantic Segmentation Model on Pascal VOC 2012 using KerasCV
-Author: [tanzhenyu](https://github.com/tanzhenyu)
-Date created: 2022/10/25
-Last modified: 2022/10/25
-Description: Use KerasCV to train a FCN on Pascal VOC 2012.
-"""
 
 import sys
 
@@ -25,6 +18,8 @@ import tensorflow as tf
 from absl import flags
 from absl import logging
 
+import keras_cv
+import keras_cv.layers.preprocessing as preprocessing
 from keras_cv.datasets.pascal_voc.segmentation import load
 from keras_cv.models.segmentation.fcn import FCN
 
@@ -61,7 +56,7 @@ print("Number of accelerators: ", strategy.num_replicas_in_sync)
 
 # parameters from FasterRCNN [paper](https://arxiv.org/pdf/1506.01497.pdf)
 
-local_batch = 4
+local_batch = 20
 global_batch = local_batch * strategy.num_replicas_in_sync
 base_lr = 0.007 * global_batch / 16
 
@@ -73,66 +68,39 @@ base_lr = 0.007 * global_batch / 16
 train_ds = load(split="sbd_train", data_dir=None)
 eval_ds = load(split="sbd_eval", data_dir=None)
 
-resize_layer = tf.keras.layers.Resizing(512, 512, interpolation="nearest")
 
-image_size = [512, 512, 3]
+def resize_image(img, cls_seg, augment=False):
+    img = tf.keras.layers.Resizing(224, 224, interpolation="nearest")(img)
+    cls_seg = tf.keras.layers.Resizing(224, 224, interpolation="nearest")(cls_seg)
+    cls_seg = tf.cast(cls_seg, tf.int32)
 
-
-# TODO(tanzhenyu): move to KPL.
-def flip_fn(image, cls_seg):
-    if tf.random.uniform([], minval=0, maxval=1, dtype=tf.float32) > 0.5:
-        image = tf.image.flip_left_right(image)
-        cls_seg = tf.reverse(cls_seg, axis=[1])
-    return image, cls_seg
+    inputs = {"images": img, "segmentation_masks": cls_seg}
+    return inputs["images"], inputs["segmentation_masks"]
 
 
-def proc_train_fn(examples):
+def process(examples):
     image = examples.pop("image")
-    image = tf.cast(image, tf.float32)
-    image = resize_layer(image)
     cls_seg = examples.pop("class_segmentation")
-    cls_seg = tf.cast(cls_seg, tf.float32)
-    cls_seg = resize_layer(cls_seg)
-    image, cls_seg = flip_fn(image, cls_seg)
-    cls_seg = tf.cast(cls_seg, tf.uint8)
+
+    image, cls_seg = resize_image(image, cls_seg)
+
     sample_weight = tf.equal(cls_seg, 255)
     zeros = tf.zeros_like(cls_seg)
     cls_seg = tf.where(sample_weight, zeros, cls_seg)
     return image, cls_seg
 
+train_ds = train_ds.map(lambda x: process(x, augment=True), num_parallel_calls=tf.data.AUTOTUNE)
+train_ds = train_ds.batch(16, drop_remainder=True)
 
-def proc_eval_fn(examples):
-    image = examples.pop("image")
-    image = tf.cast(image, tf.float32)
-    image = resize_layer(image)
-    cls_seg = examples.pop("class_segmentation")
-    cls_seg = tf.cast(cls_seg, tf.float32)
-    cls_seg = resize_layer(cls_seg)
-    cls_seg = tf.cast(cls_seg, tf.uint8)
-    sample_weight = tf.equal(cls_seg, 255)
-    zeros = tf.zeros_like(cls_seg)
-    cls_seg = tf.where(sample_weight, zeros, cls_seg)
-    return image, cls_seg
-
-
-train_ds = train_ds.map(proc_train_fn, num_parallel_calls=tf.data.AUTOTUNE)
-train_ds = train_ds.batch(global_batch, drop_remainder=True)
-
-eval_ds = eval_ds.map(proc_eval_fn, num_parallel_calls=tf.data.AUTOTUNE)
-eval_ds = eval_ds.batch(global_batch, drop_remainder=True)
+eval_ds = eval_ds.map(lambda x: process(x), num_parallel_calls=tf.data.AUTOTUNE)
+eval_ds = eval_ds.batch(16, drop_remainder=True)
 
 train_ds = train_ds.shuffle(8)
-train_ds = train_ds.prefetch(2)
+train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
 
 with strategy.scope():
-    lr_decay = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
-        boundaries=[30000 * 16 / global_batch],
-        values=[base_lr, 0.1 * base_lr],
-    )
-    model = FCN(classes=21, backbone="vgg16", model_architecture="fcn8s")
-    optimizer = tf.keras.optimizers.SGD(
-        learning_rate=lr_decay, momentum=0.9, clipnorm=10.0
-    )
+    model = FCN(classes=21, input_shape=(224, 224, 3), include_rescaling=True, backbone='vgg16', return_mask=False, model_architecture='fcn8s')
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
     # ignore 255 as the class for semantic boundary.
     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(ignore_class=255)
     metrics = [
