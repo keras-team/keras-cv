dc593c61d93cda9a0e91047d57b23ec6177c182a
David Landup <david.landup.0@gmail.com>
2023-01-01 21:07:24 +0900

    test with tanzhenyu's augmentations

--- examples/training/semantic_segmentation/pascal_voc/deeplab_v3.py
+++ examples/training/semantic_segmentation/pascal_voc/deeplab_v3.py
@@ -97,28 +97,144 @@ resize_layer = tf.keras.layers.Resizing(512, 512, interpolation="nearest")
 image_size = [512, 512, 3]
 
 
-# TODO(tanzhenyu): move to KPL.
+# TODO: migrate to KPL.
+def resize_and_crop_image(
+    image,
+    desired_size,
+    padded_size,
+    aug_scale_min=1.0,
+    aug_scale_max=1.0,
+    seed=1,
+    method=tf.image.ResizeMethod.BILINEAR,
+):
+    with tf.name_scope("resize_and_crop_image"):
+        image_size = tf.cast(tf.shape(image)[0:2], tf.float32)
+
+        random_jittering = aug_scale_min != 1.0 or aug_scale_max != 1.0
+
+        if random_jittering:
+            random_scale = tf.random.uniform(
+                [], aug_scale_min, aug_scale_max, seed=seed
+            )
+            scaled_size = tf.round(random_scale * desired_size)
+        else:
+            scaled_size = desired_size
+
+        scale = tf.minimum(
+            scaled_size[0] / image_size[0], scaled_size[1] / image_size[1]
+        )
+        scaled_size = tf.round(image_size * scale)
+
+        # Computes 2D image_scale.
+        image_scale = scaled_size / image_size
+
+        # Selects non-zero random offset (x, y) if scaled image is larger than
+        # desired_size.
+        if random_jittering:
+            max_offset = scaled_size - desired_size
+            max_offset = tf.where(
+                tf.less(max_offset, 0), tf.zeros_like(max_offset), max_offset
+            )
+            offset = max_offset * tf.random.uniform(
+                [
+                    2,
+                ],
+                0,
+                1,
+                seed=seed,
+            )
+            offset = tf.cast(offset, tf.int32)
+        else:
+            offset = tf.zeros((2,), tf.int32)
+
+        scaled_image = tf.image.resize(
+            image, tf.cast(scaled_size, tf.int32), method=method
+        )
+
+        if random_jittering:
+            scaled_image = scaled_image[
+                offset[0] : offset[0] + desired_size[0],
+                offset[1] : offset[1] + desired_size[1],
+                :,
+            ]
+
+        output_image = tf.image.pad_to_bounding_box(
+            scaled_image, 0, 0, padded_size[0], padded_size[1]
+        )
+
+        image_info = tf.stack(
+            [
+                image_size,
+                tf.constant(desired_size, dtype=tf.float32),
+                image_scale,
+                tf.cast(offset, tf.float32),
+            ]
+        )
+        return output_image, image_info
+
+def resize_and_crop_masks(masks, image_scale, output_size, offset):
+  with tf.name_scope('resize_and_crop_masks'):
+    mask_size = tf.cast(tf.shape(masks)[1:3], tf.float32)
+    num_channels = tf.shape(masks)[3]
+    # Pad masks to avoid empty mask annotations.
+    masks = tf.concat([
+        tf.zeros([1, mask_size[0], mask_size[1], num_channels],
+                 dtype=masks.dtype), masks
+    ],
+                      axis=0)
+
+    scaled_size = tf.cast(image_scale * mask_size, tf.int32)
+    scaled_masks = tf.image.resize(
+        masks, scaled_size, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    offset = tf.cast(offset, tf.int32)
+    scaled_masks = scaled_masks[
+        :,
+        offset[0]:offset[0] + output_size[0],
+        offset[1]:offset[1] + output_size[1],
+        :]
+
+    output_masks = tf.image.pad_to_bounding_box(
+        scaled_masks, 0, 0, output_size[0], output_size[1])
+    # Remove padding.
+    output_masks = output_masks[1::]
+    return output_masks
+
+def resize_fn(image, cls_seg):
+    image, image_info = resize_and_crop_image(
+        image, image_size[:2], image_size[:2], 0.5, 2.0
+    )
+    image_scale = image_info[2, :]
+    offset = image_info[3, :]
+    cls_seg += 1
+    cls_seg = cls_seg[tf.newaxis, ...]
+    cls_seg = resize_and_crop_masks(cls_seg, image_scale, image_size[:2], offset)
+    cls_seg = cls_seg[0]
+    cls_seg -= 1
+    cls_seg = tf.where(
+        tf.equal(cls_seg, -1), 255 * tf.ones_like(cls_seg), cls_seg
+    )
+    return image, cls_seg
+
 def flip_fn(image, cls_seg):
     if tf.random.uniform([], minval=0, maxval=1, dtype=tf.float32) > 0.5:
         image = tf.image.flip_left_right(image)
         cls_seg = tf.reverse(cls_seg, axis=[1])
     return image, cls_seg
 
-
 def proc_train_fn(examples):
     image = examples.pop("image")
     image = tf.cast(image, tf.float32)
-    image = resize_layer(image)
+    # image = resize_layer(image)
     cls_seg = examples.pop("class_segmentation")
     cls_seg = tf.cast(cls_seg, tf.float32)
-    cls_seg = resize_layer(cls_seg)
+    # cls_seg = resize_layer(cls_seg)
+    image, cls_seg = resize_fn(image, cls_seg)
     image, cls_seg = flip_fn(image, cls_seg)
     cls_seg = tf.cast(cls_seg, tf.uint8)
     sample_weight = tf.equal(cls_seg, 255)
     zeros = tf.zeros_like(cls_seg)
     cls_seg = tf.where(sample_weight, zeros, cls_seg)
     return image, cls_seg
-
 
 def proc_eval_fn(examples):
     image = examples.pop("image")
@@ -133,12 +249,11 @@ def proc_eval_fn(examples):
     cls_seg = tf.where(sample_weight, zeros, cls_seg)
     return image, cls_seg
 
-
 train_ds = train_ds.map(proc_train_fn, num_parallel_calls=tf.data.AUTOTUNE)
-train_ds = train_ds.batch(global_batch, drop_remainder=True)
+train_ds = train_ds.batch(global_batch)
 
 eval_ds = eval_ds.map(proc_eval_fn, num_parallel_calls=tf.data.AUTOTUNE)
-eval_ds = eval_ds.batch(global_batch, drop_remainder=True)
+eval_ds = eval_ds.batch(global_batch)
 
 train_ds = train_ds.shuffle(8)
 train_ds = train_ds.prefetch(2)
