820b7445ddadc2ac7b83ed809beec665ba7827d5
Luke Wood <lukewoodcs@gmail.com>
2023-01-05 08:58:44 -0800

    Fix bounding box utilities test

--- keras_cv/bounding_box/utils_test.py
+++ keras_cv/bounding_box/utils_test.py
@@ -59,135 +59,97 @@ class BoundingBoxUtilTest(tf.test.TestCase):
         # Test xyxy format unbatched
         height = 256
         width = 256
-        bounding_boxes = tf.convert_to_tensor(
-            [[257, 257, 400, 400, 0], [100, 100, 300, 300, 0]]
-        )
+        bounding_boxes = {
+            "boxes": tf.convert_to_tensor([[257, 257, 400, 400], [100, 100, 300, 300]]),
+            "classes": tf.convert_to_tensor([0, 0]),
+        }
         image = tf.ones(shape=(height, width, 3))
         bounding_boxes = bounding_box.clip_to_image(
             bounding_boxes, bounding_box_format="xyxy", images=image
         )
+
         self.assertAllEqual(
-            bounding_boxes,
-            tf.convert_to_tensor([[-1, -1, -1, -1, -1], [100, 100, 256, 256, 0]]),
+            bounding_boxes["boxes"],
+            tf.convert_to_tensor([[-1, -1, -1, -1], [100, 100, 256, 256]]),
+        ),
+        self.assertAllEqual(
+            bounding_boxes["classes"],
+            tf.convert_to_tensor([-1, 0]),
         )
 
     def test_clip_to_image_filters_fully_out_bounding_boxes_negative_area(self):
         # Test xyxy format unbatched
         height = 256
         width = 256
-        bounding_boxes = tf.convert_to_tensor(
-            [[0, float("NaN"), 100, 100, 0], [100, 100, 300, 300, 0]]
-        )
+        bounding_boxes = {
+            "boxes": tf.convert_to_tensor([[110, 120, 100, 100], [100, 100, 300, 300]]),
+            "classes": [0, 0],
+        }
         image = tf.ones(shape=(height, width, 3))
         bounding_boxes = bounding_box.clip_to_image(
             bounding_boxes, bounding_box_format="xyxy", images=image
         )
         self.assertAllEqual(
-            bounding_boxes,
-            tf.convert_to_tensor([[-1, -1, -1, -1, -1], [100, 100, 256, 256, 0]]),
+            bounding_boxes["boxes"],
+            tf.convert_to_tensor(
+                [
+                    [
+                        -1,
+                        -1,
+                        -1,
+                        -1,
+                    ],
+                    [
+                        100,
+                        100,
+                        256,
+                        256,
+                    ],
+                ]
+            ),
+        )
+        self.assertAllEqual(
+            bounding_boxes["classes"],
+            tf.convert_to_tensor([-1, 0]),
         )
 
     def test_clip_to_image_filters_nans(self):
         # Test xyxy format unbatched
         height = 256
         width = 256
-        bounding_boxes = tf.convert_to_tensor(
-            [[257, 257, 100, 100, 0], [100, 100, 300, 300, 0]]
-        )
+        bounding_boxes = {
+            "boxes": tf.convert_to_tensor(
+                [[0, float("NaN"), 100, 100], [100, 100, 300, 300]]
+            ),
+            "classes": [0, 0],
+        }
         image = tf.ones(shape=(height, width, 3))
         bounding_boxes = bounding_box.clip_to_image(
             bounding_boxes, bounding_box_format="xyxy", images=image
         )
         self.assertAllEqual(
-            bounding_boxes,
-            tf.convert_to_tensor([[-1, -1, -1, -1, -1], [100, 100, 256, 256, 0]]),
+            bounding_boxes["boxes"],
+            tf.convert_to_tensor(
+                [
+                    [
+                        -1,
+                        -1,
+                        -1,
+                        -1,
+                    ],
+                    [
+                        100,
+                        100,
+                        256,
+                        256,
+                    ],
+                ]
+            ),
         )
-
-    def test_pad_with_sentinels(self):
-        bounding_boxes = tf.ragged.constant(
-            [[[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]], [[1, 2, 3, 4, 5]]]
+        self.assertAllEqual(
+            bounding_boxes["classes"],
+            tf.convert_to_tensor([-1, 0]),
         )
-        padded_bounding_boxes = bounding_box.pad_with_sentinels(bounding_boxes)
-        expected_output = tf.constant(
-            [
-                [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]],
-                [[1, 2, 3, 4, 5], [-1, -1, -1, -1, -1]],
-            ]
-        )
-        self.assertAllEqual(padded_bounding_boxes, expected_output)
-
-    def test_filter_sentinels(self):
-        bounding_boxes = tf.ragged.constant(
-            [[[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, -1]], [[1, 2, 3, 4, 5]]]
-        )
-        filtered_bounding_boxes = bounding_box.filter_sentinels(bounding_boxes)
-        expected_output = tf.ragged.constant(
-            [[[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]], [[1, 2, 3, 4, 5]]], ragged_rank=1
-        )
-        self.assertAllEqual(filtered_bounding_boxes, expected_output)
-
-    def test_filter_sentinels_unbatched(self):
-        bounding_boxes = tf.convert_to_tensor(
-            [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, -1]]
-        )
-        filtered_bounding_boxes = bounding_box.filter_sentinels(bounding_boxes)
-        expected_output = tf.convert_to_tensor(
-            [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]],
-        )
-        print(filtered_bounding_boxes, expected_output)
-        self.assertAllEqual(filtered_bounding_boxes, expected_output)
-
-    def test_filter_sentinels_tensor(self):
-        bounding_boxes = tf.convert_to_tensor(
-            [
-                [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]],
-                [[1, 2, 3, 4, 5], [1, 2, 3, 4, -1]],
-            ]
-        )
-        filtered_bounding_boxes = bounding_box.filter_sentinels(bounding_boxes)
-
-        expected_output = tf.ragged.constant(
-            [[[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]], [[1, 2, 3, 4, 5]]], ragged_rank=1
-        )
-        self.assertAllEqual(filtered_bounding_boxes, expected_output)
-
-    def test_pad_with_class_id_ragged(self):
-        bounding_boxes = tf.ragged.constant(
-            [[[1, 2, 3, 4], [1, 2, 3, 4]], [[1, 2, 3, 4]]]
-        )
-        padded_bounding_boxes = bounding_box.add_class_id(bounding_boxes)
-        expected_output = tf.ragged.constant(
-            [[[1, 2, 3, 4, 0], [1, 2, 3, 4, 0]], [[1, 2, 3, 4, 0]]]
-        )
-        self.assertAllEqual(padded_bounding_boxes, expected_output)
-
-    def test_pad_with_class_id_unbatched(self):
-        bounding_boxes = tf.convert_to_tensor([[1, 2, 3, 4], [1, 2, 3, 4]])
-        padded_bounding_boxes = bounding_box.add_class_id(bounding_boxes)
-        expected_output = tf.convert_to_tensor([[1, 2, 3, 4, 0], [1, 2, 3, 4, 0]])
-        self.assertAllEqual(padded_bounding_boxes, expected_output)
-
-    def test_pad_with_class_id_exists(self):
-        bounding_boxes = tf.ragged.constant(
-            [[[1, 2, 3, 4, 0], [1, 2, 3, 4, 0]], [[1, 2, 3, 4, 0]]]
-        )
-        with self.assertRaisesRegex(
-            ValueError,
-            "The number of values along the final axis of `bounding_boxes` is "
-            "expected to be 4. But got 5.",
-        ):
-            bounding_box.add_class_id(bounding_boxes)
-
-    def test_pad_with_class_id_wrong_rank(self):
-        bounding_boxes = tf.ragged.constant(
-            [[[[1, 2, 3, 4], [1, 2, 3, 4]], [[1, 2, 3, 4]]]]
-        )
-        with self.assertRaisesRegex(
-            ValueError,
-            f"`bounding_boxes` should be of rank 2 or 3. However "
-            f"add_class_id received `bounding_boxes` of rank={4}",
-        ):
-            bounding_box.add_class_id(bounding_boxes)
 
     def test_preserve_rel_util(self):
         target_format = "xyxy"
