aaba05edc37d5000bee30c44ba4f9ede6bd6edc7
Luke Wood <lukewoodcs@gmail.com>
2023-01-05 14:05:56 -0800

    Fix to ragged test

--- keras_cv/bounding_box/to_ragged_test.py
+++ keras_cv/bounding_box/to_ragged_test.py
@@ -32,16 +32,19 @@ class ToRaggedTest(tf.test.TestCase):
         self.assertEqual(bounding_boxes["boxes"][0].shape, [0, 4])
 
     def test_round_trip(self):
-        bounding_boxes = {
+        original = {
             "boxes": tf.constant(
-                [[[0, 0, 0, 0], [0, 0, 0, 0]], [[2, 3, 4, 5], [0, 1, 2, 3]]]
+                [[[0, 0, 0, 0], [-1, -1, -1, -1]], [[-1, -1, -1, -1], [-1, -1, -1, -1]]]
             ),
             "classes": tf.constant([[1, -1], [-1, -1]]),
         }
-        bounding_boxes = bounding_box.to_ragged(bounding_boxes)
+        bounding_boxes = bounding_box.to_ragged(original)
         bounding_boxes = bounding_box.to_dense(bounding_boxes, max_boxes=2)
 
         self.assertEqual(bounding_boxes["boxes"][1].shape, [2, 4])
         self.assertEqual(bounding_boxes["classes"][1].shape, [2])
         self.assertEqual(bounding_boxes["classes"][0].shape, [2])
         self.assertEqual(bounding_boxes["boxes"][0].shape, [2, 4])
+
+        self.assertAllEqual(bounding_boxes["boxes"], original["boxes"])
+        self.assertAllEqual(bounding_boxes["classes"], original["classes"])
