77ebb7a3ec2b173e6fb2fd9d5d552d46f12fb886
Luke Wood <lukewoodcs@gmail.com>
2023-01-06 09:33:31 -0800

    Update fasterRCNN with safety check

--- keras_cv/models/object_detection/faster_rcnn.py
+++ keras_cv/models/object_detection/faster_rcnn.py
@@ -388,12 +388,11 @@ class FasterRCNN(tf.keras.Model):
         rpn_classification_loss=None,
         weight_decay=0.0001,
         loss=None,
-        metrics=None,
         **kwargs,
     ):
         # TODO(tanzhenyu): Add metrics support once COCOMap issue is addressed.
         # https://github.com/keras-team/keras-cv/issues/915
-        if metrics is not None and len(metrics) != 0:
+        if "metrics" in kwargs.keys():
             raise ValueError(
                 "`RetinaNet` does not currently support the use of "
                 "`metrics` due to performance and distribution concerns. "
@@ -491,6 +490,11 @@ class FasterRCNN(tf.keras.Model):
         if sample_weight is not None:
             raise ValueError("`sample_weight` is currently not supported.")
         gt_boxes = y["boxes"]
+        if len(y["classes"]) != 2:
+            raise ValueError(
+                "Expected 'classes' to be a tf.Tensor of rank 2. "
+                f"Got y['classes'].shape={y['classes'].shape}."
+            )
         gt_classes = tf.expand_dims(y["classes"], axis=-1)
         with tf.GradientTape() as tape:
             total_loss = self.compute_loss(images, gt_boxes, gt_classes, training=True)
