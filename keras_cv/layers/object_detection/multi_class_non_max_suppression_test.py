2a6a22bad1cc161aa8d2ec87c9561112fc8f08c7
bhack <bhack@users.noreply.github.com>
2023-01-14 15:35:40 +0000

    Refactor test to not mix pytest and unnittest

--- keras_cv/layers/object_detection/multi_class_non_max_suppression_test.py
+++ keras_cv/layers/object_detection/multi_class_non_max_suppression_test.py
@@ -14,56 +14,59 @@
 
 import unittest
 
-import pytest
 import tensorflow as tf
 
 from keras_cv import layers as cv_layers
 
 
+def decode_predictions_output_shapes():
+    classes = 10
+    predictions_shape = (8, 98208, 4 + classes)
+
+    predictions = tf.random.uniform(
+        shape=predictions_shape, minval=0.0, maxval=1.0, dtype=tf.float32
+    )
+    box_pred = predictions[..., :4]
+    confidence_pred = predictions[..., 4:]
+
+    layer = cv_layers.MultiClassNonMaxSuppression(
+        bounding_box_format="xyxy",
+        from_logits=True,
+        max_detections=100,
+    )
+
+    result = layer(box_prediction=box_pred, confidence_prediction=confidence_pred)
+    return result
+
+
 class NmsPredictionDecoderTest(tf.test.TestCase):
     def test_decode_predictions_output_shapes(self):
-        classes = 10
-        predictions_shape = (8, 98208, 4 + classes)
-
-        predictions = tf.random.uniform(
-            shape=predictions_shape, minval=0.0, maxval=1.0, dtype=tf.float32
-        )
-        box_pred = predictions[..., :4]
-        confidence_pred = predictions[..., 4:]
-
-        layer = cv_layers.MultiClassNonMaxSuppression(
-            bounding_box_format="xyxy",
-            from_logits=True,
-            max_detections=100,
-        )
-
-        result = layer(box_prediction=box_pred, confidence_prediction=confidence_pred)
-
+        result = decode_predictions_output_shapes()
         self.assertEqual(result["boxes"].shape, [8, 100, 4])
         self.assertEqual(result["classes"].shape, [8, 100])
         self.assertEqual(result["confidence"].shape, [8, 100])
 
-    @unittest.expectedFailure
-    def test_with_xla(self):
-        # TODO if the teardown is working correctly the next line is not required
-        tf.config.experimental.disable_mlir_bridge()
-        xla_function = tf.function(
-            self.test_decode_predictions_output_shapes, jit_compile=True
-        )
-        xla_function()
 
-    @pytest.fixture()
-    def prepare_xla_mlir_bridge_compiled_function(self):
+@unittest.expectedFailure
+class NmsPredictionDecoderTestWithXLA(tf.test.TestCase):
+    def test_decode_predictions_output_shapes(self):
+        xla_function = tf.function(decode_predictions_output_shapes, jit_compile=True)
+        result = xla_function()
+        self.assertEqual(result["boxes"].shape, [8, 100, 4])
+        self.assertEqual(result["classes"].shape, [8, 100])
+        self.assertEqual(result["confidence"].shape, [8, 100])
+
+class NmsPredictionDecoderTestWithXLAMlirBridge(tf.test.TestCase):
+    def setUp(self):
         tf.config.experimental.enable_mlir_bridge()
-        xla_function = tf.function(
-            self.test_decode_predictions_output_shapes, jit_compile=True
-        )
-        yield xla_function
+
+    def tearDown(self):
         tf.config.experimental.disable_mlir_bridge()
 
-    @pytest.fixture(autouse=True)
-    def store_xla_mlir(self, prepare_xla_mlir_bridge_compiled_function):
-        self._xla_function = prepare_xla_mlir_bridge_compiled_function
-
-    def test_with_xla_mlir_bridge(self):
-        self._xla_function()
+    #@unittest.expectedFailure
+    def test_decode_predictions_output_shapes(self):
+        xla_function = tf.function(decode_predictions_output_shapes, jit_compile=True)
+        result = xla_function()
+        self.assertEqual(result["boxes"].shape, [8, 100, 4])
+        self.assertEqual(result["classes"].shape, [8, 100])
+        self.assertEqual(result["confidence"].shape, [8, 100])
