1dd7676a09a838a75171624fc2c36b09c68b5878
Luke Wood <lukewoodcs@gmail.com>
2023-01-18 17:19:38 -0800

    Update component for repeated augment

--- keras_cv/layers/preprocessing/repeated_augmentation.py
+++ keras_cv/layers/preprocessing/repeated_augmentation.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import tensorflow as tf
-from tensorflow import keras
 
 from keras_cv.layers.preprocessing.base_image_augmentation_layer import (
     BaseImageAugmentationLayer,
