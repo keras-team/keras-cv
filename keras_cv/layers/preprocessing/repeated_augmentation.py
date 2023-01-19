4824ef2a65692af682f158f1e547a01c88258f96
Luke Wood <lukewoodcs@gmail.com>
2023-01-18 16:53:42 -0800

    fix formatting

--- keras_cv/layers/preprocessing/repeated_augmentation.py
+++ keras_cv/layers/preprocessing/repeated_augmentation.py
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from tensorflow import keras
 import tensorflow as tf
+from tensorflow import keras
 
 from keras_cv.layers.preprocessing.base_image_augmentation_layer import (
     BaseImageAugmentationLayer,
