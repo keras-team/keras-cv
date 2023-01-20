32701437b713050de951351529335f1fc725ceb0
bhack <bhack@users.noreply.github.com>
2023-01-20 20:26:23 +0100

    Test compilation of cv_layers.MultiClassNonMaxSuppression (#1270)
    * Test compilation of cv_layers.MultiClassNonMaxSuppression
    
    * Fix linting
    
    * Refactor tests
    
    * Temp Explicit disable the bridge
    
    * Refactor test to not mix pytest and unnittest
    
    * Fix lint
    
    * Fix compilation with MLIR bridge
    
    * Fix lint

--- keras_cv/layers/object_detection/multi_class_non_max_suppression_test.py
+++ keras_cv/layers/object_detection/multi_class_non_max_suppression_test.py
@@ -12,30 +12,62 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import unittest
+
 import tensorflow as tf
 
 from keras_cv import layers as cv_layers
 
 
+def decode_predictions_output_shapes():
+    classes = 10
+    predictions_shape = (8, 98208, 4 + classes)
+
+    predictions = tf.random.stateless_uniform(
+        shape=predictions_shape, seed=(2, 3), minval=0.0, maxval=1.0, dtype=tf.float32
+    )
+    box_pred = predictions[..., :4]
+    confidence_pred = predictions[..., 4:]
+
+    layer = cv_layers.MultiClassNonMaxSuppression(
+        bounding_box_format="xyxy",
+        from_logits=True,
+        max_detections=100,
+    )
+
+    result = layer(box_prediction=box_pred, confidence_prediction=confidence_pred)
+    return result
+
+
 class NmsPredictionDecoderTest(tf.test.TestCase):
     def test_decode_predictions_output_shapes(self):
-        classes = 10
-        predictions_shape = (8, 98208, 4 + classes)
+        result = decode_predictions_output_shapes()
+        self.assertEqual(result["boxes"].shape, [8, 100, 4])
+        self.assertEqual(result["classes"].shape, [8, 100])
+        self.assertEqual(result["confidence"].shape, [8, 100])
 
-        predictions = tf.random.uniform(
-            shape=predictions_shape, minval=0.0, maxval=1.0, dtype=tf.float32
-        )
-        box_pred = predictions[..., :4]
-        confidence_pred = predictions[..., 4:]
 
-        layer = cv_layers.MultiClassNonMaxSuppression(
-            bounding_box_format="xyxy",
-            from_logits=True,
-            max_detections=100,
-        )
+@unittest.expectedFailure
+class NmsPredictionDecoderTestWithXLA(tf.test.TestCase):
+    def test_decode_predictions_output_shapes(self):
+        xla_function = tf.function(decode_predictions_output_shapes, jit_compile=True)
+        result = xla_function()
+        self.assertEqual(result["boxes"].shape, [8, 100, 4])
+        self.assertEqual(result["classes"].shape, [8, 100])
+        self.assertEqual(result["confidence"].shape, [8, 100])
 
-        result = layer(box_prediction=box_pred, confidence_prediction=confidence_pred)
 
+class NmsPredictionDecoderTestWithXLAMlirBridge(tf.test.TestCase):
+    def setUp(self):
+        tf.config.experimental.enable_mlir_bridge()
+
+    def tearDown(self):
+        tf.config.experimental.disable_mlir_bridge()
+
+    # @unittest.expectedFailure
+    def test_decode_predictions_output_shapes(self):
+        xla_function = tf.function(decode_predictions_output_shapes, jit_compile=True)
+        result = xla_function()
         self.assertEqual(result["boxes"].shape, [8, 100, 4])
         self.assertEqual(result["classes"].shape, [8, 100])
         self.assertEqual(result["confidence"].shape, [8, 100])
