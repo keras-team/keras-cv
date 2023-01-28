28e98c996ef387747394137afdc0b2ac35a386f4
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-01-28 21:51:47 +0530

    [api-polish] Renaming api's  (#1267)
    * API polish
    
    * Update
    
    * Update
    
    * Update
    
    * Typo fix
    
    * Updated
    
    * Update global_random_scaling_test.py
    
    * Updated
    
    * Update
    
    * Update

--- keras_cv/losses/penalty_reduced_focal_loss.py
+++ keras_cv/losses/penalty_reduced_focal_loss.py
@@ -18,7 +18,7 @@ import tensorflow as tf
 # TODO(tanzhenyu): consider inherit from LossFunctionWrapper to
 # get the dimension squeeze.
 @tf.keras.utils.register_keras_serializable(package="keras_cv")
-class BinaryPenaltyReducedFocalXent(tf.keras.losses.Loss):
+class BinaryPenaltyReducedFocalCrossEntropy(tf.keras.losses.Loss):
     """Implements CenterNet modified Focal loss.
 
     Compared with `keras.losses.BinaryFocalCrossentropy`, this loss discounts for negative
@@ -56,7 +56,7 @@ class BinaryPenaltyReducedFocalXent(tf.keras.losses.Loss):
         positive_weight=1.0,
         negative_weight=1.0,
         reduction=tf.keras.losses.Reduction.AUTO,
-        name="binary_penalty_reduced_focal_xent",
+        name="binary_penalty_reduced_focal_cross_entropy",
     ):
         super().__init__(reduction=reduction, name=name)
         self.alpha = alpha
