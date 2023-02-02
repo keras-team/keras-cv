90fc832d00808251885ac4fff3c2aebd564b7a79
Luke Wood <lukewoodcs@gmail.com>
2023-02-02 09:10:51 -0800

    Add gs loading to weights

--- keras_cv/models/weights.py
+++ keras_cv/models/weights.py
@@ -21,7 +21,7 @@ def parse_weights(weights, include_top, model_type):
         weights = weights.replace("gs://", "https://storage.googleapis.com/")
         return data_utils.get_file(
             origin=weights,
-            cache_subdir="custom_models",
+            cache_subdir="models",
         )
     if weights in ALIASES[model_type]:
         weights = ALIASES[model_type][weights]
