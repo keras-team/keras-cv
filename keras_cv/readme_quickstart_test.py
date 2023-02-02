298c197a0afd3ae9accfcd5393f27a960483d4d8
Luke Wood <lukewoodcs@gmail.com>
2023-02-02 11:05:15 -0800

    Fix isort

--- keras_cv/readme_quickstart_test.py
+++ keras_cv/readme_quickstart_test.py
@@ -11,10 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+
+import pytest
 import tensorflow as tf
 import tensorflow_datasets as tfds
-import pytest
-import os
+
 import keras_cv
 
 """
