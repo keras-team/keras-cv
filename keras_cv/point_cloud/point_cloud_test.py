b785ac43f4d8ba2ece698a56f1af0987d02e38d8
Luke Wood <lukewoodcs@gmail.com>
2023-02-06 11:19:45 -0800

    Update point cloud

--- keras_cv/point_cloud/point_cloud_test.py
+++ keras_cv/point_cloud/point_cloud_test.py
@@ -19,15 +19,13 @@ import pytest
 import tensorflow as tf
 from absl.testing import parameterized
 
-from keras_cv import ops
+from keras_cv import point_cloud
 
 
 class AngleTest(tf.test.TestCase):
     def test_wrap_angle_radians(self):
-        self.assertAllClose(
-            -np.pi + 0.1, ops.point_cloud.wrap_angle_radians(np.pi + 0.1)
-        )
-        self.assertAllClose(0.0, ops.point_cloud.wrap_angle_radians(2 * np.pi))
+        self.assertAllClose(-np.pi + 0.1, point_cloud.wrap_angle_radians(np.pi + 0.1))
+        self.assertAllClose(0.0, point_cloud.wrap_angle_radians(2 * np.pi))
 
 
 class Boxes3DTestCase(tf.test.TestCase, parameterized.TestCase):
@@ -38,7 +36,7 @@ class Boxes3DTestCase(tf.test.TestCase, parameterized.TestCase):
                 [[1, 2, 3, 4, 3, 6, np.pi / 2.0], [1, 2, 3, 4, 3, 6, np.pi / 2.0]],
             ]
         )
-        corners = ops._center_xyzWHD_to_corner_xyz(boxes)
+        corners = point_cloud._center_xyzWHD_to_corner_xyz(boxes)
         self.assertEqual((2, 2, 8, 3), corners.shape)
         for i in [0, 1]:
             self.assertAllClose(-1, np.min(corners[0, i, :, 0]))
@@ -75,7 +73,7 @@ class Boxes3DTestCase(tf.test.TestCase, parameterized.TestCase):
             ],
             dtype=tf.float32,
         )
-        is_inside = ops.is_within_box2d(points, boxes)
+        is_inside = point_cloud.is_within_box2d(points, boxes)
         expected = [[False]] * 8 + [[True]] * 2
         self.assertAllEqual(expected, is_inside)
 
@@ -98,7 +96,7 @@ class Boxes3DTestCase(tf.test.TestCase, parameterized.TestCase):
             ],
             dtype=tf.float32,
         )
-        is_inside = ops.is_within_box2d(points, bbox)
+        is_inside = point_cloud.is_within_box2d(points, bbox)
         expected = [[False]] * 10
         self.assertAllEqual(expected, is_inside)
 
@@ -106,11 +104,11 @@ class Boxes3DTestCase(tf.test.TestCase, parameterized.TestCase):
         v1 = tf.constant([[0.0, 0.0]], dtype=tf.float32)
         v2 = tf.constant([[1.0, 0.0]], dtype=tf.float32)
         p = tf.constant([[0.5, 0.5], [-1.0, -3], [-1.0, 1.0]], dtype=tf.float32)
-        res = ops._is_on_lefthand_side(p, v1, v2)
+        res = point_cloud._is_on_lefthand_side(p, v1, v2)
         self.assertAllEqual([[True, False, True]], res)
-        res = ops._is_on_lefthand_side(v1, v1, v2)
+        res = point_cloud._is_on_lefthand_side(v1, v1, v2)
         self.assertAllEqual([[True]], res)
-        res = ops._is_on_lefthand_side(v2, v1, v2)
+        res = point_cloud._is_on_lefthand_side(v2, v1, v2)
         self.assertAllEqual([[True]], res)
 
     @parameterized.named_parameters(
@@ -138,7 +136,7 @@ class Boxes3DTestCase(tf.test.TestCase, parameterized.TestCase):
             return tf.matmul(bbox, rotation_matrix)
 
         rotated_bboxes = _rotate(boxes, angle)
-        res = ops._box_area(rotated_bboxes)
+        res = point_cloud._box_area(rotated_bboxes)
         self.assertAllClose(expected, res)
 
     def test_within_box3d(self):
@@ -205,7 +203,7 @@ class Boxes3DTestCase(tf.test.TestCase, parameterized.TestCase):
         assert bboxes.shape[0] == num_boxes
         assert expected_is_inside.shape[0] == num_points
         assert expected_is_inside.shape[1] == num_boxes
-        is_inside = ops.is_within_box3d(points, bboxes)
+        is_inside = point_cloud.is_within_box2d(points, bboxes)
         self.assertAllEqual([num_points, num_boxes], is_inside.shape)
         self.assertAllEqual(expected_is_inside, is_inside)
         # Add a batch dimension to the data and see that it still works
@@ -213,7 +211,7 @@ class Boxes3DTestCase(tf.test.TestCase, parameterized.TestCase):
         batch_size = 3
         points = tf.tile(points[tf.newaxis, ...], [batch_size, 1, 1])
         bboxes = tf.tile(bboxes[tf.newaxis, ...], [batch_size, 1, 1])
-        is_inside = ops.is_within_box3d(points, bboxes)
+        is_inside = point_cloud.is_within_box2d(points, bboxes)
         self.assertAllEqual([batch_size, num_points, num_boxes], is_inside.shape)
         for batch_idx in range(batch_size):
             self.assertAllEqual(expected_is_inside, is_inside[batch_idx])
@@ -244,7 +242,7 @@ class Boxes3DTestCase(tf.test.TestCase, parameterized.TestCase):
             dtype=tf.float32,
         )
 
-        result = ops.coordinate_transform(replicated_points, pose)
+        result = point_cloud.coordinate_transform(replicated_points, pose)
 
         # We expect the point to be translated close to the car, and then rotated
         # mostly around the x-axis.
@@ -256,7 +254,7 @@ class Boxes3DTestCase(tf.test.TestCase, parameterized.TestCase):
     def testSphericalCoordinatesTransform(self):
         np_xyz = np.random.randn(5, 6, 3)
         points = tf.constant(np_xyz, dtype=tf.float32)
-        spherical_coordinates = ops.spherical_coordinate_transform(points)
+        spherical_coordinates = point_cloud.spherical_coordinate_transform(points)
 
         # Convert coordinates back to xyz to verify.
         dist = spherical_coordinates[..., 0]
@@ -312,7 +310,7 @@ class Boxes3DTestCase(tf.test.TestCase, parameterized.TestCase):
                 ],
                 dtype=tf.float32,
             )
-            res = ops.group_points_by_boxes(points, bboxes)
+            res = point_cloud.group_points_by_boxes(points, bboxes)
             expected_result = tf.ragged.constant(
                 [[0, 1, 2], [5, 6, 7, 16], [10, 11, 12], [17]]
             )
@@ -322,19 +320,19 @@ class Boxes3DTestCase(tf.test.TestCase, parameterized.TestCase):
         center = tf.constant([1.0, 1.0, 1.0])
         points = tf.constant([[0.0, 0.0, 0.0], [1.0, 2.0, 1.0], [1.0, 0.0, 1.0]])
 
-        point_mask = ops.within_a_frustum(
+        point_mask = point_cloud.within_a_frustum(
             points, center, r_distance=1.0, theta_width=1.0, phi_width=1.0
         )
         target_point_mask = tf.constant([False, True, False])
         self.assertAllClose(point_mask, target_point_mask)
 
-        point_mask = ops.within_a_frustum(
+        point_mask = point_cloud.within_a_frustum(
             points, center, r_distance=1.0, theta_width=3.14, phi_width=3.14
         )
         target_point_mask = tf.constant([False, True, True])
         self.assertAllClose(point_mask, target_point_mask)
 
-        point_mask = ops.within_a_frustum(
+        point_mask = point_cloud.within_a_frustum(
             points, center, r_distance=3.0, theta_width=1.0, phi_width=1.0
         )
         target_point_mask = tf.constant([False, False, False])
