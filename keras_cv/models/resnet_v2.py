4215515de83fb5b754b814aaaa42d80cb59fb845
Jonathan Bischof <bischof@google.com>
2023-02-15 21:54:27 +0000

    Clean up docstrings

--- keras_cv/models/resnet_v2.py
+++ keras_cv/models/resnet_v2.py
@@ -56,6 +56,7 @@ MODEL_CONFIGS = {
 }
 
 BN_AXIS = 3
+BN_EPSILON = 1.001e-5
 
 BASE_DOCSTRING = """Instantiates the {name} architecture.
     Reference:
@@ -72,12 +73,13 @@ BASE_DOCSTRING = """Instantiates the {name} architecture.
         learning & fine-tuning](https://keras.io/guides/transfer_learning/).
     
     Args:
-        include_rescaling: whether or not to Rescale the inputs.If set to True,
-            inputs will be passed through a `Rescaling(1/255.0)` layer.
-        include_top: whether to include the fully-connected layer at the top of the
-            network.  If provided, classes must be provided.
-        classes: optional number of classes to classify images into, only to be
-            specified if `include_top` is True.
+        include_rescaling: bool, whether or not to Rescale the inputs. If set
+            to `True`, inputs will be passed through a `Rescaling(1/255.0)`
+            layer.
+        include_top: bool, whether to include the fully-connected layer at
+            the top of the network.  If provided, `classes` must be provided.
+        classes: optional int, number of classes to classify images into (only
+            to be specified if `include_top` is `True`).
         weights: one of `None` (random initialization), a pretrained weight file
             path, or a reference to pre-trained weights (e.g. 'imagenet/classification')
             (see available pre-trained weights in weights.py)
@@ -107,22 +109,26 @@ def BasicBlock(
     x, filters, kernel_size=3, stride=1, dilation=1, conv_shortcut=False, name=None
 ):
     """A basic residual block (v2).
+
     Args:
         x: input tensor.
-        filters: integer, filters of the basic layer.
-        kernel_size: default 3, kernel size of the bottleneck layer.
-        stride: default 1, stride of the first layer.
-        conv_shortcut: default False, use convolution shortcut if True,
-          otherwise identity shortcut.
-        name: string, block label.
+        filters: int, filters of the basic layer.
+        kernel_size: int, kernel size of the bottleneck layer. Defaults to 3.
+        stride: int, stride of the first layer. Defaults to 1.
+        dilation: int, the dilation rate to use for dilated convolution.
+            Defaults to 1.
+        conv_shortcut: bool, uses convolution shortcut if `True`. If `False`
+            (default), uses identity or pooling shortcut, based on stride.
+
     Returns:
       Output tensor for the residual block.
     """
+
     if name is None:
         name = f"v2_basic_block_{backend.get_uid('v2_basic_block')}"
 
     use_preactivation = layers.BatchNormalization(
-        axis=BN_AXIS, epsilon=1.001e-5, name=name + "_use_preactivation_bn"
+        axis=BN_AXIS, epsilon=BN_EPSILON, name=name + "_use_preactivation_bn"
     )(x)
 
     use_preactivation = layers.Activation(
@@ -149,9 +155,9 @@ def BasicBlock(
         use_bias=False,
         name=name + "_1_conv",
     )(use_preactivation)
-    x = layers.BatchNormalization(axis=BN_AXIS, epsilon=1.001e-5, name=name + "_1_bn")(
-        x
-    )
+    x = layers.BatchNormalization(
+        axis=BN_AXIS, epsilon=BN_EPSILON, name=name + "_1_bn"
+    )(x)
     x = layers.Activation("relu", name=name + "_1_relu")(x)
 
     x = layers.Conv2D(
@@ -173,14 +179,17 @@ def Block(
     x, filters, kernel_size=3, stride=1, dilation=1, conv_shortcut=False, name=None
 ):
     """A residual block (v2).
+
     Args:
         x: input tensor.
-        filters: integer, filters of the bottleneck layer.
-        kernel_size: default 3, kernel size of the bottleneck layer.
-        stride: default 1, stride of the first layer.
-        conv_shortcut: default False, use convolution shortcut if True,
-          otherwise identity shortcut.
-        name: string, block label.
+        filters: int, filters of the basic layer.
+        kernel_size: int, kernel size of the bottleneck layer. Defaults to 3.
+        stride: int, stride of the first layer. Defaults to 1.
+        dilation: int, the dilation rate to use for dilated convolution.
+            Defaults to 1.
+        conv_shortcut: bool, uses convolution shortcut if `True`. If `False`
+            (default), uses identity or pooling shortcut, based on stride.
+
     Returns:
       Output tensor for the residual block.
     """
@@ -188,7 +197,7 @@ def Block(
         name = f"v2_block_{backend.get_uid('v2_block')}"
 
     use_preactivation = layers.BatchNormalization(
-        axis=BN_AXIS, epsilon=1.001e-5, name=name + "_use_preactivation_bn"
+        axis=BN_AXIS, epsilon=BN_EPSILON, name=name + "_use_preactivation_bn"
     )(x)
 
     use_preactivation = layers.Activation(
@@ -213,9 +222,9 @@ def Block(
     x = layers.Conv2D(filters, 1, strides=1, use_bias=False, name=name + "_1_conv")(
         use_preactivation
     )
-    x = layers.BatchNormalization(axis=BN_AXIS, epsilon=1.001e-5, name=name + "_1_bn")(
-        x
-    )
+    x = layers.BatchNormalization(
+        axis=BN_AXIS, epsilon=BN_EPSILON, name=name + "_1_bn"
+    )(x)
     x = layers.Activation("relu", name=name + "_1_relu")(x)
 
     x = layers.Conv2D(
@@ -227,9 +236,9 @@ def Block(
         dilation_rate=dilation,
         name=name + "_2_conv",
     )(x)
-    x = layers.BatchNormalization(axis=BN_AXIS, epsilon=1.001e-5, name=name + "_2_bn")(
-        x
-    )
+    x = layers.BatchNormalization(
+        axis=BN_AXIS, epsilon=BN_EPSILON, name=name + "_2_bn"
+    )(x)
     x = layers.Activation("relu", name=name + "_2_relu")(x)
 
     x = layers.Conv2D(4 * filters, 1, name=name + "_3_conv")(x)
@@ -237,7 +246,7 @@ def Block(
     return x
 
 
-@keras.utils.register_keras_serializable(package="keras_cv.models")
+@keras.utils.register_keras_serializable(package="keras_cv.models.resnet_v2")
 def Stack(
     x,
     filters,
@@ -250,18 +259,21 @@ def Stack(
     stack_index=1,
 ):
     """A set of stacked blocks.
+
     Args:
         x: input tensor.
-        filters: integer, filters of the layer in a block.
-        blocks: integer, blocks in the stacked blocks.
-        stride: default 2, stride of the first layer in the first block.
-        name: string, stack label.
+        filters: int, filters of the layer in a block.
+        blocks: int, blocks in the stacked blocks.
+        stride: int, stride of the first layer in the first block. Defaults to 2.
+        dilation: int, the dilation rate to use for dilated convolution. Defaults to 1.
         block_fn: callable, `Block` or `BasicBlock`, the block function to stack.
-        first_shortcut: default True, use convolution shortcut if True,
-          otherwise identity shortcut.
+        first_shortcut: bool. Use convolution shortcut if `True` (default),
+            otherwise uses identity or pooling shortcut, based on stride.
+
     Returns:
         Output tensor for the stacked blocks.
     """
+
     if name is None:
         name = f"v2_stack_{stack_index}"
 
@@ -278,19 +290,24 @@ def Stack(
     return x
 
 
-@keras.utils.register_keras_serializable(package="keras_cv.models.resnet_v2")
+@keras.utils.register_keras_serializable(package="keras_cv.models")
 class ResNetV2(keras.Model):
     """Instantiates the ResNetV2 architecture.
 
     Args:
-        stackwise_filters: number of filters for each stack in the model.
-        stackwise_blocks: number of blocks for each stack in the model.
-        stackwise_strides: stride for each stack in the model.
-        include_rescaling: whether or not to Rescale the inputs. If set to True,
-            inputs will be passed through a `Rescaling(1/255.0)` layer.
-            name: string, model name.
-        include_top: whether to include the fully-connected
+        stackwise_filters: list of ints, number of filters for each stack in
+            the model.
+        stackwise_blocks: list of ints, number of blocks for each stack in the
+            model.
+        stackwise_strides: list of ints, stride for each stack in the model.
+        include_rescaling: bool, whether or not to Rescale the inputs. If set
+            to `True`, inputs will be passed through a `Rescaling(1/255.0)`
+            layer.
+        include_top: bool, whether to include the fully-connected
             layer at the top of the network.
+        stackwise_dialations: list of ints, dialation for each stack in the
+            model. If `None` (default), dialation will not be used.
+        name: string, model name.
         weights: one of `None` (random initialization),
             or the path to the weights file to be loaded.
         input_shape: optional shape tuple, defaults to (None, None, 3).
@@ -314,7 +331,6 @@ class ResNetV2(keras.Model):
             `classifier_activation=None` to return the logits of the "top" layer.
         block_fn: callable, `Block` or `BasicBlock`, the block function to stack.
             Use 'basic_block' for ResNet18 and ResNet34.
-        **kwargs: Pass-through keyword arguments to `tf.keras.Model`.
     """
 
     def __init__(
@@ -388,7 +404,9 @@ class ResNetV2(keras.Model):
             )
             stack_level_outputs[stack_index + 2] = x
 
-        x = layers.BatchNormalization(axis=BN_AXIS, epsilon=1.001e-5, name="post_bn")(x)
+        x = layers.BatchNormalization(axis=BN_AXIS, epsilon=BN_EPSILON, name="post_bn")(
+            x
+        )
         x = layers.Activation("relu", name="post_relu")(x)
 
         if include_top:
