01ff967bee8e3cb2440e6a223a1ebe06038ecec8
Luke Wood <lukewoodcs@gmail.com>
2023-02-16 14:07:45 -0800

    Add box delta metric

--- keras_cv/metrics/box_counts.py
+++ keras_cv/metrics/box_counts.py
@@ -18,36 +18,31 @@ import tensorflow.keras.initializers as initializers
 from keras_cv import bounding_box
 
 
-class BoxCounts(keras.metrics.Metric):
-    """BoxCounts computes average predictions per image."""
+class MeanBoxCountDelta(keras.metrics.Metric):
+    """MeanBoxCountDelta computes average difference between ground truth boxes
+    and predicted boxes on a per image basis.
+    """
 
     def __init__(
         self,
         **kwargs,
     ):
         super().__init__(**kwargs)
-        self.ground_truths = self.add_weight(
-            name="ground_truths",
+        self.delta_sum = self.add_weight(
+            name="delta_sum",
             shape=(),
-            dtype=tf.float32,
-            initializer=initializers.Zeros(),
-        )
-        self.predictions = self.add_weight(
-            name="predictions",
-            shape=(),
-            dtype=tf.float32,
+            dtype=self.compute_dtype,
             initializer=initializers.Zeros(),
         )
         self.samples = self.add_weight(
             name="samples",
             shape=(),
-            dtype=tf.float32,
+            dtype=self.compute_dtype,
             initializer=initializers.Zeros(),
         )
 
     def reset_state(self):
-        self.ground_truths.assign(tf.zeros_like(self.ground_truths))
-        self.predictions.assign(tf.zeros_like(self.predictions))
+        self.delta_sum.assign(tf.zeros_like(self.delta_sum))
         self.samples.assign(tf.zeros_like(self.samples))
 
     def update_state(self, y_true, y_pred, sample_weight=None):
@@ -58,25 +53,25 @@ class BoxCounts(keras.metrics.Metric):
             sample_weight: Currently unsupported.
         """
         del sample_weight
-        y_pred = bounding_box.to_ragged(y_pred)
-        y_true = bounding_box.to_ragged(y_true)
+        y_pred = bounding_box.to_dense(y_pred)
+        y_true = bounding_box.to_dense(y_true)
 
-        tf.print(tf.math.reduce_min(y_pred["confidence"]))
-        for image in tf.range(tf.shape(y_pred["classes"])[0]):
-            self.predictions.assign_add(
-                tf.cast(tf.shape(y_pred["classes"][image])[0], tf.float32)
-            )
-            self.ground_truths.assign_add(
-                tf.cast(tf.shape(y_true["classes"][image])[0], tf.float32)
-            )
+        ground_truth_boxes = tf.cast(y_true["classes"] == -1, tf.int32)
+        predicted_boxes = tf.cast(y_pred["classes"] == -1, tf.int32)
 
-        self.samples.assign_add(tf.cast(tf.shape(y_pred["classes"])[0], tf.float32))
+        ground_truth_boxes = tf.math.reduce_sum(ground_truth_boxes, axis=-1)
+        predicted_boxes = tf.math.reduce_sum(predicted_boxes, axis=-1)
+
+        delta_sum = ground_truth_boxes - predicted_boxes
+        self.delta_sum.assign_add(
+            tf.cast(tf.math.reduce_sum(delta_sum), self.compute_dtype)
+        )
+        self.samples.assign_add(
+            tf.cast(tf.shape(y_pred["classes"])[0], self.compute_dtype)
+        )
 
     def result(self):
-        return {
-            "predicted_boxes": self.predictions / self.samples,
-            "true_boxes": self.ground_truths / self.samples,
-        }
+        return self.delta_sum / self.samples
 
     def get_config(self):
         return super().get_config()
