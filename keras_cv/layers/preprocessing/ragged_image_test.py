ca1f4a594c5978269ac5b3175133bf69b8868de5
HongYu <20734616+james77777778@users.noreply.github.com>
2023-02-17 15:33:52 +0000

    Update unit test. Add RandomBrightness and RandomContrast

--- keras_cv/layers/preprocessing/ragged_image_test.py
+++ keras_cv/layers/preprocessing/ragged_image_test.py
@@ -1,4 +1,4 @@
-# Copyright 2022 The KerasCV Authors
+# Copyright 2023 The KerasCV Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -38,10 +38,16 @@ CONSISTENT_OUTPUT_TEST_CONFIGURATIONS = [
         {"height_factor": 0.2, "width_factor": 0.2},
     ),
     (
-        "RandomHue",
-        layers.RandomHue,
+        "RandomBrightness",
+        layers.RandomBrightness,
+        {"factor": (0.3, 0.8), "value_range": (0, 255)},
+    ),
+    (
+        "RandomContrast",
+        layers.RandomContrast,
         {"factor": 0.5, "value_range": (0, 255)},
     ),
+    ("RandomHue", layers.RandomHue, {"factor": 0.5}),
     (
         "RandomChannelShift",
         layers.RandomChannelShift,
