5009f7dcab118e29b05e2a56780c6cfb64db8423
Vision <gitesh.ch.0912@gmail.com>
2023-03-07 01:47:25 +0530

    update

--- keras_cv/models/efficientnet_v2.py
+++ keras_cv/models/efficientnet_v2.py
@@ -651,6 +651,8 @@ class EfficientNetV2(keras.Model):
         if blocks_args == "default":
             blocks_args = DEFAULT_BLOCKS_ARGS[model_name]
 
+        copy_blocks_args = copy.deepcopy(blocks_args)
+
         if weights and not tf.io.gfile.exists(weights):
             raise ValueError(
                 "The `weights` argument should be either `None` or the path to the "
@@ -826,7 +828,7 @@ class EfficientNetV2(keras.Model):
         self.min_depth = min_depth
         self.bn_momentum = bn_momentum
         self.activation = activation
-        self.blocks_args = blocks_args
+        self.blocks_args = copy_blocks_args
         self.model_name = model_name
         self.input_tensor = input_tensor
         self.pooling = pooling
