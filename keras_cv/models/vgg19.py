4de3ef2f76686a67d3f0b99bb54ad2e5eeed2b14
Srikesh <srikeshhp@gmail.com>
2023-03-08 23:30:31 +0530

    updated VGG19

--- keras_cv/models/vgg19.py
+++ keras_cv/models/vgg19.py
@@ -25,39 +25,52 @@ from tensorflow.keras import layers
 
 from keras_cv.models import utils
 
+# Copyright 2022 The KerasCV Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 
-def VGG19(
-    *,
-    include_rescaling,
-    include_top,
-    classes=None,
-    weights=None,
-    input_shape=(224, 224, 3),
-    input_tensor=None,
-    pooling=None,
-    classifier_activation="softmax",
-    name="VGG19",
-    **kwargs,
-):
+"""VGG19 model for KerasCV.
+Reference:
+  - [Very Deep Convolutional Networks for Large-Scale Image Recognition](https://arxiv.org/abs/1409.1556) (ICLR 2015)
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from keras_cv.models import utils
+from keras_cv.models.vgg16 import apply_vgg_block
+
+
+@keras.utils.register_keras_serializable(package="keras_cv.models")
+class VGG19(keras.Model):
     """Instantiates the VGG19 architecture.
-
     Reference:
     - [Very Deep Convolutional Networks for Large-Scale Image Recognition](https://arxiv.org/abs/1409.1556) (ICLR 2015)
-
     This function returns a Keras VGG19 model.
-
     Args:
-      include_rescaling: whether or not to Rescale the inputs.If set to True,
+      include_rescaling: bool, whether or not to Rescale the inputs.If set to True,
         inputs will be passed through a `Rescaling(1/255.0)` layer.
-      include_top: whether to include the 3 fully-connected
+      include_top: bool, whether to include the 3 fully-connected
         layers at the top of the network. If provided, classes must be provided.
-      classes: optional number of classes to classify images into, only to be
+      classes: int, optional number of classes to classify images into, only to be
         specified if `include_top` is True.
-      weights: one of `None` (random initialization), or a pretrained weight file path.
-      input_shape: optional shape tuple, defaults to (224, 224, 3).
-      input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
+      weights: os.PathLike or None, one of `None` (random initialization), or a pretrained weight file path.
+      input_shape: tuple, optional shape tuple, defaults to (224, 224, 3).
+      input_tensor: Tensor, optional Keras tensor (i.e. output of `layers.Input()`)
         to use as image input for the model.
-      pooling: Optional pooling mode for feature extraction
+      pooling: bool, Optional pooling mode for feature extraction
         when `include_top` is `False`.
         - `None` means that the output of the model will be
             the 4D tensor output of the
@@ -68,112 +81,145 @@ def VGG19(
             the output of the model will be a 2D tensor.
         - `max` means that global max pooling will
             be applied.
-      classifier_activation: A `str` or callable. The activation function to use
+      classifier_activation:`str` or callable. The activation function to use
         on the "top" layer. Ignored unless `include_top=True`. Set
         `classifier_activation=None` to return the logits of the "top" layer.
         When loading pretrained weights, `classifier_activation` can only
         be `None` or `"softmax"`.
       name: (Optional) name to pass to the model.  Defaults to "VGG19".
-
     Returns:
       A `keras.Model` instance.
     """
-    if weights and not tf.io.gfile.exists(weights):
-        raise ValueError(
-            "The `weights` argument should be either `None` or the path to the "
-            "weights file to be loaded. Weights file not found at location: {weights}"
+
+    def __init__(
+        self,
+        input_tensor=None,
+        include_rescaling=True,
+        include_top=True,
+        classes=None,
+        weights=None,
+        input_shape=(224, 224, 3),
+        pooling=None,
+        classifier_activation="softmax",
+        name="VGG19",
+        **kwargs,
+    ):
+        if weights and not tf.io.gfile.exists(weights):
+            raise ValueError(
+                "The `weights` argument should be either `None` or the path to the "
+                "weights file to be loaded. Weights file not found at location: {weights}"
+            )
+
+        if include_top and not classes:
+            raise ValueError(
+                "If `include_top` is True, you should specify `classes`. "
+                f"Received: classes={classes}"
+            )
+
+        if include_top and pooling:
+            raise ValueError(
+                f"`pooling` must be `None` when `include_top=True`."
+                f"Received pooling={pooling} and include_top={include_top}. "
+            )
+
+        inputs = utils.parse_model_inputs(input_shape, input_tensor)
+        x = inputs
+
+        if include_rescaling:
+            x = layers.Rescaling(1 / 255.0)(x)
+
+        x = apply_vgg_block(
+            x=x,
+            num_layers=2,
+            filters=64,
+            kernel_size=(3, 3),
+            activation="relu",
+            padding="same",
+            max_pool=True,
+            name="block1",
         )
 
-    if include_top and not classes:
-        raise ValueError(
-            "If `include_top` is True, you should specify `classes`. "
-            f"Received: classes={classes}"
+        x = apply_vgg_block(
+            x=x,
+            num_layers=2,
+            filters=128,
+            kernel_size=(3, 3),
+            activation="relu",
+            padding="same",
+            max_pool=True,
+            name="block2",
         )
 
-    inputs = utils.parse_model_inputs(input_shape, input_tensor)
+        x = apply_vgg_block(
+            x=x,
+            num_layers=4,
+            filters=256,
+            kernel_size=(3, 3),
+            activation="relu",
+            padding="same",
+            max_pool=True,
+            name="block3",
+        )
 
-    x = inputs
-    if include_rescaling:
-        x = layers.Rescaling(1 / 255.0)(x)
+        x = apply_vgg_block(
+            x=x,
+            num_layers=4,
+            filters=512,
+            kernel_size=(3, 3),
+            activation="relu",
+            padding="same",
+            max_pool=True,
+            name="block4",
+        )
 
-    # Block 1
-    x = layers.Conv2D(
-        64, (3, 3), activation="relu", padding="same", name="block1_conv1"
-    )(x)
-    x = layers.Conv2D(
-        64, (3, 3), activation="relu", padding="same", name="block1_conv2"
-    )(x)
-    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name="block1_pool")(x)
+        x = apply_vgg_block(
+            x=x,
+            num_layers=4,
+            filters=512,
+            kernel_size=(3, 3),
+            activation="relu",
+            padding="same",
+            max_pool=True,
+            name="block5",
+        )
 
-    # Block 2
-    x = layers.Conv2D(
-        128, (3, 3), activation="relu", padding="same", name="block2_conv1"
-    )(x)
-    x = layers.Conv2D(
-        128, (3, 3), activation="relu", padding="same", name="block2_conv2"
-    )(x)
-    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name="block2_pool")(x)
+        if include_top:
+            x = layers.Flatten(name="flatten")(x)
+            x = layers.Dense(4096, activation="relu", name="fc1")(x)
+            x = layers.Dense(4096, activation="relu", name="fc2")(x)
+            x = layers.Dense(
+                classes, activation=classifier_activation, name="predictions"
+            )(x)
+        else:
+            if pooling == "avg":
+                x = layers.GlobalAveragePooling2D()(x)
+            elif pooling == "max":
+                x = layers.GlobalMaxPooling2D()(x)
 
-    # Block 3
-    x = layers.Conv2D(
-        256, (3, 3), activation="relu", padding="same", name="block3_conv1"
-    )(x)
-    x = layers.Conv2D(
-        256, (3, 3), activation="relu", padding="same", name="block3_conv2"
-    )(x)
-    x = layers.Conv2D(
-        256, (3, 3), activation="relu", padding="same", name="block3_conv3"
-    )(x)
-    x = layers.Conv2D(
-        256, (3, 3), activation="relu", padding="same", name="block3_conv4"
-    )(x)
-    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name="block3_pool")(x)
+        super().__init__(inputs=inputs, outputs=x, name=name, **kwargs)
+        if weights is not None:
+            self.load_weights(weights)
 
-    # Block 4
-    x = layers.Conv2D(
-        512, (3, 3), activation="relu", padding="same", name="block4_conv1"
-    )(x)
-    x = layers.Conv2D(
-        512, (3, 3), activation="relu", padding="same", name="block4_conv2"
-    )(x)
-    x = layers.Conv2D(
-        512, (3, 3), activation="relu", padding="same", name="block4_conv3"
-    )(x)
-    x = layers.Conv2D(
-        512, (3, 3), activation="relu", padding="same", name="block4_conv4"
-    )(x)
-    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name="block4_pool")(x)
+        self.include_rescaling = include_rescaling
+        self.include_top = include_top
+        self.classes = classes
+        self.input_tensor = input_tensor
+        self.pooling = pooling
+        self.classifier_activation = classifier_activation
 
-    # Block 5
-    x = layers.Conv2D(
-        512, (3, 3), activation="relu", padding="same", name="block5_conv1"
-    )(x)
-    x = layers.Conv2D(
-        512, (3, 3), activation="relu", padding="same", name="block5_conv2"
-    )(x)
-    x = layers.Conv2D(
-        512, (3, 3), activation="relu", padding="same", name="block5_conv3"
-    )(x)
-    x = layers.Conv2D(
-        512, (3, 3), activation="relu", padding="same", name="block5_conv4"
-    )(x)
-    x = layers.MaxPooling2D((2, 2), strides=(2, 2), name="block5_pool")(x)
+    def get_config(self):
+        return {
+            "include_rescaling": self.include_rescaling,
+            "include_top": self.include_top,
+            "name": self.name,
+            "input_shape": self.input_shape[1:],
+            "input_tensor": self.input_tensor,
+            "pooling": self.pooling,
+            "classes": self.classes,
+            "classifier_activation": self.classifier_activation,
+            "trainable": self.trainable,
+        }
 
-    if include_top:
-        x = layers.Flatten(name="flatten")(x)
-        x = layers.Dense(4096, activation="relu", name="fc1")(x)
-        x = layers.Dense(4096, activation="relu", name="fc2")(x)
-        x = layers.Dense(
-            classes, activation=classifier_activation, name="predictions"
-        )(x)
-    else:
-        if pooling == "avg":
-            x = layers.GlobalAveragePooling2D()(x)
-        elif pooling == "max":
-            x = layers.GlobalMaxPooling2D()(x)
-
-    model = keras.Model(inputs, x, name=name, **kwargs)
-
-    if weights is not None:
-        model.load_weights(weights)
-    return model
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
