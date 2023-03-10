f9ca22a7823338e5ba3c448405bef048d7695554
HongYu <20734616+james77777778@users.noreply.github.com>
2023-03-11 00:53:38 +0800

    Vectorize RandomTranslation (#1480)
    * vectorize RandomTranslation
    
    * update ragged_image_test for RandomTranslation
    
    * update with_labels_test for RandomTranslation

--- keras_cv/layers/preprocessing/ragged_image_test.py
+++ keras_cv/layers/preprocessing/ragged_image_test.py
@@ -1,4 +1,4 @@
-# Copyright 2022 The KerasCV Authors
+# Copyright 2023 The KerasCV Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -73,6 +73,11 @@ CONSISTENT_OUTPUT_TEST_CONFIGURATIONS = [
         {"factor": 0.5, "value_range": (0, 255)},
     ),
     ("RandomShear", layers.RandomShear, {"x_factor": 0.3, "x_factor": 0.3}),
+    (
+        "RandomTranslation",
+        layers.RandomTranslation,
+        {"height_factor": 0.5, "width_factor": 0.5},
+    ),
     ("Solarization", layers.Solarization, {"value_range": (0, 255)}),
 ]
 
