a3c9ab104408d4504db75965d458a3bb39b9011d
Vision <gitesh.ch.0912@gmail.com>
2023-03-11 03:10:25 +0530

    yolov8

--- keras_cv/models/object_detection/yolov8/sppf.py
+++ keras_cv/models/object_detection/yolov8/sppf.py
@@ -17,37 +17,52 @@ from tensorflow.keras import layers
 
 
 @tf.keras.utils.register_keras_serializable(package="keras_cv")
-class SPP(layers.Layer):
+class SpatialPyramidPooling(layers.Layer):
     """
     Performs Spatial Pyramid Pooling.
+
+    References:
+        [Spatial Pyramid Pooling in Deep Convolutional Networks for Visual Recognition]
+        (https://arxiv.org/abs/1406.4729)
+
+    Spatial Pyramid Pooling (SPP) is a layer used in convolutional neural networks (CNNs) 
+    that enables them to accept images of different sizes instead of requiring a fixed 
+    input image size. To achieve this, an SPP layer is added on top of the final 
+    convolutional layer of the CNN. The SPP layer pools the features obtained from the 
+    previous layer and generates fixed-length outputs, thereby removing the fixed-size 
+    constraint of the network.
+
+    Args:
+        filters: int, the number of input filters
+        kernel_sizes: int, kernel size or pool size for `MaxPool2D` layers, Defaults to (5, 9, 13).
     """
-    
-    def __init__(self, filters, kernels=(5, 9, 13), **kwargs):
+
+    def __init__(self, filters, kernel_sizes=(5, 9, 13), **kwargs):
         super().__init__(**kwargs)
         self.filters = filters
-        self.kernels = kernels
+        self.kernel_sizes = kernel_sizes
         self.num_filters = filters // 2
-        self.conv1 = layers.Conv2D(
+        self.input_conv = layers.Conv2D(
             filters=self.num_filters, kernel_size=1, strides=1
         )
-        self.conv2 = layers.Conv2D(
+        self.output_conv = layers.Conv2D(
             filters=self.filters, kernel_size=1, strides=1
         )
         self.modules = [
             layers.MaxPool2D(pool_size=x, strides=1, padding="SAME")
-            for x in self.kernels
+            for x in self.kernel_sizes
         ]
 
     def call(self, x):
-        x = self.conv1(x)
-        return self.conv2(
+        x = self.input_conv(x)
+        return self.output_conv(
             tf.concat([x] + [module(x) for module in self.modules], axis=-1)
         )
 
     def get_config(self):
         config = {
             "filters": self.filters,
-            "kernels": self.kernels,
+            "kernels": self.kernel_sizes,
             "num_filters": self.num_filters,
         }
         base_config = super().get_config()
@@ -55,36 +70,46 @@ class SPP(layers.Layer):
 
 
 @tf.keras.utils.register_keras_serializable(package="keras_cv")
-class SPPF(layers.Layer):
+class FastSpatialPyramidPooling(layers.Layer):
     """
-    Performs Spatial Pyramid Pooling.
+    Performs Spatial Pyramid Pooling Faster.
+
+    SPPF(Spatial Pyramid Pooling Fast) is an optimized version of SPP, which has less
+    FLOPS(loating point operations per second).
+    
+    Args:
+        filters: int, the number of input filters
+        kernel_sizes: int, kernel size or pool size for `MaxPool2D` layers, Defaults to 5.
     """
+
     # Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher
-    def __init__(self, filters, kernel=5, **kwargs):
+    def __init__(self, filters, kernel_size=5, **kwargs):
         super().__init__(**kwargs)
         self.filters = filters
-        self.kernel = kernel
+        self.kernel_size = kernel_size
         self.num_filters = filters // 2
-        self.conv1 = layers.Conv2D(
+        self.input_conv = layers.Conv2D(
             filters=self.num_filters, kernel_size=1, strides=1
         )
-        self.conv2 = layers.Conv2D(
+        self.output_conv = layers.Conv2D(
             filters=self.filters, kernel_size=1, strides=1
         )
-        self.module = layers.MaxPool2D(
-            pool_size=self.kernel, strides=1, padding="SAME"
+        self.max_pool = layers.MaxPool2D(
+            pool_size=self.kernel_size, strides=1, padding="SAME"
         )
 
     def call(self, x):
-        x = self.conv1(x)
-        x1 = self.module(x)
-        x2 = self.module(x1)
-        return self.conv2(tf.concat([x, x1, x2, self.module(x2)], axis=-1))
+        x = self.input_conv(x)
+        x1 = self.max_pool(x)
+        x2 = self.max_pool(x1)
+        return self.output_conv(
+            tf.concat([x, x1, x2, self.max_pool(x2)], axis=-1)
+        )
 
     def get_config(self):
         config = {
             "filters": self.filters,
-            "kernel": self.kernel,
+            "kernel": self.kernel_size,
             "num_filters": self.num_filters,
         }
         base_config = super().get_config()
