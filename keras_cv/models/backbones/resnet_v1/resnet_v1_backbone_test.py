070108e0f6a3c8b34a87b68dbd6e3de76dc64996
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-03-15 23:53:24 +0000

    add unittests

--- keras_cv/models/backbones/resnet_v1/resnet_v1_backbone_test.py
+++ keras_cv/models/backbones/resnet_v1/resnet_v1_backbone_test.py
@@ -12,60 +12,179 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 import tensorflow as tf
 from absl.testing import parameterized
-from packaging import version
+from tensorflow import keras
 
-from keras_cv.models.backbones.resnet_v1 import resnet_v1_backbone as resnet_v1
-from keras_cv.models.models_test import ModelsTest
+from keras_cv.models.backbones.resnet_v1.resnet_v1_backbone import (
+    ResNet18Backbone,
+)
+from keras_cv.models.backbones.resnet_v1.resnet_v1_backbone import (
+    ResNet50Backbone,
+)
+from keras_cv.models.backbones.resnet_v1.resnet_v1_backbone import (
+    ResNet101Backbone,
+)
+from keras_cv.models.backbones.resnet_v1.resnet_v1_backbone import (
+    ResNet152Backbone,
+)
+from keras_cv.models.backbones.resnet_v1.resnet_v1_backbone import (
+    ResNetBackbone,
+)
+from keras_cv.utils.train import get_feature_extractor
 
-MODEL_LIST = [
-    (resnet_v1.ResNet18Backbone, 512, {}),
-]
 
-"""
-Below are other configurations that we omit from our CI but that can/should
-be tested manually when making changes to this model.
-(resnet_v1.ResNet34, 512, {}),
-(resnet_v1.ResNet50, 2048, {}),
-(resnet_v1.ResNet101, 2048, {}),
-(resnet_v1.ResNet152, 2048, {}),
-"""
+class ResNetBackboneTest(tf.test.TestCase, parameterized.TestCase):
+    def setUp(self):
+        self.input_batch = tf.ones(shape=(2, 224, 224, 3))
 
-
-class ResNetV1Test(ModelsTest, tf.test.TestCase, parameterized.TestCase):
-    @parameterized.parameters(*MODEL_LIST)
-    def test_application_base(self, app, _, args):
-        super()._test_application_base(app, _, args)
-
-    @parameterized.parameters(*MODEL_LIST)
-    def test_application_with_rescaling(self, app, last_dim, args):
-        super()._test_application_with_rescaling(app, last_dim, args)
-
-    @parameterized.parameters(*MODEL_LIST)
-    def test_application_pooling(self, app, last_dim, args):
-        super()._test_application_pooling(app, last_dim, args)
-
-    @parameterized.parameters(*MODEL_LIST)
-    def test_application_variable_input_channels(self, app, last_dim, args):
-        super()._test_application_variable_input_channels(app, last_dim, args)
-
-    @parameterized.parameters(*MODEL_LIST)
-    def test_model_serialization_tf(self, app, last_dim, args):
-        super()._test_model_serialization(
-            app, last_dim, args, save_format="tf", filename="model"
+    def test_valid_call(self):
+        model = ResNetBackbone(
+            stackwise_filters=[64, 128, 256, 512],
+            stackwise_blocks=[2, 2, 2, 2],
+            stackwise_strides=[1, 2, 2, 2],
+            include_rescaling=False,
         )
+        model(self.input_batch)
 
-    @parameterized.parameters(*MODEL_LIST)
-    def test_model_serialization_keras_format(self, app, last_dim, args):
-        if version.parse(tf.__version__) >= version.parse("2.12.0-dev0"):
-            super()._test_model_serialization(
-                app,
-                last_dim,
-                args,
-                save_format="keras_v3",
-                filename="model.keras",
-            )
+    def test_valid_call_applications_model(self):
+        model = ResNet50Backbone()
+        model(self.input_batch)
+
+    def test_valid_call_with_rescaling(self):
+        model = ResNetBackbone(
+            stackwise_filters=[64, 128, 256, 512],
+            stackwise_blocks=[2, 2, 2, 2],
+            stackwise_strides=[1, 2, 2, 2],
+            include_rescaling=True,
+        )
+        model(self.input_batch)
+
+    @parameterized.named_parameters(
+        ("tf_format", "tf", "model"),
+        ("keras_format", "keras_v3", "model.keras"),
+    )
+    def test_saved_model(self, save_format, filename):
+        model = ResNetBackbone(
+            stackwise_filters=[64, 128, 256, 512],
+            stackwise_blocks=[2, 2, 2, 2],
+            stackwise_strides=[1, 2, 2, 2],
+            include_rescaling=False,
+        )
+        model_output = model(self.input_batch)
+        save_path = os.path.join(self.get_temp_dir(), filename)
+        model.save(save_path, save_format=save_format)
+        restored_model = keras.models.load_model(save_path)
+
+        # Check we got the real object back.
+        self.assertIsInstance(restored_model, ResNetBackbone)
+
+        # Check that output matches.
+        restored_output = restored_model(self.input_batch)
+        self.assertAllClose(model_output, restored_output)
+
+    @parameterized.named_parameters(
+        ("tf_format", "tf", "model"),
+        ("keras_format", "keras_v3", "model.keras"),
+    )
+    def test_saved_alias_model(self, save_format, filename):
+        model = ResNet50Backbone()
+        model_output = model(self.input_batch)
+        save_path = os.path.join(self.get_temp_dir(), filename)
+        model.save(save_path, save_format=save_format)
+        restored_model = keras.models.load_model(save_path)
+
+        # Check we got the real object back.
+        # Note that these aliases serialized as the base class
+        self.assertIsInstance(restored_model, ResNetBackbone)
+
+        # Check that output matches.
+        restored_output = restored_model(self.input_batch)
+        self.assertAllClose(model_output, restored_output)
+
+    def test_model_backbone_layer_names_stability(self):
+        model = ResNetBackbone(
+            stackwise_filters=[64, 128, 256, 512],
+            stackwise_blocks=[2, 2, 2, 2],
+            stackwise_strides=[1, 2, 2, 2],
+            include_rescaling=False,
+        )
+        model_2 = ResNetBackbone(
+            stackwise_filters=[64, 128, 256, 512],
+            stackwise_blocks=[2, 2, 2, 2],
+            stackwise_strides=[1, 2, 2, 2],
+            include_rescaling=False,
+        )
+        layers_1 = model.layers
+        layers_2 = model_2.layers
+        for i in range(len(layers_1)):
+            if "input" in layers_1[i].name:
+                continue
+            self.assertEquals(layers_1[i].name, layers_2[i].name)
+
+    def test_create_backbone_model_from_alias_model(self):
+        model = ResNet50Backbone(
+            include_rescaling=False,
+        )
+        backbone_model = get_feature_extractor(
+            model,
+            model.pyramid_level_inputs.values(),
+            model.pyramid_level_inputs.keys(),
+        )
+        inputs = tf.keras.Input(shape=[256, 256, 3])
+        outputs = backbone_model(inputs)
+        # Resnet50 backbone has 4 level of features (2 ~ 5)
+        self.assertLen(outputs, 4)
+        self.assertEquals(list(outputs.keys()), [2, 3, 4, 5])
+        self.assertEquals(outputs[2].shape, [None, 64, 64, 256])
+        self.assertEquals(outputs[3].shape, [None, 32, 32, 512])
+        self.assertEquals(outputs[4].shape, [None, 16, 16, 1024])
+        self.assertEquals(outputs[5].shape, [None, 8, 8, 2048])
+
+    def test_create_backbone_model_with_level_config(self):
+        model = ResNetBackbone(
+            stackwise_filters=[64, 128, 256, 512],
+            stackwise_blocks=[2, 2, 2, 2],
+            stackwise_strides=[1, 2, 2, 2],
+            include_rescaling=False,
+            input_shape=[256, 256, 3],
+        )
+        levels = [3, 4]
+        layer_names = [model.pyramid_level_inputs[level] for level in [3, 4]]
+        backbone_model = get_feature_extractor(model, layer_names, levels)
+        inputs = tf.keras.Input(shape=[256, 256, 3])
+        outputs = backbone_model(inputs)
+        self.assertLen(outputs, 2)
+        self.assertEquals(list(outputs.keys()), [3, 4])
+        self.assertEquals(outputs[3].shape, [None, 32, 32, 512])
+        self.assertEquals(outputs[4].shape, [None, 16, 16, 1024])
+
+    @parameterized.named_parameters(
+        ("one_channel", 1),
+        ("four_channels", 4),
+    )
+    def test_application_variable_input_channels(self, num_channels):
+        # ResNet50 model
+        model = ResNetBackbone(
+            stackwise_filters=[64, 128, 256, 512],
+            stackwise_blocks=[3, 4, 6, 3],
+            stackwise_strides=[1, 2, 2, 2],
+            input_shape=(None, None, num_channels),
+            include_rescaling=False,
+        )
+        self.assertEqual(model.output_shape, (None, None, None, 2048))
+
+    @parameterized.named_parameters(
+        ("18", ResNet18Backbone),
+        ("50", ResNet50Backbone),
+        ("101", ResNet101Backbone),
+        ("152", ResNet152Backbone),
+    )
+    def test_specific_arch_forward_pass(self, arch_class):
+        backbone = arch_class()
+        backbone(tf.random.uniform(shape=[2, 256, 256, 3]))
 
 
 if __name__ == "__main__":
