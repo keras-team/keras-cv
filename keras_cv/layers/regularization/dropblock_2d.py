8cc7015df4ba7d1fa27a64a28c6874cec2bfe8b3
ID6109 <ya6109@gmail.com>
2023-03-16 23:37:38 +0530

    Minor change

--- keras_cv/layers/regularization/dropblock_2d.py
+++ keras_cv/layers/regularization/dropblock_2d.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import tensorflow as tf
-from keras.__internal__.layers import BaseRandomLayer
+from tensorflow.keras.__internal__.layers import BaseRandomLayer
 
 from keras_cv.utils import conv_utils
 
