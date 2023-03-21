75e80a988b01591fa6793fdd0b2bdebd9b9b4a91
Nihar Jani <niharmaheshjani@gmail.com>
2023-03-21 11:49:04 +0530

    Replace tf.keras with keras throughout resenet_v1.py

--- keras_cv/models/resnet_v1.py
+++ keras_cv/models/resnet_v1.py
@@ -21,8 +21,8 @@ import types
 
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import backend
-from tensorflow.keras import layers
+from keras import backend
+from keras import layers
 
 from keras_cv.models import utils
 from keras_cv.models.weights import parse_weights
