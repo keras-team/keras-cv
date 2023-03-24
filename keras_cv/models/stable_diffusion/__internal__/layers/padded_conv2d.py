035950c8e434f3ca4c73c7f8b7c88a930478bd18
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2023-03-24 16:54:36 -0600

    Fix padding issue in StableDiffusion encoder

--- keras_cv/models/stable_diffusion/__internal__/layers/padded_conv2d.py
+++ keras_cv/models/stable_diffusion/__internal__/layers/padded_conv2d.py
@@ -16,10 +16,20 @@ from tensorflow import keras
 
 
 class PaddedConv2D(keras.layers.Layer):
-    def __init__(self, filters, kernel_size, padding=0, strides=1, **kwargs):
+    def __init__(
+        self,
+        filters,
+        kernel_size,
+        padding=0,
+        strides=1,
+        conv_padding="valid",
+        **kwargs
+    ):
         super().__init__(**kwargs)
         self.padding2d = keras.layers.ZeroPadding2D(padding)
-        self.conv2d = keras.layers.Conv2D(filters, kernel_size, strides=strides)
+        self.conv2d = keras.layers.Conv2D(
+            filters, kernel_size, strides=strides, padding=conv_padding
+        )
 
     def call(self, inputs):
         x = self.padding2d(inputs)
