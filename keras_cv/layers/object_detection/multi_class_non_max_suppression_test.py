d5c53d20eeefcf06ec5cc03ddac43c611b3bb7b9
Luke Wood <LukeWood@users.noreply.github.com>
2023-03-28 10:07:30 -0700

    Fix CI (#1599)

--- keras_cv/layers/object_detection/multi_class_non_max_suppression_test.py
+++ keras_cv/layers/object_detection/multi_class_non_max_suppression_test.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
-
 import tensorflow as tf
 
 from keras_cv import layers as cv_layers
