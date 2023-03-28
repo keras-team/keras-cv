0188e060c16c83fef776ed965380a048a2a48027
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2023-03-28 08:43:22 -0600

    Add  license to init

---
+++ keras_cv/layers/preprocessing_3d/waymo/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2022 Waymo LLC.
+#
+# Licensed under the terms in https://github.com/waymo-research/waymo-open-dataset/blob/master/src/waymo_open_dataset/wdl_limited/camera/LICENSE
+# and https://github.com/waymo-research/waymo-open-dataset/blob/master/src/waymo_open_dataset/wdl_limited/camera/PATENTS
