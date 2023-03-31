2530f325ab23b834c20bf23895fe6c3e2373c53a
Sabri Barac <sabribarac@gmail.com>
2023-04-01 01:50:13 +0200

    removed rest of five crop

--- keras_cv/layers/__init__.py
+++ keras_cv/layers/__init__.py
@@ -33,7 +33,6 @@ from keras_cv.layers.preprocessing.base_image_augmentation_layer import (
 from keras_cv.layers.preprocessing.channel_shuffle import ChannelShuffle
 from keras_cv.layers.preprocessing.cut_mix import CutMix
 from keras_cv.layers.preprocessing.equalization import Equalization
-from keras_cv.layers.preprocessing.five_crop import FiveCrop
 from keras_cv.layers.preprocessing.fourier_mix import FourierMix
 from keras_cv.layers.preprocessing.grayscale import Grayscale
 from keras_cv.layers.preprocessing.grid_mask import GridMask
