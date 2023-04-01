3df13b5546bf5a04c7ef8717003951824c690f62
Sabri Barac <sabribarac@gmail.com>
2023-04-01 19:14:12 +0200

    added added rest of vgg19 to migrate

--- keras_cv/models/backbones/vgg19/vgg19_backbone.py
+++ keras_cv/models/backbones/vgg19/vgg19_backbone.py
@@ -17,13 +17,19 @@
 Reference:
   - [Very Deep Convolutional Networks for Large-Scale Image Recognition](https://arxiv.org/abs/1409.1556) (ICLR 2015)
 """
+import copy
+
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
 from keras_cv.models import utils
-from keras_cv.models.backbones.vgg19.vgg19_backbone import apply_vgg_block
+from keras_cv.models.backbones.vgg19.vgg16_backbone import apply_vgg_block
 from keras_cv.models.backbones.backbone import Backbone
+from keras_cv.models.backbones.vgg19.vgg19_backbone_presets import (
+    backbone_presets,
+)
+from keras_cv.utils.python_utils import classproperty
 
 @keras.utils.register_keras_serializable(package="keras_cv.models")
 class VGG19Backbone(Backbone):
@@ -34,65 +40,29 @@ class VGG19Backbone(Backbone):
     Args:
       include_rescaling: bool, whether or not to Rescale the inputs.If set to True,
         inputs will be passed through a `Rescaling(1/255.0)` layer.
-      include_top: bool, whether to include the 3 fully-connected
-        layers at the top of the network. If provided, num_classes must be provided.
-      num_classes: int, optional number of classes to classify images into, only to be
-        specified if `include_top` is True.
-      weights: os.PathLike or None, one of `None` (random initialization), or a pretrained weight file path.
       input_shape: tuple, optional shape tuple, defaults to (224, 224, 3).
       input_tensor: Tensor, optional Keras tensor (i.e. output of `layers.Input()`)
         to use as image input for the model.
-      pooling: bool, Optional pooling mode for feature extraction
-        when `include_top` is `False`.
-        - `None` means that the output of the model will be
-            the 4D tensor output of the
-            last convolutional block.
-        - `avg` means that global average pooling
-            will be applied to the output of the
-            last convolutional block, and thus
-            the output of the model will be a 2D tensor.
-        - `max` means that global max pooling will
-            be applied.
-      classifier_activation:`str` or callable. The activation function to use
-        on the "top" layer. Ignored unless `include_top=True`. Set
-        `classifier_activation=None` to return the logits of the "top" layer.
-        When loading pretrained weights, `classifier_activation` can only
-        be `None` or `"softmax"`.
-      name: (Optional) name to pass to the model.  Defaults to "VGG19".
-    Returns:
-      A `keras.Model` instance.
+    
+    ```python
+    input_data = tf.ones(shape=(3, 224, 224, 3))
+
+    # Randomly initialized backbone with a custom config
+    model = VGG19Backbone(
+        include_rescaling=False,
+    )
+    output = model(input_data)
+    ```
     """
 
     def __init__(
         self,
+        *,
         include_rescaling,
-        include_top,
         input_tensor=None,
-        num_classes=None,
-        weights=None,
         input_shape=(224, 224, 3),
-        pooling=None,
-        classifier_activation="softmax",
-        name="VGG19",
         **kwargs,
     ):
-        if weights and not tf.io.gfile.exists(weights):
-            raise ValueError(
-                "The `weights` argument should be either `None` or the path to the "
-                "weights file to be loaded. Weights file not found at location: {weights}"
-            )
-
-        if include_top and not num_classes:
-            raise ValueError(
-                "If `include_top` is True, you should specify `num_classes`. "
-                f"Received: num_classes={num_classes}"
-            )
-
-        if include_top and pooling:
-            raise ValueError(
-                f"`pooling` must be `None` when `include_top=True`."
-                f"Received pooling={pooling} and include_top={include_top}. "
-            )
 
         inputs = utils.parse_model_inputs(input_shape, input_tensor)
         x = inputs
@@ -155,45 +125,23 @@ class VGG19Backbone(Backbone):
             name="block5",
         )
 
-        if include_top:
-            x = layers.Flatten(name="flatten")(x)
-            x = layers.Dense(4096, activation="relu", name="fc1")(x)
-            x = layers.Dense(4096, activation="relu", name="fc2")(x)
-            x = layers.Dense(
-                num_classes,
-                activation=classifier_activation,
-                name="predictions",
-            )(x)
-        else:
-            if pooling == "avg":
-                x = layers.GlobalAveragePooling2D()(x)
-            elif pooling == "max":
-                x = layers.GlobalMaxPooling2D()(x)
-
-        super().__init__(inputs=inputs, outputs=x, name=name, **kwargs)
-        if weights is not None:
-            self.load_weights(weights)
+        super().__init__(inputs=inputs, outputs=x, **kwargs)
 
         self.include_rescaling = include_rescaling
-        self.include_top = include_top
-        self.num_classes = num_classes
         self.input_tensor = input_tensor
-        self.pooling = pooling
-        self.classifier_activation = classifier_activation
 
     def get_config(self):
-        return {
-            "include_rescaling": self.include_rescaling,
-            "include_top": self.include_top,
-            "name": self.name,
-            "input_shape": self.input_shape[1:],
-            "input_tensor": self.input_tensor,
-            "pooling": self.pooling,
-            "num_classes": self.num_classes,
-            "classifier_activation": self.classifier_activation,
-            "trainable": self.trainable,
-        }
+        config = super().get_config()
+        config.update(
+            {
+                "include_rescaling": self.include_rescaling,
+                "input_shape": self.input_shape[1:],
+                "input_tensor": self.input_tensor,
+            }
+        )
+        return config
 
-    @classmethod
-    def from_config(cls, config):
-        return cls(**config)
+    @classproperty
+    def presets(cls):
+        """Dictionary of preset names and configurations."""
+        return copy.deepcopy(backbone_presets)
