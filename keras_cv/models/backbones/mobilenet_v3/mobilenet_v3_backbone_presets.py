b3d4423a456f8de199337f0022f5f2801b437bac
ID6109 <ya6109@gmail.com>
2023-04-01 20:52:11 +0530

    Minor Fix

--- keras_cv/models/backbones/mobilenet_v3/mobilenet_v3_backbone_presets.py
+++ keras_cv/models/backbones/mobilenet_v3/mobilenet_v3_backbone_presets.py
@@ -16,94 +16,91 @@
 
 from tensorflow.keras import layers
 
-from keras_cv.models.backbones.mobilenet_v3.mobilenet_v3_backbone import (
-    apply_inverted_res_block,
-)
-from keras_cv.models.backbones.mobilenet_v3.mobilenet_v3_backbone import depth
+import keras_cv.models.backbones.mobilenet_v3.mobilenet_v3_backbone as backbone
 
 
 def stack_fn_s(x, kernel, activation, se_ratio, alpha=1.0):
-    x = apply_inverted_res_block(
-        x, 1, depth(16 * alpha), 3, 2, se_ratio, layers.ReLU(), 0
+    x = backbone.apply_inverted_res_block(
+        x, 1, backbone.depth(16 * alpha), 3, 2, se_ratio, layers.ReLU(), 0
     )
-    x = apply_inverted_res_block(
-        x, 72.0 / 16, depth(24 * alpha), 3, 2, None, layers.ReLU(), 1
+    x = backbone.apply_inverted_res_block(
+        x, 72.0 / 16, backbone.depth(24 * alpha), 3, 2, None, layers.ReLU(), 1
     )
-    x = apply_inverted_res_block(
-        x, 88.0 / 24, depth(24 * alpha), 3, 1, None, layers.ReLU(), 2
+    x = backbone.apply_inverted_res_block(
+        x, 88.0 / 24, backbone.depth(24 * alpha), 3, 1, None, layers.ReLU(), 2
     )
-    x = apply_inverted_res_block(
-        x, 4, depth(40 * alpha), kernel, 2, se_ratio, activation, 3
+    x = backbone.apply_inverted_res_block(
+        x, 4, backbone.depth(40 * alpha), kernel, 2, se_ratio, activation, 3
     )
-    x = apply_inverted_res_block(
-        x, 6, depth(40 * alpha), kernel, 1, se_ratio, activation, 4
+    x = backbone.apply_inverted_res_block(
+        x, 6, backbone.depth(40 * alpha), kernel, 1, se_ratio, activation, 4
     )
-    x = apply_inverted_res_block(
-        x, 6, depth(40 * alpha), kernel, 1, se_ratio, activation, 5
+    x = backbone.apply_inverted_res_block(
+        x, 6, backbone.depth(40 * alpha), kernel, 1, se_ratio, activation, 5
     )
-    x = apply_inverted_res_block(
-        x, 3, depth(48 * alpha), kernel, 1, se_ratio, activation, 6
+    x = backbone.apply_inverted_res_block(
+        x, 3, backbone.depth(48 * alpha), kernel, 1, se_ratio, activation, 6
     )
-    x = apply_inverted_res_block(
-        x, 3, depth(48 * alpha), kernel, 1, se_ratio, activation, 7
+    x = backbone.apply_inverted_res_block(
+        x, 3, backbone.depth(48 * alpha), kernel, 1, se_ratio, activation, 7
     )
-    x = apply_inverted_res_block(
-        x, 6, depth(96 * alpha), kernel, 2, se_ratio, activation, 8
+    x = backbone.apply_inverted_res_block(
+        x, 6, backbone.depth(96 * alpha), kernel, 2, se_ratio, activation, 8
     )
-    x = apply_inverted_res_block(
-        x, 6, depth(96 * alpha), kernel, 1, se_ratio, activation, 9
+    x = backbone.apply_inverted_res_block(
+        x, 6, backbone.depth(96 * alpha), kernel, 1, se_ratio, activation, 9
     )
-    x = apply_inverted_res_block(
-        x, 6, depth(96 * alpha), kernel, 1, se_ratio, activation, 10
+    x = backbone.apply_inverted_res_block(
+        x, 6, backbone.depth(96 * alpha), kernel, 1, se_ratio, activation, 10
     )
     return x
 
 
 def stack_fn_l(x, kernel, activation, se_ratio, alpha=1.0):
-    x = apply_inverted_res_block(
-        x, 1, depth(16 * alpha), 3, 1, None, layers.ReLU(), 0
+    x = backbone.apply_inverted_res_block(
+        x, 1, backbone.depth(16 * alpha), 3, 1, None, layers.ReLU(), 0
     )
-    x = apply_inverted_res_block(
-        x, 4, depth(24 * alpha), 3, 2, None, layers.ReLU(), 1
+    x = backbone.apply_inverted_res_block(
+        x, 4, backbone.depth(24 * alpha), 3, 2, None, layers.ReLU(), 1
     )
-    x = apply_inverted_res_block(
-        x, 3, depth(24 * alpha), 3, 1, None, layers.ReLU(), 2
+    x = backbone.apply_inverted_res_block(
+        x, 3, backbone.depth(24 * alpha), 3, 1, None, layers.ReLU(), 2
     )
-    x = apply_inverted_res_block(
-        x, 3, depth(40 * alpha), kernel, 2, se_ratio, layers.ReLU(), 3
+    x = backbone.apply_inverted_res_block(
+        x, 3, backbone.depth(40 * alpha), kernel, 2, se_ratio, layers.ReLU(), 3
     )
-    x = apply_inverted_res_block(
-        x, 3, depth(40 * alpha), kernel, 1, se_ratio, layers.ReLU(), 4
+    x = backbone.apply_inverted_res_block(
+        x, 3, backbone.depth(40 * alpha), kernel, 1, se_ratio, layers.ReLU(), 4
     )
-    x = apply_inverted_res_block(
-        x, 3, depth(40 * alpha), kernel, 1, se_ratio, layers.ReLU(), 5
+    x = backbone.apply_inverted_res_block(
+        x, 3, backbone.depth(40 * alpha), kernel, 1, se_ratio, layers.ReLU(), 5
     )
-    x = apply_inverted_res_block(
-        x, 6, depth(80 * alpha), 3, 2, None, activation, 6
+    x = backbone.apply_inverted_res_block(
+        x, 6, backbone.depth(80 * alpha), 3, 2, None, activation, 6
     )
-    x = apply_inverted_res_block(
-        x, 2.5, depth(80 * alpha), 3, 1, None, activation, 7
+    x = backbone.apply_inverted_res_block(
+        x, 2.5, backbone.depth(80 * alpha), 3, 1, None, activation, 7
     )
-    x = apply_inverted_res_block(
-        x, 2.3, depth(80 * alpha), 3, 1, None, activation, 8
+    x = backbone.apply_inverted_res_block(
+        x, 2.3, backbone.depth(80 * alpha), 3, 1, None, activation, 8
     )
-    x = apply_inverted_res_block(
-        x, 2.3, depth(80 * alpha), 3, 1, None, activation, 9
+    x = backbone.apply_inverted_res_block(
+        x, 2.3, backbone.depth(80 * alpha), 3, 1, None, activation, 9
     )
-    x = apply_inverted_res_block(
-        x, 6, depth(112 * alpha), 3, 1, se_ratio, activation, 10
+    x = backbone.apply_inverted_res_block(
+        x, 6, backbone.depth(112 * alpha), 3, 1, se_ratio, activation, 10
     )
-    x = apply_inverted_res_block(
-        x, 6, depth(112 * alpha), 3, 1, se_ratio, activation, 11
+    x = backbone.apply_inverted_res_block(
+        x, 6, backbone.depth(112 * alpha), 3, 1, se_ratio, activation, 11
     )
-    x = apply_inverted_res_block(
-        x, 6, depth(160 * alpha), kernel, 2, se_ratio, activation, 12
+    x = backbone.apply_inverted_res_block(
+        x, 6, backbone.depth(160 * alpha), kernel, 2, se_ratio, activation, 12
     )
-    x = apply_inverted_res_block(
-        x, 6, depth(160 * alpha), kernel, 1, se_ratio, activation, 13
+    x = backbone.apply_inverted_res_block(
+        x, 6, backbone.depth(160 * alpha), kernel, 1, se_ratio, activation, 13
     )
-    x = apply_inverted_res_block(
-        x, 6, depth(160 * alpha), kernel, 1, se_ratio, activation, 14
+    x = backbone.apply_inverted_res_block(
+        x, 6, backbone.depth(160 * alpha), kernel, 1, se_ratio, activation, 14
     )
     return x
 
