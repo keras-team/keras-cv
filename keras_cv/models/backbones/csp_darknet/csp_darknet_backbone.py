5d8213814b677ada3f1e2a54776a2ba55b0da699
ID6109 <ya6109@gmail.com>
2023-04-01 21:22:28 +0530

    Update the docstrings

--- keras_cv/models/backbones/csp_darknet/csp_darknet_backbone.py
+++ keras_cv/models/backbones/csp_darknet/csp_darknet_backbone.py
@@ -41,95 +41,56 @@ from keras_cv.models.backbones.csp_darknet.csp_darknet_backbone_presets import (
 )
 from keras_cv.utils.python_utils import classproperty
 
-BASE_DOCSTRING = """Represents the {name} architecture.
-    The CSPDarkNet architectures are commonly used for detection tasks. It is
-    possible to extract the intermediate dark2 to dark5 layers from the model for
-    creating a feature pyramid Network.
-    Reference:
-        - [YoloV4 Paper](https://arxiv.org/abs/1804.02767)
-        - [CSPNet Paper](https://arxiv.org/pdf/1911.11929)
-        - [YoloX Paper](https://arxiv.org/abs/2107.08430)
-        - [YoloX implementation](https://github.com/ultralytics/yolov3)
-    For transfer learning use cases, make sure to read the
-    [guide to transfer learning & fine-tuning](https://keras.io/guides/transfer_learning/).
-    Args:
-        include_rescaling: bool, whether or not to rescale the inputs. If set to True,
-            inputs will be passed through a `Rescaling(1/255.0)` layer.
-        include_top: bool, whether to include the fully-connected layer at the top of
-            the network. If provided, `num_classes` must be provided.
-        use_depthwise: a boolean value used to decide whether a depthwise conv block
-            should be used over a regular darknet block. Defaults to False
-        num_classes: integer, optional number of classes to classify images into. Only to be
-            specified if `include_top` is True.
-        weights: one of `None` (random initialization), a pretrained weight file
-            path, or a reference to pre-trained weights (e.g. 'imagenet/classification')
-            (see available pre-trained weights in weights.py)
-        input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
-            to use as image input for the model.
-        input_shape: optional shape tuple, defaults to (None, None, 3).
-        pooling: optional pooling mode for feature extraction when `include_top`
-            is `False`.
-            - `None` means that the output of the model will be the 4D tensor output
-                of the last convolutional block.
-            - `avg` means that global average pooling will be applied to the
-                output of the last convolutional block, and thus the output of the
-                model will be a 2D tensor.
-            - `max` means that global max pooling will be applied.
-        classifier_activation: A `str` or callable. The activation function to use
-            on the "top" layer. Ignored unless `include_top=True`. Set
-            `classifier_activation=None` to return the logits of the "top" layer.
-        name: string, optional name to pass to the model, defaults to "{name}".
-    Returns:
-        A `keras.Model` instance.
-"""
-
 
 @keras.utils.register_keras_serializable(package="keras_cv.models")
 class CSPDarkNetBackbone(Backbone):
-    """This class represents the CSPDarkNet architecture.
-    Although the DarkNet architecture is commonly used for detection tasks, it is
-    possible to extract the intermediate dark2 to dark5 layers from the model for
-    creating a feature pyramid Network.
+    """Instantiates the CSPDarkNet architecture.
+
+    Although the DarkNet architecture is commonly used for detection tasks, it
+    is possible to extract the intermediate dark2 to dark5 layers from the model
+    for creating a feature pyramid Network.
     Reference:
         - [YoloV4 Paper](https://arxiv.org/abs/1804.02767)
         - [CSPNet Paper](https://arxiv.org/pdf/1911.11929)
         - [YoloX Paper](https://arxiv.org/abs/2107.08430)
         - [YoloX implementation](https://github.com/ultralytics/yolov3)
+
     For transfer learning use cases, make sure to read the
-    [guide to transfer learning & fine-tuning](https://keras.io/guides/transfer_learning/).
+    [guide to transfer learning & fine-tuning]
+    (https://keras.io/guides/transfer_learning/).
+
     Args:
-        depth_multiplier: A float value used to calculate the base depth of the model
-            this changes based the detection model being used.
-        width_multiplier: A float value used to calculate the base width of the model
-            this changes based the detection model being used.
-        include_rescaling: bool ,whether or not to Rescale the inputs.If set to True,
+        depth_multiplier: A float value used to calculate the base depth of the
+            model this changes based the detection model being used.
+        width_multiplier: A float value used to calculate the base width of the
+            model this changes based the detection model being used.
+        include_rescaling: bool, whether to rescale the inputs. If set to True,
             inputs will be passed through a `Rescaling(1/255.0)` layer.
-        include_top: bool, whether to include the fully-connected layer at the top of
-            the network.  If provided, `num_classes` must be provided.
-        use_depthwise: a boolean value used to decide whether a depthwise conv block
-            should be used over a regular darknet block. Defaults to False
-        num_classes: optional int,optional number of classes to classify images into, only to be
-            specified if `include_top` is True.
-        weights: one of `None` (random initialization), a pretrained weight file
-            path, or a reference to pre-trained weights (e.g. 'imagenet/classification')
-            (see available pre-trained weights in weights.py)
+        use_depthwise: a boolean value used to decide whether a depthwise conv
+            block should be used over a regular darknet block. Defaults to
+            False.
         input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
             to use as image input for the model.
-        input_shape: optional shape tuple, defaults to (None, None, 3).
-        pooling: optional pooling mode for feature extraction when `include_top`
-            is `False`.
-            - `None` means that the output of the model will be the 4D tensor output
-                of the last convolutional block.
-            - `avg` means that global average pooling will be applied to the
-                output of the last convolutional block, and thus the output of the
-                model will be a 2D tensor.
-            - `max` means that global max pooling will be applied.
-        classifier_activation: A `str` or callable. The activation function to use
-            on the "top" layer. Ignored unless `include_top=True`. Set
-            `classifier_activation=None` to return the logits of the "top" layer.
-        name: (Optional) name to pass to the model.  Defaults to "CSPDarkNet".
-    Returns:
-        A `keras.Model` instance.
+        input_shape: optional shape tuple. Defaults to (None, None, 3).
+
+    Examples:
+    ```python
+    input_data = tf.ones(shape=(8, 224, 224, 3))
+
+    # Pretrained backbone
+    model = keras_cv.models.CSPDarkNetBackbone.from_preset(
+        "cspdarknettiny_imagenet"
+    )
+    output = model(input_data)
+
+    # Randomly initialized backbone with a custom config
+    model = ResNetBackbone(
+        depth_multiplier=0.33,
+        width_multiplier=0.375,
+        include_rescaling=False,
+    )
+    output = model(input_data)
+    ```
     """
 
     def __init__(
@@ -247,8 +208,41 @@ class CSPDarkNetBackbone(Backbone):
 
     @classproperty
     def presets_with_weights(cls):
-        """Dictionary of preset names and configurations that include weights."""
+        """Dictionary of preset names and configurations that include
+        weights."""
         return copy.deepcopy(backbone_presets_with_weights)
+
+
+ALIAS_DOCSTRING = """CSPDarkNetBackbone model with {depth_multiplier} depth
+    multiplier and {width_multiplier} width multiplier.
+
+    The CSPDarkNet architectures are commonly used for detection tasks. It is
+    possible to extract the intermediate dark2 to dark5 layers from the model
+    for creating a feature pyramid Network.
+    Reference:
+        - [YoloV4 Paper](https://arxiv.org/abs/1804.02767)
+        - [CSPNet Paper](https://arxiv.org/pdf/1911.11929)
+        - [YoloX Paper](https://arxiv.org/abs/2107.08430)
+        - [YoloX implementation](https://github.com/ultralytics/yolov3)
+    For transfer learning use cases, make sure to read the
+    [guide to transfer learning & fine-tuning]
+    (https://keras.io/guides/transfer_learning/).
+    Args:
+        include_rescaling: bool, whether or not to rescale the inputs. If set to
+            True, inputs will be passed through a `Rescaling(1/255.0)` layer.
+        input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
+            to use as image input for the model.
+        input_shape: optional shape tuple. Defaults to (None, None, 3).
+
+    Examples:
+    ```python
+    input_data = tf.ones(shape=(8, 224, 224, 3))
+
+    # Randomly initialized backbone
+    model = CSPDarkNetTinyBackbone()
+    output = model(input_data)
+    ```
+"""
 
 
 class CSPDarkNetTinyBackbone(CSPDarkNetBackbone):
@@ -276,7 +270,8 @@ class CSPDarkNetTinyBackbone(CSPDarkNetBackbone):
 
     @classproperty
     def presets_with_weights(cls):
-        """Dictionary of preset names and configurations that include weights."""
+        """Dictionary of preset names and configurations that include
+        weights."""
         return {}
 
 
@@ -353,7 +348,8 @@ class CSPDarkNetLBackbone(CSPDarkNetBackbone):
 
     @classproperty
     def presets_with_weights(cls):
-        """Dictionary of preset names and configurations that include weights."""
+        """Dictionary of preset names and configurations that include
+        weights."""
         return {}
 
 
@@ -384,17 +380,25 @@ class CSPDarkNetXBackbone(CSPDarkNetBackbone):
 setattr(
     CSPDarkNetTinyBackbone,
     "__doc__",
-    BASE_DOCSTRING.format(name="CSPDarkNetTiny"),
+    ALIAS_DOCSTRING.format(depth_multiplier="0.33", width_multiplier="0.375"),
 )
 setattr(
-    CSPDarkNetSBackbone, "__doc__", BASE_DOCSTRING.format(name="CSPDarkNetS")
+    CSPDarkNetSBackbone,
+    "__doc__",
+    ALIAS_DOCSTRING.format(depth_multiplier="0.33", width_multiplier="0.50"),
 )
 setattr(
-    CSPDarkNetMBackbone, "__doc__", BASE_DOCSTRING.format(name="CSPDarkNetM")
+    CSPDarkNetMBackbone,
+    "__doc__",
+    ALIAS_DOCSTRING.format(depth_multiplier="0.67", width_multiplier="0.75"),
 )
 setattr(
-    CSPDarkNetLBackbone, "__doc__", BASE_DOCSTRING.format(name="CSPDarkNetL")
+    CSPDarkNetLBackbone,
+    "__doc__",
+    ALIAS_DOCSTRING.format(depth_multiplier="1.00", width_multiplier="1.00"),
 )
 setattr(
-    CSPDarkNetXBackbone, "__doc__", BASE_DOCSTRING.format(name="CSPDarkNetX")
+    CSPDarkNetXBackbone,
+    "__doc__",
+    ALIAS_DOCSTRING.format(depth_multiplier="1.33", width_multiplier="1.25"),
 )
