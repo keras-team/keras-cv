6eea5e9898d46d547957bffa871162966927b236
ID6109 <ya6109@gmail.com>
2023-04-01 23:17:17 +0530

    Add unit tests

--- keras_cv/models/backbones/mobilenet_v3/mobilenet_v3_backbone_presets_test.py
+++ keras_cv/models/backbones/mobilenet_v3/mobilenet_v3_backbone_presets_test.py
@@ -11,3 +11,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import pytest
+import tensorflow as tf
+from absl.testing import parameterized
+
+from keras_cv.models.backbones.mobilenet_v3.mobilenet_v3_backbone import (
+    MobileNetV3Backbone,
+)
+
+
+@pytest.mark.extra_large
+class MobileNetV3PresetFullTest(tf.test.TestCase, parameterized.TestCase):
+    """
+    Test the full enumeration of our preset.
+    This tests every preset for MobileNetV3 and is only run manually.
+    Run with:
+    `pytest keras_cv/models/backbones/mobilenet_v3/mobilenet_v3_backbone_presets_test.py --run_extra_large`
+    """
+
+    def test_load_resnet(self):
+        input_data = tf.ones(shape=(2, 224, 224, 3))
+        for preset in MobileNetV3Backbone.presets:
+            model = MobileNetV3Backbone.from_preset(preset)
+            model(input_data)
