307f527905b538469a6c1a3c9abe51acf1ca06e9
Kethupio <bhattandbhatt.999@gmail.com>
2023-04-02 06:24:26 +0530

    Update efficientnet_lite_backbone.py

--- keras_cv/models/backbones/efficientnet_lite/efficientnet_lite_backbone.py
+++ keras_cv/models/backbones/efficientnet_lite/efficientnet_lite_backbone.py
@@ -30,7 +30,7 @@ from tensorflow.keras import layers
 
 from keras_cv.models import utils
 
-from keras_cv.models.weights import parse_weights
+# from keras_cv.models.weights import parse_weights
 from keras_cv.models.backbones.backbone import Backbone
 from keras_cv.models.backbones.efficientnet_lite.efficientnet_lite_backbone_presets import (
     backbone_presets,
