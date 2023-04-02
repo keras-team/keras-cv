888d0b565e856a263e7d65eec4781ebdd0095429
Kethupio <bhattandbhatt.999@gmail.com>
2023-04-02 05:36:58 +0530

    Update __init__.py

--- keras_cv/models/__init__.py
+++ keras_cv/models/__init__.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from keras_cv.models.backbones.efficientnet_lite.efficientnet_lite_backbone import (
-    EfficientNetLiteB0Backbone,
-)
+# from keras_cv.models.backbones.efficientnet_lite.efficientnet_lite_backbone import (
+#     EfficientNetLiteB0Backbone,
+# )
 
 # from keras_cv.models.backbones.efficientnet_lite.efficientnet_lite_backbone import (
 #     EfficientNetLiteB1Backbone,
