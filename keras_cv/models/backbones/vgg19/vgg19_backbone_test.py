517e3802cc9ec3eda23ce4279813e0a7e5fcbfa0
Sabri Barac <sabribarac@gmail.com>
2023-04-02 16:17:29 +0200

    added tests for vgg19 backbone

--- keras_cv/models/backbones/vgg19/vgg19_backbone_test.py
+++ keras_cv/models/backbones/vgg19/vgg19_backbone_test.py
@@ -12,38 +12,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 import tensorflow as tf
 from absl.testing import parameterized
 
-from keras_cv.models.backbones.vgg19 import vgg19_backbone as vgg19
-from keras_cv.models.models_test import ModelsTest
+from keras_cv.models.backbones.vgg19.vgg19_backbone import VGG19Backbone
+from tensorflow import keras
 
-MODEL_LIST = [
-    (vgg19.VGG19Backbone, 512, {}),
-]
+class VGG19BackboneTest(tf.test.TestCase, parameterized.TestCase):
+    def setUp(self):
+        self.input_batch = tf.ones(shape=(2, 224, 224, 3))
 
+    def test_valid_call(self):
+        model = VGG19Backbone(
+            include_rescaling=False
+        )
+        model(self.input_batch)
 
-class VGG19BackboneTest(ModelsTest, tf.test.TestCase, parameterized.TestCase):
-    @parameterized.parameters(*MODEL_LIST)
-    def test_application_base(self, app, _, args):
-        super()._test_application_base(app, _, args)
+    def test_valid_call_applications_model(self):
+        model = VGG19Backbone()
+        model(self.input_batch)
+    
+    @parameterized.named_parameters(
+        ("tf_format", "tf", "model"),
+        ("keras_format", "keras_v3", "model.keras")
+    )
+    def test_saved_alias_model(self, save_format, filename):
+        model = VGG19Backbone()
 
-    @parameterized.parameters(*MODEL_LIST)
-    def test_application_with_rescaling(self, app, last_dim, args):
-        super()._test_application_with_rescaling(app, last_dim, args)
+        model_output = model(self.input_batch)
+        save_path = os.path.join(self.get_temp_dir(), filename)
+        model.save(save_path, save_format=save_format)
+        restored_model = keras.models.load_model(save_path)
 
-    @parameterized.parameters(*MODEL_LIST)
-    def test_application_pooling(self, app, last_dim, args):
-        super()._test_application_pooling(app, last_dim, args)
+        self.assertIsInstance(restored_model, VGG19Backbone)
 
-    @parameterized.parameters(*MODEL_LIST)
-    def test_application_variable_input_channels(self, app, last_dim, args):
-        super()._test_application_variable_input_channels(app, last_dim, args)
-
-    @parameterized.parameters(*MODEL_LIST)
-    def test_model_can_be_used_as_backbone(self, app, last_dim, args):
-        super()._test_model_can_be_used_as_backbone(app, last_dim, args)
-
-
+        restored_output = restored_model(self.input_batch)
+        self.assertAllClose(model_output, restored_output)
+######
 if __name__ == "__main__":
     tf.test.main()
