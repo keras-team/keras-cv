933f5e7a86bb8d01fbfe3796d94d705adb8950fb
Ian Stenbit <3072903+ianstenbit@users.noreply.github.com>
2023-04-03 23:10:46 -0600

    Apply sample weight to box regression loss in CenterNet (#1650)

--- keras_cv/models/object_detection_3d/center_pillar.py
+++ keras_cv/models/object_detection_3d/center_pillar.py
@@ -214,16 +214,23 @@ class MultiHeadCenterPillar(keras.Model):
             prediction = predictions[head_name]
             heatmap_pred = tf.nn.softmax(prediction[..., :2])[..., 1]
             box_pred = prediction[..., 2:]
+
             box = targets[head_name]["boxes"]
             heatmap = targets[head_name]["heatmap"]
             index = targets[head_name]["top_k_index"]
+
             # the prediction returns 2 outputs for background vs object
             y_pred["heatmap_" + head_name] = heatmap_pred
             y_true["heatmap_" + head_name] = heatmap
             sample_weight["heatmap_" + head_name] = tf.ones_like(heatmap)
-            # heatmap_groundtruth_gather = tf.gather_nd(heatmap, index, batch_dims=1)
-            # TODO(tanzhenyu): loss heatmap threshold be configurable.
-            # box_regression_mask = heatmap_groundtruth_gather >= 0.95
+
+            # TODO(ianstenbit): loss heatmap threshold should be configurable.
+            box_regression_mask = (
+                tf.gather_nd(heatmap, index, batch_dims=1) >= 0.95
+            )
+            sample_weight["box_" + head_name] = tf.cast(
+                box_regression_mask, tf.float32
+            )
             box = tf.gather_nd(box, index, batch_dims=1)
             box_pred = tf.gather_nd(box_pred, index, batch_dims=1)
             y_pred["box_" + head_name] = tf.squeeze(box_pred)
