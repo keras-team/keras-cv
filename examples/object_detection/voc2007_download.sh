0786be34318145d3e0b591d01541d88b46a53c50
davide.cozzolino <davide.cozzolino@unina.it>
2023-04-07 18:28:13 +0000

    fix script

--- examples/object_detection/voc2007_download.sh
+++ examples/object_detection/voc2007_download.sh
@@ -1,11 +1,11 @@
-if [ ! -f voc2007_trainval ]
+if [ ! -d voc2007_trainval ]
 then
   wget -nc http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
   mkdir voc2007_trainval
   tar -xvf './VOCtrainval_06-Nov-2007.tar' -C voc2007_trainval
 fi
 
-if [ ! -f voc2007_test ]
+if [ ! -d voc2007_test ]
 then
   wget -nc http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
   mkdir voc2007_test
