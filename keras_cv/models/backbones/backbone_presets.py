2f616894b6d5531316d0375074b049fefc37da33
Kethupio <bhattandbhatt.999@gmail.com>
2023-04-14 05:43:02 +0530

    Update backbone_presets.py

--- keras_cv/models/backbones/backbone_presets.py
+++ keras_cv/models/backbones/backbone_presets.py
@@ -13,15 +13,15 @@
 # limitations under the License.
 """All Backbone presets"""
 
-from keras_cv.models.backbones.efficientnet_lite import
-    efficientnet_lite_backbone_presets
+from keras_cv.models.backbones.effnet_lite import
+    effnet_lite_backbone_presets
 from keras_cv.models.backbones.resnet_v1 import resnet_v1_backbone_presets
 from keras_cv.models.backbones.resnet_v2 import resnet_v2_backbone_presets
 
 backbone_presets_no_weights = {
     **resnet_v1_backbone_presets.backbone_presets_no_weights,
     **resnet_v2_backbone_presets.backbone_presets_no_weights,
-    **efficientnet_lite_backbone_presets.backbone_presets_no_weights,
+    **effnet_lite_backbone_presets.backbone_presets_no_weights,
 }
 
 backbone_presets_with_weights = {
