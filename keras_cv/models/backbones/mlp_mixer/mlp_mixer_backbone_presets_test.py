106c511ce0ac588633ef669982ae268149a35bb8
ID6109 <ya6109@gmail.com>
2023-04-16 11:23:01 +0530

    Add unit tests

--- keras_cv/models/backbones/mlp_mixer/mlp_mixer_backbone_presets_test.py
+++ keras_cv/models/backbones/mlp_mixer/mlp_mixer_backbone_presets_test.py
@@ -11,3 +11,29 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""Tests for loading pretrained model presets."""
+
+import pytest
+import tensorflow as tf
+from absl.testing import parameterized
+
+from keras_cv.models.backbones.mlp_mixer.mlp_mixer_backbone import (
+    MLPMixerBackbone,
+)
+
+
+@pytest.mark.extra_large
+class MLPMixerPresetFullTest(tf.test.TestCase, parameterized.TestCase):
+    """
+    Test the full enumeration of our preset.
+    This tests every preset for MobileNetV3 and is only run manually.
+    Run with:
+    `pytest keras_cv/models/backbones/mlp_mixer/mlp_mixer_backbone_presets_test.py --run_extra_large`
+    """
+
+    def test_load_resnet(self):
+        input_data = tf.ones(shape=(2, 224, 224, 3))
+        for preset in MLPMixerBackbone.presets:
+            model = MLPMixerBackbone.from_preset(preset)
+            model(input_data)
