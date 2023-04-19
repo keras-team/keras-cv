a1b749e1b94537a7af81d624d83c1f49fbed5a01
Somasree Majumder <bishnupadamajumder32@gmail.com>
2023-04-19 04:53:55 +0000

    fixing

--- keras_cv/models/backbones/backbone_presets.py
+++ keras_cv/models/backbones/backbone_presets.py
@@ -13,8 +13,8 @@
 # limitations under the License.
 """All Backbone presets"""
 
-from keras_cv.models.backbones.efficientnet_v2 import (
-    efficientnet_v2_backbone_presets,
+from keras_cv.models.backbones.efficientnetv2 import (
+    efficientnetv2_backbone_presets,
 )
 from keras_cv.models.backbones.resnet_v1 import resnet_v1_backbone_presets
 from keras_cv.models.backbones.resnet_v2 import resnet_v2_backbone_presets
@@ -22,13 +22,13 @@ from keras_cv.models.backbones.resnet_v2 import resnet_v2_backbone_presets
 backbone_presets_no_weights = {
     **resnet_v1_backbone_presets.backbone_presets_no_weights,
     **resnet_v2_backbone_presets.backbone_presets_no_weights,
-    **efficientnet_v2_backbone_presets.backbone_presets_no_weights,
+    **efficientnetv2_backbone_presets.backbone_presets_no_weights,
 }
 
 backbone_presets_with_weights = {
     **resnet_v1_backbone_presets.backbone_presets_with_weights,
     **resnet_v2_backbone_presets.backbone_presets_with_weights,
-    **efficientnet_v2_backbone_presets.backbone_presets_with_weights,
+    **efficientnetv2_backbone_presets.backbone_presets_with_weights,
 }
 
 backbone_presets = {
