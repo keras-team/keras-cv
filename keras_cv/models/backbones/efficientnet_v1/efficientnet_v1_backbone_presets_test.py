49bb4d4b218acec7abec81b694724da2b7f24336
cosmo3769 <piyushthakur3769@gmail.com>
2023-04-24 13:30:23 +0530

    added preset unit test

--- keras_cv/models/backbones/efficientnet_v1/efficientnet_v1_backbone_presets_test.py
+++ keras_cv/models/backbones/efficientnet_v1/efficientnet_v1_backbone_presets_test.py
@@ -11,3 +11,30 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+import pytest
+import tensorflow as tf
+from absl.testing import parameterized
+
+from keras_cv.models.backbones.efficientnet_v1.efficientnet_v1_backbone import (
+    EfficientNetV1Backbone,
+)
+
+
+@pytest.mark.extra_large
+class EfficientNetV1PresetFullTest(tf.test.TestCase, parameterized.TestCase):
+    """
+    Test the full enumeration of our preset.
+    This every presets for EfficientNetV1 and is only run manually.
+    Run with:
+    `pytest keras_cv/models/backbones/efficientnet_v1/efficientnet_v1_backbone_presets_test.py --run_extra_large`
+    """  # noqa: E501
+
+    @parameterized.named_parameters(
+        *[(preset, preset) for preset in EfficientNetV1Backbone.presets]
+    )
+    def test_load_efficientnet(self, preset):
+        input_data = tf.ones(shape=(2, 224, 224, 3))
+        model = EfficientNetV1Backbone.from_preset(preset)
+        model(input_data)
