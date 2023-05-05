eada7c31d7ac77241d6089e568ec970e8917c1a5
Luke Wood <lukewoodcs@gmail.com>
2023-05-04 17:51:26 -0700

    Noqa update

--- keras_cv/models/classification/image_classifier_presets.py
+++ keras_cv/models/classification/image_classifier_presets.py
@@ -65,7 +65,7 @@ classifier_presets = {
             "pooling": "avg",
             "activation": "softmax",
         },
-        "weights_url": "https://storage.googleapis.com/keras-cv/models/efficientnet_v2/efficientnetv2_s_imagenet_classifier.h5",  # noqa: E501
+        "weights_url": "https://storage.googleapis.com/keras-cv/models/efficientnet_v2/efficientnetv2-s_imagenet_classifier.h5",  # noqa: E501
         "weights_hash": "4da57ade035e11aff7e1c5cb04c4235e",
     },
     "efficientnetv2_b0_imagenet_classifier": {
@@ -94,7 +94,7 @@ classifier_presets = {
             "pooling": "avg",
             "activation": "softmax",
         },
-        "weights_url": "https://storage.googleapis.com/keras-cv/models/efficientnet_v2/efficientnetv2_b0_imagenet_classifier.h5",  # noqa: E501
+        "weights_url": "https://storage.googleapis.com/keras-cv/models/efficientnet_v2/efficientnetv2-b0_imagenet_classifier.h5",  # noqa: E501
         "weights_hash": "4b739de648c346b7e4156e11a223c338",
     },
     "efficientnetv2_b1_imagenet_classifier": {
@@ -123,7 +123,7 @@ classifier_presets = {
             "pooling": "avg",
             "activation": "softmax",
         },
-        "weights_url": "https://storage.googleapis.com/keras-cv/models/efficientnet_v2/efficientnetv2_b1_imagenet_classifier.h5",  # noqa: E501
+        "weights_url": "https://storage.googleapis.com/keras-cv/models/efficientnet_v2/efficientnetv2-b1_imagenet_classifier.h5",  # noqa: E501
         "weights_hash": "78c1c879143dbd8f74e6ffc4d3180197",
     },
     "efficientnetv2_b2_imagenet_classifier": {
@@ -152,7 +152,7 @@ classifier_presets = {
             "pooling": "avg",
             "activation": "softmax",
         },
-        "weights_url": "https://storage.googleapis.com/keras-cv/models/efficientnet_v2/efficientnetv2_b2_imagenet_classifier.h5",  # noqa: E501
+        "weights_url": "https://storage.googleapis.com/keras-cv/models/efficientnet_v2/efficientnetv2-b2_imagenet_classifier.h5",  # noqa: E501
         "weights_hash": "07eda1c48aee27e12a3fe2545e6c65ed",
     },
 }
--- keras_cv/models/classification/image_classifier_test.py
+++ keras_cv/models/classification/image_classifier_test.py
@@ -111,15 +111,10 @@ class ImageClassifierPresetSmokeTest(tf.test.TestCase, parameterized.TestCase):
 
     @parameterized.named_parameters(
         (
-            "efficientnetv2_s_imagenet_classifier",
-            "efficientnetv2_s_imagenet_classifier",
-            [-0.268132, -0.262596, -0.278071, -0.257657, -0.271685],
-        ),
-        (
             "efficientnetv2_b0_imagenet_classifier",
             "efficientnetv2_b0_imagenet_classifier",
             [-0.278459, -0.278462, -0.159786, -0.277514, 0.537921],
-        ),
+        )
     )
     def test_efficientnet_v2_preset(self, preset, expected):
         model = ImageClassifier.from_preset(
