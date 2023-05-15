c239c9ec33219ef759b3c1ab9c8419da180cb04b
Ian Stenbit <3072903+ianstenbit@users.noreply.github.com>
2023-05-15 17:00:10 -0600

    Refactor correct_pad_downsample as a util (#1787)

--- keras_cv/models/utils.py
+++ keras_cv/models/utils.py
@@ -14,6 +14,7 @@
 # ==============================================================================
 """Utility functions for models"""
 
+from keras import backend
 from keras import layers
 from tensorflow import keras
 
@@ -26,3 +27,28 @@ def parse_model_inputs(input_shape, input_tensor):
             return layers.Input(tensor=input_tensor, shape=input_shape)
         else:
             return input_tensor
+
+
+def correct_pad_downsample(inputs, kernel_size):
+    """Returns a tuple for zero-padding for 2D convolution with downsampling.
+
+    Args:
+        inputs: Input tensor.
+        kernel_size: An integer or tuple/list of 2 integers.
+
+    Returns:
+        A tuple.
+    """
+    img_dim = 1
+    input_size = backend.int_shape(inputs)[img_dim : (img_dim + 2)]
+    if isinstance(kernel_size, int):
+        kernel_size = (kernel_size, kernel_size)
+    if input_size[0] is None:
+        adjust = (1, 1)
+    else:
+        adjust = (1 - input_size[0] % 2, 1 - input_size[1] % 2)
+    correct = (kernel_size[0] // 2, kernel_size[1] // 2)
+    return (
+        (correct[0] - adjust[0], correct[0]),
+        (correct[1] - adjust[1], correct[1]),
+    )
