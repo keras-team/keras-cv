c7d0a78bec3584728b6325fa453fc43bebcddfdd
ID6109 <ya6109@gmail.com>
2023-05-26 03:23:37 +0530

    Add unit tests

--- keras_cv/models/backbones/convnext/convnext_backbone_presets_test.py
+++ keras_cv/models/backbones/convnext/convnext_backbone_presets_test.py
@@ -11,3 +11,29 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""Tests for loading pretrained model presets."""
+
+import pytest
+import tensorflow as tf
+from absl.testing import parameterized
+
+from keras_cv.models.backbones.convnext.convnext_backbone import (
+    ConvNeXtBackbone,
+)
+
+
+@pytest.mark.extra_large
+class ConvNeXtPresetFullTest(tf.test.TestCase, parameterized.TestCase):
+    """
+    Test the full enumeration of our preset.
+    This tests every preset for ConvNeXt and is only run manually.
+    Run with:
+    `pytest keras_cv/models/backbones/convnext/convnext_backbone_presets_test.py --run_extra_large`
+    """  # noqa: E501
+
+    def test_load_mobilenet_v3(self):
+        input_data = tf.ones(shape=(2, 224, 224, 3))
+        for preset in ConvNeXtBackbone.presets:
+            model = ConvNeXtBackbone.from_preset(preset)
+            model(input_data)
