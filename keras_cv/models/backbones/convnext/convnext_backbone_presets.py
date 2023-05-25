ad1c535c0a44a34b9bd6a18025a0d30851079e1f
ID6109 <ya6109@gmail.com>
2023-05-25 22:30:48 +0530

    Adding presets

--- keras_cv/models/backbones/convnext/convnext_backbone_presets.py
+++ keras_cv/models/backbones/convnext/convnext_backbone_presets.py
@@ -11,3 +11,84 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""ConvNeXT model preset configurations."""
+
+backbone_presets_no_weights = {
+    "convnext_tiny": {
+        "metadata": {
+            "description": "DESCRIPTION",
+        },
+        "class_name": "keras_cv.models>ConvNeXtBackbone",
+        "config": {
+            "depths": [3, 3, 9, 3],
+            "projection_dims": [96, 192, 384, 768],
+            "default_size": 224,
+            "include_rescaling": True,
+            "input_shape": (None, None, 3),
+            "input_tensor": None,
+        },
+    },
+    "convnext_small": {
+        "metadata": {
+            "description": "DESCRIPTION",
+        },
+        "class_name": "keras_cv.models>ConvNeXtBackbone",
+        "config": {
+            "depths": [3, 3, 27, 3],
+            "projection_dims": [96, 192, 384, 768],
+            "default_size": 224,
+            "include_rescaling": True,
+            "input_shape": (None, None, 3),
+            "input_tensor": None,
+        },
+    },
+    "convnext_base": {
+        "metadata": {
+            "description": "DESCRIPTION",
+        },
+        "class_name": "keras_cv.models>ConvNeXtBackbone",
+        "config": {
+            "depths": [3, 3, 27, 3],
+            "projection_dims": [128, 256, 512, 1024],
+            "default_size": 224,
+            "include_rescaling": True,
+            "input_shape": (None, None, 3),
+            "input_tensor": None,
+        },
+    },
+    "convnext_large": {
+        "metadata": {
+            "description": "DESCRIPTION",
+        },
+        "class_name": "keras_cv.models>ConvNeXtBackbone",
+        "config": {
+            "depths": [3, 3, 27, 3],
+            "projection_dims": [192, 384, 768, 1536],
+            "default_size": 224,
+            "include_rescaling": True,
+            "input_shape": (None, None, 3),
+            "input_tensor": None,
+        },
+    },
+    "convnext_xlarge": {
+        "metadata": {
+            "description": "DESCRIPTION",
+        },
+        "class_name": "keras_cv.models>ConvNeXtBackbone",
+        "config": {
+            "depths": [3, 3, 27, 3],
+            "projection_dims": [256, 512, 1024, 2048],
+            "default_size": 224,
+            "include_rescaling": True,
+            "input_shape": (None, None, 3),
+            "input_tensor": None,
+        },
+    },
+}
+
+backbone_presets_with_weights = {}
+
+backbone_presets = {
+    **backbone_presets_no_weights,
+    **backbone_presets_with_weights,
+}
