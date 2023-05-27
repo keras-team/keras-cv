e587c5b14239138ac5487b0c130a3b29d1064187
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-05-27 08:28:37 +0000

    update: minor changes

--- keras_cv/models/segmentation/deeplab_v3_backbone_presets.py
+++ keras_cv/models/segmentation/deeplab_v3_backbone_presets.py
@@ -33,6 +33,7 @@ backbone_presets_with_weights = {
         },
         "class_name": "keras_cv.models>DeepLabV3Backbone",
         "config": backbone_presets_no_weights["deeplab_v3_test"]["config"],
+        # Add DeepLabV3 Weights URL
         "weights_url": "https://storage.googleapis.com/keras-cv/models/mobilenetv3/mobilenetv3_large_imagenet_backbone.h5",   # noqa: E501
         "weights_hash": "ec55ea2f4f4ee9a2ddf3ee8e2dd784e9d5732690c1fc5afc7e1b2a66703f3337"  # noqa: E501
     }
