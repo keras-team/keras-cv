e3aecca2462525d993c07ef57e051465270653de
Vision <gitesh.ch.0912@gmail.com>
2023-06-02 01:46:13 +0530

    Update

--- keras_cv/losses/ciou_loss_test.py
+++ keras_cv/losses/ciou_loss_test.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import numpy as np
 import tensorflow as tf
 
 from keras_cv.losses.ciou_loss import CIoULoss
@@ -65,21 +66,38 @@ class CIoUTest(tf.test.TestCase):
         self.assertAllEqual(ciou_loss(y_true, y_pred).shape, ())
 
     def test_output_value(self):
-        y_true = [
-            [0, 0, 1, 1],
-            [0, 0, 2, 3],
-            [4, 5, 3, 6],
-            [2, 2, 3, 3],
-        ]
+        def test_loss_format(bounding_box_format):
+            y_true = [
+                [0, 0, 1, 1],
+                [0, 0, 2, 3],
+                [4, 5, 3, 6],
+                [2, 2, 3, 3],
+            ]
 
-        y_pred = [
-            [0, 0, 5, 6],
-            [0, 0, 7, 3],
-            [4, 5, 5, 6],
-            [2, 1, 3, 3],
-        ]
+            y_pred = [
+                [0, 0, 5, 6],
+                [0, 0, 7, 3],
+                [4, 5, 5, 6],
+                [2, 1, 3, 3],
+            ]
 
-        ciou_loss = CIoULoss(bounding_box_format="xyxy")
+            if bounding_box_format == "xyxy":
+                ciou_loss = CIoULoss(bounding_box_format="xyxy")
+                # expected value is 1.032025
+                expected_loss = 1.032025
+            elif bounding_box_format == "rel_xyxy":
+                scale_factor = 1 / 640.0
+                y_true_scaled = np.array(y_true) * scale_factor
+                y_pred_scaled = np.array(y_pred) * scale_factor
+                ciou_loss = CIoULoss(bounding_box_format="xyxy")
 
-        # expected value for these values is 1.0320253
-        self.assertAllClose(ciou_loss(y_true, y_pred), 1.0320253)
+                y_true = tf.constant(y_true_scaled, dtype=tf.float32)
+                y_pred = tf.constant(y_pred_scaled, dtype=tf.float32)
+
+                # expected value is 1.032029
+                expected_loss = 1.032029
+
+            self.assertAllClose(ciou_loss(y_true, y_pred), expected_loss)
+
+        test_loss_format("xyxy")
+        test_loss_format("rel_xyxy")
