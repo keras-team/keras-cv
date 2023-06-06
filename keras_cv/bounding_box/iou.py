de28953f76fb29e770d9e12acf6ecf2888eb7dd5
Vision <gitesh.ch.0912@gmail.com>
2023-06-06 12:10:57 +0530

    compute_ciou

--- keras_cv/bounding_box/iou.py
+++ keras_cv/bounding_box/iou.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 """Contains functions to compute ious of bounding boxes."""
 import tensorflow as tf
-
+import math
 from keras_cv import bounding_box
 
 
@@ -172,3 +172,39 @@ def compute_iou(
     )
     iou_lookup_table = tf.where(background_mask, mask_val_t, res)
     return iou_lookup_table
+
+
+def compute_ciou(box1, box2, eps=1e-7):
+    b1_x1, b1_y1, b1_x2, b1_y2 = tf.split(box1, 4, axis=-1)
+    b2_x1, b2_y1, b2_x2, b2_y2 = tf.split(box2, 4, axis=-1)
+    w1, h1 = b1_x2 - b1_x1, b1_y2 - b1_y1 + eps
+    w2, h2 = b2_x2 - b2_x1, b2_y2 - b2_y1 + eps
+
+    # Intersection area
+    inter = tf.math.maximum(
+        tf.math.minimum(b1_x2, b2_x2) - tf.math.maximum(b1_x1, b2_x1), 0
+    ) * tf.math.maximum(
+        tf.math.minimum(b1_y2, b2_y2) - tf.math.maximum(b1_y1, b2_y1), 0
+    )
+
+    # Union Area
+    union = w1 * h1 + w2 * h2 - inter + eps
+
+    # IoU
+    iou = inter / union
+
+    cw = tf.math.maximum(b1_x2, b2_x2) - tf.math.minimum(
+        b1_x1, b2_x1
+    )  # convex (smallest enclosing box) width
+    ch = tf.math.maximum(b1_y2, b2_y2) - tf.math.minimum(
+        b1_y1, b2_y1
+    )  # convex height
+    c2 = cw**2 + ch**2 + eps  # convex diagonal squared
+    rho2 = (
+        (b2_x1 + b2_x2 - b1_x1 - b1_x2) ** 2
+        + (b2_y1 + b2_y2 - b1_y1 - b1_y2) ** 2
+    ) / 4  # center dist ** 2
+    v = tf.pow((4 / math.pi**2) * (tf.atan(w2 / h2) - tf.atan(w1 / h1)), 2)
+    alpha = v / (v - iou + (1 + eps))
+
+    return iou - (rho2 / c2 + v * alpha)
--- keras_cv/models/object_detection/yolo_v8/yolo_v8_label_encoder.py
+++ keras_cv/models/object_detection/yolo_v8/yolo_v8_label_encoder.py
@@ -19,7 +19,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-from keras_cv.losses.ciou_loss import CIoULoss
+from keras_cv.bounding_box.iou import compute_ciou
 
 def select_highest_overlaps(mask_pos, overlaps, max_num_boxes):
     """Break ties when two GT boxes match to the same anchor.
@@ -296,7 +296,7 @@ class YOLOV8LabelEncoder(layers.Layer):
 
         gt_boxes = tf.repeat(tf.expand_dims(gt_bboxes, axis=2), na, axis=2)
 
-        iou = CIoULoss(gt_boxes, pd_boxes)
+        iou = tf.squeeze(compute_ciou(gt_boxes, pd_boxes), axis=-1)
         iou = tf.where(iou > 0, iou, 0)
 
         iou = tf.reshape(iou, (-1, max_num_boxes, na))
