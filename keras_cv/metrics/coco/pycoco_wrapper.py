ca3fb777e9059f746f502e05ebc422c691e260ad
HongYu <20734616+james77777778@users.noreply.github.com>
2023-06-13 02:03:23 +0000

    Revert

--- keras_cv/metrics/coco/pycoco_wrapper.py
+++ keras_cv/metrics/coco/pycoco_wrapper.py
@@ -152,12 +152,7 @@ def _convert_groundtruths_to_coco_dataset(groundtruths, label_map=None):
     gt_annotations = []
     num_batches = len(groundtruths["source_id"])
     for i in range(num_batches):
-        if len(groundtruths["classes"][i].shape) < 2:  # ragged np array
-            max_num_instances = max(
-                classes.shape[0] for classes in groundtruths["classes"][i]
-            )
-        else:  # dense np array
-            max_num_instances = groundtruths["classes"][i].shape[1]
+        max_num_instances = max(x.shape[0] for x in groundtruths["classes"][i])
         batch_size = groundtruths["source_id"][i].shape[0]
         for j in range(batch_size):
             num_instances = groundtruths["num_detections"][i][j]
