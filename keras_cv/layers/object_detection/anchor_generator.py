Fix off-by-one issue with RetinaNet anchor generation (#1882)
* Fix RetinaNet anchor generation

* Use tf for cail

* Get rid of bad pruning code

* lol return=

* Return deleted comment

* Update FasterRCNN tests

keras_cv/layers/object_detection/anchor_generator.py
@@ -262,11 +262,15 @@ class _SingleAnchorGenerator:
         # make sure range of `cx` is within limit of `image_width` with
         # `stride`, also for sizes where `image_width % stride != 0`.
         # [W]
-        cx = tf.range(0.5 * stride, (image_width // stride) * stride, stride)
+        cx = tf.range(
+            0.5 * stride, tf.math.ceil(image_width / stride) * stride, stride
+        )
         # make sure range of `cy` is within limit of `image_height` with
         # `stride`, also for sizes where `image_height % stride != 0`.
         # [H]
-        cy = tf.range(0.5 * stride, (image_height // stride) * stride, stride)
+        cy = tf.range(
+            0.5 * stride, tf.math.ceil(image_height / stride) * stride, stride
+        )
         # [H, W]
         cx_grid, cy_grid = tf.meshgrid(cx, cy)
         # [H, W, 1]

keras_cv/models/legacy/object_detection/faster_rcnn/faster_rcnn_test.py
@@ -28,10 +28,15 @@ from keras_cv.models.object_detection.__test_utils__ import (
 
 
 class FasterRCNNTest(tf.test.TestCase, parameterized.TestCase):
+    # TODO(ianstenbit): Make FasterRCNN support shapes that are not multiples
+    # of 128, perhaps by adding a flag to the anchor generator for whether to
+    # include anchors centered outside of the image. (RetinaNet does use those,
+    # while FasterRCNN doesn't). For more context on why this is the case, see
+    # https://github.com/keras-team/keras-cv/pull/1882
     @parameterized.parameters(
-        ((2, 640, 480, 3),),
+        ((2, 640, 384, 3),),
         ((2, 512, 512, 3),),
-        ((2, 224, 224, 3),),
+        ((2, 128, 128, 3),),
     )
     def test_faster_rcnn_infer(self, batch_shape):
         model = FasterRCNN(
@@ -46,9 +51,9 @@ class FasterRCNNTest(tf.test.TestCase, parameterized.TestCase):
         self.assertAllEqual([2, 1000, 4], outputs[0].shape)
 
     @parameterized.parameters(
-        ((2, 640, 480, 3),),
+        ((2, 640, 384, 3),),
         ((2, 512, 512, 3),),
-        ((2, 224, 224, 3),),
+        ((2, 128, 128, 3),),
     )
     def test_faster_rcnn_train(self, batch_shape):
         model = FasterRCNN(