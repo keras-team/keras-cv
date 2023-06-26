0833bc8d61105e6c2dc2d1613039ad12f26939b0
Suvaditya Mukherjee <suvadityamuk@gmail.com>
2023-06-26 19:46:55 +0530

    chore: fix with isort
    Signed-off-by: Suvaditya Mukherjee <suvadityamuk@gmail.com>

--- keras_cv/visualization/plot_image_gallery.py
+++ keras_cv/visualization/plot_image_gallery.py
@@ -13,9 +13,11 @@
 # limitations under the License.
 
 import math
-import keras_cv
+
 import numpy as np
 import tensorflow as tf
+
+import keras_cv
 from keras_cv import utils
 from keras_cv.utils import assert_matplotlib_installed
 
