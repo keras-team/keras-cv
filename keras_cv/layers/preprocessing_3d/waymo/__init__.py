fe2a7a806919970a8ed1af14fdf2c146d9f9b4a6
ianjjohnson <3072903+ianstenbit@users.noreply.github.com>
2023-06-27 11:27:16 -0600

    Update license

--- keras_cv/layers/preprocessing_3d/waymo/__init__.py
+++ keras_cv/layers/preprocessing_3d/waymo/__init__.py
@@ -1,4 +1,3 @@
 # Copyright 2022 Waymo LLC.
 #
-# Licensed under the terms in https://github.com/waymo-research/waymo-open-dataset/blob/master/src/waymo_open_dataset/wdl_limited/camera/LICENSE
-# and https://github.com/waymo-research/waymo-open-dataset/blob/master/src/waymo_open_dataset/wdl_limited/camera/PATENTS
+# Licensed under the terms in https://github.com/keras-team/keras-cv/blob/master/keras_cv/layers/preprocessing_3d/waymo/LICENSE
--- keras_cv/layers/preprocessing_3d/waymo/frustum_random_point_feature_noise_test.py
+++ keras_cv/layers/preprocessing_3d/waymo/frustum_random_point_feature_noise_test.py
@@ -1,7 +1,6 @@
 # Copyright 2022 Waymo LLC.
 #
-# Licensed under the terms in https://github.com/waymo-research/waymo-open-dataset/blob/master/src/waymo_open_dataset/wdl_limited/camera/LICENSE
-# and https://github.com/waymo-research/waymo-open-dataset/blob/master/src/waymo_open_dataset/wdl_limited/camera/PATENTS
+# Licensed under the terms in https://github.com/keras-team/keras-cv/blob/master/keras_cv/layers/preprocessing_3d/waymo/LICENSE
 
 import numpy as np
 import tensorflow as tf
