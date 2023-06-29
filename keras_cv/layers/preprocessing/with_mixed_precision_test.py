Use correct policy API for mixed precision

keras_cv/layers/preprocessing/with_mixed_precision_test.py
@@ -156,7 +156,7 @@ class WithMixedPrecisionTest(tf.test.TestCase, parameterized.TestCase):
                     "`tf.image.adjust_hue`. Skipping."
                 )
 
-        keras.mixed_precision.set_dtype_policy("mixed_float16")
+        keras.mixed_precision.set_global_policy("mixed_float16")
 
         img = tf.random.uniform(
             shape=(3, 512, 512, 3), minval=0, maxval=255, dtype=tf.float32
@@ -170,7 +170,7 @@ class WithMixedPrecisionTest(tf.test.TestCase, parameterized.TestCase):
     @classmethod
     def tearDownClass(cls) -> None:
         # Do not affect other tests
-        keras.mixed_precision.set_dtype_policy("float32")
+        keras.mixed_precision.set_global_policy("float32")
 
 
 if __name__ == "__main__":