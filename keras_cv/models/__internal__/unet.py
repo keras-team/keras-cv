ba7acfa033b2b29761b51de7b5bf727fca9d2ee7
Nihar Jani <84491997+NiharJani2002@users.noreply.github.com>
2023-07-18 23:55:40 +0530

    Update unet.py
    U-Net Research Paper In The Same Line, Along With It Linter Issue Is Resolved.
    The Bitly Link Is Used.

--- keras_cv/models/__internal__/unet.py
+++ keras_cv/models/__internal__/unet.py
@@ -178,9 +178,7 @@ def UNet(
     function that acts on tensors as inputs.
 
     Reference:
-        - [U-Net: Convolutional Networks for Biomedical Image Segmentation]
-          (https://arxiv.org/abs/1505.04597)
-        
+        - [U-Net: Convolutional Networks for Biomedical Image Segmentation](https://bit.ly/3rG7Fda)       
     Args:
         input_shape: the rank 3 shape of the input to the UNet
         down_block_configs: a list of (filter_count, num_blocks) tuples
