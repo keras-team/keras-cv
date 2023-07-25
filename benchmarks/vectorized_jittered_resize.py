3ba1d7645b640086fc2fa34983a359e029e44dca
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-07-25 17:45:57 -0400

    [benchmarks/vectorized_jittered_resize.py] Address nit from @ianstenbit

--- benchmarks/vectorized_jittered_resize.py
+++ benchmarks/vectorized_jittered_resize.py
@@ -87,7 +87,7 @@ class OldJitteredResize(BaseImageAugmentationLayer):
             This factor is used to scale the input image.
             To replicate the results of the MaskRCNN paper pass `(0.8, 1.25)`.
         crop_size: (Optional) the size of the image to crop from the scaled
-            image, when not provided. Defaults to `target_size`.
+            image. Defaults to `target_size`.
         bounding_box_format: The format of bounding boxes of input boxes.
             Refer to
             keras-team/keras-cv/blob/master/keras_cv/bounding_box/converters.py
