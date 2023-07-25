03fd45f164bb5d6648fe5f04cb005bf030e6fa9b
Jay Gala <jaygala260@gmail.com>
2023-07-25 14:10:07 +0530

    fix styling issues

--- keras_cv/layers/preprocessing/with_mixed_precision_test.py
+++ keras_cv/layers/preprocessing/with_mixed_precision_test.py
@@ -211,7 +211,7 @@ class WithMixedPrecisionTest(TestCase):
                     "operations `tf.image.adjust_saturation`, and "
                     "`tf.image.adjust_hue`. Skipping."
                 )
-            
+
             if layer_cls in NO_BOUNDING_BOXES_TESTS:
                 self.skipTest(
                     "There is currently no support for bounding boxes."
