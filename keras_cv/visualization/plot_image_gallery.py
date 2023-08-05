64e7e2ab376b8b3505b9c961bf55259b033174c7
Zekrom-7780 <bhaveshmisra1@gmail.com>
2023-08-05 11:21:32 +0530

    linting

--- keras_cv/visualization/plot_image_gallery.py
+++ keras_cv/visualization/plot_image_gallery.py
@@ -117,7 +117,6 @@ def plot_image_gallery(
     """
     assert_matplotlib_installed("plot_bounding_box_gallery")
 
-    
     if path is not None and show:
         raise ValueError(
             "plot_gallery() expects either `path` to be set, or `show` "
@@ -177,13 +176,8 @@ def plot_image_gallery(
             current_axis.axis("off")
 
     if path is None and show is None:
-        #If path is None and show is None
-        # Default to showing the image
         return fig
-    
-    if path is None and not show:
-        return
-    
+
     if path is not None:
         plt.savefig(
             fname=path,
