ca4ad16c29b934ab474d521a6973cedfb767e26a
Vision <gitesh.ch.0912@gmail.com>
2023-08-15 00:20:24 +0530

    Format

--- examples/layers/preprocessing/segmentation/random_translation_demo.py
+++ examples/layers/preprocessing/segmentation/random_translation_demo.py
@@ -11,10 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""random_translation_demo.py shows how to use the RandomTranslation preprocessing layer.
-Uses the oxford iiit pet_dataset.  In this script the pets
-are loaded, then are passed through the preprocessing layers.
-Finally, they are shown using matplotlib.
+"""random_translation_demo.py shows how to use the RandomTranslation
+preprocessing layer. Uses the oxford iiit pet_dataset. In this 
+script the pets are loaded, then are passed through the 
+preprocessing layers. Finally, they are shown using matplotlib.
 """
 import demo_utils
 import tensorflow as tf
