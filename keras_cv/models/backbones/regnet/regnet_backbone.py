0d6cac7053001d95e9c60e22c7fb7298db23496b
cosmo3769 <piyushthakur3769@gmail.com>
2023-08-18 05:17:45 +0530

    update backbone

--- keras_cv/models/backbones/regnet/regnet_backbone.py
+++ keras_cv/models/backbones/regnet/regnet_backbone.py
@@ -27,9 +27,6 @@ from keras_cv.models.backbones.backbone import Backbone
 from keras_cv.models.backbones.regnet.regnet_backbone_presets import (
     backbone_presets,
 )
-from keras_cv.models.backbones.regnet.regnet_backbone_presets import (
-    backbone_presets_with_weights,
-)
 from keras_cv.utils.python_utils import classproperty
 
 
@@ -126,12 +123,6 @@ class RegNetBackbone(Backbone):
     def presets(cls):
         """Dictionary of preset names and configurations."""
         return copy.deepcopy(backbone_presets)
-
-    @classproperty
-    def presets_with_weights(cls):
-        """Dictionary of preset names and configurations that include
-        weights."""
-        return copy.deepcopy(backbone_presets_with_weights)
 
 
 def apply_conv2d_bn(
