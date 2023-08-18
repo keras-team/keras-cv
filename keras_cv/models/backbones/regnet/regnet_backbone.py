ad1b557d4d639dc368e2a4fdeeced8af602ef9c3
cosmo3769 <piyushthakur3769@gmail.com>
2023-08-18 06:08:12 +0530

    fix add layer

--- keras_cv/models/backbones/regnet/regnet_backbone.py
+++ keras_cv/models/backbones/regnet/regnet_backbone.py
@@ -261,7 +261,7 @@ def apply_x_block(
         name=name + "_conv_1x1_2",
     )
 
-    x = keras.layers.Add([x, skip], name=name + "_add")
+    x = keras.layers.Add(name=name + "_add")([x, skip])
     x = keras.layers.Activation("relu", name=name + "_exit_relu")(x)
 
     return x
@@ -353,7 +353,7 @@ def apply_y_block(
         name=name + "_conv_1x1_2",
     )
 
-    x = keras.layers.Add([x, skip], name=name + "_add")
+    x = keras.layers.Add(name=name + "_add")([x, skip])
     x = keras.layers.Activation("relu", name=name + "_exit_relu")(x)
 
     return x
@@ -441,7 +441,7 @@ def apply_z_block(
     if stride != 1:
         return x
     else:
-        return keras.layers.Add([x, inputs], name=name + "_add")
+        return keras.layers.Add(name=name + "_add")([x, inputs])
 
 
 def apply_stage(
