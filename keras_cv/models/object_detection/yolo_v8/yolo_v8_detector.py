47e6f863c70c6332e2a49c21acb6a27f9ab099b3
Piyush Thakur <53268607+cosmo3769@users.noreply.github.com>
2023-09-21 07:09:54 +0530

    [Yolov8 detector] Fix example in docstring (#2082)
    * fix example in docstring
    
    * review comments

--- keras_cv/models/object_detection/yolo_v8/yolo_v8_detector.py
+++ keras_cv/models/object_detection/yolo_v8/yolo_v8_detector.py
@@ -353,22 +353,23 @@ class YOLOV8Detector(Task):
     ```python
     images = tf.ones(shape=(1, 512, 512, 3))
     labels = {
-        "boxes": [
+        "boxes": tf.constant([
             [
                 [0, 0, 100, 100],
                 [100, 100, 200, 200],
                 [300, 300, 100, 100],
             ]
-        ],
-        "classes": [[1, 1, 1]],
+        ], dtype=tf.float32),
+        "classes": tf.constant([[1, 1, 1]], dtype=tf.int64),
     }
+
     model = keras_cv.models.YOLOV8Detector(
         num_classes=20,
         bounding_box_format="xywh",
         backbone=keras_cv.models.YOLOV8Backbone.from_preset(
-            "yolo_v8_m_coco"
+            "yolo_v8_m_backbone_coco"
         ),
-        fpn_depth=2.
+        fpn_depth=2
     )
 
     # Evaluate model without box decoding and NMS
