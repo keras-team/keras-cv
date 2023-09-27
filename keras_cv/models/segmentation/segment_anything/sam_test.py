2e09e80791f936f5654881067ab45839c8ba50e1
Ian Stenbit <3072903+ianstenbit@users.noreply.github.com>
2023-09-27 09:36:38 -0600

    Fix a flaky test for Segment Anything

--- keras_cv/models/segmentation/segment_anything/sam_test.py
+++ keras_cv/models/segmentation/segment_anything/sam_test.py
@@ -259,8 +259,8 @@ class SAMTest(TestCase):
         )
         masks_ex, iou_pred_ex = outputs_ex["masks"], outputs_ex["iou_pred"]
 
-        self.assertAllClose(masks, masks_ex, atol=5e-5)
-        self.assertAllClose(iou_pred, iou_pred_ex, atol=5e-5)
+        self.assertAllClose(masks, masks_ex, atol=1e-4)
+        self.assertAllClose(iou_pred, iou_pred_ex, atol=1e-4)
 
     @pytest.mark.extra_large
     def test_end_to_end_model_save(self):
