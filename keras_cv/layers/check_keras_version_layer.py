064882e75c5bc71310493e03af22caa2bdb50536
divyashreepathihalli <divyashreepathihalli@gmail.com>
2023-11-08 00:31:14 +0000

    update imports in keras version check layer

--- keras_cv/layers/check_keras_version_layer.py
+++ keras_cv/layers/check_keras_version_layer.py
@@ -11,6 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from tensorflow import keras
+
+
 class CheckKerasVersionLayer(keras.layers.Layer):
     """check keras version and raise an error if the layer is not supported"""
 
