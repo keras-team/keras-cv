53e741951b74e9baa8b5f2fc9d6615eecadd85ae
divyashreepathihalli <divyashreepathihalli@gmail.com>
2023-11-14 21:26:42 +0000

    add Random ops shim

--- keras_cv/backend/random.py
+++ keras_cv/backend/random.py
@@ -11,10 +11,87 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+from keras_cv.backend import keras
 from keras_cv.backend.config import keras_3
 
 if keras_3():
     from keras.random import *  # noqa: F403, F401
 else:
     from keras_core.random import *  # noqa: F403, F401
+
+
+class SeedGenerator:
+    def __init__(self, seed=None, **kwargs):
+        if keras_3():
+            self._seed_generator = keras.random.SeedGenerator(
+                seed=seed, **kwargs
+            )
+        else:
+            self._current_seed = [0, seed]
+
+    def next(self, ordered=True):
+        if keras_3():
+            return self._seed_generator.next(ordered=ordered)
+        else:
+            self._current_seed[0] += 1
+            return self._current_seed[:]
+
+
+def normal(shape, mean=0.0, stddev=1.0, dtype=None, seed=None):
+    if isinstance(seed, SeedGenerator):
+        seed = seed.next()
+        int_seed = seed[0] + seed[1]
+    if keras_3():
+        return keras.random.normal(
+            shape, mean=mean, stddev=stddev, dtype=dtype, seed=int_seed
+        )
+    else:
+        import tensorflow as tf
+
+        return tf.random.normal(
+            shape, mean=mean, stddev=stddev, dtype=dtype, seed=int_seed
+        )
+
+
+def uniform(shape, minval=0.0, maxval=1.0, dtype=None, seed=None):
+    if isinstance(seed, SeedGenerator):
+        seed = seed.next()
+        int_seed = seed[0] + seed[1]
+    if keras_3():
+        return keras.random.uniform(
+            shape, minval=minval, maxval=maxval, dtype=dtype, seed=int_seed
+        )
+    else:
+        import tensorflow as tf
+
+        return tf.random.uniform(
+            shape, minval=minval, maxval=maxval, dtype=dtype, seed=int_seed
+        )
+
+
+def shuffle(x, axis=0, seed=None):
+    if isinstance(seed, SeedGenerator):
+        seed = seed.next()
+        int_seed = seed[0] + seed[1]
+    if keras_3():
+        return keras.random.shuffle(x=x, axis=axis, seed=int_seed)
+    else:
+        import tensorflow as tf
+
+        return tf.random.shuffle(x=x, axis=axis, seed=int_seed)
+
+
+def categorical(logits, num_samples, dtype="int32", seed=None):
+    if isinstance(seed, SeedGenerator):
+        seed = seed.next()
+        int_seed = seed[0] + seed[1]
+    if keras_3():
+        return keras.random.categorical(
+            logits=logits, num_samples=num_samples, dtype=dtype, seed=int_seed
+        )
+    else:
+        import tensorflow as tf
+
+        return tf.random.categorical(
+            logits=logits, num_samples=num_samples, dtype=dtype, seed=int_seed
+        )
