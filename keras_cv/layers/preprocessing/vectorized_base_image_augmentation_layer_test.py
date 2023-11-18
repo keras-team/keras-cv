29f35eb4dd5f6e61db7a2ffb71d275e9c489afd9
divyashreepathihalli <divyashreepathihalli@gmail.com>
2023-11-18 20:44:06 +0000

    fix import

--- keras_cv/layers/preprocessing/vectorized_base_image_augmentation_layer_test.py
+++ keras_cv/layers/preprocessing/vectorized_base_image_augmentation_layer_test.py
@@ -18,7 +18,7 @@ from keras_cv import bounding_box
 from keras_cv.backend import ops
 from keras_cv.layers.preprocessing.vectorized_base_image_augmentation_layer import \
     VectorizedBaseImageAugmentationLayer  # noqa: E501
-
+from keras_cv.tests.test_case import TestCase
 
 
 class VectorizedRandomAddLayer(VectorizedBaseImageAugmentationLayer):
