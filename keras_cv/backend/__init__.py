5d926f0046caec4c716720eee95767b71265af5b
divyashreepathihalli <divyashreepathihalli@gmail.com>
2023-11-21 23:36:35 +0000

    update init

--- keras_cv/backend/__init__.py
+++ keras_cv/backend/__init__.py
@@ -11,6 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+Keras backend module.
+
+This module adds a temporary Keras API surface that is fully under KerasCV
+control. The goal is to allow us to write Keras 3-like code everywhere, while
+still supporting Keras 2. We do this by using the `keras_core` package with
+Keras 2 to backport Keras 3 numerics APIs (`keras.ops` and `keras.random`) into
+Keras 2. The sub-modules exposed are as follows:
+
+- `config`: check which version of Keras is being run.
+- `keras`: The full `keras` API with compat shims for older Keras versions.
+- `ops`: `keras.ops` for Keras 3 or `keras_core.ops` for Keras 2.
+- `random`: `keras.random` for Keras 3 or `keras_core.ops` for Keras 2.
+"""
 from keras_cv.backend import config  # noqa: E402
 from keras_cv.backend import keras  # noqa: E402
 from keras_cv.backend import ops  # noqa: E402
