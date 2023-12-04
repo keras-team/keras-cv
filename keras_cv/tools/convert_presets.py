dae173a5de72470e9cc93ce414111c87108eec72
Neel Kovelamudi <nkovela@google.com>
2023-12-04 21:35:08 +0000

    Convert preset script to functions

--- keras_cv/tools/convert_presets.py
+++ keras_cv/tools/convert_presets.py
@@ -12,97 +12,105 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO: UNCOMMENT BELOW AFTER INSTALL. DO NOT PUSH TO GITHUB.
+import os
 
-# import os
+import keras_cv  # noqa: E402
 
-# import keras_cv  # noqa: E402
-# from keras_cv.src.utils.preset_utils import save_to_preset  # noqa: E402
-
-# BUCKET = "keras-cv-kaggle"
-
-# # Save and upload Backbone presets
-
-# backbone_models = [
-#     keras_cv.models.ResNetBackbone,
-#     keras_cv.models.ResNet18Backbone,
-#     keras_cv.models.ResNet34Backbone,
-#     keras_cv.models.ResNet50Backbone,
-#     keras_cv.models.ResNet101Backbone,
-#     keras_cv.models.ResNet152Backbone,
-#     keras_cv.models.ResNetV2Backbone,
-#     keras_cv.models.ResNet18V2Backbone,
-#     keras_cv.models.ResNet34V2Backbone,
-#     keras_cv.models.ResNet50V2Backbone,
-#     keras_cv.models.ResNet101V2Backbone,
-#     keras_cv.models.ResNet152V2Backbone,
-#     keras_cv.models.YOLOV8Backbone,
-#     keras_cv.models.MobileNetV3Backbone,
-#     keras_cv.models.MobileNetV3SmallBackbone,
-#     keras_cv.models.MobileNetV3LargeBackbone,
-#     keras_cv.models.EfficientNetV2Backbone,
-#     keras_cv.models.EfficientNetV2B0Backbone,
-#     keras_cv.models.EfficientNetV2B1Backbone,
-#     keras_cv.models.EfficientNetV2B2Backbone,
-#     keras_cv.models.EfficientNetV2B3Backbone,
-#     keras_cv.models.EfficientNetV2SBackbone,
-#     keras_cv.models.EfficientNetV2MBackbone,
-#     keras_cv.models.EfficientNetV2LBackbone,
-# ]
-# for backbone_cls in backbone_models:
-#     for preset in backbone_cls.presets:
-#         backbone = backbone_cls.from_preset(preset)
-#         save_to_preset(
-#             backbone,
-#             preset,
-#             config_filename="config.json",
-#         )
-#         # Delete first to clean up any exising version.
-#         os.system(f"gsutil rm -rf gs://{BUCKET}/{preset}")
-#         os.system(f"gsutil cp -r {preset} gs://{BUCKET}/{preset}")
-#         for root, _, files in os.walk(preset):
-#             for file in files:
-#                 path = os.path.join(BUCKET, root, file)
-#                 os.system(
-#                     f"gcloud storage objects update gs://{path} "
-#                     "--add-acl-grant=entity=AllUsers,role=READER"
-#                 )
+BUCKET = "keras-cv-kaggle"
 
 
-# # Save and upload task presets
+def convert_backbone_presets():
+    # Save and upload Backbone presets
 
-# task_models = [
-#     keras_cv.models.RetinaNet,
-#     keras_cv.models.YOLOV8Detector,
-#     keras_cv.models.ImageClassifier,
-#     keras_cv.models.DeepLabV3Plus,
-# ]
-# for task_cls in task_models:
-#     # Remove backbone-specific keys
-#     task_preset_keys = set(task_cls.presets) ^ set(task_cls.backbone_presets)
-#     for preset in task_preset_keys:
-#         preset_metadata = task_cls.presets[preset]
-#         kwargs = {}
-#         if task_cls in [
-#             keras_cv.models.RetinaNet,
-#             keras_cv.models.YOLOV8Detector,
-#         ]:
-#             kwargs.update({"bounding_box_format": "xywh"})
-#             task = task_cls.from_preset(preset, **kwargs)
-#         else:
-#             task = task_cls.from_preset(preset)
-#         save_to_preset(
-#             task,
-#             preset,
-#             config_filename="config.json",
-#         )
-#         # Delete first to clean up any exising version.
-#         os.system(f"gsutil rm -rf gs://{BUCKET}/{preset}")
-#         os.system(f"gsutil cp -r {preset} gs://{BUCKET}/{preset}")
-#         for root, _, files in os.walk(preset):
-#             for file in files:
-#                 path = os.path.join(BUCKET, root, file)
-#                 os.system(
-#                     f"gcloud storage objects update gs://{path} "
-#                     "--add-acl-grant=entity=AllUsers,role=READER"
-#                 )
+    backbone_models = [
+        keras_cv.models.ResNetBackbone,
+        keras_cv.models.ResNet18Backbone,
+        keras_cv.models.ResNet34Backbone,
+        keras_cv.models.ResNet50Backbone,
+        keras_cv.models.ResNet101Backbone,
+        keras_cv.models.ResNet152Backbone,
+        keras_cv.models.ResNetV2Backbone,
+        keras_cv.models.ResNet18V2Backbone,
+        keras_cv.models.ResNet34V2Backbone,
+        keras_cv.models.ResNet50V2Backbone,
+        keras_cv.models.ResNet101V2Backbone,
+        keras_cv.models.ResNet152V2Backbone,
+        keras_cv.models.YOLOV8Backbone,
+        keras_cv.models.MobileNetV3Backbone,
+        keras_cv.models.MobileNetV3SmallBackbone,
+        keras_cv.models.MobileNetV3LargeBackbone,
+        keras_cv.models.EfficientNetV2Backbone,
+        keras_cv.models.EfficientNetV2B0Backbone,
+        keras_cv.models.EfficientNetV2B1Backbone,
+        keras_cv.models.EfficientNetV2B2Backbone,
+        keras_cv.models.EfficientNetV2B3Backbone,
+        keras_cv.models.EfficientNetV2SBackbone,
+        keras_cv.models.EfficientNetV2MBackbone,
+        keras_cv.models.EfficientNetV2LBackbone,
+    ]
+    for backbone_cls in backbone_models:
+        for preset in backbone_cls.presets:
+            backbone = backbone_cls.from_preset(preset)
+            save_to_preset(
+                backbone,
+                preset,
+                config_filename="config.json",
+            )
+            # Delete first to clean up any exising version.
+            os.system(f"gsutil rm -rf gs://{BUCKET}/{preset}")
+            os.system(f"gsutil cp -r {preset} gs://{BUCKET}/{preset}")
+            for root, _, files in os.walk(preset):
+                for file in files:
+                    path = os.path.join(BUCKET, root, file)
+                    os.system(
+                        f"gcloud storage objects update gs://{path} "
+                        "--add-acl-grant=entity=AllUsers,role=READER"
+                    )
+
+
+def convert_task_presets():
+    # Save and upload task presets
+
+    task_models = [
+        keras_cv.models.RetinaNet,
+        keras_cv.models.YOLOV8Detector,
+        keras_cv.models.ImageClassifier,
+        keras_cv.models.DeepLabV3Plus,
+    ]
+    for task_cls in task_models:
+        # Remove backbone-specific keys
+        task_preset_keys = set(task_cls.presets) ^ set(
+            task_cls.backbone_presets
+        )
+        for preset in task_preset_keys:
+            kwargs = {}
+            if task_cls in [
+                keras_cv.models.RetinaNet,
+                keras_cv.models.YOLOV8Detector,
+            ]:
+                kwargs.update({"bounding_box_format": "xywh"})
+                task = task_cls.from_preset(preset, **kwargs)
+            else:
+                task = task_cls.from_preset(preset)
+            save_to_preset(
+                task,
+                preset,
+                config_filename="config.json",
+            )
+            # Delete first to clean up any exising version.
+            os.system(f"gsutil rm -rf gs://{BUCKET}/{preset}")
+            os.system(f"gsutil cp -r {preset} gs://{BUCKET}/{preset}")
+            for root, _, files in os.walk(preset):
+                for file in files:
+                    path = os.path.join(BUCKET, root, file)
+                    os.system(
+                        f"gcloud storage objects update gs://{path} "
+                        "--add-acl-grant=entity=AllUsers,role=READER"
+                    )
+
+
+if __name__ == "__main__":
+    from keras_cv.src.utils.preset_utils import save_to_preset  # noqa: E402
+
+    convert_backbone_presets()
+    convert_task_presets()
