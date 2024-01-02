d76020f968bbc85c768d7a59462cfa6ba1708793
Neel Kovelamudi <nkovela@google.com>
2024-01-02 17:14:07 +0000

    Rebase update preset_utils

--- keras_cv/utils/preset_utils.py
+++ keras_cv/utils/preset_utils.py
@@ -13,9 +13,13 @@
 # limitations under the License.
 
 import datetime
+import inspect
 import json
 import os
 
+import h5py
+
+from keras_cv.backend import config as backend_config
 from keras_cv.backend import keras
 
 try:
@@ -29,6 +33,10 @@ GS_PREFIX = "gs://"
 
 def get_file(preset, path):
     """Download a preset file in necessary and return the local path."""
+    if not isinstance(preset, str):
+        raise ValueError(
+            f"A preset identifier must be a string. Received: preset={preset}"
+        )
     if preset.startswith(KAGGLE_PREFIX):
         if kagglehub is None:
             raise ImportError(
@@ -63,9 +71,19 @@ def get_file(preset, path):
             url,
             cache_subdir=os.path.join("models", subdir),
         )
-    else:
+    elif os.path.exists(preset):
         # Assume a local filepath.
         return os.path.join(preset, path)
+    else:
+        raise ValueError(
+            "Unknown preset identifier. A preset must be a one of:\n"
+            "1) a built in preset identifier like `'mobilenet_v3_small'`\n"
+            "2) a Kaggle Models handle like `'kaggle://keras/mobilenetv3/mobilenet_v3_small'`\n"  # noqa: E501
+            "3) a path to a local preset directory like `'./mobilenet_v3_small`\n"  # noqa: E501
+            "Use `print(cls.presets.keys())` to view all built-in presets for "
+            "API symbol `cls`.\n"
+            f"Received: preset='{preset}'"
+        )
 
 
 def recursive_pop(config, key):
@@ -126,6 +144,7 @@ def save_to_preset(
 def load_from_preset(
     preset,
     load_weights=None,
+    input_shape=None,
     config_file="config.json",
     config_overrides={},
 ):
@@ -136,6 +155,8 @@ def load_from_preset(
         config = json.load(config_file)
     config["config"] = {**config["config"], **config_overrides}
     layer = keras.saving.deserialize_keras_object(config)
+    if input_shape is not None:
+        layer.build(input_shape)
 
     # Check load_weights flag does not violate preset config.
     if load_weights is True and config["weights"] is None:
@@ -147,7 +168,10 @@ def load_from_preset(
     # Default to loading weights if available.
     if load_weights is not False and config["weights"] is not None:
         weights_path = get_file(preset, config["weights"])
-        layer.load_weights(weights_path)
+        if hasattr(layer, "_layer_checkpoint_dependencies"):
+            legacy_load_weights(layer, weights_path)
+        else:
+            layer.load_weights(weights_path)
 
     return layer
 
@@ -183,3 +207,91 @@ def check_preset_class(
             f"Received: `{cls}`."
         )
     return cls
+
+
+def legacy_load_weights(layer, weights_path):
+    # Hacky fix for TensorFlow 2.13 and 2.14 when loading a `.weights.h5` file.
+    # We find the `Functional` class, and temporarily remove the
+    # `_layer_checkpoint_dependencies` property, which on older version of
+    # TensorFlow complete broke the variable paths for functional models.
+    functional_cls = None
+    for cls in inspect.getmro(layer.__class__):
+        if cls.__name__ == "Functional":
+            functional_cls = cls
+    property = functional_cls._layer_checkpoint_dependencies
+    functional_cls._layer_checkpoint_dependencies = {}
+
+    from keras_cv.models.task import Task
+
+    if not backend_config.keras_3() and isinstance(layer, Task):
+        # Hacky fix for Keras 2 backwards compatibility. Keras 2 traverses
+        # loading weights in the reverse order, causing a naming mismatch when
+        # loading Kaggle weights saved from Keras 3.
+        new_weights_path = os.path.join(
+            os.path.dirname(weights_path),
+            "legacy_" + os.path.basename(weights_path),
+        )
+        os.rename(weights_path, new_weights_path)
+        weights_path = new_weights_path
+        f = h5py.File(weights_path, "r+")
+        if "_backbone" in f.keys():
+            # Transfer layers key (more lightweight than backbone)
+            data = f["_backbone"]
+            backbone_name = layer.backbone.name
+            if not backbone_name.endswith("backbone"):
+                backbone_name = backbone_name.split("_")[:-1]
+                backbone_name = "_".join(backbone_name)
+            # Copy to functional key due to traversal of `layers` attribute
+            if "functional" in f["layers"]:
+                del f["layers"]["functional"]
+                backbone_name = "functional"
+            # Reset as the actual backbone name under `layers` attribute
+            f["layers"][backbone_name] = data
+            del f["_backbone"]
+        if layer.__class__.__name__ == "SegmentAnythingModel":
+            _sam_fix(layer, backbone_name, f)
+        if layer.__class__.__name__ == "RetinaNet":
+            layer = _retinanet_load_weights(
+                layer, backbone_name, f, weights_path
+            )
+            functional_cls._layer_checkpoint_dependencies = property
+            return  # File closed inside helper
+        f.close()
+
+    layer.load_weights(weights_path)
+    functional_cls._layer_checkpoint_dependencies = property
+
+
+def _sam_fix(layer, backbone_name, h5_file):
+    data = h5_file["layers"]["sam_prompt_encoder"]
+    h5_file["prompt_encoder"] = data
+    data = h5_file["layers"]["sam_mask_decoder"]
+    h5_file["mask_decoder"] = data
+    for key in h5_file["layers"][backbone_name]["layers"].keys():
+        data = h5_file["layers"][backbone_name]["layers"][key]
+        h5_file["layers"][key] = data
+
+
+def _retinanet_load_weights(layer, backbone_name, h5_file, weights_path):
+    for key in h5_file.keys():
+        if key not in ["layers", "vars"]:
+            data = h5_file[key]
+            h5_file["layers"][key] = data
+            del h5_file[key]
+    # Copy to functional key due to traversal of `layers` attribute
+    data = h5_file["layers"][backbone_name]
+    h5_file["layers"]["functional"] = data
+    # Hacky fix for traversal order to ensure `layers` attribute
+    # is traversed after prediction heads in Keras 2
+    layer.z_box_head = layer.box_head
+    layer.z_classification_head = layer.classification_head
+    data = h5_file["layers"]["box_head"]
+    h5_file["z_box_head"] = data
+    data = h5_file["layers"]["classification_head"]
+    h5_file["z_classification_head"] = data
+
+    layer.load_weights(weights_path)
+
+    delattr(layer, "z_box_head")
+    delattr(layer, "z_classification_head")
+    return layer
