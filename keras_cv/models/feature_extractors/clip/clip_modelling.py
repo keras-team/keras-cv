7925de9bf2356536b1c3571285654bbb13017e6b
Divyashree Sreepathihalli <divyashreepathihalli>
2024-01-20 21:45:34 +0000

    add copywrite

--- keras_cv/models/feature_extractors/clip/clip_modelling.py
+++ keras_cv/models/feature_extractors/clip/clip_modelling.py
@@ -1,6 +1,16 @@
-import tensorflow as tf
-from keras_nlp.layers import TransformerEncoder
-
+# Copyright 2023 The KerasCV Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from keras_cv.backend import keras
 from keras_cv.backend import ops
 
@@ -75,30 +85,6 @@ class QuickGELU(keras.layers.Layer):
         return x * ops.sigmoid(1.702 * x)
 
 
-class ResidualTransformerEncoder(keras.layers.Layer):
-    def __init__(self, width, layers, heads, attn_mask=None, **kwargs):
-        super().__init__(**kwargs)
-        self.width = width
-        self.layers = layers
-        self.heads = heads
-        self.attn_mask = attn_mask
-        self.resblocks = keras.Sequential(
-            [
-                ResidualAttention(self.width, self.heads, self.attn_mask)
-                for _ in range(self.layers)
-            ]
-        )
-
-    def build(self, input_shape):
-        super().build(input_shape)
-
-    def call(self, x):
-        return self.resblocks(x)
-
-    def compute_output_shape(self, inputs_shape):
-        return inputs_shape
-
-
 class ResidualAttention(keras.layers.Layer):
     def __init__(
         self,
@@ -142,6 +128,30 @@ class ResidualAttention(keras.layers.Layer):
         x = x + self.attention(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
+
+    def compute_output_shape(self, inputs_shape):
+        return inputs_shape
+
+
+class ResidualTransformerEncoder(keras.layers.Layer):
+    def __init__(self, width, layers, heads, attn_mask=None, **kwargs):
+        super().__init__(**kwargs)
+        self.width = width
+        self.layers = layers
+        self.heads = heads
+        self.attn_mask = attn_mask
+        self.resblocks = keras.Sequential(
+            [
+                ResidualAttention(self.width, self.heads, self.attn_mask)
+                for _ in range(self.layers)
+            ]
+        )
+
+    def build(self, input_shape):
+        super().build(input_shape)
+
+    def call(self, x):
+        return self.resblocks(x)
 
     def compute_output_shape(self, inputs_shape):
         return inputs_shape
