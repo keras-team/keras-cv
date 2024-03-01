3343db1a8f17a9206d715c685c615a0b788dbbed
innat <innat.dev@gmail.com>
2024-03-01 16:18:17 +0600

    add: patch embedding layer

--- keras_cv/layers/video_swin_transformer_layers.py
+++ keras_cv/layers/video_swin_transformer_layers.py
@@ -170,7 +170,7 @@ class MLP(layers.Layer):
         - [Video Swin Transformer](https://arxiv.org/abs/2106.13230)
         - [Video Swin Transformer GitHub](https://github.com/SwinTransformer/Video-Swin-Transformer)
     """  # noqa: E501
-    
+
     def __init__(
         self,
         hidden_dim,
@@ -210,6 +210,92 @@ class MLP(layers.Layer):
                 "hidden_dim": self.hidden_dim,
                 "drop_rate": self.drop_rate,
                 'activation': self._activation_identifier
+            }
+        )
+        return config
+    
+
+class PatchEmbed3D(keras.Model):
+    """Video to Patch Embedding layer.
+
+    Args:
+        patch_size (int): Patch token size. Default: (2,4,4).
+        embed_dim (int): Number of linear projection output channels. Default: 96.
+        norm_layer (keras.layers, optional): Normalization layer. Default: None
+
+    References:
+        - [Video Swin Transformer](https://arxiv.org/abs/2106.13230)
+        - [Video Swin Transformer GitHub](https://github.com/SwinTransformer/Video-Swin-Transformer)
+    """  # noqa: E501
+        
+    def __init__(
+        self, 
+        patch_size=(2, 4, 4), 
+        embed_dim=96, 
+        norm_layer=None,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.patch_size = patch_size
+        self.embed_dim = embed_dim
+        self.norm_layer = norm_layer
+
+    def _compute_padding(self, dim, patch_size):
+        pad_amount = patch_size - (dim % patch_size)
+        return [
+            0, pad_amount if pad_amount != patch_size else 0
+        ]
+
+    def build(self, input_shape):
+        self.pads = [
+            [0, 0], 
+            self._compute_padding(input_shape[1], self.patch_size[0]),
+            self._compute_padding(input_shape[2], self.patch_size[1]),
+            self._compute_padding(input_shape[3], self.patch_size[2]),
+            [0, 0]   
+        ]
+        
+        self.proj = layers.Conv3D(
+            self.embed_dim, 
+            kernel_size=self.patch_size,
+            strides=self.patch_size, 
+            name='embed_proj'
+        )
+        self.proj.build((None, None, None, None, input_shape[-1]))
+        
+        self.norm = None
+        if self.norm_layer is not None:
+            self.norm = self.norm_layer(
+                axis=-1, epsilon=1e-5, name='embed_norm'
+            )
+            self.norm.build(
+                (None, None, None, None, self.embed_dim)
+            )
+        self.built = True
+
+    def call(self, x):
+        x = ops.pad(x, self.pads)
+        x = self.proj(x)
+        
+        if self.norm is not None:
+            x = self.norm(x) 
+            
+        return x
+    
+    def compute_output_shape(self, input_shape):
+        spatial_dims = [
+            (dim - self.patch_size[i]) // self.patch_size[i] + 1
+            for i, dim in enumerate(input_shape[1:-1])
+        ]
+        output_shape = (input_shape[0],) + tuple(spatial_dims) + (self.embed_dim,)
+        return output_shape
+
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "patch_size": self.patch_size,
+                "embed_dim": self.embed_dim,
             }
         )
         return config
