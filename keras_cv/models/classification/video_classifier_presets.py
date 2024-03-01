d2d883d741063dc9fb12b36b981a635d5859e227
innat <innat.dev@gmail.com>
2024-03-01 19:57:07 +0600

    add: video swin classifier presets

--- keras_cv/models/classification/video_classifier_presets.py
+++ keras_cv/models/classification/video_classifier_presets.py
@@ -12,3 +12,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """VideoClassifier Task presets."""
+
+classifier_presets = {
+    "videoswin_tiny_kinetics_classifier": {
+        "metadata": {
+            "description": (
+                "videoswin_tiny_kinetics " # TODO: update
+            ),
+            "params": 25_613_800,
+            "official_name": "VideoClassifier",
+            "path": "video_classifier",
+        },
+    },
+    "videoswin_small_kinetics_classifier": {
+        "metadata": {
+            "description": (
+                "videoswin_small_kinetics " # TODO: update
+            ),
+            "params": 25_613_800, # TODO: update
+            "official_name": "VideoClassifier",
+            "path": "video_classifier",
+        },
+    },
+    "videoswin_base_kinetics_classifier": {
+        "metadata": {
+            "description": (
+                "videoswin_base_kinetics " # TODO: update
+            ),
+            "params": 25_613_800, # TODO: update
+            "official_name": "VideoClassifier",
+            "path": "video_classifier",
+        },
+    },
+    "videoswin_base_something_something_v2_classifier": {
+        "metadata": {
+            "description": (
+                "videoswin_base_something_something_v2 " # TODO: update
+            ),
+            "params": 25_613_800, # TODO: update
+            "official_name": "VideoClassifier",
+            "path": "video_classifier",
+        },
+    },
+}
