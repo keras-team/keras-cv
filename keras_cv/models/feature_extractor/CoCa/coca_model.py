3feacb6c7e0f2f8618ad7f6ed2aa65a840c4afec
Varun Singh <varunsng@google.com>
2024-03-05 15:35:58 -0800

    Improved documentation and added output sizing to call(), also built each layer with expected sizing

--- keras_cv/models/feature_extractor/CoCa/coca_model.py
+++ keras_cv/models/feature_extractor/CoCa/coca_model.py
@@ -23,12 +23,7 @@ from keras_cv.layers.vit_layers import PatchingAndEmbedding
 
 @keras_cv_export(["keras_cv.models.CoCa"])
 class CoCa(Task):
-    """ Contrastive Captioner foundational model implementation.
-
-    CoCa Paper: https://arxiv.org/pdf/2205.01917.pdf"""
     def __init__(self,
-                 img_query_dim,
-                 text_proj_dim,
                  img_patch_size=18,
                  encoder_depth=40,
                  encoder_heads=16,
@@ -43,20 +38,18 @@ class CoCa(Task):
                  captioning_query_length=256,
                  contrastive_attn_heads=16,
                  captioning_attn_heads=16,
-                 captioning_loss_weight=0.5,
                  contrastive_loss_weight=0.5,
+                 captioning_loss_weight=0.5,
                  **kwargs):
         super().__init__(**kwargs)
 
         self.img_patch_size = img_patch_size
-        self.img_query_dim = img_query_dim
 
         self.encoder_depth = encoder_depth
         self.encoder_heads = encoder_heads
         self.encoder_width = encoder_width
         self.encoder_intermediate_dim = encoder_intermediate_dim
 
-        self.text_proj_dim = text_proj_dim
         self.unimodal_decoder_depth = unimodal_decoder_depth
         self.multimodal_decoder_depth = multimodal_decoder_depth
         self.decoder_intermediate_dim = decoder_intermediate_dim
@@ -74,7 +67,7 @@ class CoCa(Task):
         # Layer Definitions
         self.image_patching = PatchingAndEmbedding(self.encoder_width, self.img_patch_size)
         self.image_encoder = Sequential([
-            CVTransformerEncoder(self.img_query_dim, self.encoder_heads, self.encoder_intermediate_dim)
+            CVTransformerEncoder(self.encoder_width, self.encoder_heads, self.encoder_intermediate_dim)
             for _ in range(self.encoder_depth)
         ])
 
@@ -88,13 +81,56 @@ class CoCa(Task):
             for _ in range(self.multimodal_decoder_depth)
         ])
 
-        self.contrastive_attn_pooling = AttentionPooling(self.img_query_dim, self.contrastive_attn_heads)
-        self.captioning_attn_pooling = AttentionPooling(self.img_query_dim, self.captioning_attn_heads)
+        self.contrastive_attn_pooling = AttentionPooling(self.encoder_width, self.contrastive_attn_heads)
+        self.captioning_attn_pooling = AttentionPooling(self.encoder_width, self.captioning_attn_heads)
 
         # These are learnable weights defined in build as per Keras recommendations
         self.cls_token = None
         self.contrastive_query = None
         self.captioning_query = None
+    """ Contrastive Captioner foundational model implementation.
+
+    This model implements the "Contrastive Captioners are image-Text Foundational Models" by Yu, et al.
+    (https://arxiv.org/pdf/2205.01917.pdf). In short, the CoCa model combines the ideas of Contrastive techniques
+    such as CLIP, with Generative Captioning approaches such as SimVLM.
+
+    The architecture of clip can be described as an Image Visual Transformer Encoder in parallel to self-attention-only
+    Text Transformer Decoder, the outputs of both of which are passed into a multimodal Transformer Decoder. The
+    contrastive loss from the ViT and the uni-modal Text Decoder is combined with a captioning loss from the multi-modal
+    Decoder in order to produce the combined total loss.
+
+    Basic Usage:
+    ```python
+
+    images = ... # [batch_size, height, width, channel]
+    text = ... # [batch_size, text_dim, sequence_length]
+
+    coca = CoCa()
+
+    # [batch_size, sequence_length, captioning_query_length]
+    output = coca(images, text)
+    ```
+
+    All default arguments should be consistent with the original paper's details.
+
+    Args:
+        img_patch_size: N of each NxN patch generated from linearization of the input images
+        encoder_depth: number of image encoder blocks
+        encoder_heads: number of attention heads used in each image encoder block
+        encoder_intermediate_dim: dimensionality of the encoder blocks' intermediate representation (MLP dimensionality)
+        encoder_width: dimensionality of the encoder's projection, consistent with wording used in CoCa paper.
+        unimodal_decoder_depth: number of decoder blocks used for text self-attention/embedding
+        multimodal_decoder_depth: number of decoder blocks used for image-text cross-attention and captioning
+        decoder_intermediate_dim: dimensionality of the decoder blocks' MLPs
+        unimodal_decoder_heads: number of attention heads in the unimodal decoder
+        multimodal_decoder_heads: number of attention heads in the multimodal decoder
+        contrastive_query_length: number of tokens to use to represent contrastive query
+        captioning_query_length: number of tokens to use to represent captioning query
+        contrastive_attn_heads: number of attention heads used for the contrastive attention pooling
+        captioning_attn_heads: number of attention heads used for the captioning attention pooling
+        contrastive_loss_weight: weighting of contrastive loss
+        captioning_loss_weight: weighting of captioning loss
+    """
 
     def build(self, input_shape):
         super().build(input_shape)
@@ -121,26 +157,29 @@ class CoCa(Task):
 
         # Build Layers
         self.image_patching.build(images_shape)
-        self.image_encoder.build((batch_size, self.image_patching.num_patches, self.encoder_width))
+
+        # Add 1 for CLs token appended by patching
+        num_patches = (images_shape[1] // self.img_patch_size) * (images_shape[2] // self.img_patch_size) + 1
+        self.image_encoder.build((batch_size, self.encoder_width, num_patches))
 
         text_shape_with_cls_token = [s for s in text_shape]
-        text_shape_with_cls_token[-1] += 1
+        text_shape_with_cls_token[1] += 1
         self.text_embedding.build(text_shape_with_cls_token)
 
         self.unimodal_text_decoder.build(text_shape_with_cls_token)
 
-        self.contrastive_attn_pooling.build((batch_size, text_dim, self.contrastive_query_length))
-        self.captioning_attn_pooling.build((batch_size, text_dim, self.captioning_query_length))
+        self.contrastive_attn_pooling.build((batch_size, num_patches, self.encoder_width))
+        self.captioning_attn_pooling.build((batch_size, num_patches, self.encoder_width))
 
-        self.multimodal_text_decoder.build((batch_size, self.image_patching.num_patches, self.encoder_width),
+        self.multimodal_text_decoder.build((batch_size, self.encoder_width, self.captioning_query_length),
                                            text_shape_with_cls_token)
 
         # Learnable Weights
         self.cls_token = self.add_weight(shape=(batch_size, 1, text_dim), name="cls_token", trainable=True)
 
-        self.contrastive_query = self.add_weight(shape=(batch_size, text_dim, self.contrastive_query_length),
+        self.contrastive_query = self.add_weight(shape=(batch_size, self.encoder_width, self.contrastive_query_length),
                                                  trainable=True)
-        self.captioning_query = self.add_weight(shape=(batch_size, text_dim, self.captioning_query_length),
+        self.captioning_query = self.add_weight(shape=(batch_size, self.encoder_width, self.captioning_query_length),
                                                 trainable=True)
 
     def call(self, images, texts):
@@ -156,18 +195,24 @@ class CoCa(Task):
         Returns:
             Output: Output of the captioning Transformer Decoder with captioning cross-attention
         """
-        img_encoding = self.image_patching(images)
-        img_encoding = self.image_encoder(img_encoding)  # [batch, patches_len+1, img_query_dim]
+        img_encoding = self.image_patching(images) # [batch_size, encoder_width, img_patches_len+1]
+        img_encoding = self.image_encoder(img_encoding)  # [batch_size, img_patches_len+1, encoder_width]
 
         # This is only needed for loss calculations
         # contrastive_feature = self.con_attn_pooling(self.contrastive_query, img_encoding)
+
+        # [batch_size, encoder_width, captioning_query_length]
         captioning_feature = self.captioning_attn_pooling(self.captioning_query, img_encoding)
 
+        # [batch_size, sequence_length+1, text_dim]
         text_tokens = np.concatenate(texts, self.cls_token)
         mask = np.concatenate((np.ones_like(texts), np.zeros_like(self.cls_token)))
 
+        # [batch_size, sequence_length+1, text_dim]
         embed_text = self.text_embedding(text_tokens)
         unimodal_out = self.unimodal_text_decoder(embed_text, attention_mask=mask)
+
+        # [batch_size, sequence_length, captioning_query_length], notice we remove the CLs token
         multimodal_out = self.multimodal_text_decoder(unimodal_out[:, :-1, :],
                                                       encoder_sequence=captioning_feature,
                                                       decoder_attention_mask=mask)
@@ -179,12 +224,10 @@ class CoCa(Task):
         config.update(
             {
                 "img_patch_size": self.img_patch_size,
-                "img_query_dim": self.img_query_dim,
                 "encoder_depth": self.encoder_depth,
                 "encoder_heads": self.encoder_heads,
                 "encoder_width": self.encoder_width,
                 "encoder_intermediate_dim": self.encoder_intermediate_dim,
-                "text_proj_dim": self.text_proj_dim,
                 "unimodal_decoder_depth": self.unimodal_decoder_depth,
                 "multimodal_decoder_depth": self.multimodal_decoder_depth,
                 "decoder_intermediate_dim": self.decoder_intermediate_dim,
