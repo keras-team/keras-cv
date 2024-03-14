9c85dfc80dfb6f1018fd179f791559afc195d61d
ariG23498 <aritra.born2fly@gmail.com>
2024-03-14 21:26:35 +0530

    chore: adding init and compute loss

--- demo.py
+++ demo.py
@@ -1,17 +1,16 @@
-import numpy as np
-
-import keras_cv
-
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-# Note: We absolutely need this while creating the Backbone
-batch_size = 32
+import keras
+import keras_cv
+from keras_cv.models import FasterRCNN
+
+batch_size = 1
 image_shape = (512, 512, 3)
 
-images = np.ones((batch_size,) + image_shape)
+images = keras.ops.ones((batch_size,) + image_shape)
 labels = {
-    "boxes": np.array(
+    "boxes": keras.ops.array(
         [
             [
                 [0, 0, 100, 100],
@@ -19,13 +18,13 @@ labels = {
                 [300, 300, 100, 100],
             ]
         ],
-        dtype=np.float32,
+        dtype="float32",
     ),
-    "classes": np.array([[1, 1, 1]], dtype=np.float32),
+    "classes": keras.ops.array([[1, 1, 1]], dtype="float32"),
 }
-model = keras_cv.models.FasterRCNN(
+model = FasterRCNN(
     batch_size=batch_size,
-    num_classes=20,
+    num_classes=2,
     bounding_box_format="xywh",
     backbone=keras_cv.models.ResNet50Backbone.from_preset(
         "resnet50_imagenet",
@@ -33,17 +32,19 @@ model = keras_cv.models.FasterRCNN(
     ),
 )
 
-# # Evaluate model without box decoding and NMS
-# model(images)
+# Call the model
+outputs = model(images)
+print("outputs")
+for key, value in outputs.items():
+    print(f"{key}: {value.shape}")
 
-# # Prediction with box decoding and NMS
-# model.predict(images)
-
-# # Train model
-# model.compile(
-#     classification_loss="focal",
-#     box_loss="smoothl1",
-#     optimizer=keras.optimizers.SGD(global_clipnorm=10.0),
-#     jit_compile=False,
-# )
-# model.fit(images, labels)
+model.compile(
+    optimizer=keras.optimizers.Adam(),
+    box_loss=keras.losses.Huber(),
+    classification_loss=keras.losses.CategoricalCrossentropy(),
+    rpn_box_loss=keras.losses.Huber(),
+    rpn_classification_loss=keras.losses.BinaryCrossentropy(from_logits=True),
+)
+# Train the model
+loss = model.compute_loss(x=images, y=labels, y_pred=None, sample_weight=None)
+print(loss)
