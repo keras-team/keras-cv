bfeba125d00974003845cd37ab656b75e7a39958
Mohammed Innat <innat.dev@gmail.com>
2024-04-06 00:30:29 +0600

    Add Video Swin Transformer (#2369)
    * init video swin
    
    * add: 3d window size computation
    
    * add: mlp layer
    
    * add: patch embedding layer
    
    * add: patch merging layer
    
    * add: window attention layer
    
    * add: basic layer for video swin
    
    * update: basic layer for video swin
    
    * add: swin blocks for video swin
    
    * create and add: video swin backbone
    
    * rename: video swin layers to model specific
    
    * update module import
    
    * update module import
    
    * set class method to private usage
    
    * set init params for backbone
    
    * rm redundant imports
    
    * add video swin layer test cases
    
    * add: videoswin backbone aliases
    
    * add: video swin backbone presets
    
    * add: video swin backbone presets test
    
    * update: video swin backbone presets test
    
    * add: video classifier task
    
    * add: video swin classifier presets
    
    * run formatters
    
    * rename module name/id"
    
    * add hard-coded normalization for include rescaling=true
    
    * add docstring for videoswin backbone
    
    * update metadata: backbone presets no weights
    
    * update: backbone presets no weights test
    
    * update video swin aliases for no weights
    
    * add: video swin backbone presets with weights
    
    * update: video swin aliases with weights presets
    
    * update video swin layer test cases
    
    * added patch merging test
    
    * imported video swins presets to backbone presets list"
    
    * fix: typos"
    
    * run formatters"
    
    * fix: linting issue
    
    * fix: linting issue
    
    * fix: video swin layer test cases"
    
    * add: video swin backbone test
    
    * rm redundant code
    
    * disable preset test temporary
    
    * set include rescale to true
    
    * add video swin components to __init__
    
    * update docstrings: video siwn layers scripts
    
    * update copywrite status: video siwn layers test scripts
    
    * update copywrite status: video siwn backbone scripts
    
    * bug fixes: video swin backbone layers
    
    * update get config of video swin backbone
    
    * enable: video swin backbone test cases
    
    * update: video swin backbone test cases
    
    * update: video swin backbone preset test cases
    
    * run formatters
    
    * fix typos: video swin backbone test cases
    
    * add: non implemented property for test reason
    
    * fix: typos
    
    * add: video classifier test
    
    * update: video classifier test
    
    * update: video classifier test input shape
    
    * bug fix: mlp layer build method
    
    * updated: swin back layer build method
    
    * bug fix: use tf.TensorShape in compute_output_shape method
    
    * update: video_classifier_test model.predict to model.call
    
    * update test cases and format the code
    
    * update docstrings and preset config
    
    * fix jax DynamicJaxprTrace issue for
    
    * update config of backbone aliases
    
    * add can run in mixed precision test
    
    * add can run on gray video
    
    * minor fix
    
    * specify axis in keras.ops.take to match with tf.gather
    
    * specify include rescaling to backbone class
    
    * remove shift size form get config of video basic layer
    
    * add support arbitrary input shape
    
    * minor updates to swin layers
    
    * test method update for swin layers
    
    * update test method to swin backbone
    
    * remove unsed code
    
    * bug fix in call method of patch embed layer
    
    * fix typo in patch merging layer
    
    * minor fix
    
    * fix keras.ops.cond issue with jax
    
    * no test for jit compile in torch
    
    * reduce tensor size for forward test
    
    * minor fix
    
    * remove kcv export decorator
    
    * update keras.Layer import
    
    * remove unused layer import
    
    * replace keras.layers instead of layers
    
    * update keras.Layer to keras.layers.Layer for keras2
    
    * add window_size param to aliases
    
    * move vide swin layer to model specific directory
    
    * minor fix

---
+++ keras_cv/models/backbones/video_swin/video_swin_layers_test.py
@@ -0,0 +1,96 @@
+# Copyright 2024 The KerasCV Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from keras_cv.backend import ops
+from keras_cv.models.backbones.video_swin.video_swin_layers import (
+    VideoSwinPatchingAndEmbedding,
+)
+from keras_cv.models.backbones.video_swin.video_swin_layers import (
+    VideoSwinPatchMerging,
+)
+from keras_cv.models.backbones.video_swin.video_swin_layers import (
+    VideoSwinWindowAttention,
+)
+from keras_cv.tests.test_case import TestCase
+
+
+class TestVideoSwinPatchingAndEmbedding(TestCase):
+    def test_patch_embedding_compute_output_shape(self):
+        patch_embedding_model = VideoSwinPatchingAndEmbedding(
+            patch_size=(2, 4, 4), embed_dim=96, norm_layer=None
+        )
+        input_array = ops.ones(shape=(1, 16, 32, 32, 3))
+        output_shape = patch_embedding_model(input_array).shape
+        expected_output_shape = (1, 8, 8, 8, 96)
+        self.assertEqual(output_shape, expected_output_shape)
+
+    def test_patch_embedding_get_config(self):
+        patch_embedding_model = VideoSwinPatchingAndEmbedding(
+            patch_size=(4, 4, 4), embed_dim=96
+        )
+        config = patch_embedding_model.get_config()
+        assert isinstance(config, dict)
+        assert config["patch_size"] == (4, 4, 4)
+        assert config["embed_dim"] == 96
+
+
+class TestVideoSwinWindowAttention(TestCase):
+
+    def setUp(self):
+        self.window_attention_model = VideoSwinWindowAttention(
+            input_dim=32,
+            window_size=(2, 4, 4),
+            num_heads=8,
+            qkv_bias=True,
+            qk_scale=None,
+            attn_drop_rate=0.1,
+            proj_drop_rate=0.1,
+        )
+
+    def test_window_attention_output_shape(self):
+        input_shape = (2, 16, 32)
+        input_array = ops.ones(input_shape)
+        output_shape = self.window_attention_model(input_array).shape
+        expected_output_shape = input_shape
+        self.assertEqual(output_shape, expected_output_shape)
+
+    def test_window_attention_get_config(self):
+        config = self.window_attention_model.get_config()
+        # Add assertions based on the specific requirements
+        assert isinstance(config, dict)
+        assert config["window_size"] == (2, 4, 4)
+        assert config["num_heads"] == 8
+        assert config["qkv_bias"] is True
+        assert config["qk_scale"] is None
+        assert config["attn_drop_rate"] == 0.1
+        assert config["proj_drop_rate"] == 0.1
+
+
+class TestVideoSwinPatchMerging(TestCase):
+    def setUp(self):
+        self.patch_merging = VideoSwinPatchMerging(input_dim=32)
+
+    def test_output_shape(self):
+        input_shape = (2, 4, 32, 32, 3)
+        input_tensor = ops.ones(input_shape)
+        output_shape = self.patch_merging(input_tensor).shape
+        expected_shape = (
+            input_shape[0],
+            input_shape[1],
+            input_shape[2] // 2,
+            input_shape[3] // 2,
+            2 * 32,
+        )
+        self.assertEqual(output_shape, expected_shape)
---
+++ keras_cv/models/classification/video_classifier_test.py
@@ -0,0 +1,111 @@
+# Copyright 2024 The KerasCV Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for VideoClassifier."""
+
+
+import os
+
+import numpy as np
+import pytest
+import tensorflow as tf
+from absl.testing import parameterized
+
+from keras_cv.backend import keras
+from keras_cv.backend import ops
+from keras_cv.models.backbones.video_swin.video_swin_backbone import (
+    VideoSwinBackbone,  # TODO: update with aliases (kaggle handle)
+)
+from keras_cv.models.classification.video_classifier import VideoClassifier
+from keras_cv.tests.test_case import TestCase
+
+
+class VideoClassifierTest(TestCase):
+    def setUp(self):
+        self.input_batch = np.ones(shape=(10, 8, 224, 224, 3))
+        self.dataset = tf.data.Dataset.from_tensor_slices(
+            (self.input_batch, tf.one_hot(tf.ones((10,), dtype="int32"), 10))
+        ).batch(4)
+
+    def test_valid_call(self):
+        model = VideoClassifier(
+            backbone=VideoSwinBackbone(
+                input_shape=(8, 224, 224, 3), include_rescaling=False
+            ),
+            num_classes=10,
+        )
+        model(self.input_batch)
+
+    @parameterized.named_parameters(
+        ("jit_compile_false", False), ("jit_compile_true", True)
+    )
+    @pytest.mark.large  # Fit is slow, so mark these large.
+    def test_classifier_fit(self, jit_compile):
+        if jit_compile and keras.backend.backend() == "torch":
+            self.skipTest("TODO: Torch Backend `jit_compile` fails on GPU.")
+            self.supports_jit = False
+        model = VideoClassifier(
+            backbone=VideoSwinBackbone(
+                input_shape=(8, 224, 224, 3), include_rescaling=True
+            ),
+            num_classes=10,
+        )
+        model.compile(
+            loss="categorical_crossentropy",
+            optimizer="adam",
+            metrics=["accuracy"],
+            jit_compile=jit_compile,
+        )
+        model.fit(self.dataset)
+
+    @parameterized.named_parameters(
+        ("avg_pooling", "avg"), ("max_pooling", "max")
+    )
+    def test_pooling_arg_call(self, pooling):
+        input_batch = np.ones(shape=(2, 8, 224, 224, 3))
+        model = VideoClassifier(
+            backbone=VideoSwinBackbone(
+                input_shape=(8, 224, 224, 3), include_rescaling=True
+            ),
+            num_classes=10,
+            pooling=pooling,
+        )
+        model(input_batch)
+
+    @pytest.mark.large  # Saving is slow, so mark these large.
+    def test_saved_model(self):
+        input_batch = np.ones(shape=(2, 8, 224, 224, 3))
+        model = VideoClassifier(
+            backbone=VideoSwinBackbone(
+                input_shape=(8, 224, 224, 3), include_rescaling=False
+            ),
+            num_classes=10,
+        )
+        model_output = model(input_batch)
+        save_path = os.path.join(self.get_temp_dir(), "video_classifier.keras")
+        model.save(save_path)
+        restored_model = keras.models.load_model(save_path)
+
+        # Check we got the real object back.
+        self.assertIsInstance(restored_model, VideoClassifier)
+
+        # Check that output matches.
+        restored_output = restored_model(input_batch)
+        self.assertAllClose(
+            ops.convert_to_numpy(model_output),
+            ops.convert_to_numpy(restored_output),
+        )
+
+
+if __name__ == "__main__":
+    tf.test.main()
