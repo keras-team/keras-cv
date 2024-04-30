7136ceb3c05335c90670651cc4f5a2ebc1a645ac
Ramesh Sampath <1437573+sampathweb@users.noreply.github.com>
2024-04-29 19:45:20 -0500

    Update version 0.10 (#2430)

--- keras_cv/src/version_utils.py
+++ keras_cv/src/version_utils.py
@@ -15,7 +15,7 @@
 from keras_cv.src.api_export import keras_cv_export
 
 # Unique source of truth for the version number.
-__version__ = "0.9.0"
+__version__ = "0.10.0"
 
 
 @keras_cv_export("keras_cv.version")
