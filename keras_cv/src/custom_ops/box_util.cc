005feb16e67f10c4d5364bfdcb3ee2c80c4faa59
Ramesh Sampath <1437573+sampathweb@users.noreply.github.com>
2024-05-01 15:48:27 -0500

    Update cc codepaths for customops (#2436)

--- keras_cv/src/custom_ops/box_util.cc
+++ keras_cv/src/custom_ops/box_util.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "keras_cv/custom_ops/box_util.h"
+#include "keras_cv/src/custom_ops/box_util.h"
 
 #include <algorithm>
 #include <cmath>
