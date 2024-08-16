0a78ebd40d9b5025f09d934eb5acaa55d7dd83e6
Hongyu Chiu <20734616+james77777778@users.noreply.github.com>
2024-08-16 11:46:00 +0800

    Add ModelSamplingDiscreteFlow

---
+++ keras_cv/src/models/stable_diffusion_v3/model_sampling_discrete_flow.py
@@ -0,0 +1,56 @@
+# Copyright 2024 The KerasCV Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from keras import ops
+
+
+class ModelSamplingDiscreteFlow:
+    """Helper for sampler scheduling for Discrete Flow models"""
+
+    def __init__(self, shift=1.0):
+        self.shift = shift
+        self.timesteps = 1000
+        self.sigmas = self.sigma(
+            ops.arange(1, self.timesteps + 1, 1, dtype="float32")
+        )
+        self.sigma_min = self.sigmas[0]
+        self.sigma_max = self.sigmas[-1]
+
+    def timestep(self, sigma):
+        return ops.multiply(sigma, self.timesteps)
+
+    def sigma(self, timestep):
+        timestep = ops.divide(timestep, self.timesteps)
+
+        if self.shift == 1.0:
+            sigmas = timestep
+        else:
+            sigmas = self.shift * timestep / (1 + (self.shift - 1) * timestep)
+        return sigmas
+
+    def calculate_denoised(self, sigma, model_output, model_input):
+        sigma = ops.convert_to_tensor(sigma)
+        model_output = ops.convert_to_tensor(model_output)
+        model_input = ops.convert_to_tensor(model_input)
+
+        ndim = model_output.ndim
+        shape = (-1) + (1,) * (ndim - 1)
+        sigma = ops.reshape(sigma, shape)
+        return model_input - model_output * sigma
+
+    def noise_scaling(self, sigma, noise, latent_image):
+        sigma = ops.convert_to_tensor(sigma)
+        noise = ops.convert_to_tensor(noise)
+        latent_image = ops.convert_to_tensor(latent_image)
+        return sigma * noise + (1.0 - sigma) * latent_image
---
+++ keras_cv/src/models/stable_diffusion_v3/model_sampling_discrete_flow_test.py
@@ -0,0 +1,61 @@
+# Copyright 2024 The KerasCV Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+from keras import ops
+
+from keras_cv.src.models.stable_diffusion_v3.model_sampling_discrete_flow import (
+    ModelSamplingDiscreteFlow,
+)
+from keras_cv.src.tests.test_case import TestCase
+
+
+class ClipUtilsTest(TestCase):
+    def test_sigma_stats(self):
+        model_sampling = ModelSamplingDiscreteFlow()
+        self.assertEqual(model_sampling.sigma_min, 0.001)
+        self.assertEqual(model_sampling.sigma_max, 1.0)
+
+    def test_timestep(self):
+        steps = 50
+        model_sampling = ModelSamplingDiscreteFlow()
+        start = model_sampling.timestep(model_sampling.sigma_max)
+        end = model_sampling.timestep(model_sampling.sigma_min)
+        timesteps = ops.linspace(start, end, steps)
+
+        golden_start = [1000.0, 979.61224, 959.2245, 938.83673, 918.449]
+        golden_end = [82.55102, 62.16326, 41.77551, 21.387754, 1.0]
+        self.assertAllClose(timesteps[:5], golden_start)
+        self.assertAllClose(timesteps[-5:], golden_end, atol=1e-4)
+
+    def test_sigma(self):
+        timesteps = np.linspace(1000, 1, 50)
+        model_sampling = ModelSamplingDiscreteFlow()
+        sigmas = model_sampling.sigma(timesteps)
+        sigmas = ops.pad(sigmas, [0, 1])
+
+        golden_start = [1.0, 0.97961223, 0.95922446, 0.93883675, 0.918449]
+        golden_end = [0.06216326, 0.04177551, 0.02138775, 0.001, 0.0]
+        self.assertAllClose(sigmas[:5], golden_start)
+        self.assertAllClose(sigmas[-5:], golden_end)
+
+    def test_noise_scaling(self):
+        latent = np.ones((1, 40, 40, 16)).astype("float32") * 0.0609
+        noise = np.zeros((1, 40, 40, 16)).astype("float32")  # Fixed noise
+        sigma = 0.5
+        model_sampling = ModelSamplingDiscreteFlow()
+        noise_scaled = model_sampling.noise_scaling(sigma, noise, latent)
+        self.assertAllClose(
+            noise_scaled, sigma * noise + (1.0 - sigma) * latent
+        )
