0b1862786df67c3498664cbf47da9954608a89a7
Hongyu Chiu <20734616+james77777778@users.noreply.github.com>
2024-08-19 17:28:24 +0800

    Fix formatting

--- keras_cv/src/models/stable_diffusion_v3/model_sampling.py
+++ keras_cv/src/models/stable_diffusion_v3/model_sampling.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from keras import ops
+from keras_cv.src.backend import ops
 
 
 class ModelSampling:
